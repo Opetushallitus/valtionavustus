830e9ac36d71eb6afd0dbdaeb2f188f52a0168cb
Harri Salokorpi <harri.salokorpi@reaktor.fi>
2015-05-15 11:55:18 +0300

    Comment deps and add initial candidates for basic libraries

--- backend/project.clj
+++ backend/project.clj
@@ -4,11 +4,22 @@
   :license {:name "EUPL licence"
             :url "http://opensource.org/licenses/EUPL-1.1"}
   :dependencies [[org.clojure/clojure "1.6.0"]
+
+                 ;; HTTP server
                  [javax.servlet/servlet-api "2.5"]
                  [http-kit "2.1.18"]
                  [ring/ring-devel "1.3.2"]
                  [ring/ring-core "1.3.2"]
-                 [compojure "1.3.4"]]
+
+                 ;; Routing
+                 [compojure "1.3.4"]
+
+                 ;; JSON
+                 [cheshire "5.4.0"]
+
+                 ;; SQL + migrations
+                 [yesql "0.4.0"]
+                 [ragtime "0.3.8"]]
   :main ^:skip-aot oph.va.server
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
