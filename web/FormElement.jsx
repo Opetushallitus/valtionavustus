9e2efb6adc1736f66b715d32988b84f57d98d5dc
Harri Salokorpi <harri.salokorpi@reaktor.fi>
2015-05-25 19:00:01 +0300

    Use mapping instead of if-else

--- web/FormElement.jsx
+++ web/FormElement.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import LocalizedString from './LocalizedString.jsx'
 
-class TextField extends React.Component {
+class BasicTextField extends React.Component {
   render() {
     var field = this.props.field
     return (<input id={field.id} type="text" name={field.id}></input>)
   }
 }
 
-class TextArea extends React.Component {
+class BasicTextArea extends React.Component {
   render() {
     var field = this.props.field
     return (<textarea id={field.id} rows="4" cols="50" />)
@@ -19,18 +19,21 @@ export default class FormElement extends React.Component {
 
   constructor(props) {
     super(props)
+    this.fieldTypeMapping = {
+      "textField": BasicTextField,
+      "textArea": BasicTextArea
+    }
   }
 
   render() {
     var field = this.props.field;
     var displayAs = field.displayAs
-    console.log("field", field)
     var input = <span>Unsupported field type {displayAs}</span>
-    if (displayAs == "textField") {
-      input = <TextField {...this.props} />
-    } else if (displayAs == "textArea") {
-      input = <TextArea {...this.props} />
+
+    if (displayAs in this.fieldTypeMapping) {
+      input = React.createElement(this.fieldTypeMapping[displayAs], this.props)
     }
+
     return (
       <div>
         <label><LocalizedString data={field.label} lang={this.props.lang} /></label>
