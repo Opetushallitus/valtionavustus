5adb1e75dee99d7182c5c255d080697b9d41ae5b
Harri Salokorpi <harri.salokorpi@reaktor.fi>
2015-05-26 15:16:15 +0300

    Fix server tests

--- spec/oph/va/server_spec.clj
+++ spec/oph/va/server_spec.clj
@@ -10,15 +10,36 @@
 (defn get! [path] @(http/get (path->url path)))
 (defn json->map [body] (parse-string body true))
 
-(describe "HTTP server route /"
+(describe "HTTP server"
 
   ;; Start HTTP server for running tests
   (around-all [_]
               (let [stop-server (start-server "localhost" 9000 false)]
                 (try (_) (finally (stop-server)))))
 
-  (it "GET should return valid JSON"
-      (let [{:keys [status headers body error] :as resp} (get! "/api")]
+  (it "GET should return valid JSON from route /api/form"
+      (let [{:keys [status headers body error] :as resp} (get! "/api/form")]
         (should= 200 status)
-        (should= {:id 1, :name "lolbal"} (json->map body)))))
+        (should= {:fields [{:displayAs "textField"
+                            :description {:sv "Den sökandes namn"
+                                          :fi "Hakijan nimi"}
+                            :label {:sv "Namn"
+                                    :fi "Nimi"}
+                            :id "nimi"}
+                           {:displayAs "textField"
+                            :description {:sv "Den sökandes gatuaddress"
+                                          :fi "Hakijan katuosoite"}
+                            :label {:sv "Gatuaddress"
+                                    :fi "Katuosoite"}
+                            :id "osoite"}
+                           {:displayAs "textArea"
+                            :description {:sv ""
+                                          :fi ""}
+                            :label {:sv "Beskrivning"
+                                    :fi "Kuvaus"}
+                            :id "kuvaus"}]
+                  :name {:sv "Testform"
+                         :fi "Testilomake"}}
+                 (json->map body)))))
+
 (run-specs)
