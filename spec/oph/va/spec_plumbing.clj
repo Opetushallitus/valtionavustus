70d099472841638e64e3cf95b2227f64fad5f2a5
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2015-06-01 13:09:23 +0300

    Wrap exceptions with macro
    Which seems to really catch them.

--- spec/oph/va/spec_plumbing.clj
+++ spec/oph/va/spec_plumbing.clj
@@ -1,7 +1,5 @@
 (ns oph.va.spec-plumbing)
 
-(defn wrap-exception [f]
-  (fn []
-    (try (f [])
-      (catch Throwable e (println (.getMessage e)))))
-)
+(defmacro wrap-exception [& f]
+  `(try ~@f
+    (catch Throwable e# (.printStackTrace e# ))))
