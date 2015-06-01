e1007e79745260789b3403be6b1bedd3fa6bca79
Jyri Kytömäki <jyri.kytomaki@reaktor.com>
2015-06-01 08:47:38 +0300

    Fix name

--- web/LocalizedString.jsx
+++ web/LocalizedString.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default class Form extends React.Component {
+export default class LocalizedString extends React.Component {
   constructor(props) {
     super(props)
   }
