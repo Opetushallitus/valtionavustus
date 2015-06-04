33568687ac33418493960ed2a0bdcfe57fa4d4e8
Jyri Kytömäki <jyri.kytomaki@reaktor.com>
2015-06-04 14:24:14 +0300

    Fix default value

--- web/LocalizedString.jsx
+++ web/LocalizedString.jsx
@@ -8,9 +8,9 @@ export default class LocalizedString extends React.Component {
     var value = this.props.data[this.props.lang]
     if(!value) {
       console.error("No translation found: " + JSON.stringify(this.props))
-      for (var i=0; i < this.props.data.length; i++) {
-        if(this.props.data[i]) {
-          value = this.props.data[i];
+      for (var key in this.props.data) {
+        if (this.props.data[key]) {
+          value = this.props.data[key]
           break;
         }
       }
