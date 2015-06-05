83e60654d3a2c248a7b3657202509808f356bbf3
Jyri Kytömäki <jyri.kytomaki@reaktor.com>
2015-06-05 13:58:33 +0300

    Also label have to have key

--- web/FormElement.jsx
+++ web/FormElement.jsx
@@ -79,7 +79,7 @@ class RadioButton extends BasicFieldComponent {
 
     if(field.options) {
       for (var i=0; i < field.options.length; i++) {
-        const label = field.options[i].label ? <label htmlFor={field.id + ".radio." + i}><LocalizedString key={field.id + "." + field.options[i].value + ".label"} data={field.options[i].label} lang={this.props.lang} /></label> : field.options[i].value
+        const label = field.options[i].label ? <label key={field.id + "." + field.options[i].value + ".label"} htmlFor={field.id + ".radio." + i}><LocalizedString key={field.id + "." + field.options[i].value + ".text"} data={field.options[i].label} lang={this.props.lang} /></label> : field.options[i].value
         radiobuttons.push(<input {...this.props} type="radio" id={field.id + ".radio." + i} key={field.id + "." + field.options[i].value} name={field.id} required={field.required} value={field.options[i].value} onChange={this.handleChange} checked={field.options[i].value === this.props.value ? true: null}/>)
         radiobuttons.push(label)
       }
