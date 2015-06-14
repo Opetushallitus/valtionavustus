98250f0a7d685954e810e68682d319e54669ae58
Harri Salokorpi <harri.salokorpi@reaktor.fi>
2015-06-14 15:57:51 +0300

    Create new version on submission update

--- resources/sql/submission/update.sql
+++ resources/sql/submission/update.sql
@@ -1,1 +1,9 @@
-UPDATE form_submissions SET (answers) = (:answers) WHERE form = :form_id AND id = :submission_id AND closed_at IS NULL
+UPDATE form_submissions SET (closed_at) = now() WHERE form = :form_id AND id = :submission_id AND closed_at IS NULL;
+
+INSERT INTO form_submissions (id, version, form, answers)
+SELECT :submission_id,
+       max(version) + 1,
+       :form_id,
+       :answers
+FROM form_submissions
+WHERE id = :submission_id AND form = :form_id
