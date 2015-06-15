755908db3b9b3ef10ec2562b1114249efbfbc7b0
Harri Salokorpi <harri.salokorpi@reaktor.fi>
2015-06-15 14:36:55 +0300

    Fix incorrect comparison - use IS for NULL

--- resources/sql/submission/exists.sql
+++ resources/sql/submission/exists.sql
@@ -1,1 +1,1 @@
-SELECT 1 from form_submissions where id = :submission_id and form = :form_id and version_closed = NULL
+SELECT 1 from form_submissions where id = :submission_id and form = :form_id and version_closed IS NULL
