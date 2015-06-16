93652ea6e0e3a161ad987f4e2c88af466edfa363
Jyri Kytömäki <jyri.kytomaki@reaktor.com>
2015-06-16 12:41:55 +0300

    Remove unused consts

--- web/InfoElement.jsx
+++ web/InfoElement.jsx
@@ -8,7 +8,7 @@ class BasicInfoComponent extends React.Component {
   }
 
   asTimeString(date) {
-    var options = {hour: "numeric", minute: "numeric"}
+    const options = {hour: "numeric", minute: "numeric"}
     return date.toLocaleTimeString("fi-FI", options)
   }
 
@@ -34,7 +34,6 @@ class H1InfoElement extends React.Component {
   render() {
     const values = this.props.values
     const key = this.props.field.id
-    const field = this.props.field
     const lang = this.props.lang
     return <h1><LocalizedString translations={values} translationKey={key} lang={lang}/></h1>
   }
@@ -90,7 +89,6 @@ class DateRangeInfoElement extends BasicInfoComponent {
     const values = this.props.values
     const lang = this.props.lang
     const key = this.props.field.id
-    const field = this.props.field
     const value = values[key]
     const start = new Date(value.start)
     const startDateTime = this.asDateTimeString(start)
@@ -139,8 +137,6 @@ export default class InfoElement extends React.Component {
   render() {
     const field = this.props.field;
     const displayAs = field.displayAs
-    const values = this.props.values
-    const lang = this.props.lang
 
     var element = <span>Unsupported field type {displayAs}</span>
 
