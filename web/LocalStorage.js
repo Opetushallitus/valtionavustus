41bf099cfe24016023dd0b412e39b418cbea23b2
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2015-06-22 17:45:42 +0300

    Fix state saving
    local storage can only hold strings

--- web/LocalStorage.js
+++ web/LocalStorage.js
@@ -1,11 +1,12 @@
 export default class LocalStorage {
   static save(uiStateIdentifierSource, state, fieldUpdate) {
     // TODO : Track unsaved delta of fieldUpdates
-    window.localStorage.setItem(LocalStorage.createIdentifier(uiStateIdentifierSource, state), state)
+    window.localStorage.setItem(LocalStorage.createIdentifier(uiStateIdentifierSource, state), JSON.stringify(state))
   }
 
   static load(uiStateIdentifierSource, state) {
-    return window.localStorage.getItem(LocalStorage.createIdentifier(uiStateIdentifierSource, state))
+    var stateString = window.localStorage.getItem(LocalStorage.createIdentifier(uiStateIdentifierSource, state))
+    return JSON.parse(stateString)
   }
 
   static createIdentifier(uiStateIdentifierSource, state) {
