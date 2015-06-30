ed268c66c499854b5d76bd15d678573015b0673e
Tuomas Hakkarainen <tuomas.hakkarainen@reaktor.fi>
2015-06-30 13:12:21 +0300

    Add jest to unit test components

---
+++ web/test/FormBranchGrowerTest.js
@@ -0,0 +1,7 @@
+jest.dontMock('../form/FormBranchGrower.js')
+describe('Test', function() {
+  it('is test', function() {
+    var FormBranchGrower = require('../form/FormBranchGrower.js')
+    expect(1).toEqual(1)
+  })
+})
