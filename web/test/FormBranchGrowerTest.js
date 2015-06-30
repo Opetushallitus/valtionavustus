a97101c5baccd9d5f1d57a35559a64649e3dbd73
Tuomas Hakkarainen <tuomas.hakkarainen@reaktor.fi>
2015-06-30 16:44:34 +0300

    Dump jest and instead use mocha and jsdom to test React modules

--- web/test/FormBranchGrowerTest.js
+++
@@ -1,7 +0,0 @@
-jest.dontMock('../form/FormBranchGrower.js')
-describe('Test', function() {
-  it('is test', function() {
-    var FormBranchGrower = require('../form/FormBranchGrower.js')
-    expect(1).toEqual(1)
-  })
-})
---
+++ web/test/LocalizedStringTest.js
@@ -0,0 +1,19 @@
+import jsdom from 'mocha-jsdom'
+import { expect } from 'chai'
+import translations from '../../resources/public/translations.json'
+
+describe('Localized string', function() {
+  jsdom()
+
+  it('has test', function() {
+    var React = require('react/addons')
+    var TestUtils = React.addons.TestUtils
+    var LocalizedString = require('../form/LocalizedString')
+
+    var string = TestUtils.renderIntoDocument(
+      <LocalizedString translations={translations.form} translationKey="lengthleft" lang="fi" />
+    )
+    var text = TestUtils.findRenderedDOMComponentWithTag(string, 'span');
+    expect(text.getDOMNode().textContent).to.equal("merkkiä jäljellä")
+  })
+})
