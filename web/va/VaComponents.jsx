c36137250c99c6838ebe702eaf4ae7af3108158a
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2015-07-08 15:26:38 +0300

    Add initial label
    No styling yet

--- web/va/VaComponents.jsx
+++ web/va/VaComponents.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import LocalizedString from '../form/LocalizedString.jsx'
+
 export default class VaComponentFactory {
   constructor(props) {
     this.componentTypeMapping = {
@@ -29,6 +31,7 @@ class VaBudgetElement extends React.Component {
     const htmlId = this.props.htmlId
     return (
         <fieldset id={htmlId}>
+          <label htmlFor={this.props.htmlId} className={field.required ? "required" : ""}><LocalizedString translations={field} translationKey="label" lang={this.props.lang} /></label>
           <span>Hello from VaFieldFactory.jsx!</span>
           {children}
         </fieldset>
