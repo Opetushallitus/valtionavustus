91c6202008c759da442cbadef7e9cb349a5b5016
Tuomas Hakkarainen <tuomas.hakkarainen@reaktor.fi>
2015-07-08 17:51:23 +0300

    Add support for field sizes in preview

--- web/form/FormPreviewElement.jsx
+++ web/form/FormPreviewElement.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import ClassNames from 'classnames'
+import _ from 'lodash'
 import LocalizedString from './LocalizedString.jsx'
 
 class BasicValue extends React.Component {
@@ -29,7 +31,6 @@ class OptionsValue extends React.Component {
 }
 
 export default class FormPreviewElement extends React.Component {
-
   constructor(props) {
     super(props)
     this.fieldTypeMapping = {
@@ -50,15 +51,42 @@ export default class FormPreviewElement extends React.Component {
       preview = React.createElement(this.fieldTypeMapping[displayAs], this.props)
     }
 
-    const label = this.props.renderingParameters && this.props.renderingParameters.hideLabels === true ?
-      "" :
-      <LocalizedString className="key" translations={field} translationKey="label" lang={this.props.lang} />
-
     return (
-      <div>
-        {label}
+      <div className={this.className()}>
+        {this.label()}
         {preview}
       </div>
     )
+  }
+
+  className() {
+    const classNames = ClassNames(this.sizeClassName())
+    return !_.isEmpty(classNames) ? classNames : undefined
+  }
+
+  sizeClassName() {
+    if (this.param("size") && !Number.isInteger(this.param("size"))) return this.param("size")
+    else return undefined
+  }
+
+  label() {
+    if( this.hideLabel()) return undefined
+    else {
+      return (<LocalizedString
+                className="key"
+                translations={this.props.field}
+                translationKey="label"
+                lang={this.props.lang} />)
+    }
+  }
+
+  hideLabel() {
+    return this.props.renderingParameters && this.props.renderingParameters.hideLabels === true
+  }
+
+  param(param, defaultValue) {
+    if (!this.props.field.params) return defaultValue
+    if (this.props.field.params[param] !== undefined) return this.props.field.params[param]
+    return defaultValue
   }
 }
