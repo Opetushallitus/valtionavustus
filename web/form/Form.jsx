67a16a95afeae4a0f676120f806afe563e4b3a3f
Tuomas Hakkarainen <tuomas.hakkarainen@reaktor.fi>
2015-07-10 16:08:18 +0300

    Refactor styles disabled, error etc. modifications

--- web/form/Form.jsx
+++ web/form/Form.jsx
@@ -87,7 +87,7 @@ export default class Form extends React.Component {
     }
 
     return (
-      <form>
+      <form className="soresu-form">
         {
           fields.map(renderField)
         }
