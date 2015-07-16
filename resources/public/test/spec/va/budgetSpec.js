ddd2560d497cdb930e0f13b70fe3d57c2d2bcc34
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2015-07-16 17:19:12 +0300

    Add spec for required description

--- resources/public/test/spec/va/budgetSpec.js
+++ resources/public/test/spec/va/budgetSpec.js
@@ -16,15 +16,40 @@
     enterValidValues(page)
   }
 
-  describe('Oikein täytetty budjettitaulukko', function () {
+  describe('Budjettitaulukko', function () {
     before(
       page.openStartPage('fi'),
       enterValidValuesToPage,
       page.waitAutoSave
     )
 
-    it("Mahdollistaa hakemuksen lähettämisen", function() {
+    it('Mahdollistaa hakemuksen lähettämisen oikein täytettynä', function() {
+      expect(page.validationErrors()).to.equal('')
       expect(page.submitButton().isEnabled()).to.equal(true)
+    })
+
+    describe('Ilman kuvausta kentältä, jonka arvo on yli nollan', function() {
+      before(
+        page.setInputValue('service-purchase-costs-row.description', ''),
+        page.setInputValue('service-purchase-costs-row.amount', '1000')
+      )
+
+      it('ilmoittaa kuvauksen pakollisuudesta', function() {
+        expect(page.validationErrors()).to.equal('1 vastauksessa puutteita')
+        expect(page.submitButton().isEnabled()).to.equal(false)
+      })
+
+      describe('Kuvauksen syöttämisen jälkeen', function() {
+        before(
+          page.setInputValue('service-purchase-costs-row.description', 'Nopean oppimisen konsultointia'),
+          page.waitAutoSave
+        )
+
+        it('sallii tallentamisen', function() {
+          expect(page.validationErrors()).to.equal('')
+          expect(page.submitButton().isEnabled()).to.equal(true)
+        })
+      })
     })
   })
 })()
