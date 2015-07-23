4cf900db01d652db2ddfcb8acb3f7f03c699bf82
Tuomas Hakkarainen <tuomas.hakkarainen@reaktor.fi>
2015-07-23 16:07:09 +0300

    Refactor Form and FormPreview

--- web/form/Form.jsx
+++ web/form/Form.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import _ from 'lodash'
+
 import FormComponent from './component/FormComponent.jsx'
 import InfoElement from './component/InfoElement.jsx'
 import WrapperComponent from './component/wrapper/WrapperComponent.jsx'
 import InputValueStorage from './InputValueStorage.js'
-import _ from 'lodash'
 
 export default class Form extends React.Component {
   render() {
@@ -16,69 +17,75 @@ export default class Form extends React.Component {
 
     const renderField = function (field, renderingParameters) {
       const htmlId = controller.constructHtmlId(fields, field.id)
-      const formOperations = state.extensionApi.formOperations
-      const saved = controller.isSaveDraftAllowed(state)
-      const fieldDisabled = !formOperations.isFieldEnabled(saved, field.id) || field.forceDisabled === true
       const fieldProperties = { lang: state.configuration.lang, key: htmlId, htmlId: htmlId, field: field, translations: state.configuration.translations }
 
-      if (field.type == "formField") {
-        const existingInputValue = InputValueStorage.readValue(fields, values, field.id)
-        const value = _.isUndefined(existingInputValue) ? "" : existingInputValue
-        const fieldErrors = _.get(validationErrors, field.id, [])
-        return <FormComponent {...fieldProperties}
-                              validationErrors={fieldErrors}
-                              controller={controller}
-                              value={value}
-                              renderingParameters={renderingParameters}
-                              disabled={fieldDisabled}
-                              onChange={controller.componentOnChangeListener}/>
-      } else if (field.type == "infoElement") {
-        return <InfoElement {...fieldProperties}
-                            values={infoElementValues} />
-      } else if (field.type == "wrapperElement") {
-        const children = []
-        for (var i=0; i < field.children.length; i++) {
-          function resolveChildRenderingParameters(childIndex) {
-            const result = _.isObject(renderingParameters) ? _.cloneDeep(renderingParameters) : { }
-            result.childIndex = childIndex
-            result.removeMe = function() {
-              controller.removeField(field.children[childIndex])
-            }
-            const isFirstChild = childIndex === 0
-            if (field.params && field.params.showOnlyFirstLabels === true && !isFirstChild) {
-              result.hideLabels = true
-            }
-            const isSecondToLastChild = childIndex === field.children.length - 2
-            if (isSecondToLastChild) {
-              const nextChild = field.children[childIndex + 1]
-              const nextChildIsDisabled = _.isObject(nextChild) ? nextChild.forceDisabled : false
-              if (nextChildIsDisabled) {
-                result.rowMustNotBeRemoved = true
-              }
-            }
-            return result
-          }
+      if (field.type == "infoElement") {
+        return createInfoElement(fieldProperties)
+      } else {
+        const formOperations = state.extensionApi.formOperations
+        const saved = controller.isSaveDraftAllowed(state)
+        const fieldDisabled = !formOperations.isFieldEnabled(saved, field.id) || field.forceDisabled === true
+        const extendedProperties = _.extend(fieldProperties, { controller: controller, disabled: fieldDisabled, renderingParameters: renderingParameters })
 
-          const childRenderingParameters = resolveChildRenderingParameters(i)
-          children.push(renderField(field.children[i], childRenderingParameters))
+        if (field.type == "formField") {
+          return createFormComponent(field, extendedProperties)
+        } else if (field.type == "wrapperElement") {
+          return createWrapperElement(field, fieldProperties, renderingParameters)
         }
-        const customProperties = controller.getCustomWrapperComponentProperties(state);
-        return <WrapperComponent {...fieldProperties}
-                                 children={children}
-                                 disabled={fieldDisabled}
-                                 renderingParameters={renderingParameters}
-                                 controller={controller}
-                                 customProps={customProperties}
-                                 answersObject={values} />
       }
     }
 
-    return (
-      <form className="soresu-form">
-        {
-          fields.map(renderField)
+    return <form className="soresu-form">
+            {fields.map(renderField)}
+           </form>
+
+    function createInfoElement(fieldProperties) {
+      return <InfoElement {...fieldProperties}
+                          values={infoElementValues} />
+    }
+
+    function createFormComponent(field, extendedProperties) {
+      const existingInputValue = InputValueStorage.readValue(fields, values, field.id)
+      const value = _.isUndefined(existingInputValue) ? "" : existingInputValue
+      const fieldErrors = _.get(validationErrors, field.id, [])
+      return <FormComponent {...extendedProperties}
+                            validationErrors={fieldErrors}
+                            value={value}
+                            onChange={controller.componentOnChangeListener} />
+    }
+
+    function createWrapperElement(field, fieldProperties, renderingParameters) {
+      const children = []
+      for (var i = 0; i < field.children.length; i++) {
+        function resolveChildRenderingParameters(childIndex) {
+          const result = _.isObject(renderingParameters) ? _.cloneDeep(renderingParameters) : {}
+          result.childIndex = childIndex
+          result.removeMe = function () {
+            controller.removeField(field.children[childIndex])
+          }
+          const isFirstChild = childIndex === 0
+          if (field.params && field.params.showOnlyFirstLabels === true && !isFirstChild) {
+            result.hideLabels = true
+          }
+          const isSecondToLastChild = childIndex === field.children.length - 2
+          if (isSecondToLastChild) {
+            const nextChild = field.children[childIndex + 1]
+            const nextChildIsDisabled = _.isObject(nextChild) ? nextChild.forceDisabled : false
+            if (nextChildIsDisabled) {
+              result.rowMustNotBeRemoved = true
+            }
+          }
+          return result
         }
-      </form>
-    )
+
+        const childRenderingParameters = resolveChildRenderingParameters(i)
+        children.push(renderField(field.children[i], childRenderingParameters))
+      }
+      const customProperties = controller.getCustomWrapperComponentProperties(state);
+      return <WrapperComponent {...fieldProperties}
+                               children={children}
+                               customProps={customProperties}
+                               answersObject={values} />
+    }
   }
 }
--- web/form/FormPreview.jsx
+++ web/form/FormPreview.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import _ from 'lodash'
+
 import FormPreviewComponent from './preview/FormPreviewComponent.jsx'
 import InfoElement from './component/InfoElement.jsx'
 import WrapperPreviewComponent from './preview/wrapper/WrapperPreviewComponent.jsx'
 import InputValueStorage from './InputValueStorage.js'
-import _ from 'lodash'
 
 export default class FormPreview extends React.Component {
   render() {
@@ -18,52 +19,60 @@ export default class FormPreview extends React.Component {
       const htmlId = controller.constructHtmlId(fields, field.id)
       const fieldProperties = { lang: state.configuration.lang, key: htmlId, htmlId: htmlId, field: field }
       if (field.type == "formField") {
-        var existingInputValue = InputValueStorage.readValue(fields, values, field.id)
-        const value = _.isUndefined(existingInputValue) ? "" : existingInputValue
-        return <FormPreviewComponent {...fieldProperties}
-                                     controller={controller}
-                                     value={value}
-                                     renderingParameters={renderingParameters} />
+        return createFormPreviewComponent(field, fieldProperties, renderingParameters);
       } else if (field.type == "infoElement") {
-        return <InfoElement {...fieldProperties}
-                            values={infoElementValues}
-                            translations={translations} />
+        return createInfoComponent(fieldProperties);
       } else if (field.type == "wrapperElement") {
-        const children = []
-        for (var i=0; i < field.children.length; i++) {
-          function resolveChildRenderingParameters(childIndex) {
-            const result = _.isObject(renderingParameters) ? _.cloneDeep(renderingParameters) : { }
-            result.childIndex = childIndex
-            const isFirstChild = childIndex === 0
-            if (field.params && field.params.showOnlyFirstLabels === true && !isFirstChild) {
-              result.hideLabels = true
-            }
-            const existingInputValue = InputValueStorage.readValue(fields, values, field.children[childIndex].id)
-            if (_.isEmpty(existingInputValue)) {
-              result.valueIsEmpty = true
-            }
-            return result
-          }
-          const childRenderingParameters = resolveChildRenderingParameters(i)
-          children.push(renderField(field.children[i], childRenderingParameters))
-        }
-        const customProperties = controller.getCustomWrapperComponentProperties(state)
-        return <WrapperPreviewComponent {...fieldProperties}
-                                        children={children}
-                                        translations={translations}
-                                        renderingParameters={renderingParameters}
-                                        controller={controller}
-                                        customProps={customProperties}
-                                        answersObject={values} />
+        return createWrapperComponent(field, fieldProperties, renderingParameters);
       }
     }
 
-    return (
-      <div className="soresu-preview">
-        {
-          fields.map(renderField)
+    return  <div className="soresu-preview">
+              {fields.map(renderField) }
+            </div>
+
+    function createFormPreviewComponent(field, fieldProperties, renderingParameters) {
+      var existingInputValue = InputValueStorage.readValue(fields, values, field.id)
+      const value = _.isUndefined(existingInputValue) ? "" : existingInputValue
+      return <FormPreviewComponent {...fieldProperties}
+                                   value={value}
+                                   renderingParameters={renderingParameters}/>
+    }
+
+    function createInfoComponent(fieldProperties) {
+      return <InfoElement {...fieldProperties}
+                          values={infoElementValues}
+                          translations={translations} />
+    }
+
+    function createWrapperComponent(field, fieldProperties, renderingParameters) {
+      const children = []
+      for (var i = 0; i < field.children.length; i++) {
+        function resolveChildRenderingParameters(childIndex) {
+          const result = _.isObject(renderingParameters) ? _.cloneDeep(renderingParameters) : {}
+          result.childIndex = childIndex
+          const isFirstChild = childIndex === 0
+          if (field.params && field.params.showOnlyFirstLabels === true && !isFirstChild) {
+            result.hideLabels = true
+          }
+          const existingInputValue = InputValueStorage.readValue(fields, values, field.children[childIndex].id)
+          if (_.isEmpty(existingInputValue)) {
+            result.valueIsEmpty = true
+          }
+          return result
         }
-      </div>
-    )
+
+        const childRenderingParameters = resolveChildRenderingParameters(i)
+        children.push(renderField(field.children[i], childRenderingParameters))
+      }
+      const customProperties = controller.getCustomWrapperComponentProperties(state)
+      return <WrapperPreviewComponent {...fieldProperties}
+                                      children={children}
+                                      translations={translations}
+                                      renderingParameters={renderingParameters}
+                                      controller={controller}
+                                      customProps={customProperties}
+                                      answersObject={values} />
+    }
   }
 }
