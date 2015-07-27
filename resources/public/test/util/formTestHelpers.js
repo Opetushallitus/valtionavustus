73bcd39586e235674cdff4abb65830559522d5e9
Jyri Kytömäki <jyri.kytomaki@reaktor.com>
2015-07-27 14:50:37 +0300

    Fix waiting of autosave

--- resources/public/test/util/formTestHelpers.js
+++ resources/public/test/util/formTestHelpers.js
@@ -2,7 +2,7 @@
 function enterValidValues(applicationPage) {
   applicationPage.setInputValue("organization", "Testi Organisaatio")()
   applicationPage.setInputValue("primary-email", "yhteyshenkilo@example.com")()
-  applicationPage.waitAutoSave()
+  applicationPage.waitAutoSave().then()
   applicationPage.setInputValue("signature", "Matti Allekirjoitusoikeudellinen")()
   applicationPage.setInputValue("signature-email", "matti.allekirjoitusoikeudellinen@example.com")()
   applicationPage.setInputValue("other-organizations.other-organizations-1.name", "Muu Testi Organisaatio")()
