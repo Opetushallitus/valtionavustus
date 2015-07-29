e9678cdcb98c5b3111d6964d3d5efb749041885d
Tuomas Hakkarainen <tuomas.hakkarainen@reaktor.fi>
2015-07-29 11:19:44 +0300

    Formatting

--- web/form/component/TextButton.jsx
+++ web/form/component/TextButton.jsx
@@ -6,13 +6,13 @@ import LocalizedString from './LocalizedString.jsx'
 export default class TextButton extends React.Component {
   render() {
     const props = this.props
-    return (<button id={props.htmlId}
+    return <button id={props.htmlId}
                     className="soresu-text-button"
                     onClick={props.onClick}
                     disabled={props.disabled}>
-              <LocalizedString translations={props.translations}
-                               translationKey={props.translationKey}
-                               lang={props.lang} />
-            </button>)
+             <LocalizedString translations={props.translations}
+                              translationKey={props.translationKey}
+                              lang={props.lang} />
+           </button>
   }
 }
