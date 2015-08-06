71a33fa286357d5254b87a459980d58a14b80e68
Jyri Kytömäki <jyri.kytomaki@reaktor.com>
2015-08-06 18:27:50 +0300

    Support hidden attribute

--- web/form/component/LocalizedString.jsx
+++ web/form/component/LocalizedString.jsx
@@ -8,6 +8,6 @@ export default class LocalizedString extends React.Component {
   render() {
     const translator = new Translator(this.props.translations)
     const value = translator.translate(this.props.translationKey, this.props.lang, this.props.defaultValue, this.props.keyValues)
-    return (<span className={this.props.className} onClick={this.props.onClick}>{value}</span>)
+    return (<span hidden={this.props.hidden} className={this.props.className} onClick={this.props.onClick}>{value}</span>)
   }
 }
