503c909a9e266b43b151652b86bcd7a82a62e576
Antti Viljakainen <antti.viljakainen@reaktor.fi>
2015-08-07 11:40:54 +0300

    Add Opetushallitus signature to sv email

--- resources/email-templates/new-hakemus.plain.sv
+++ resources/email-templates/new-hakemus.plain.sv
@@ -6,3 +6,14 @@ Pääset muokkaamaan avustushakemusta tästä linkistä: {{& url }}
 
 Huom! Voit jakaa tämän viestien muille hankkeen kumppaneille, mutta huomaa että
 jokainen linkin tietävä voi muokata hakemusta.
+
+Lisätietoja antaa opetusneuvos Leena Koski, puh. 029 533 1106,
+sähköposti leena.koski@oph.fi
+
+Opetushallitus
+Hakaniemenranta 6
+PL 380, 00531 Helsinki
+
+puhelin 029 533 1000
+faksi 029 533 1035
+etunimi.sukunimi@oph.fi
