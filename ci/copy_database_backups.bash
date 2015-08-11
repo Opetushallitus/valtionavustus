a6a57c20e8141c2decc739f323362b8b5377678f
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2015-08-11 15:44:09 +0300

    Fix server name handling for db backup copy

--- ci/copy_database_backups.bash
+++ ci/copy_database_backups.bash
@@ -2,21 +2,25 @@
 set -euo pipefail
 
 if [ -z ${2+x} ]; then
-  echo "Usage: $0 <database server> <backup location on server>"
+  echo "Usage: $0 <comma-separated list of database servers> <backup location on server>"
   exit 3
 fi
 
-database_server=$1
+comma_separated_server_list=$1
 backups_path_on_server=$2
 
 timestamp=`date +'%Y%m%d%H%M%S'`
 ephemeral_disk_mount_point=/mnt
 copy_target=${ephemeral_disk_mount_point}/database-backup/${timestamp}
-mkdir -p ${copy_target}
 
 SSH_KEY=~/.ssh/id_deploy
 SSH_USER=va-deploy
 
-copy_command="scp -Cpr -i ${SSH_KEY} ${SSH_USER}@${database_server}:${backups_path_on_server} ${copy_target}"
-echo "Running $copy_command"
-time eval ${copy_command}
+for server in ${comma_separated_server_list//,/ }
+do
+  server_copy_target=${copy_target}-${server}
+  mkdir -p ${server_copy_target}
+  copy_command="scp -Cpr -i ${SSH_KEY} ${SSH_USER}@${server}:${backups_path_on_server} ${server_copy_target}"
+  echo "Running $copy_command"
+  time eval ${copy_command}
+done
