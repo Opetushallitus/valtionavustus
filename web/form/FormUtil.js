cfd43f7b873798fab9c89e0056f041aab47cfaa7
Jyri Kytömäki <jyri.kytomaki@reaktor.com>
2015-08-13 15:11:42 +0300

    Need some more offset so that field labels are left  visible

--- web/form/FormUtil.js
+++ web/form/FormUtil.js
@@ -11,7 +11,7 @@ export default class FormUtil {
       return Math.abs(current - target) < 1
     }
     const startScrollPos = window.pageYOffset
-    const offsetFromTop = document.getElementById("container").getBoundingClientRect().top + startScrollPos
+    const offsetFromTop = document.getElementById("container").getBoundingClientRect().top + startScrollPos + 18
     const targetScrollPos = element.getBoundingClientRect().top + startScrollPos - offsetFromTop
     if (aboutSame(startScrollPos, targetScrollPos)) {
       afterScroll()
