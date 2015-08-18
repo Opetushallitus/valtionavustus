ccc514bc993587fb17c76d74ac521a0cb5daa510
Juho Helminen <juho.helminen@oph.fi>
2015-08-18 17:03:02 +0300

    Update hakemus-submitted.plain.sv

--- resources/email-templates/hakemus-submitted.plain.sv
+++ resources/email-templates/hakemus-submitted.plain.sv
@@ -1,16 +1,17 @@
-Avustushakemus: {{ avustushaku }}
+Ansökan om understöd: {{ avustushaku }}
 
-Hakuaika: {{ start-date }} klo {{ start-time }} — {{ end-date }} klo {{ end-time }}
+Ansökningstid: {{ start-date }} klo {{ start-time }} — {{ end-date }} klo {{ end-time }}
 
-Hakemustanne voitte tarkastella tästä linkistä: {{& url }}
+Du kan se ansökan via länken: {{& url }}
 
-Lisätietoja antaa opetusneuvos Leena Koski, puh. 029 533 1106,
-sähköposti leena.koski@oph.fi
+Mer information ger undervisningsrådet Helena Öhman, tfn 029 533 1111 och Leena Koski, tfn 029 533 1106. 
+E-post: fornamn.efternamn@oph.fi
 
-Opetushallitus
-Hakaniemenranta 6
-PL 380, 00531 Helsinki
+Utbildningsstyrelsen
+Hagnäskajen 6
+PB 380
+00531 Helsingfors
 
-puhelin 029 533 1000
-faksi 029 533 1035
-etunimi.sukunimi@oph.fi
+telefon 029 533 1000
+fax 029 533 1035
+fornamn.efternamn@oph.fi
