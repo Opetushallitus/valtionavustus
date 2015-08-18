38c08962f3800eec4997010e6cb1d345ca748a1d
Tuomas Hakkarainen <tuomas.hakkarainen@reaktor.fi>
2015-08-18 18:03:51 +0300

    Add preview type for budget item

--- web/va/VaPreviewComponentFactory.js
+++ web/va/VaPreviewComponentFactory.js
@@ -3,6 +3,7 @@ import _ from 'lodash'
 import ClassNames from 'classnames'
 
 import ComponentFactory from '../form/ComponentFactory.js'
+import LocalizedString from '../form/component/LocalizedString.jsx'
 import {VaBudgetElement, SummingBudgetElement, BudgetItemElement, BudgetSummaryElement} from './VaBudgetComponents.jsx'
 
 export default class VaPreviewComponentFactory extends ComponentFactory {
@@ -10,13 +11,33 @@ export default class VaPreviewComponentFactory extends ComponentFactory {
     const fieldTypeMapping = {
       "vaBudget": VaPreviewBudgetElement,
       "vaSummingBudgetElement": SummingBudgetElement,
-      "vaBudgetItemElement": BudgetItemElement,
+      "vaBudgetItemElement": VaPreviewBudgetItemElement,
       "vaBudgetSummaryElement": BudgetSummaryElement,
       "vaProjectDescription": VaProjectDescriptionPreview
     }
     super(fieldTypeMapping)
   }
 }
+
+class VaPreviewBudgetItemElement extends React.Component {
+  render() {
+    const field = this.props.field
+    const children = this.props.children
+    const htmlId = this.props.htmlId
+    const descriptionComponent = children[0]
+    const amountComponent = children[1]
+    return (
+      <tr id={htmlId} className="budget-item">
+        <td className="label-column">
+          <LocalizedString translations={field} translationKey="label" lang={this.props.lang} />
+        </td>
+        <td>{descriptionComponent}</td>
+        <td className="amount-column">{amountComponent}</td>
+      </tr>
+    )
+  }
+}
+
 
 class VaPreviewBudgetElement extends VaBudgetElement {
   html(htmlId, children) {
