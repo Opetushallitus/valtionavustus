6e88098ac1cb6df2dbc5584ed46de14bdca3565f
Harri Salokorpi <harri.salokorpi@reaktor.fi>
2015-08-26 15:56:20 +0300

    Fix missing imports

--- va-common/src/oph/common/routes.clj
+++ va-common/src/oph/common/routes.clj
@@ -1,5 +1,6 @@
 (ns oph.common.routes
-  (:require [ring.util.response :as resp]
+  (:require [ring.util.http-response :refer :all]
+            [ring.util.response :as resp]
             [ring.swagger.middleware :as swagger]
             [clojure.tools.logging :as log]))
 
@@ -8,12 +9,12 @@
       (content-type "text/html")
       (charset  "utf-8")))
 
-(defn- exception-handler [^Exception e]
+(defn exception-handler [^Exception e]
   (log/warn e e)
   (internal-server-error {:type "unknown-exception"
                           :class (.getName (.getClass e))}))
 
-(defn- validation-error-handler [{:keys [error]}]
+(defn validation-error-handler [{:keys [error]}]
   (let [error-str (swagger/stringify-error error)]
     (log/warn (format "Request validation error: %s" (print-str error-str)))
     (bad-request {:errors error-str})))
