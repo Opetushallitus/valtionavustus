7c9b1bf6f2e6f27840791d408334e638c00b6a8b
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2015-08-31 16:23:37 +0300

    Take hakemus list SQL to use in REST API

--- va-virkailija/resources/sql/hakemus/list.sql
+++
@@ -1,4 +0,0 @@
-select h.status, s.answers->'value'
-from hakija.hakemukset h
-  join hakija.form_submissions s on h.form_submission_id = s.id
-where s.version_closed is null and h.version_closed is null
--- va-virkailija/resources/sql/hakija/hakemus/list.sql
+++ va-virkailija/resources/sql/hakija/hakemus/list.sql
@@ -1,1 +1,4 @@
-select * from hakemukset where version_closed IS null
+select h.status, s.answers->'value' as answer_values
+from hakija.hakemukset h
+  join hakija.form_submissions s on h.form_submission_id = s.id
+where s.version_closed is null and h.version_closed is null
