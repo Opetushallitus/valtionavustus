33f44183cf6dd584c7358a4d7a32d0f621958717
Jyri Kytömäki <jyri.kytomaki@reaktor.com>
2015-09-01 13:29:11 +0300

    Use TimeoutTransitionGroup instead of React.addons.CSSTransitionGroup
    Because tarnsitions where failing at least with phantomjs.
    See: https://github.com/facebook/react/issues/1326

--- va-hakija/web/form/component/wrapper/CSSTransitionGroup.jsx
+++ va-hakija/web/form/component/wrapper/CSSTransitionGroup.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 
+import TimeoutTransitionGroup from 'timeout-transition-group'
+
 export default class CSSTransitionGroup extends React.Component {
   render() {
     const children = this.props.children
     const transitionName = this.props.transitionName
-    const ReactTransitionGroup = React.addons.CSSTransitionGroup
     return (
-      <ReactTransitionGroup component="div" transitionName={transitionName}>
+      <TimeoutTransitionGroup component="div" transitionName={transitionName} enterTimeout={500} leaveTimeout={500}>
         {children}
-      </ReactTransitionGroup>
+      </TimeoutTransitionGroup>
     )
   }
 }
