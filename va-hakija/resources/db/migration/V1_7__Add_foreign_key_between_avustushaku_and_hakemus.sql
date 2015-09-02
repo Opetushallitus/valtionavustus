c326281949fdcdd4bcbf80b7b1e44fdeaa0c3ecc
Harri Salokorpi <harri.salokorpi@reaktor.fi>
2015-09-02 17:51:06 +0300

    Link avustushaut and hakemukset with direct foreign key
    This avoids costly join through form_submission and form when
    hakemukset are queried by avustushaku.

---
+++ va-hakija/resources/db/migration/V1_7__Add_foreign_key_between_avustushaku_and_hakemus.sql
@@ -0,0 +1,4 @@
+ALTER TABLE hakemukset ADD COLUMN avustushaku INTEGER DEFAULT 1 NOT NULL;
+ALTER TABLE hakemukset ADD FOREIGN KEY(avustushaku) REFERENCES avustushaut(id);
+ALTER TABLE hakemukset ALTER COLUMN avustushaku DROP DEFAULT;
+
