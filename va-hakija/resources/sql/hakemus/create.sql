f8547dce5d75fb0e1592911e30558a1ac9b11c1b
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2015-09-03 13:59:10 +0300

    Order metadata fields last
    No functional changes

--- va-hakija/resources/sql/hakemus/create.sql
+++ va-hakija/resources/sql/hakemus/create.sql
@@ -1,13 +1,13 @@
-INSERT INTO hakemukset (id, avustushaku, version, user_key, form_submission_id, form_submission_version, last_status_change_at,
-                       budget_total, budget_oph_share)
+INSERT INTO hakemukset (id, avustushaku, version, user_key, form_submission_id, form_submission_version,
+                        budget_total, budget_oph_share, last_status_change_at)
 SELECT nextval('hakemukset_id_seq'),
        :avustushaku_id,
        0,
        :user_key,
        submissions.id,
        submissions.version,
-       now(),
        :budget_total,
-       :budget_oph_share
+       :budget_oph_share,
+       now()
 FROM form_submissions submissions
 WHERE id = :form_submission AND version_closed IS NULL
--- va-hakija/resources/sql/hakemus/update-submission.sql
+++ va-hakija/resources/sql/hakemus/update-submission.sql
@@ -1,15 +1,15 @@
-INSERT INTO hakemukset(id, avustushaku, user_key, version, form_submission_id, form_submission_version, status, last_status_change_at,
-                       budget_total, budget_oph_share)
+INSERT INTO hakemukset(id, avustushaku, user_key, version, form_submission_id, form_submission_version,
+                       budget_total, budget_oph_share, status, last_status_change_at)
 SELECT id,
        :avustushaku_id,
        :user_key,
        version + 1,
        :form_submission_id,
        :form_submission_version,
-       status,
-       last_status_change_at,
        :budget_total,
-       :budget_oph_share
+       :budget_oph_share,
+       status,
+       last_status_change_at
 FROM hakemukset
 WHERE user_key = :user_key AND form_submission_id = :form_submission_id
 GROUP BY id, status, last_status_change_at, version
