91e9c149c6eaa15593a3b464204ac6b8e4e7033f
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2015-09-07 14:20:09 +0300

    Skip cancelled hakemus rows

--- va-virkailija/resources/sql/hakija/hakemus/list.sql
+++ va-virkailija/resources/sql/hakija/hakemus/list.sql
@@ -3,5 +3,6 @@ select h.organization_name, h.project_name, h.status, h.budget_oph_share,
 from hakija.hakemukset h
   join hakija.form_submissions s on h.form_submission_id = s.id
 where h.avustushaku = :avustushaku_id
+      and h.status != 'cancelled'
       and s.version_closed is null
       and h.version_closed is null
