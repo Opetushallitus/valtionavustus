2e285f5ae4847748f8b57553cb33fe4d79bdd83d
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2015-09-07 14:24:08 +0300

    Don't crash when no hakemus selected

--- va-virkailija/web/va/hakemus-details/HakemusPreview.jsx
+++ va-virkailija/web/va/hakemus-details/HakemusPreview.jsx
@@ -3,8 +3,8 @@ import React, { Component } from 'react'
 export default class HakemusPreview extends Component {
   render() {
     const hakemus = this.props.hakemus
-    const previewUrl = "http://localhost:8080/avustushaku/1/nayta?avustushaku=1&hakemus=" +
-          hakemus.user_key + "&preview=true"
+    const previewUrl = hakemus ? "http://localhost:8080/avustushaku/1/nayta?avustushaku=1&hakemus=" +
+          hakemus.user_key + "&preview=true" : undefined
     return <iframe src={previewUrl} />
   }
 }
