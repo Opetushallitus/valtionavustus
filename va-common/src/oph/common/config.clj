c39a3d95047bd06f2900f88bc213dbef254c3a25
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2015-09-08 12:55:13 +0300

    Add reading optional secret configuration

--- va-common/src/oph/common/config.clj
+++ va-common/src/oph/common/config.clj
@@ -1,6 +1,7 @@
 (ns oph.common.config
   (:require [clojure.edn]
-            [environ.core :refer [env]]))
+            [environ.core :refer [env]]
+            [clojure.tools.logging :as log]))
 
 (defn config-name [] (env :config))
 
@@ -11,10 +12,25 @@
                       (slurp)
                       (clojure.edn/read-string)))
 
+(defn- slurp-if-found [path]
+  (try
+    (slurp path)
+    (catch Exception e
+      (log/warn (str "Could not read configuration from '" path "'"))
+      "{}")))
+
+(defonce secrets (->  (or (env :configsecrets) (str "../../valtionavustus-secret/" (config-name)))
+                      (slurp-if-found)
+                      (clojure.edn/read-string)))
+
 (defn merge-with-defaults [config]
   (merge-with merge defaults config))
+
+(defn merge-with-secrets [config]
+  (merge-with merge secrets config))
 
 (defonce config (->> (or (env :config) "config/dev.edn")
                     (slurp)
                     (clojure.edn/read-string)
-                    (merge-with-defaults)))
+                    (merge-with-defaults)
+                    (merge-with-secrets)))
