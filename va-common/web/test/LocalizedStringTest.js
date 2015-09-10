0529ec43716016830813f4f328b1d2b49551950f
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2015-09-10 13:51:02 +0300

    Move LocalizedStringTest to va-common

--- va-common/web/test/LocalizedStringTest.js
+++ va-common/web/test/LocalizedStringTest.js
@@ -1,6 +1,6 @@
 import jsdom from 'mocha-jsdom'
 import { expect } from 'chai'
-import translations from '../../resources/public/translations.json'
+import translations from '../../../va-hakija/resources/public/translations.json'
 require("babel/register")({
   only: /(va-hakija\/web|va-common\/web)/
 });
@@ -11,7 +11,7 @@ describe('Localized string', function() {
   it('has test', function() {
     var React = require('react/addons')
     var TestUtils = React.addons.TestUtils
-    var LocalizedString = require('va-common/web/form/component/LocalizedString')
+    var LocalizedString = require('../form/component/LocalizedString')
 
     var string = TestUtils.renderIntoDocument(
       <LocalizedString translations={translations.form} translationKey="lengthleft" lang="fi" />
