12f47c2cbd5d68957deab55b32865db1dab40a02
Jyri Kytömäki <jyri.kytomaki@reaktor.com>
2015-09-10 15:41:32 +0300

    Add support for specifying button type

--- va-common/web/form/component/TextButton.jsx
+++ va-common/web/form/component/TextButton.jsx
@@ -6,8 +6,10 @@ import LocalizedString from 'va-common/web/form/component/LocalizedString.jsx'
 export default class TextButton extends React.Component {
   render() {
     const props = this.props
+    const type = this.props.type ? this.props.type : "button"
     return <button id={props.htmlId}
                    className="soresu-text-button"
+                   type={type}
                    onClick={props.onClick}
                    disabled={props.disabled}>
              <LocalizedString translations={props.translations}
