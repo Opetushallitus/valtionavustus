a153429a1023fcb86be89b245890b9c6d3062e45
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2015-09-16 10:00:34 +0300

    Separate form preview to its own component
    No functional changes.

--- va-virkailija/web/va/hakemus-details/HakemusDetails.jsx
+++ va-virkailija/web/va/hakemus-details/HakemusDetails.jsx
@@ -1,13 +1,6 @@
 import React, { Component } from 'react'
-import _ from 'lodash'
-import Immutable from 'seamless-immutable'
 
-import FormPreview from 'va-common/web/form/FormPreview.jsx'
-import VaPreviewComponentFactory from 'va-common/web/va/VaPreviewComponentFactory'
-import VaBudgetCalculator from 'va-common/web/va/VaBudgetCalculator'
-import FormBranchGrower from 'va-common/web/form/FormBranchGrower'
-import FormRules from 'va-common/web/form/FormRules'
-
+import HakemusPreview from './HakemusPreview.jsx'
 import HakemusArviointi from './HakemusArviointi.jsx'
 
 export default class HakemusDetails extends Component {
@@ -18,67 +11,11 @@ export default class HakemusDetails extends Component {
     const hakuData = this.props.hakuData
     const translations = this.props.translations
 
-    const answers = hakemus.answers
-    const formSpecification = hakuData.form
-    const effectiveForm = _.cloneDeep(formSpecification)
-    effectiveForm.validationErrors = Immutable({})
-    FormRules.applyRulesToForm(formSpecification, effectiveForm, answers)
-
-    const formState = {
-      configuration: {
-        translations: translations,
-        lang: "fi"
-      },
-      form: effectiveForm,
-      saveStatus: {
-        values: answers
-      }
-    }
-    const formElementProps = {
-      state: formState,
-      infoElementValues: avustushaku,
-      controller: new FakeFormController(avustushaku)
-    }
-
-    FormBranchGrower.addFormFieldsForGrowingFieldsInInitialRender(formSpecification.content, effectiveForm.content, answers)
-
-    const budgetCalculator = new VaBudgetCalculator()
-    budgetCalculator.populateBudgetCalculatedValuesForAllBudgetFields(formState, true)
-
     return (
       <div id="hakemus-details">
-        <div id="preview-container">
-          <FormPreview {...formElementProps}/>
-        </div>
+        <HakemusPreview hakemus={hakemus} avustushaku={avustushaku} hakuData={hakuData} translations={translations}/>
         <HakemusArviointi hakemus={hakemus} controller={controller}/>
       </div>
     )
-  }
-}
-
-class FakeFormController {
-  constructor(avustushaku) {
-    this.avustushaku = avustushaku
-    this.customPreviewComponentFactory = new VaPreviewComponentFactory()
-  }
-
-  constructHtmlId(fields, fieldId) {
-    return fieldId
-  }
-
-  getCustomWrapperComponentProperties() {
-    return this.avustushaku
-  }
-
-  getCustomPreviewComponentTypeMapping() {
-    return this.customPreviewComponentFactory.fieldTypeMapping
-  }
-
-  createCustomPreviewComponent(componentProps) {
-    return this.customPreviewComponentFactory.createComponent(componentProps)
-  }
-
-  getCustomWrapperComponentProperties(state) {
-    return { "avustushaku": this.avustushaku }
   }
 }
---
+++ va-virkailija/web/va/hakemus-details/HakemusPreview.jsx
@@ -0,0 +1,78 @@
+import React, { Component } from 'react'
+import _ from 'lodash'
+import Immutable from 'seamless-immutable'
+
+import FormPreview from 'va-common/web/form/FormPreview.jsx'
+import VaPreviewComponentFactory from 'va-common/web/va/VaPreviewComponentFactory'
+import VaBudgetCalculator from 'va-common/web/va/VaBudgetCalculator'
+import FormBranchGrower from 'va-common/web/form/FormBranchGrower'
+import FormRules from 'va-common/web/form/FormRules'
+
+export default class HakemusPreview extends Component {
+  render() {
+    const hakemus = this.props.hakemus
+    const avustushaku = this.props.avustushaku
+    const hakuData = this.props.hakuData
+    const translations = this.props.translations
+
+    const answers = hakemus.answers
+    const formSpecification = hakuData.form
+    const effectiveForm = _.cloneDeep(formSpecification)
+    effectiveForm.validationErrors = Immutable({})
+    FormRules.applyRulesToForm(formSpecification, effectiveForm, answers)
+
+    const formState = {
+      configuration: {
+        translations: translations,
+        lang: "fi"
+      },
+      form: effectiveForm,
+      saveStatus: {
+        values: answers
+      }
+    }
+    const formElementProps = {
+      state: formState,
+      infoElementValues: avustushaku,
+      controller: new FakeFormController(avustushaku)
+    }
+
+    FormBranchGrower.addFormFieldsForGrowingFieldsInInitialRender(formSpecification.content, effectiveForm.content, answers)
+
+    const budgetCalculator = new VaBudgetCalculator()
+    budgetCalculator.populateBudgetCalculatedValuesForAllBudgetFields(formState, true)
+
+    return (
+      <div id="preview-container">
+        <FormPreview {...formElementProps}/>
+      </div>
+    )
+  }
+}
+
+class FakeFormController {
+  constructor(avustushaku) {
+    this.avustushaku = avustushaku
+    this.customPreviewComponentFactory = new VaPreviewComponentFactory()
+  }
+
+  constructHtmlId(fields, fieldId) {
+    return fieldId
+  }
+
+  getCustomWrapperComponentProperties() {
+    return this.avustushaku
+  }
+
+  getCustomPreviewComponentTypeMapping() {
+    return this.customPreviewComponentFactory.fieldTypeMapping
+  }
+
+  createCustomPreviewComponent(componentProps) {
+    return this.customPreviewComponentFactory.createComponent(componentProps)
+  }
+
+  getCustomWrapperComponentProperties(state) {
+    return { "avustushaku": this.avustushaku }
+  }
+}
