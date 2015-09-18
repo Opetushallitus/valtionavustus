807b0d1a911a45f2deeb71133d1f8b70b58dd726
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2015-09-18 17:46:23 +0300

    Add missing attachment create sql

---
+++ va-hakija/resources/sql/attachment/create.sql
@@ -0,0 +1,2 @@
+INSERT INTO attachments (hakemus_id, hakemus_version, version, field_id, filename, content_type, file_size, file_data)
+VALUES (:hakemus_id, :hakemus_version, :version, :field_id, :filename, :content_type, :file_size, :file_data)
