9e9c366d66d2e70d7386c430e7f9178f3550c8dd
Jyri Kytömäki <jyri.kytomaki@reaktor.com>
2015-09-18 17:26:25 +0300

    Add html id to rows

--- va-virkailija/web/va/haku-list/HakuListing.jsx
+++ va-virkailija/web/va/haku-list/HakuListing.jsx
@@ -8,8 +8,7 @@ export default class HakuListing extends Component {
     const selectedHaku = this.props.selectedHaku
     const controller = this.props.controller
     const hakuElements = _.map(hakuList, haku => {
-      return <HakuRow haku={haku} key={hakuList.indexOf(haku)}
-                      selectedHaku={selectedHaku} controller={controller}/> })
+      return <HakuRow haku={haku} key={haku.id} selectedHaku={selectedHaku} controller={controller}/> })
     return (
       <table key="hakuListing" className="haku-list overview-list">
         <thead><tr>
@@ -32,12 +31,12 @@ class HakuRow extends Component {
   }
 
   render() {
-    const key = this.props.key
     const haku = this.props.haku
-    const selectedHaku = this.props.selectedHaku
-    const rowClass = haku === selectedHaku ? "selected overview-row" : "unselected overview-row"
+    const htmlId = "haku-" + haku.id
+    const thisIsSelected = haku === this.props.selectedHaku
+    const rowClass = thisIsSelected ? "selected overview-row" : "unselected overview-row"
     const controller = this.props.controller
-    return <tr className={rowClass} key={key} onClick={controller.selectHaku(haku)}>
+    return <tr id={htmlId} className={rowClass} onClick={controller.selectHaku(haku)}>
       <td className="name-column">{haku.content.name.fi}</td>
       <td className="start-column">{this.toDateStr(haku.content.duration.start)}</td>
       <td className="end-column">{this.toDateStr(haku.content.duration.end)}</td>
