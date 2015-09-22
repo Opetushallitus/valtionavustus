dba16125d84151b003ca1d869921f0137324b90a
Jyri Kytömäki <jyri.kytomaki@reaktor.com>
2015-09-22 15:19:13 +0300

    Extract own class for setting status of hakemus

--- va-virkailija/web/va/hakemus-details/HakemusArviointi.jsx
+++ va-virkailija/web/va/hakemus-details/HakemusArviointi.jsx
@@ -9,35 +9,48 @@ export default class HakemusArviointi extends Component {
     const hakemus = this.props.hakemus
     const comments = hakemus.comments
     const loadingComments = this.props.loadingComments
+    return (
+     <div id="hakemus-arviointi">
+       <HakemusComments controller={controller} hakemus={hakemus} comments={comments} loadingComments={loadingComments}/>
+       <SetStatus controller={controller} hakemus={hakemus} />
+     </div>
+    )
+  }
+}
+
+class SetStatus extends React.Component {
+  render() {
+    const hakemus = this.props.hakemus
     const arvio = hakemus.arvio
     const status = arvio ? arvio.status : undefined
+    const controller = this.props.controller
     const statuses = []
     const statusValues = ['unhandled', 'plausible', 'rejected', 'accepted'];
     for (var i=0; i < statusValues.length; i++) {
       const htmlId = "set-status-" + statusValues[i]
       statuses.push(
-        <input id={htmlId}
-               type="radio"
-               key={htmlId}
-               name="status"
-               value={statusValues[i]}
-               onChange={controller.setHakemusArvioStatus(hakemus, statusValues[i])}
-               checked={statusValues[i] === status ? true: null}
-        />
+          <input id={htmlId}
+                 type="radio"
+                 key={htmlId}
+                 name="status"
+                 value={statusValues[i]}
+                 onChange={controller.setHakemusArvioStatus(hakemus, statusValues[i])}
+                 checked={statusValues[i] === status ? true: null}
+              />
       )
       statuses.push(
-        <label key={htmlId + "-label"}
-               htmlFor={htmlId}>
-          <HakemusStatus status={statusValues[i]}/>
-        </label>
+          <label key={htmlId + "-label"}
+                 htmlFor={htmlId}>
+            <HakemusStatus status={statusValues[i]}/>
+          </label>
       )
     }
+
     return (
-     <div id="hakemus-arviointi">
-       <HakemusComments controller={controller} hakemus={hakemus} comments={comments} loadingComments={loadingComments}/>
-       <h2>Tila</h2>
-       {statuses}
-     </div>
+      <div>
+        <h2>Tila</h2>
+        {statuses}
+      </div>
     )
   }
 }
