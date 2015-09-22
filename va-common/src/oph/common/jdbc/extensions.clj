a75a996239d8906e5d778aa13e73779110e61cb1
Jyri Kytömäki <jyri.kytomaki@reaktor.com>
2015-09-22 10:03:33 +0300

    Inline sql-value functions into extend-protocoll

--- va-common/src/oph/common/jdbc/extensions.clj
+++ va-common/src/oph/common/jdbc/extensions.clj
@@ -6,30 +6,24 @@
             [oph.common.jdbc.enums :refer :all])
   (:import org.postgresql.util.PGobject))
 
-(defn value-to-jsonb-pgobject [value]
-  (doto (PGobject.)
-    (.setType "jsonb")
-    (.setValue (json/generate-string value))))
-
-(defn value-to-status [^clojure.lang.Keyword value]
-  (doto (PGobject.)
-    (.setType "status")
-    (.setValue (name value))))
-
-(defn value-to-haku-status [^oph.common.jdbc.enums.HakuStatus value]
-  (doto (PGobject.)
-    (.setType "haku_status")
-    (.setValue (.value value))))
-
 (extend-protocol jdbc/ISQLValue
   clojure.lang.IPersistentCollection
-  (sql-value [value] (value-to-jsonb-pgobject value))
+  (sql-value [value]
+    (doto (PGobject.)
+      (.setType "jsonb")
+      (.setValue (json/generate-string value))))
 
   oph.common.jdbc.enums.HakuStatus
-  (sql-value [value] (value-to-haku-status value))
+  (sql-value [value]
+    (doto (PGobject.)
+      (.setType "haku_status")
+      (.setValue (.value value))))
 
   clojure.lang.Keyword
-  (sql-value [value] (value-to-status value)))
+  (sql-value [value]
+    (doto (PGobject.)
+      (.setType "status")
+      (.setValue (name value)))))
 
 (extend-protocol jdbc/IResultSetReadColumn
   PGobject
