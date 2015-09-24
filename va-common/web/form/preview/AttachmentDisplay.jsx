1d91be72c7dce6bf1cbb36bfb01e03dc13b29a40
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2015-09-24 11:28:24 +0300

    Some slightly broken stylings
    Work in progress

--- va-common/web/form/preview/AttachmentDisplay.jsx
+++ va-common/web/form/preview/AttachmentDisplay.jsx
@@ -17,12 +17,12 @@ class ExistingAttachmentDisplay extends BasicInfoComponent {
   render() {
     const attachment = this.props.attachment
     const dateTimeString = this.asDateTimeString(attachment.created_at)
-    return <span>
+    return <div className="soresu-attachment-display">
              <a href={this.props.downloadUrl} target="_blank">{attachment.filename}</a>
                <span> (<LocalizedString translations={this.props.translations.form.attachment}
                                         translationKey="attached"
                                         lang={this.props.lang}/> {dateTimeString})
                </span>
-           </span>
+           </div>
   }
 }
