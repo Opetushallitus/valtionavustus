4eedc8c39d548ec84fc3bf1c7e608f8f98ccd568
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2015-09-24 10:42:04 +0300

    Display when attachment was created

--- va-common/web/form/preview/AttachmentDisplay.jsx
+++ va-common/web/form/preview/AttachmentDisplay.jsx
@@ -2,26 +2,26 @@ import React from 'react'
 import ClassNames from 'classnames'
 
 import PreviewComponent from './PreviewComponent.jsx'
+import {BasicInfoComponent} from '../component/InfoElement.jsx'
 import LocalizedString from '../component/LocalizedString.jsx'
 
 export default class AttachmentDisplay extends PreviewComponent {
   render() {
-    const attachment = this.props.attachment
-    const downloadUrl = this.props.downloadUrl
-    return attachment ?
-      <ExistingAttachmentDisplay attachment={attachment} downloadUrl={downloadUrl} labelText="liitetty" /> :
+    return this.props.attachment ?
+      <ExistingAttachmentDisplay {...this.props} labelText="liitetty" /> :
       <span/>
   }
 }
 
-class ExistingAttachmentDisplay extends React.Component {
+class ExistingAttachmentDisplay extends BasicInfoComponent {
   render() {
     const attachment = this.props.attachment
     const labelText = this.props.labelText
+    const dateTimeString = this.asDateTimeString(attachment.created_at)
     const downloadUrl = this.props.downloadUrl
     return <span>
              <a href={downloadUrl} target="_blank">{attachment.filename}</a>
-               <span> ({labelText} TODO 0)</span>
+               <span> (TODO {labelText} {dateTimeString})</span>
            </span>
   }
 }
