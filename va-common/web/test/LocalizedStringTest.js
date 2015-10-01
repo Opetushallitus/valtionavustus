be51618cbee604f48e76a50586401762bd88b3e1
Harri Salokorpi <harri.salokorpi@reaktor.fi>
2015-10-01 18:20:14 +0300

    Fix test import paths in va-common

--- va-common/web/test/LocalizedStringTest.js
+++ va-common/web/test/LocalizedStringTest.js
@@ -11,7 +11,7 @@ describe('Localized string', function() {
   it('has test', function() {
     var React = require('react/addons')
     var TestUtils = React.addons.TestUtils
-    var LocalizedString = require('../form/component/LocalizedString')
+    var LocalizedString = require('soresu-form/web/form/component/LocalizedString')
 
     var string = TestUtils.renderIntoDocument(
       <LocalizedString translations={translations.form} translationKey="lengthleft" lang="fi" />
