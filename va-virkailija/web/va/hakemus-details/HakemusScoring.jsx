978efdeff43d3473e27536a1414e45bb4c017e02
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2015-10-09 11:50:24 +0300

    Color stars according to current user scores per peruste

--- va-virkailija/web/va/hakemus-details/HakemusScoring.jsx
+++ va-virkailija/web/va/hakemus-details/HakemusScoring.jsx
@@ -5,10 +5,22 @@ export default class HakemusScoring extends Component {
   render() {
     const controller = this.props.controller
     const hakemus = this.props.hakemus
+    const allScoresOfHakemus = hakemus.scores
+    const userInfo = this.props.userInfo
+
+    function findScores(perusteIndex) {
+      return _.filter(allScoresOfHakemus, s => { return s["selection-criteria-index"] === perusteIndex })
+    }
+
     const avustushaku = this.props.avustushaku
     const valintaperusteet = _.get(avustushaku, "content.selection-criteria.items")
-    const valintaPerusteRows = _.map(valintaperusteet, peruste => { return <ValintaPerusteRow valintaperuste={peruste}/> } )
-    return <div id="hakemus-scoring-container">
+    var perusteIndex = 0
+    const valintaPerusteRows = _.map(valintaperusteet,
+                                     peruste => { return <ValintaPerusteRow valintaperuste={peruste}
+                                                                            scores={findScores(perusteIndex)}
+                                                                            userInfo={userInfo}
+                                                                            key={perusteIndex++} /> })
+    return <div key="hakemus-scoring-container" id="hakemus-scoring-container">
              <div className="valintaperuste-list">
                {valintaPerusteRows}
              </div>
@@ -16,19 +28,31 @@ export default class HakemusScoring extends Component {
   }
 }
 
-class ValintaPerusteRow {
+class ValintaPerusteRow extends Component {
   render() {
     const valintaperuste = this.props.valintaperuste
+    const allScoresOfThisPeruste = this.props.scores
+    const userInfo = this.props.userInfo
+    const myScore = _.find(allScoresOfThisPeruste, s => { return s["person-oid"] === userInfo["person-oid"] })
+    const starElements = _.map(_.range(4), i => <StarElement key={i} index={i} myScore={myScore} />)
+
     const textInFinnish = valintaperuste.fi
     const textInSwedish = valintaperuste.sv
-    return <div className="single-valintaperuste" >
+
+    return <div className="single-valintaperuste">
             <div className="valintaperuste-text" title={textInFinnish + " / " + textInSwedish}>{textInFinnish}</div>
             <div className="score-row">
-              <span className="single-score"><img src="/img/star_off.png"/></span>
-              <span className="single-score"><img src="/img/star_off.png"/></span>
-              <span className="single-score"><img src="/img/star_off.png"/></span>
-              <span className="single-score"><img src="/img/star_off.png"/></span>
+              {starElements}
             </div>
            </div>
+  }
+}
+
+class StarElement extends Component {
+  render() {
+    const index = this.props.index
+    const myScore = this.props.myScore
+    const starImage = myScore && myScore.score >= index ? "/img/star_on.png" : "/img/star_off.png"
+    return <span className="single-score"><img src={starImage}/></span>
   }
 }
