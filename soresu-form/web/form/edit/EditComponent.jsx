4e0a580f71b1fca874204c242fa4ef943e8e165c
Jyri Kytömäki <jyri.kytomaki@reaktor.com>
2015-10-22 11:07:53 +0300

    Extract translation table rendering to own function

--- soresu-form/web/form/edit/EditComponent.jsx
+++ soresu-form/web/form/edit/EditComponent.jsx
@@ -2,59 +2,62 @@ import React from 'react'
 import ClassNames from 'classnames'
 
 export default class EditComponent extends React.Component {
+
+  fieldValueUpdater(valueContainerGetter, valueName, newValue) {
+    const field = this.props.field
+    const formEditorController = this.props.formEditorController
+    return e => {
+      formEditorController.editField(field.id, valueContainerGetter, valueName, typeof newValue === 'undefined' ? e.target.value : newValue)
+    }
+
+  }
+
+  renderTranslationTable(htmlId, name, valueGetter) {
+    const field = this.props.field
+    if(typeof valueGetter(field) === 'undefined') {
+      return undefined
+    }
+    return (
+      <table className="translation">
+        <thead><th>{name}</th><th>{name + " ruotsiksi"}</th></thead>
+        <tr>
+          <td><textarea onChange={this.fieldValueUpdater(valueGetter, "fi")} name={htmlId+"-fi"} value={valueGetter(field).fi}></textarea></td>
+          <td><textarea onChange={this.fieldValueUpdater(valueGetter, "sv")} name={htmlId+"-sv"} value={valueGetter(field).sv}></textarea></td>
+        </tr>
+      </table>
+    )
+  }
+
   render(fieldSpecificEdit) {
     const field = this.props.field
     const formEditorController = this.props.formEditorController
     const htmlId = this.props.htmlId
-    var labelEdit = undefined
-    const fieldValueUpdater =  function(valueContainerGetter, valueName, newValue) {
-      return e => {
-        formEditorController.editField(field.id, valueContainerGetter, valueName, typeof newValue === 'undefined' ? e.target.value : newValue)
-      }
-    }
-    if(field.label) {
-      labelEdit = (
-        <table className="translation">
-          <thead><th>Kysymys</th><th>Kysymys ruotsiksi</th></thead>
-          <tr>
-            <td><textarea onChange={fieldValueUpdater(x => x.label, "fi")} name={htmlId+"-label-fi"} value={field.label.fi}></textarea></td>
-            <td><textarea onChange={fieldValueUpdater(x => x.label, "sv")} name={htmlId+"-label-sv"} value={field.label.sv}></textarea></td>
-          </tr>
-        </table>
-      )
-    }
-    var helpTextEdit = undefined
-    if(field.helpText) {
-      helpTextEdit = (
-        <table className="translation">
-          <thead><th>Ohjeteksti</th><th>Ohjeteksti ruotsiksi</th></thead>
-          <tr>
-            <td><textarea onChange={fieldValueUpdater(x => x.helpText, "fi")} name={htmlId+"-help-text-fi"} value={field.helpText.fi}></textarea></td>
-            <td><textarea onChange={fieldValueUpdater(x => x.helpText, "sv")} name={htmlId+"-help-text-sv"} value={field.helpText.sv}></textarea></td>
-          </tr>
-        </table>
-      )
-    }
+    var labelEdit = this.renderTranslationTable(htmlId + "-label", this.labelName(), x => x.label)
+    var helpTextEdit = this.renderTranslationTable(htmlId + "-help-text", "Ohjeteksti", x => x.helpText)
     var requiredEdit = undefined
     if(typeof field.required != 'undefined') {
       requiredEdit = (
-       <div>
-         <label htmlFor={htmlId+"-required"}>Pakollinen tieto</label>
-         <input onChange={fieldValueUpdater(x => x, "required", !field.required)} type="checkbox" id={htmlId+"-required"} name={htmlId+"-required"} checked={field.required}/>
-       </div>
+        <div>
+          <label htmlFor={htmlId+"-required"}>Pakollinen tieto</label>
+          <input onChange={this.fieldValueUpdater(x => x, "required", !field.required)} type="checkbox" id={htmlId+"-required"} name={htmlId+"-required"} checked={field.required}/>
+        </div>
       )
     }
     const removeField = e => { formEditorController.removeField(field) }
     return (
-      <div key={htmlId} className={this.className()}>
-        <h3>{this.componentName()}</h3>
-        <span onClick={removeField} className="soresu-edit soresu-field-remove">Poista</span>
-        {labelEdit}
-        {requiredEdit}
-        {helpTextEdit}
-        {fieldSpecificEdit}
-      </div>
+        <div key={htmlId} className={this.className()}>
+          <h3>{this.componentName()}</h3>
+          <span onClick={removeField} className="soresu-edit soresu-field-remove">Poista</span>
+          {labelEdit}
+          {requiredEdit}
+          {helpTextEdit}
+          {fieldSpecificEdit}
+        </div>
     )
+  }
+
+  labelName() {
+    return "Kysymys"
   }
 
   componentName() {
