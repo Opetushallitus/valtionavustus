17335ad0c5aa90614021144a9163b029677a6493
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2015-10-22 17:19:55 +0300

    Support empty help text by language

--- soresu-form/web/form/component/HelpTooltip.jsx
+++ soresu-form/web/form/component/HelpTooltip.jsx
@@ -4,6 +4,9 @@ import Translator from '../Translator.js'
 export default class HelpTooltip extends React.Component {
   render() {
     const translator = new Translator(this.props)
+    if (!_.get(this.props, "content." + this.props.lang)) {
+      return <span/>
+    }
     const value = translator.translate("content", this.props.lang)
     return <a className="soresu-tooltip soresu-tooltip-up">
       <img src="img/show_tooltip.png"/>
