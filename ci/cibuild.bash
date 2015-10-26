47faa5fdb19bc029f95903416ec94383c5fa3562
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2015-10-26 15:57:23 +0200

    Let lein modules run soresu tests too

--- ci/cibuild.bash
+++ ci/cibuild.bash
@@ -32,13 +32,6 @@ function clean() {
   find $PROJECTROOTPATH -depth -type d -name 'node_modules' -exec rm -rf {} \;
 }
 
-function run_soresu_form_tests() {
-  cd soresu-form
-  time npm install
-  time npm run test-junit
-  cd ..
-}
-
 function install_module() {
   cd $1
   time $LEIN install
@@ -71,7 +64,6 @@ function run_tests() {
     start_postgresql_in_docker
   fi
 
-  run_soresu_form_tests
   install_module soresu-form
   install_module va-common
   time $LEIN with-profile ci modules spec -f junit || true
