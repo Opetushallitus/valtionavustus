0ff88a465994645f00990623b77660fe2de10cbf
Harri Salokorpi <harri.salokorpi@reaktor.fi>
2015-11-04 17:37:59 +0200

    Fix focus area data generation

--- va-hakija/src/oph/va/hakija/cmd/populate.clj
+++ va-hakija/src/oph/va/hakija/cmd/populate.clj
@@ -136,7 +136,7 @@
 (defmethod generate "emailField" [field] "foobar@example.org")
 (defmethod generate "vaEmailNotification" [field] "foobar@example.org")
 (defmethod generate "radioButton" [field] (:value (first (:options field))))
-(defmethod generate "vaFocusAreas" [field] [(:value (first (:options field)))])
+(defmethod generate "vaFocusAreas" [field] ["focus-area-0"])
 (defmethod generate "checkboxButton" [field] [(:value (first (:options field)))])
 (defmethod generate "textArea" [field] (generate-text random-text field))
 
