92677a78a774534bbb1de1e3f9e3abfce3b27acc
Harri Salokorpi <harri.salokorpi@reaktor.fi>
2015-11-13 16:11:02 +0200

    Extract functions

--- va-virkailija/src/oph/va/virkailija/export.clj
+++ va-virkailija/src/oph/va/virkailija/export.clj
@@ -2,29 +2,36 @@
   (:use [clojure.tools.trace :only [trace]])
   (:require [clojure.java.io :as io]
             [dk.ative.docjure.spreadsheet :as spreadsheet]
-            [oph.va.virkailija.handlers :as handlers])
+            [oph.va.virkailija.hakudata :as hakudata])
   (:import [java.io ByteArrayOutputStream ByteArrayInputStream]))
 
+(def main-sheet-name "Hakemukset")
 (def main-sheet-columns ["Hakijaorganisaatio"
                          "Diaarinumero"
                          "Hankkeen nimi"
                          "Ehdotettu budjetti"
                          "OPH:n avustuksen osuus"])
 
+(defn- valid-hakemus? [hakemus]
+  (= (:status hakemus) "submitted"))
+
+(defn- hakemus->main-sheet-rows [hakemus]
+  [(:organization-name hakemus)
+   (:register-number hakemus)
+   (:project-name hakemus)
+   (:budget-total hakemus)
+   (:budget-oph-share hakemus)])
+
 (defn export-avustushaku [avustushaku-id]
-  (let [avustushaku (handlers/get-combined-avustushaku-data avustushaku-id)
+  (let [avustushaku (hakudata/get-combined-avustushaku-data avustushaku-id)
         output (ByteArrayOutputStream.)
         rows (->> (:hakemukset avustushaku)
-                  (filter (fn [hakemus] (= (:status hakemus) "submitted")))
-                  (mapv (fn [hakemus]
-                          [(:organization-name hakemus)
-                           (:register-number hakemus)
-                           (:project-name hakemus)
-                           (:budget-total hakemus)
-                           (:budget-oph-share hakemus)])))
-        wb (spreadsheet/create-workbook "Hakemukset"
+                  (filter valid-hakemus?)
+                  (mapv hakemus->main-sheet-rows))
+        wb (spreadsheet/create-workbook main-sheet-name
                                         (apply conj [main-sheet-columns] rows))
-        main-sheet (spreadsheet/select-sheet "Hakemukset" wb)]
+        main-sheet (spreadsheet/select-sheet main-sheet-name wb)]
+    ;; Make columns fit the data
     (doseq [index (range 0 (count main-sheet-columns))]
       (.autoSizeColumn main-sheet index))
     (.write wb output)
