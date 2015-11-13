fe03dcc2bf71248b185a47378280facf579ccd30
Harri Salokorpi <harri.salokorpi@reaktor.fi>
2015-11-13 17:57:05 +0200

    Add secondary answers sheet skeleton, clean up code

--- va-virkailija/src/oph/va/virkailija/export.clj
+++ va-virkailija/src/oph/va/virkailija/export.clj
@@ -14,6 +14,13 @@
                          "Myönnetty avustus"
                          "Arviokeskiarvo"])
 
+(def answers-sheet-name "Vastaukset")
+(def answers-sheet-columns ["Hakijaorganisaatio"
+                            "Diaarinumero"
+                            "Hankkeen nimi"
+                            "Ehdotettu budjetti"
+                            "OPH:n avustuksen osuus"])
+
 (defn- valid-hakemus? [hakemus]
   (= (:status hakemus) "submitted"))
 
@@ -26,24 +33,37 @@
         (comp :budget-granted :arvio)
         (comp :score-total-average :scoring :arvio)))
 
+(defn- fit-columns [columns sheet]
+  ;; Make columns fit the data
+  (doseq [index (range 0 (count columns))]
+    (.autoSizeColumn sheet index)))
+
 (defn export-avustushaku [avustushaku-id]
   (let [avustushaku (hakudata/get-combined-avustushaku-data avustushaku-id)
+        hakemus-list (->> (:hakemukset avustushaku)
+                          (filter valid-hakemus?))
         output (ByteArrayOutputStream.)
-        rows (->> (:hakemukset avustushaku)
-                  (filter valid-hakemus?)
-                  (mapv hakemus->main-sheet-rows))
+        main-sheet-rows (mapv hakemus->main-sheet-rows hakemus-list)
         wb (spreadsheet/create-workbook main-sheet-name
-                                        (apply conj [main-sheet-columns] rows))
+                                        (apply conj [main-sheet-columns] main-sheet-rows))
+
         main-sheet (spreadsheet/select-sheet main-sheet-name wb)
-        main-header-row (first (spreadsheet/row-seq main-sheet))]
+        main-header-row (first (spreadsheet/row-seq main-sheet))
+
+        answers-sheet (let [sheet (spreadsheet/add-sheet! wb answers-sheet-name)]
+                        (spreadsheet/add-row! sheet answers-sheet-columns)
+                        sheet)
+        answers-header-row (first (spreadsheet/row-seq answers-sheet))
+
+        header-style (spreadsheet/create-cell-style! wb {:background :yellow
+                                                         :font {:bold true}})]
+
+    (fit-columns main-sheet-columns main-sheet)
+    (fit-columns answers-sheet-columns answers-sheet)
 
     ;; Style first row
-    (->> (spreadsheet/create-cell-style! wb {:background :yellow
-                                             :font {:bold true}})
-         (spreadsheet/set-row-style! main-header-row))
+    (spreadsheet/set-row-style! main-header-row header-style)
+    (spreadsheet/set-row-style! answers-header-row header-style)
 
-    ;; Make columns fit the data
-    (doseq [index (range 0 (count main-sheet-columns))]
-      (.autoSizeColumn main-sheet index))
     (.write wb output)
     (ByteArrayInputStream. (.toByteArray output))))
