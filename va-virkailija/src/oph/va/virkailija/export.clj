be5391fe66e9c779fa250df39e1d5a3f30bb3234
Harri Salokorpi <harri.salokorpi@reaktor.fi>
2015-11-13 15:55:05 +0200

    Autoscale columns

--- va-virkailija/src/oph/va/virkailija/export.clj
+++ va-virkailija/src/oph/va/virkailija/export.clj
@@ -5,13 +5,27 @@
             [oph.va.virkailija.handlers :as handlers])
   (:import [java.io ByteArrayOutputStream ByteArrayInputStream]))
 
+(def main-sheet-columns ["Hakijaorganisaatio"
+                         "Diaarinumero"
+                         "Hankkeen nimi"
+                         "Ehdotettu budjetti"
+                         "OPH:n avustuksen osuus"])
+
 (defn export-avustushaku [avustushaku-id]
   (let [avustushaku (handlers/get-combined-avustushaku-data avustushaku-id)
         output (ByteArrayOutputStream.)
         rows (->> (:hakemukset avustushaku)
+                  (filter (fn [hakemus] (= (:status hakemus) "submitted")))
                   (mapv (fn [hakemus]
-                          [(:organization-name hakemus) (:project-name hakemus)])))
-        wb (spreadsheet/create-workbook "Avustushaut"
-                                        (trace (apply conj [["Hakijaorganisaatio" "Hankkeen nimi"]] rows)))]
+                          [(:organization-name hakemus)
+                           (:register-number hakemus)
+                           (:project-name hakemus)
+                           (:budget-total hakemus)
+                           (:budget-oph-share hakemus)])))
+        wb (spreadsheet/create-workbook "Hakemukset"
+                                        (apply conj [main-sheet-columns] rows))
+        main-sheet (spreadsheet/select-sheet "Hakemukset" wb)]
+    (doseq [index (range 0 (count main-sheet-columns))]
+      (.autoSizeColumn main-sheet index))
     (.write wb output)
     (ByteArrayInputStream. (.toByteArray output))))
