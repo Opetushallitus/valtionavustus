5966e5641c5dfcf91dfcb3cac815b45ba6f7087a
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2015-11-16 14:04:58 +0200

    Show "no results" when nothing found

--- va-virkailija/web/va/haku-details/HakuRoles.jsx
+++ va-virkailija/web/va/haku-details/HakuRoles.jsx
@@ -65,11 +65,12 @@ class PersonSelectList extends React.Component {
 
       return <li key={r["person-oid"]}>{addButton} {displayText}</li>
     })
+    const resultRows = personRows.length === 0 ? [ <li>Ei hakutuloksia.</li>] : personRows
     const searchResultClassNames = ClassNames(undefined, { loading: ldapSearch.loading,
                                                            hidden: ldapSearch.input.length < LdapSearchParameters.minimumSearchInputLength() })
     return <div id="ldap-search-results" className={searchResultClassNames}>
                <ul className={searchResultClassNames}>
-                 {personRows}
+                 {resultRows}
                </ul>
            </div>
   }
