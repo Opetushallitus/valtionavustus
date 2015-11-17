c0c006cf0b2602c3b3170faf02c888b6e09cb6b6
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2015-11-17 16:32:41 +0200

    Save role edit immediately
    Deferring save to blur does not work well with this new UI which populates the
    user details to role automatically from LDAP.

--- va-virkailija/web/va/haku-details/HakuRoles.jsx
+++ va-virkailija/web/va/haku-details/HakuRoles.jsx
@@ -105,21 +105,12 @@ class RoleRow extends React.Component {
   constructor(props) {
     super(props)
     this.handleChange = this.handleChange.bind(this)
-    this.handleBlur = this.handleBlur.bind(this)
-    this.state = {changes: false}
   }
 
   handleChange(event) {
     this.props.role[event.target.name] = event.target.value
-    this.setState({changes: true})
+    this.props.controller.saveRole(this.props.avustushaku, this.props.role)
     this.props.controller.reRender()
-  }
-
-  handleBlur(event) {
-    if(this.state.changes) {
-      this.setState({changes: false})
-      this.props.controller.saveRole(this.props.avustushaku, this.props.role)
-    }
   }
 
   render() {
@@ -133,7 +124,7 @@ class RoleRow extends React.Component {
     return (
       <tr>
         <td>
-          <select onChange={this.handleChange} name="role" onBlur={this.handleBlur} value={role.role}>
+          <select onChange={this.handleChange} name="role" value={role.role}>
             <option value="presenting_officer">Esittelijä</option>
             <option value="evaluator">Arvioija</option>
           </select>
