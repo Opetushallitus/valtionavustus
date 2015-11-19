11c3f2fd9c15b65a2301be6433e64d0f486ce63d
Jyri Kytömäki <jyri.kytomaki@reaktor.com>
2015-11-19 14:14:05 +0200

    join also by form submission version

--- va-virkailija/resources/sql/hakija/hakemus/list-by-avustushaku.sql
+++ va-virkailija/resources/sql/hakija/hakemus/list-by-avustushaku.sql
@@ -1,10 +1,9 @@
 select h.id, h.organization_name, h.project_name, h.status, h.budget_total, h.budget_oph_share,
   s.answers->'value' as answer_values, h.user_key, h.register_number
 from hakija.hakemukset h
-  join hakija.form_submissions s on h.form_submission_id = s.id
+  join hakija.form_submissions s on (h.form_submission_id = s.id and h.form_submission_version = s.version)
 where h.avustushaku = :avustushaku_id
       and h.status != 'cancelled'
       and h.status != 'new'
-      and s.version_closed is null
       and h.version_closed is null
 order by upper(h.organization_name), upper(h.project_name)
