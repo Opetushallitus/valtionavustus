56c8fca0b8fc144214731606c8a3ef19870d6b47
Harri Salokorpi <harri.salokorpi@reaktor.fi>
2015-12-21 14:33:34 +0200

    Use checkboxbutton instead of replicating the functionality

--- va-common/web/va/VaTraineeDayCalculator.jsx
+++ va-common/web/va/VaTraineeDayCalculator.jsx
@@ -1,34 +1,37 @@
 import React from 'react'
 
 import BasicFieldComponent from 'soresu-form/web/form/component/BasicFieldComponent.jsx'
+import CheckboxButton from 'soresu-form/web/form/component/CheckboxButton.jsx'
 
 // Koulutettavapäivälaskuri in finnish
 export default class VaTraineeDayCalculator extends React.Component {
   render() {
     const props = this.props;
+    const onChange = (param) => {
+      console.log("Param", param)
+    }
+    const selectionOptions = [
+      {
+        "value": "fi",
+        "label": {
+          "fi": "Suomi",
+          "sv": "Finska"
+        }
+      },
+      {
+        "value": "sv",
+        "label": {
+          "fi": "Ruotsi",
+          "sv": "Svenska"
+        }
+      }
+    ]
     return (
-      <div className="soresu-radio">
-
-        <input type="radio" id={props.htmlId + ".studypoints-checkbox"}
-                            key={props.htmlId + ".studypoints-checkbox"}
-                            name="study-points"
-                            value="study-points"
-                            checked="false" />
-        <label key={props.htmlId + "." + "studypoints-checkbox.label"}
-               htmlFor={props.htmlId + ".studypoints-checkbox"}>
-          Opintopisteet
-        </label>
-
-        <input type="radio" id={props.htmlId + ".trainingdays-checkbox"}
-                            key={props.htmlId + ".trainingdays-checkbox"}
-                            name="training-days"
-                            value="training-days"
-                            checked="" />
-        <label key={props.htmlId + "." + "trainingdays-checkbox.label"}
-               htmlFor={props.htmlId + ".trainingdays-checkbox"}>
-          Koulutuspäivät
-        </label>
-      </div>
+      <CheckboxButton options={selectionOptions}
+                      onChange={onChange}
+                      value="fi"
+                      translations={this.props.translations}
+                      lang={this.props.lang} />
     )
   }
 }
