5bb2b918b9261c9ff8677d10c11c92486eaf9bbb
Jyri Kytömäki <jyri.kytomaki@reaktor.com>
2016-01-11 14:28:56 +0200

    Use vmware servers

--- servers/ssh_to_va-prod.bash
+++ servers/ssh_to_va-prod.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Update the IP address when needed
-`dirname $0`/ssh_to_va.bash 192.168.1.13 -L20876:localhost:10876 -L21322:localhost:11322 $@
+`dirname $0`/ssh_to_va.bash oph-va-app-prod01.csc.fi -L20876:localhost:10876 -L21322:localhost:11322 $@
--- servers/ssh_to_va-test.bash
+++ servers/ssh_to_va-test.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Update the IP address when needed
-`dirname $0`/ssh_to_va.bash 192.168.1.12 -L10876:localhost:10876 -L11322:localhost:11322 $@
+`dirname $0`/ssh_to_va.bash oph-va-app-test01.csc.fi -L10876:localhost:10876 -L11322:localhost:11322 $@
