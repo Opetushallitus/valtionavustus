23b15f4b7f97581a4436b37ff7703e87cc9a2a19
Eero Anttila <eea@iki.fi>
2016-02-26 17:21:19 +0200

    Add some dynamic data and basic styles for decision

--- va-virkailija/web/va/PaatosApp.jsx
+++ va-virkailija/web/va/PaatosApp.jsx
@@ -7,15 +7,44 @@ import RouteParser from 'route-parser'
 import HakemusArviointiStatuses from './hakemus-details/HakemusArviointiStatuses.js'
 
 import PaatosController from './PaatosController.jsx'
+import style from './paatos.less'
 
 export default class PaatosApp extends Component {
   render() {
     const data = this.props.state.paatosData
     return (
         <section>
-        <h1>Päätös</h1>
-        <h2>{data.avustushaku.content.name.fi}</h2>
-        <h2>{HakemusArviointiStatuses.statusToFI(data.hakemus.arvio.status)}</h2>
+          <header>
+            <h1>Päätös</h1>
+          </header>
+          <div className="body">
+            <section className="section">
+              <h2>Asia</h2>
+              <div className="content">
+                {data.avustushaku.content.name.fi}
+              </div>
+            </section>
+            <section className="section">
+              <h2>Päätös</h2>
+              <div className="content">
+                <p>{HakemusArviointiStatuses.statusToFI(data.hakemus.arvio.status)}</p>
+                <p>Hakija: {data.hakemus['organization-name']}<br/>
+                  Hanke: {data.hakemus['project-name']}</p>
+
+                <p>Opetushallitus hyväksyy avustuksen saajan valtionavustukseen oikeuttavina menoina xx euroa.
+                  Valtionavustuksena tästä myönnetään {(100 - data.avustushaku.content['self-financing-percentage'])} %
+                  eli {data.hakemus.arvio['budget-granted']} euroa</p>
+
+              </div>
+            </section>
+            <section className="section">
+              <h2>Avustuksen maksu</h2>
+              <div className="content">
+                Pankkitili: xx
+              </div>
+            </section>
+          </div>
+          <h2>{HakemusArviointiStatuses.statusToFI(data.hakemus.arvio.status)}</h2>
         </section>
     )
   }
