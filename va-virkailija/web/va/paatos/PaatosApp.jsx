d6f65d65ceca686a71d87e1df857c9760ba2d34f
Eero Anttila <eea@iki.fi>
2016-03-01 08:50:02 +0200

    Split accepted and rejected decision to separate components

--- va-virkailija/web/va/paatos/PaatosApp.jsx
+++ va-virkailija/web/va/paatos/PaatosApp.jsx
@@ -24,25 +24,8 @@ export default class PaatosApp extends Component {
                 {data.avustushaku.content.name.fi}
               </div>
             </section>
-            <section className="section">
-              <h2>Päätös</h2>
-              <div className="content">
-                <p>{HakemusArviointiStatuses.statusToFI(data.hakemus.arvio.status)}</p>
-                <p>Hakija: {data.hakemus['organization-name']}<br/>
-                  Hanke: {data.hakemus['project-name']}</p>
-
-                <p>Opetushallitus hyväksyy avustuksen saajan valtionavustukseen oikeuttavina menoina xx euroa.
-                  Valtionavustuksena tästä myönnetään {(100 - data.avustushaku.content['self-financing-percentage'])} %
-                  eli {data.hakemus.arvio['budget-granted']} euroa</p>
-
-              </div>
-            </section>
-            <section className="section">
-              <h2>Avustuksen maksu</h2>
-              <div className="content">
-                Pankkitili: xx
-              </div>
-            </section>
+            {data.hakemus.arvio.status == 'rejected' ? <RejectedDecision hakemus={data.hakemus}/> :
+                <AcceptedDecision hakemus={data.hakemus} avustushaku={data.avustushaku}/>}
           </div>
           <h2>{HakemusArviointiStatuses.statusToFI(data.hakemus.arvio.status)}</h2>
         </section>
@@ -50,6 +33,51 @@ export default class PaatosApp extends Component {
   }
 }
 
+class AcceptedDecision extends Component {
+  render() {
+    const hakemus = this.props.hakemus
+    const avustushaku = this.props.avustushaku
+    return (
+        <section>
+          <section className="section">
+            <h2>Päätös</h2>
+            <div className="content">
+              <p>Opetushallitus on päättänyt myöntää valtionavustusta seuraavasti:</p>
+              <p>Hakija: {hakemus['organization-name']}<br/>
+                Hanke: {hakemus['project-name']}</p>
+              <p>Opetushallitus hyväksyy avustuksen saajan valtionavustukseen oikeuttavina menoina xx euroa.
+                Valtionavustuksena tästä myönnetään {(100 - avustushaku.content['self-financing-percentage'])} %
+                eli {hakemus.arvio['budget-granted']} euroa</p>
+            </div>
+          </section>
+          <section className="section">
+            <h2>Avustuksen maksu</h2>
+            <div className="content">
+              Pankkitili: xx
+            </div>
+          </section>
+        </section>
+    )
+  }
+}
+
+class RejectedDecision extends Component {
+  render() {
+    const hakemus = this.props.hakemus
+    return (
+        <section>
+          <section className="section">
+            <h2>Päätös</h2>
+            <div className="content">
+              <p>Opetushallitus on päättänyt olla myöntämättä valtionavustusta seuraavasti:</p>
+              <p>Hakija: {hakemus['organization-name']}<br/>
+                Hanke: {hakemus['project-name']}</p>
+            </div>
+          </section>
+        </section>
+    )
+  }
+}
 const parsedRoute = new RouteParser('/paatos/avustushaku/:avustushaku_id/hakemus/:hakemus_id').match(location.pathname)
 const controller = new PaatosController()
 const stateP = controller.initializeState(parsedRoute)
