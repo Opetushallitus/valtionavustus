7f87bd0ca58cfc393b66f5c97e7ab55ca6fd8b14
Eero Anttila <eea@iki.fi>
2016-03-04 15:20:10 +0200

    Move iban and bic under accepted decision component and use plain function for accepted decision

--- va-virkailija/web/va/paatos/PaatosApp.jsx
+++ va-virkailija/web/va/paatos/PaatosApp.jsx
@@ -31,9 +31,7 @@ export default class PaatosApp extends Component {
     const avustushaku = paatosData.avustushaku
     const roles = paatosData.roles
     const decisionStatus = hakemus.arvio.status
-    const answers = paatosData.hakemus.answers
-    const iban = InputValueStorage.readValues(answers, 'iban')[0].value
-    const bic = InputValueStorage.readValues(answers, 'bic')[0].value
+
     return (
         <section>
           <header>
@@ -50,57 +48,52 @@ export default class PaatosApp extends Component {
               </div>
             </section>
             {decisionStatus == 'rejected' ? <RejectedDecision hakemus={hakemus}/> :
-                <AcceptedDecision hakemus={hakemus} avustushaku={avustushaku} iban={iban} bic={bic} roles={roles}/>}
+                <AcceptedDecision hakemus={hakemus} avustushaku={avustushaku} roles={roles}/>}
           </div>
         </section>
     )
   }
 }
 
-class AcceptedDecision extends Component {
-  render() {
-    const hakemus = this.props.hakemus
-    const avustushaku = this.props.avustushaku
-    const iban = this.props.iban
-    const bic = this.props.bic
-    const roles = this.props.roles
-    const selectedRoleId = hakemus.arvio['presenter-role-id']
-    const role = selectedRoleId ? roles.find(role => role.id === selectedRoleId) : roles[0]
-
-    return (
-        <section>
-          <section className="section">
-            <h2>Päätös</h2>
-            <div className="content">
-              <p>Opetushallitus on päättänyt myöntää valtionavustusta seuraavasti:</p>
-              <p>Hakija: {hakemus['organization-name']}<br/>
-                Hanke: {hakemus['project-name']}</p>
-              <p>Opetushallitus hyväksyy avustuksen saajan valtionavustukseen oikeuttavina menoina xx euroa.
-                Valtionavustuksena tästä myönnetään {(100 - avustushaku.content['self-financing-percentage'])} %
-                eli {hakemus.arvio['budget-granted']} euroa</p>
-            </div>
-          </section>
-          <section className="section">
-            <h2>Päätöksen perustelut</h2>
-            <div className="content">
-              {hakemus.arvio.perustelut}
-            </div>
-          </section>
-          <section className="section">
-            <h2>Avustuksen maksu</h2>
-            <div className="content">
-              Pankkitili: <strong>{iban}, {bic}</strong>
-            </div>
-          </section>
-          <section className="section">
-            <h2>Lisätietoja</h2>
-            <div className="content">
-              Lisätietoja antaa: {role.name} &lt;{role.email}&gt;
-            </div>
-          </section>
+const AcceptedDecision = ({hakemus, avustushaku, roles}) => {
+  const selectedRoleId = hakemus.arvio['presenter-role-id']
+  const role = selectedRoleId ? roles.find(role => role.id === selectedRoleId) : roles[0]
+  const answers = hakemus.answers
+  const iban = InputValueStorage.readValues(answers, 'iban')[0].value
+  const bic = InputValueStorage.readValues(answers, 'bic')[0].value
+  return (
+      <section>
+        <section className="section">
+          <h2>Päätös</h2>
+          <div className="content">
+            <p>Opetushallitus on päättänyt myöntää valtionavustusta seuraavasti:</p>
+            <p>Hakija: {hakemus['organization-name']}<br/>
+              Hanke: {hakemus['project-name']}</p>
+            <p>Opetushallitus hyväksyy avustuksen saajan valtionavustukseen oikeuttavina menoina xx euroa.
+              Valtionavustuksena tästä myönnetään {(100 - avustushaku.content['self-financing-percentage'])} %
+              eli {hakemus.arvio['budget-granted']} euroa</p>
+          </div>
         </section>
-    )
-  }
+        <section className="section">
+          <h2>Päätöksen perustelut</h2>
+          <div className="content">
+            {hakemus.arvio.perustelut}
+          </div>
+        </section>
+        <section className="section">
+          <h2>Avustuksen maksu</h2>
+          <div className="content">
+            Pankkitili: <strong>{iban}, {bic}</strong>
+          </div>
+        </section>
+        <section className="section">
+          <h2>Lisätietoja</h2>
+          <div className="content">
+            Lisätietoja antaa: {role.name} &lt;{role.email}&gt;
+          </div>
+        </section>
+      </section>
+  )
 }
 
 const RejectedDecision = ({hakemus}) =>
