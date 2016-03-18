7594201956b7b2db46db556ac8e50d2723c73b00
Sami Mensola <sami.mensola@reaktor.fi>
2016-03-18 14:52:37 +0200

    Rejected constant texts. Real texts missing.

--- va-virkailija/web/va/hakemus-details/Perustelut.jsx
+++ va-virkailija/web/va/hakemus-details/Perustelut.jsx
@@ -8,12 +8,35 @@ export default class Perustelut extends React.Component {
     const allowEditing = this.props.allowEditing
     const perustelut = _.get(hakemus, "arvio.perustelut","")
     const controller = this.props.controller
-    const onChange = (event) => controller.setArvioPerustelut(hakemus, event.target.value)
+    const setReason = (reason) => controller.setArvioPerustelut(hakemus, reason)
+    const onChange = (event) => setReason(event.target.value)
+    const rejected = _.get(hakemus,"arvio.status","")=="rejected"
+    const language= _.find(hakemus.answers,(a)=>a.key=="language")
+    const languageValue = language ? language.value : "fi"
+    const languageTitle = languageValue=="fi" ? "suomeksi" : "ruotsiksi"
+    const rejectedReasonsByLanguage = {
+      fi:["Muotovirhe","Ei käy","Ei todellakaan käy","EI EI EI"],
+      sv:["SV Muotovirhe","SV Nej käy","SV Ei todellakaan käy","SV NE NEJ NEJ"]
+    }
 
-    return <div className="value-edit">
-      <label htmlFor="perustelut">Perustelut hakijalle</label>
-      <textarea id="perustelut" rows="5" disabled={!allowEditing} value={perustelut} title={perustelut}
-                onChange={onChange}/>
-    </div>
+    const rejectedReasons = rejectedReasonsByLanguage[languageValue]
+    return(
+      <div>
+
+        <div className="value-edit">
+          <label htmlFor="perustelut">Perustelut hakijalle <strong>{languageTitle}</strong></label>
+          <div hidden={!rejected} className="radio-container radio-container--perustelut">
+            {rejectedReasons.map((reason)=>
+              <div className={`radio-row ${reason==perustelut ? "radio-row--selected" : ""}`}>
+                <div onClick={_.partial(setReason,reason)}>{reason}</div>
+              </div>
+            )}
+
+          </div>
+          <textarea id="perustelut" rows="5" disabled={!allowEditing} value={perustelut} title={perustelut}
+                    onChange={onChange}/>
+        </div>
+      </div>
+    )
   }
 }
