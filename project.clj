3811e9fe905a387fe5514ed7985f5519431d4ba2
Eero Anttila <eea@iki.fi>
2016-03-31 17:42:18 +0300

    Show Koulutettavatpäivät

--- project.clj
+++ project.clj
@@ -18,11 +18,4 @@
                    "va-common"
                    "va-virkailija"
                    "va-hakija"]
-            :inherited {:repositories [["releases" {:url "https://artifactory.oph.ware.fi/artifactory/oph-sade-release-local"
-                                                    :sign-releases false
-                                                    :snapshots false}]
-                                       ["snapshots" {:url "https://artifactory.oph.ware.fi/artifactory/oph-sade-snapshot-local"}]
-                                       ["ext-snapshots" {:url "https://artifactory.oph.ware.fi/artifactory/ext-snapshot-local"}]
-                                       ["Laughing Panda" {:url "http://maven.laughingpanda.org/maven2"
-                                                          :snapshots false}]]}
             :subprocess "../lein"})
--- va-virkailija/web/va/paatos/PaatosApp.jsx
+++ va-virkailija/web/va/paatos/PaatosApp.jsx
@@ -89,6 +89,7 @@ const AcceptedDecision = ({hakemus, avustushaku, role, formContent, L}) => {
   const totalCosts = formatNumber(hakemus.arvio.useDetailedCosts ? _.sum(budgetItems.map(i=>Number(i.overridden))) : hakemus.arvio.costsGranted)
   const totalRahoitus = formatNumber(_.sum(rahoitusItems.map(i=>Number(i.original))))
   const totalGranted = formatNumber(hakemus.arvio['budget-granted'])
+  const koulutusosiot = hakemus.answers.find(item => item.key === 'koulutusosiot')
   return (
      <section>
        <Section title="paatos" L={L}>
@@ -123,9 +124,75 @@ const AcceptedDecision = ({hakemus, avustushaku, role, formContent, L}) => {
           totalRahoitus={totalRahoitus}
           L={L}
        />
+       {koulutusosiot && <Koulutusosiot list={koulutusosiot.value} answers={hakemus.arvio['overridden-answers'].value}/>}
      </section>
   )
 }
+
+const traineeCalcObj = obj => ({
+  scope: findByKeyEnd(obj, '.scope'),
+  personCount: findByKeyEnd(obj, '.person-count'),
+  scopeType: findByKeyEnd(obj, '.scope-type'),
+  total: findByKeyEnd(obj, '.total')
+})
+
+const sumTraineeCalculations = list => _.sum(list.map(x=>Number(x.replace(',', '.'))))
+
+const Koulutusosiot = ({list, answers}) => {
+  var rows = list
+    .map(x => {
+      var value = x.value
+      var appliedObj = FormUtil.findFieldsByFieldType(value, 'vaTraineeDayCalculator')[0]
+      var grantedRowObj = answers.find(answer => answer.key === appliedObj.key).value
+      return {
+        name: _.get(FormUtil.findFieldsByFieldType(value, 'nameField'), '[0].value', ''),
+        applied: traineeCalcObj(appliedObj.value),
+        granted: traineeCalcObj(grantedRowObj)
+      }
+    })
+  return <section className="section">
+    <p>Valtionavustusta / määrärahaa voidaan käyttää seuraaviin koulutusosioihin:</p>
+    <table>
+      <thead>
+      <tr>
+        <th rowSpan="2" >Koulutusosiot</th>
+        <th colSpan="2" className="groupTitle">Laajuus</th>
+        <th colSpan="2" className="groupTitle">Osallistujat</th>
+        <th colSpan="2" className="groupTitle">Koulutettavatpäivät</th>
+      </tr>
+      <tr>
+        <th className="amount">Haettu</th>
+        <th className="amount">Myönnetty</th>
+        <th className="amount">Haettu</th>
+        <th className="amount">Myönnetty</th>
+        <th className="amount">Haettu</th>
+        <th className="amount">Myönnetty</th>
+      </tr>
+      </thead>
+      <tbody>
+      {rows
+        .map((row, i) => <tr key={i}>
+          <td>{row.name}</td>
+          <td className="amount">{row.applied.scope} {row.applied.scopeType}</td>
+          <td className="amount">{row.granted.scope} {row.granted.scopeType}</td>
+          <td className="amount">{row.applied.personCount}</td>
+          <td className="amount">{row.granted.personCount}</td>
+          <td className="amount">{row.applied.total}</td>
+          <td className="amount">{row.granted.total}</td>
+        </tr>)}
+      </tbody>
+      <tfoot>
+      <tr>
+        <th colSpan="5">Koulutettavatpäivät yhteensä</th>
+        <th className="amount">{sumTraineeCalculations(rows.map(x=>x.applied.total))}</th>
+        <th className="amount">{sumTraineeCalculations(rows.map(x=>x.granted.total))}</th>
+      </tr>
+      </tfoot>
+    </table>
+  </section>
+}
+
+const findByKeyEnd = (list, keyEnd) => _.get(list.find(x => x.key.endsWith(keyEnd)), 'value', '')
 
 class SendDecisionButton extends React.Component {
   constructor(props) {
@@ -184,9 +251,9 @@ const Kayttosuunnitelma = ({budgetItems, rahoitusItems, avustushaku, hakemus, to
          <thead>
          <tr>
            <th><L translationKey="menot" /></th>
-           <th className="amount"><L translationKey="haettu" />
+           <th className="amount budgetAmount"><L translationKey="haettu" />
            </th>
-           <th className="amount"><L translationKey="hyvaksytty" />
+           <th className="amount budgetAmount"><L translationKey="hyvaksytty" />
            </th>
          </tr>
          </thead>
@@ -196,13 +263,13 @@ const Kayttosuunnitelma = ({budgetItems, rahoitusItems, avustushaku, hakemus, to
          <tfoot>
          <tr>
            <th><L translationKey="yhteensa" /></th>
-           <th className="amount">{totalOriginalCosts}</th>
-           <th className="amount">{totalCosts}</th>
+           <th className="amount budgetAmount">{totalOriginalCosts}</th>
+           <th className="amount budgetAmount">{totalCosts}</th>
          </tr>
          <tr>
            <th colSpan="2"><L translationKey="myonnetty-avustus" />
            </th>
-           <th className="amount">{totalGranted}</th>
+           <th className="amount budgetAmount">{totalGranted}</th>
          </tr>
          </tfoot>
        </table>
@@ -211,7 +278,7 @@ const Kayttosuunnitelma = ({budgetItems, rahoitusItems, avustushaku, hakemus, to
          <thead>
          <tr>
            <th><L translationKey="rahoitus" /></th>
-           <th className="amount"><L translationKey="summa" />
+           <th className="amount budgetAmount"><L translationKey="summa" />
            </th>
          </tr>
          </thead>
@@ -221,7 +288,7 @@ const Kayttosuunnitelma = ({budgetItems, rahoitusItems, avustushaku, hakemus, to
          <tfoot>
          <tr>
            <th><L translationKey="yhteensa" /></th>
-           <th className="amount">{totalRahoitus}</th>
+           <th className="amount budgetAmount">{totalRahoitus}</th>
          </tr>
          </tfoot>
        </table>
@@ -231,14 +298,14 @@ const Kayttosuunnitelma = ({budgetItems, rahoitusItems, avustushaku, hakemus, to
 const BudgetItemRow = ({item, lang, useDetailedCosts}) =>
    <tr>
      <td>{item.label[lang]}</td>
-     <td className="amount">{formatNumber(item.original)}</td>
-     <td className="amount">{useDetailedCosts && formatNumber(item.overridden)}</td>
+     <td className="amount budgetAmount">{formatNumber(item.original)}</td>
+     <td className="amount budgetAmount">{useDetailedCosts && formatNumber(item.overridden)}</td>
    </tr>
 
 const IncomeBudgetItemRow = ({item, lang}) =>
    <tr>
      <td>{item.label[lang]}</td>
-     <td className="amount">{formatNumber(item.original)}</td>
+     <td className="amount budgetAmount">{formatNumber(item.original)}</td>
    </tr>
 
 const RejectedDecision = ({avustushaku, hakemus, role, L}) =>
