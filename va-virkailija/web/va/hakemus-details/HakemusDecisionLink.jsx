0e897d47633233e473bde63bbadba8bbf9dc7a57
Eero Anttila <eea@iki.fi>
2016-04-07 13:19:19 +0300

    Show also public decision link

--- va-virkailija/web/va/hakemus-details/HakemusDecisionLink.jsx
+++ va-virkailija/web/va/hakemus-details/HakemusDecisionLink.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 
 export default class HakemusDecisionLink extends Component {
   render() {
@@ -6,9 +6,13 @@ export default class HakemusDecisionLink extends Component {
     const avustushaku = this.props.avustushaku
     const avustushakuId = avustushaku.id
     const previewUrl = hakemus ? "/paatos/avustushaku/" + avustushakuId + "/hakemus/" + hakemus.id : undefined
+    const publicUrl = hakemus ? "/public/paatos/avustushaku/" + avustushakuId + "/hakemus/" + hakemus.id : undefined
 
     return hakemus && hakemus["user-key"] ?
-      <a href={previewUrl} target="_blank" className="decision">Päätös</a>
+      <span className="decision"><span className="decision">Päätös:</span>
+        <a href={previewUrl} target="_blank" className="decision">Luonnos</a>|
+        <a href={publicUrl} target="_blank" className="decision">Julkinen</a>
+      </span>
       : <span/>
   }
 }
