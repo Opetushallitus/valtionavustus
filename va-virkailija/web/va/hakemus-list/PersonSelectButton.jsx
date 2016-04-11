f784a7912225b47ea3c755124a12d1fe3213b891
Perttu Auramo <perttu.auramo@reaktor.fi>
2016-04-11 10:23:57 +0300

    Performance improvement
    Do not run all the rendering code of PersonSelectPanel and PersonSelectButton unless it's necessary. The code does a lot of filtering etc. which is heavy when there is a lot of applications.

--- va-virkailija/web/va/hakemus-list/PersonSelectButton.jsx
+++ va-virkailija/web/va/hakemus-list/PersonSelectButton.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ClassNames from 'classnames'
 
 const RoleButton = ({role,roleField,controller,hakemus}) => {
-  const onClick = (roleId) => controller.toggleHakemusRole(roleId,hakemus,roleField)
+  const onClick = () => controller.toggleHakemusRole(role.id,hakemus,roleField)
   const currentRoles = hakemus.arvio.roles[roleField]
   const active = roleField=="presenter" ? hakemus.arvio["presenter-role-id"]==role.id : _.includes(currentRoles,role.id)
   const buttonClass = ClassNames('btn btn-sm',{
@@ -10,7 +10,7 @@ const RoleButton = ({role,roleField,controller,hakemus}) => {
     'btn-simple': !active
   })
   return (
-    <button className={buttonClass} onClick={onClick.bind(this,role.id)}>{role.name}</button>
+    <button className={buttonClass} onClick={onClick}>{role.name}</button>
   )
 }
 
@@ -23,15 +23,16 @@ const RoleContainer = ({roleName,roleField,roles,controller,hakemus}) => {
   )
 }
 
-const PersonSelectPanel = ({hakemus,state,controller}) =>{
+const PersonSelectPanel = ({hakemus,state,controller}) => {
   const show = state.personSelectHakemusId==hakemus.id
+  if (!show) return <span/>
   const roles = state.hakuData.roles
   const filterByRole = (roles,role)=> _.sortBy(roles.filter((currentRole)=>currentRole.role==role),'name')
   const presenters = filterByRole(roles,"presenting_officer")
   const evaluators = filterByRole(roles,"evaluator")
   const onCloseClick = () => controller.togglePersonSelect(undefined)
   return (
-    <div hidden={!show} className="panel person-panel person-panel--top">
+    <div className="panel person-panel person-panel--top">
       <button className="close" onClick={onCloseClick}>x</button>
       <RoleContainer roleName="Esittelijä" roleField="presenter" roles={presenters} controller={controller} hakemus={hakemus}/>
       <RoleContainer roleName="Arvioijat" roleField="evaluators" roles={evaluators} controller={controller} hakemus={hakemus}/>
@@ -41,16 +42,17 @@ const PersonSelectPanel = ({hakemus,state,controller}) =>{
 
 
 const PersonSelectButton = ({controller,hakemus,state,show}) => {
-  const onClick = (hakemus) => {
+  if (!show) return <span/>
+  const onClick = () => {
     controller.togglePersonSelect(hakemus.id)
   }
   const roles = _.pluck(state.hakuData.roles,'id')
   const presenterRoleId = hakemus.arvio["presenter-role-id"];
   const personCount = hakemus.arvio.roles.evaluators.filter((id)=>_.includes(roles,id)).length + (presenterRoleId && _.includes(roles,presenterRoleId) ? 1 : 0)
-  const countIndicator = personCount >0 ? personCount : "+"
+  const countIndicator = personCount > 0 ? personCount : "+"
   return(
-    <div hidden={!show}>
-      <button onClick={onClick.bind(this,hakemus)} className="btn btn-sm btn-simple btn-role"><span className="btn-role__count">{countIndicator}</span></button>
+    <div>
+      <button onClick={onClick} className="btn btn-sm btn-simple btn-role"><span className="btn-role__count">{countIndicator}</span></button>
       <PersonSelectPanel hakemus={hakemus} state={state} controller={controller}/>
     </div>
   )
