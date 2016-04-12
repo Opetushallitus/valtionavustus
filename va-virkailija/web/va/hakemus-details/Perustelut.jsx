666576b22222137c7b5ab99151ec5029948f9fbb
Perttu Auramo <perttu.auramo@reaktor.fi>
2016-04-12 15:32:48 +0300

    Improve performance of Perustelut
    Perustelut was slow, because it required calling rendering code of the whole site on change. Now there is a local state and a debounced stream which delays the update by one second, so writing should be fast.

--- va-virkailija/web/va/hakemus-details/Perustelut.jsx
+++ va-virkailija/web/va/hakemus-details/Perustelut.jsx
@@ -1,7 +1,26 @@
+import Bacon from 'baconjs'
 import React, { Component } from 'react'
 import _ from "lodash"
 import rejectedReasonsByLanguage from './rejectedReasonsByLanguage.json'
 export default class Perustelut extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.reasonBus = new Bacon.Bus()
+    this.reasonBus.debounce(1000).onValue(([hakemus, newReason]) => { this.props.controller.setArvioPerustelut(hakemus, newReason) })
+    this.state = {perustelut: getPerustelut(props)}
+  }
+
+  reasonUpdated(newReason) {
+    this.setState({...this.state, perustelut: newReason})
+    this.reasonBus.push([this.props.hakemus, newReason])
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.hakemus.id !== nextProps.hakemus.id) {
+      this.setState({perustelut: getPerustelut(nextProps)})
+    }
+  }
 
   render() {
     const hakemus = this.props.hakemus
@@ -13,7 +32,6 @@ export default class Perustelut extends React.Component {
     const perustelut = _.get(hakemus, "arvio.perustelut") || ""
     const controller = this.props.controller
     const setReason = (reason) => controller.setArvioPerustelut(hakemus, reason)
-    const onChange = (event) => setReason(event.target.value)
     const rejected = _.get(hakemus,"arvio.status","")=="rejected"
     const language= _.find(hakemus.answers,(a)=>a.key=="language")
     const languageValue = language ? language.value : "fi"
@@ -21,7 +39,6 @@ export default class Perustelut extends React.Component {
     const rejectedReasons = rejectedReasonsByLanguage[languageValue]
     return(
       <div>
-
         <div className="value-edit">
           <label htmlFor="perustelut">Perustelut hakijalle <strong>{languageTitle}</strong></label>
           {
@@ -34,10 +51,17 @@ export default class Perustelut extends React.Component {
                 )}
               </div>
           }
-          <textarea id="perustelut" rows="5" disabled={!allowEditing} value={perustelut} title={perustelut}
-                    onChange={onChange}/>
+          <textarea id="perustelut"
+                    rows="5"
+                    disabled={!allowEditing}
+                    value={this.state.perustelut}
+                    onChange={(evt) => this.reasonUpdated(evt.target.value)} />
         </div>
       </div>
     )
   }
+}
+
+function getPerustelut(props) {
+  return _.get(props.hakemus, "arvio.perustelut") || ""
 }
