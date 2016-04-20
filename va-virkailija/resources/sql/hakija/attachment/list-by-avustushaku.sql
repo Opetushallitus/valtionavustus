4bd12ce3f81dd149c3b80bbbce9d7b9e49d6c2ba
Sami Mensola <sami.mensola@reaktor.fi>
2016-04-20 14:43:24 +0300

    Order attachement by hakemus_id Fixes bug that not all attachments were shown in arviointi ui

--- va-virkailija/resources/sql/hakija/attachment/list-by-avustushaku.sql
+++ va-virkailija/resources/sql/hakija/attachment/list-by-avustushaku.sql
@@ -5,3 +5,4 @@ join hakemukset on hakemus_id = hakemukset.id AND
                    hakemus_version = hakemukset.version
 where hakemukset.avustushaku = :avustushaku_id and
       attachments.version_closed is null
+order by hakemus_id
