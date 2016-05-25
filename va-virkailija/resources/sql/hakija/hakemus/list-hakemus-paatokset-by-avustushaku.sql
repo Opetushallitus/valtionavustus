6b127bc75fa32c2528a0be61169ec02165d0795a
Sami Mensola <sami.mensola@reaktor.fi>
2016-05-25 09:03:04 +0300

    Do not use hakemus version in paatos query
    If hakemus is changed by virkailija version changes and thinks that mail has not be sent

--- va-virkailija/resources/sql/hakija/hakemus/list-hakemus-paatokset-by-avustushaku.sql
+++ va-virkailija/resources/sql/hakija/hakemus/list-hakemus-paatokset-by-avustushaku.sql
@@ -1,6 +1,6 @@
 select h.id, h.version, h.organization_name, h.project_name,h.user_key, hd.sent_emails, hd.sent_time, count(hpv.view_time) as view_count
 from hakija.hakemukset h
-  left outer join hakija.hakemus_paatokset hd on (hd.hakemus_id = h.id and hd.hakemus_version = h.version)
+  left outer join hakija.hakemus_paatokset hd on (hd.hakemus_id = h.id)
   left outer join hakija.hakemus_paatokset_views hpv on (hpv.hakemus_id=h.id)
 where h.avustushaku = :avustushaku_id
       and h.status = 'submitted'
