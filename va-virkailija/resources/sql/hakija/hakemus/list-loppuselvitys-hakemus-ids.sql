180d3a9a1c6a71cb3b4a54f67da1a6cd3a74a9f0
Eero Anttila <eea@iki.fi>
2016-05-27 16:02:46 +0300

    Don't send email notification for draft hakemukset

--- va-virkailija/resources/sql/hakija/hakemus/list-loppuselvitys-hakemus-ids.sql
+++ va-virkailija/resources/sql/hakija/hakemus/list-loppuselvitys-hakemus-ids.sql
@@ -2,6 +2,7 @@ select h.id
 from hakija.hakemukset h
 where h.avustushaku = :avustushaku_id
       and h.status != 'cancelled'
+      and h.status != 'draft'
       and h.status_loppuselvitys = 'missing'
       and h.status != 'new'
       and h.version_closed is null
