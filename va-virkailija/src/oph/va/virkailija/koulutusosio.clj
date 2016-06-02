397a90b8c6d2b93aa8e56ef1b1c7384fb05ef22b
Eero Anttila <eea@iki.fi>
2016-06-02 13:54:12 +0300

    Show totals

--- va-virkailija/src/oph/va/virkailija/koulutusosio.clj
+++ va-virkailija/src/oph/va/virkailija/koulutusosio.clj
@@ -7,43 +7,55 @@
 
 (defn findByKeyEnd [list keyEnd]
   (:value (first (filter #(.endsWith (:key %) keyEnd) list))))
-;const findByKeyEnd = (list, keyEnd) => _.get(list.find(x => x.key.endsWith(keyEnd)), 'value', '')
 
 (defn formatNumber [num] num)
 
 (defn traineeCalcObj [obj]
-  {
-   :scope (findByKeyEnd obj ".scope")
+  {:scope (findByKeyEnd obj ".scope")
    :personCount (findByKeyEnd obj ".person-count")
    :scopeType (findByKeyEnd obj ".scope-type")
    :total (findByKeyEnd obj ".total")
-   :totalFormatted (formatNumber (findByKeyEnd obj ".total"))
-   })
-;      var grantedRowObj = answers.find(answer => answer.key === appliedObj.key).value
+   :totalFormatted (formatNumber (findByKeyEnd obj ".total"))})
 
-(defn map-row [translate answers koulutusosio]
+(defn map-tr [translate koulutusosio-data]
+  (let [nameField (:name koulutusosio-data)
+        applied (:applied koulutusosio-data)
+        granted (:granted koulutusosio-data)]
+    (str "<tr>"
+         "<td class='trainingName'>" nameField "</td>"
+         "<td class='amount'>" (:scope applied) " " (translate (:scopeType applied)) "</td>"
+         "<td class='amount'>" (:scope granted) " " (translate (:scopeType granted)) "</td>"
+         "<td class='amount'>" (:personCount applied) "</td>"
+         "<td class='amount'>" (:personCount granted) "</td>"
+         "<td class='amount'>" (:totalFormatted applied) "</td>"
+         "<td class='amount'>" (:totalFormatted granted) "</td>"
+         "</tr>")))
+
+(defn map-row-data [answers koulutusosio]
   (let [nameField (:value (first (filter #(= (:fieldType %) "nameField") koulutusosio)))
         applied-obj (first (filter #(= (:fieldType %) "vaTraineeDayCalculator") koulutusosio))
         applied (traineeCalcObj (:value applied-obj))
         applied-key (:key applied-obj)
         granted (traineeCalcObj (:value (first (filter #(= (:key %) applied-key) answers))))]
-    (str "<tr>"
-         "<td className='trainingName'>" nameField "</td>"
-         "<td className='amount'>" (:scope applied) " " (translate (:scopeType applied)) "</td>"
-         "<td className='amount'>" (:scope granted) " " (translate (:scopeType granted)) "</td>"
-         "<td className='amount'>" (:personCount applied) "</td>"
-         "<td className='amount'>" (:personCount granted) "</td>"
-         "<td className='amount'>" (:totalFormatted applied) "</td>"
-         "<td className='amount'>" (:totalFormatted granted) "</td>"
-         "</tr>")))
+    {:name nameField
+     :applied applied
+     :granted granted}))
+
+(defn calculate-total [key list]
+  (reduce + (map (comp bigdec #(str/replace % "," ".") :total key) list)))
 
 (defn koulutusosio [hakemus answers translate language]
   (let [template (email/load-template "templates/koulutusosio.html")
-        koulutusosiot (formutil/find-answer-value answers "koulutusosiot")
+        koulutusosiot (map :value (formutil/find-answer-value answers "koulutusosiot"))
         overridden-answers (-> hakemus :arvio :overridden-answers :value)
-        tbody (str/join " " (map (partial map-row translate overridden-answers) (map :value koulutusosiot)))
+        koulutusosiot-data (map (partial map-row-data overridden-answers) koulutusosiot)
+        total-applied (calculate-total :applied koulutusosiot-data)
+        total-granted (calculate-total :granted koulutusosiot-data)
+        tbody (str/join " " (map (partial map-tr translate) koulutusosiot-data))
         params {:t translate
                 :tbody tbody
+                :total-applied total-applied
+                :total-granted total-granted
                 }
         body (render template params)]
     body))
