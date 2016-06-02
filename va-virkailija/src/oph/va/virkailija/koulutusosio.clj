e6b8e0308af92e30aa6668f6348844a551595b44
Eero Anttila <eea@iki.fi>
2016-06-02 12:27:35 +0300

    Display granted values for koulutusosiot

--- va-virkailija/src/oph/va/virkailija/koulutusosio.clj
+++ va-virkailija/src/oph/va/virkailija/koulutusosio.clj
@@ -19,25 +19,29 @@
    :total (findByKeyEnd obj ".total")
    :totalFormatted (formatNumber (findByKeyEnd obj ".total"))
    })
+;      var grantedRowObj = answers.find(answer => answer.key === appliedObj.key).value
 
-(defn map-row [translate koulutusosio]
+(defn map-row [translate answers koulutusosio]
   (let [nameField (:value (first (filter #(= (:fieldType %) "nameField") koulutusosio)))
-        applied (traineeCalcObj (:value (first (filter #(= (:fieldType %) "vaTraineeDayCalculator") koulutusosio))))]
+        applied-obj (first (filter #(= (:fieldType %) "vaTraineeDayCalculator") koulutusosio))
+        applied (traineeCalcObj (:value applied-obj))
+        applied-key (:key applied-obj)
+        granted (traineeCalcObj (:value (first (filter #(= (:key %) applied-key) answers))))]
     (str "<tr>"
          "<td className='trainingName'>" nameField "</td>"
          "<td className='amount'>" (:scope applied) " " (translate (:scopeType applied)) "</td>"
-         "<td className='amount'>'{row.granted.scope} <L translationKey={row.granted.scopeType} /></td>"
+         "<td className='amount'>" (:scope granted) " " (translate (:scopeType granted)) "</td>"
          "<td className='amount'>" (:personCount applied) "</td>"
-         "<td className='amount'>'{row.granted.personCount}</td>"
+         "<td className='amount'>" (:personCount granted) "</td>"
          "<td className='amount'>" (:totalFormatted applied) "</td>"
-         "<td className='amount'>'{row.granted.totalFormatted}</td>"
+         "<td className='amount'>" (:totalFormatted granted) "</td>"
          "</tr>")))
 
 (defn koulutusosio [hakemus answers translate language]
   (let [template (email/load-template "templates/koulutusosio.html")
         koulutusosiot (formutil/find-answer-value answers "koulutusosiot")
         overridden-answers (-> hakemus :arvio :overridden-answers :value)
-        tbody (str/join " " (map (partial map-row translate) (map :value koulutusosiot)))
+        tbody (str/join " " (map (partial map-row translate overridden-answers) (map :value koulutusosiot)))
         params {:t translate
                 :tbody tbody
                 }
