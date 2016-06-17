b809d8632513c2cc39d78a5d871fef12a610f6b1
Sami Mensola <sami.mensola@reaktor.fi>
2016-06-17 10:28:11 +0300

    Haku listing - Order by start date desc

--- va-virkailija/resources/sql/hakija/avustushaku/list.sql
+++ va-virkailija/resources/sql/hakija/avustushaku/list.sql
@@ -1,1 +1,1 @@
-select * from avustushaut where status <> 'deleted' order by id desc;
+select * from avustushaut where status <> 'deleted' order by to_date(content#>>'{duration,start}','yyyy-MM-dd HH24:MI:SS.MS') desc, id desc;
