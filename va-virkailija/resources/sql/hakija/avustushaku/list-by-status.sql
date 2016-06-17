07ab24d025ea87a2d5aa6a7051341870a1d415d2
Sami Mensola <sami.mensola@reaktor.fi>
2016-06-17 10:34:18 +0300

    Hakemus listing - Order haut by start date desc

--- va-virkailija/resources/sql/hakija/avustushaku/list-by-status.sql
+++ va-virkailija/resources/sql/hakija/avustushaku/list-by-status.sql
@@ -1,1 +1,1 @@
-select * from avustushaut where status in (:statuses) order by status asc, id desc;
+select * from avustushaut where status in (:statuses) order by to_date(content#>>'{duration,start}','yyyy-MM-dd HH24:MI:SS.MS') desc, id desc;
