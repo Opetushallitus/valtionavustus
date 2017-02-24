6ceeef7fac9c5893d4a12f7623639b91af6852c6
Jyri Kytömäki <jyri.kytomaki@reaktor.com>
2017-02-24 10:45:01 +0200

    Use virkailija-db settings in migration

--- va-virkailija/src/oph/va/virkailija/db/migrations.clj
+++ va-virkailija/src/oph/va/virkailija/db/migrations.clj
@@ -1,7 +1,6 @@
 (ns oph.va.virkailija.db.migrations
   (:require [oph.soresu.common.db.migrations :as migrations]
             [oph.soresu.common.db :as common-db]
-            [oph.soresu.form.db :as db]
             [oph.va.hakija.api :as hakija-api]
             [yesql.core :refer [defquery]])
   (:gen-class))
@@ -17,7 +16,7 @@
 (defquery update-avustushaku-content! "db/migration/queries/m1_21-update-avustushaku-content.sql")
 (migrations/defmigration migrate-add-rahoitusalueet-for-avustushaut "1.21"
                          "Add used rahoitusalueet to avustushaut"
-                         (let [used-rahoitusalueet (common-db/exec :form-db list-used-rahoitusalueet {})
+                         (let [used-rahoitusalueet (common-db/exec :virkailija-db list-used-rahoitusalueet {})
                                avustushakujen-rahoitusalueet (group-by :avustushaku used-rahoitusalueet)]
                            (doseq [avustushaku-id (keys avustushakujen-rahoitusalueet)]
                              (let [avustushaku (hakija-api/get-avustushaku avustushaku-id)
