64b3364557f0713adde0acec9c4bb75608404a52
Tuomas Kareinen <tuomas.kareinen@reaktor.com>
2017-05-02 15:32:16 +0300

    Fix class name

--- va-virkailija/web/va/traineeday/TraineeDayEditComponentFactory.jsx
+++ va-virkailija/web/va/traineeday/TraineeDayEditComponentFactory.jsx
@@ -1,5 +1,5 @@
 import ComponentFactory from 'soresu-form/web/form/ComponentFactory'
-import VaTraineeDayCalculator from './TraineeDayEditCalculator.jsx'
+import TraineeDayEditCalculator from './TraineeDayEditCalculator.jsx'
 import {FieldOnChangePropertyMapper} from 'soresu-form/web/form/component/PropertyMapper.js'
 import TraineeDayCalculatorSummary from './TraineeDayCalculatorSummary.jsx'
 
@@ -7,7 +7,7 @@ export default class TraineeDayEditComponentFactory extends ComponentFactory {
   constructor() {
     super({
       fieldTypeMapping: {
-        vaTraineeDayCalculator: VaTraineeDayCalculator,
+        vaTraineeDayCalculator: TraineeDayEditCalculator,
         vaTraineeDayCalculatorSummary: TraineeDayCalculatorSummary
       },
       fieldPropertyMapperMapping: {
