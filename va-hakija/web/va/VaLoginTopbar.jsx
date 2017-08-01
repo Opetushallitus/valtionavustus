863a53f25ce72023614d1bc612a522997214683f
Tuomas Kareinen <tuomas.kareinen@reaktor.com>
2017-08-01 11:41:39 +0300

    Fix login page topbar layout

--- va-hakija/web/va/VaLoginTopbar.jsx
+++ va-hakija/web/va/VaLoginTopbar.jsx
@@ -5,17 +5,21 @@ import LocalizedString from 'soresu-form/web/form/component/LocalizedString.jsx'
 import EnvironmentInfo from 'va-common/web/va/EnvironmentInfo.jsx'
 
 export default class VaLoginTopbar extends React.Component {
-  constructor(props) {
-    super(props)
-  }
-
   render() {
-    return <section id="topbar">
-      <div id="top-container">
-        <img id="logo" src="img/logo-240x68@2x.png" width="240" height="68" alt="Opetushallitus / Utbildningsstyrelsen" />
-        <h1 id="topic"><LocalizedString translations={this.props.translations.form} translationKey="heading" lang={this.props.lang}/></h1>
-        <div id="server-info"><EnvironmentInfo environment={this.props.environment}/></div>
-      </div>
-    </section>
+    return (
+      <section id="topbar">
+        <div id="top-container">
+          <img id="logo" src="img/logo-240x68@2x.png" width="240" height="68" alt="Opetushallitus / Utbildningsstyrelsen" />
+          <div className="topbar-right">
+            <h1 id="topic"><LocalizedString translations={this.props.translations.form} translationKey="heading" lang={this.props.lang}/></h1>
+            <div>
+              <div className="important-info">
+                <EnvironmentInfo environment={this.props.environment}/>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    )
   }
 }
