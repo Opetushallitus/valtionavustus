a285533c3e98ec2012451d56841bd2c32af2693c
Tuomas Kareinen <tuomas.kareinen@reaktor.com>
2017-08-11 10:06:47 +0300

    Fix attachment links in seuranta tab, cleanup

--- va-virkailija/web/va/hakemus-details/SeurantaLiitteet.jsx
+++ va-virkailija/web/va/hakemus-details/SeurantaLiitteet.jsx
@@ -1,45 +1,24 @@
-import Bacon from 'baconjs'
-import React, {Component} from 'react'
-import FakeFormController from '../form/FakeFormController.js'
-import Form from 'soresu-form/web/form/Form.jsx'
-import FormController from 'soresu-form/web/form/FormController.js'
-import VaComponentFactory from 'va-common/web/va/VaComponentFactory'
-import VaPreviewComponentFactory from 'va-common/web/va/VaPreviewComponentFactory'
+import _ from 'lodash'
+import React from 'react'
 
 import AttachmentField from 'soresu-form/web/form/component/AttachmentField.jsx'
 import HttpUtil from 'va-common/web/HttpUtil'
 
-export default class SeurantaLiitteet extends Component {
-
+export default class SeurantaLiitteet extends React.Component {
   render() {
     const {controller, hakemus, hakuData, avustushaku, translations} = this.props
     const hakemusId = hakemus.id
     const avustushakuId = avustushaku.id
     const hakemusUserKey = hakemus["user-key"]
     const attachments = hakuData.attachments[hakemusId] || []
-    const field = {
-      "id": "seuranta-0",
-      "params": {},
-      "fieldClass": "formField",
-      "helpText": {
-        "fi": "",
-        "sv": ""
-      },
-      "label": {
-        "fi": "Liite",
-        "sv": "Liite"
-      },
-      "required": true,
-      "fieldType": "namedAttachment"
-    }
-    const formController = new FakeFormController(new VaComponentFactory(), new VaPreviewComponentFactory(), avustushaku, hakemus)
     const hakijaServer = _.get(hakuData,"environment.hakija-server.url.fi")
+    const fakeFormController = {componentDidMount: () => {}}
 
-    const onDrop = (field,files)=> {
+    const onDrop = (fieldId, files) => {
       const file = files[0]
       const formData = new FormData()
       formData.append('file', file)
-      HttpUtil.put(`${hakijaServer}api/avustushaku/${avustushakuId}/hakemus/${hakemusUserKey}/${hakemus.version}/attachments/${field.id}`, formData)
+      HttpUtil.put(`${hakijaServer}api/avustushaku/${avustushakuId}/hakemus/${hakemusUserKey}/${hakemus.version}/attachments/${fieldId}`, formData)
         .then(function(response) {
           controller.refreshAttachments(avustushakuId)
         })
@@ -47,11 +26,10 @@ export default class SeurantaLiitteet extends Component {
           console.error(error)
           controller.saveError()
         })
-
     }
 
-    const onRemove = (field)=> {
-      const url = `${hakijaServer}api/avustushaku/${avustushakuId}/hakemus/${hakemusUserKey}/attachments/${field.id}`
+    const onRemove = fieldId => {
+      const url = `${hakijaServer}api/avustushaku/${avustushakuId}/hakemus/${hakemusUserKey}/attachments/${fieldId}`
       HttpUtil.delete(url)
         .then(function(response) {
           controller.refreshAttachments(avustushakuId)
@@ -62,29 +40,48 @@ export default class SeurantaLiitteet extends Component {
         })
     }
 
-    const seurantaAttachmentCount = _.keys(attachments).filter((n)=>n.indexOf("seuranta")!=-1).length
-    const ids = _.range(seurantaAttachmentCount + 1)
-    const fields = ids.map((id)=>Object.assign({},field,{id:`seuranta-${id}`}))
-    const downloadUrl = `/api/avustushaku/${avustushakuId}/hakemus/${hakemusId}/attachments/${field.id}`
+    const makeDownloadUrl = fieldId =>
+      `/api/avustushaku/${avustushakuId}/hakemus/${hakemusId}/attachments/${fieldId}`
+
+    const seurantaAttachmentsCount = _.keys(attachments).filter(n => n.indexOf("seuranta") >= 0).length
+
+    const fields = _.chain(seurantaAttachmentsCount + 1)
+      .range()
+      .map(id => Object.assign(
+        {
+          params: {},
+          fieldClass: "formField",
+          helpText: {
+            fi: "",
+            sv: ""
+          },
+          label: {
+            fi: "Liite",
+            sv: "Liite"
+          },
+          required: true,
+          fieldType: "namedAttachment"
+        },
+        {id: `seuranta-${id}`}
+      ))
+      .value()
 
     return (
       <div className="seuranta-liitteet">
         <h2>Liitteet</h2>
-        {fields.map(field => {
-          return <AttachmentField
-                     field={field}
-                     key={field.id}
-                     translations={translations}
-                     lang="fi"
-                     disabled={false}
-                     allAttachments={attachments}
-                     onDrop={_.partial(onDrop,field)}
-                     onRemove={_.partial(onRemove,field)}
-                     htmlId={field.id}
-                     controller={formController}
-                     downloadUrl={downloadUrl}
-                />
-        })}
+        {_.map(fields, field =>
+          <AttachmentField field={field}
+                           key={field.id}
+                           translations={translations}
+                           lang="fi"
+                           disabled={false}
+                           allAttachments={attachments}
+                           onDrop={_.partial(onDrop, field.id)}
+                           onRemove={_.partial(onRemove, field.id)}
+                           htmlId={field.id}
+                           controller={fakeFormController}
+                           downloadUrl={makeDownloadUrl(field.id)} />
+        )}
       </div>
     )
   }
