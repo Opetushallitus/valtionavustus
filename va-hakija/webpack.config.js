3946d5d41cac8f24e98c12a331a9d39594d48038
Tuomas Kareinen <tuomas.kareinen@reaktor.com>
2017-08-28 14:56:49 +0300

    Change react to npm peer dependency, as it contains state

--- va-hakija/webpack.config.js
+++ va-hakija/webpack.config.js
@@ -62,5 +62,11 @@ module.exports = {
       }
     ]
   },
+  resolve: {
+    alias: {
+      react: path.resolve(__dirname, 'node_modules/react'),
+      "react-dom": path.resolve(__dirname, 'node_modules/react-dom')
+    }
+  },
   plugins: plugins
 }
--- va-virkailija/webpack.config.js
+++ va-virkailija/webpack.config.js
@@ -63,5 +63,11 @@ module.exports = {
       }
     ]
   },
+  resolve: {
+    alias: {
+      react: path.resolve(__dirname, 'node_modules/react'),
+      "react-dom": path.resolve(__dirname, 'node_modules/react-dom')
+    }
+  },
   plugins: plugins
 }
