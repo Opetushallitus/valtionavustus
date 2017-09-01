47394158335b929efef91aa41ef0ca55137dace1
Tuomas Kareinen <tuomas.kareinen@reaktor.com>
2017-09-01 10:44:54 +0300

    Update clj-ldap to 0.0.12

--- va-virkailija/project.clj
+++ va-virkailija/project.clj
@@ -4,7 +4,7 @@
   :license {:name "EUPL licence"
             :url "http://opensource.org/licenses/EUPL-1.1"}
   :dependencies [[oph-va/common "0.1.0-SNAPSHOT"]
-                 [org.clojars.pntblnk/clj-ldap "0.0.9"]
+                 [org.clojars.pntblnk/clj-ldap "0.0.12"]
                  [buddy/buddy-auth "0.6.1"]
                  [com.cemerick/url "0.1.1"]
                  [oph/clj-util "0.1.0"]
