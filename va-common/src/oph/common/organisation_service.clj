b3ed53d2fffaf8a4c68a387b3a1d5fd98424943f
Matti Ahinko <matti.ahinko@gmail.com>
2017-09-04 15:10:04 +0300

    Add interface for getting organisation info from organisaatio-service api

---
+++ va-common/src/oph/common/organisation_service.clj
@@ -0,0 +1,59 @@
+(ns oph.common.organisation-service
+  (:require [org.httpkit.client :as http]
+            [cheshire.core :as cheshire]))
+
+(def service-url "https://virkailija.opintopolku.fi/organisaatio-service/rest/")
+
+(def languages {:fi "kieli_fi#1" :sv "kieli_sv#1" :en "kieli_en#1" })
+
+(def compact-contact-fields #{ :osoite :postinumeroUri :postitoimipaikka })
+
+(defn filter-translations
+  "Filter vector collection of values by translation.
+  Collections of 'organisaatio-service' has :kieli key defining translation."
+  [lang col]
+    (not-empty (filter #(= (:kieli %) (lang languages)) col)))
+
+(defn- json->map [body] (cheshire/parse-string body true))
+
+(defn find-by-id
+  "Fetch organisation data by organisation id (Y-tunnus in Finnish)."
+  [organisation-id]
+  (let [url (str service-url "organisaatio/" organisation-id "?includeImage=false")]
+    (json->map (:body @(http/get url)))))
+
+(defn compact-address
+  "Convert full address info to compact one:
+  {:address :postal-number :city}
+  Postal addess is being trimmed by removing Finnish tag 'posti_'."
+  [address]
+  (let [{:keys [osoite postinumeroUri postitoimipaikka]} address]
+    {:address osoite
+     :postal-number (clojure.string/replace postinumeroUri "posti_" "")
+     :city postitoimipaikka}))
+
+(defn get-compact-translated-info
+  "Get (find) organisation with given organisation id (Y-tunnus in Finnish)
+  and compact info to contain only necessary data. Data will be in following:
+  {:name, :email, :organisation-id, :contact {:address, :postal-number, :city},
+  :county}.
+  
+  You can also give language. If data is found in given language, translated
+  info will be returned. Otherwise it will be in Finnish.
+  
+  Language can be {:fi, :sv, :en}
+  
+  Function assumes name and contact info are translated."
+  ([id lang]
+  (let [{:keys [nimi yhteystiedot ytunnus]} (find-by-id id)
+        translated-contact
+        (into {} (or
+                   (filter-translations lang yhteystiedot)
+                   (filter-translations :fi yhteystiedot)))]
+    {:name (or (lang nimi) (:fi nimi))
+     :email (:email translated-contact) 
+     :organisation-id ytunnus
+     :contact (compact-address translated-contact)
+     :county nil}))
+  ([id]
+   (get-compact-translated-info id :fi)))
