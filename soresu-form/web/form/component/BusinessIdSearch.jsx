26c32c4cb4a7c3367e1f639a9161df280160d75b
Anne-Mari Silvast <anne-mari.silvast@gofore.com>
2017-09-12 18:06:52 +0300

    added validation to business-id in modal

--- soresu-form/web/form/component/BusinessIdSearch.jsx
+++ soresu-form/web/form/component/BusinessIdSearch.jsx
@@ -13,12 +13,17 @@ export default class BusinessIdSearch extends React.Component {
     super(props)
     this.handleClick = this.handleClick.bind(this)
     this.changeFieldValue = this.changeFieldValue.bind(this)
-    this.openModal = this.openModal.bind(this);
-    this.afterOpenModal = this.afterOpenModal.bind(this);
-    this.closeModal = this.closeModal.bind(this);
+    this.openModal = this.openModal.bind(this)
+    this.afterOpenModal = this.afterOpenModal.bind(this)
+    this.closeModal = this.closeModal.bind(this)
+    this.handleChange = this.handleChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
+    this.validate = this.validate.bind(this)
     this.state = {
-        modalIsOpen: true
+        modalIsOpen: true,
+        typed: '',
+        isDisabled: true,
+        error: "error"
       }
     this.lang = this.props.state.configuration.lang
     this.translations = this.props.state.configuration.translations.misc
@@ -36,6 +41,7 @@ export default class BusinessIdSearch extends React.Component {
     this.setState({modalIsOpen: false})
   }
 
+
   changeFieldValue(data, fieldName, dataField){
     if (dataField != "contact") {
       this.props.controller.componentOnChangeListener(FormUtil.findField(this.props.state, fieldName), data[dataField])
@@ -45,11 +51,29 @@ export default class BusinessIdSearch extends React.Component {
     }
   }
 
+// events from inputting the organisational id (y-tunnus)
   handleSubmit(event) {
    this.handleClick(this.input.value)
    this.setState({modalIsOpen: false})
  }
 
+
+ handleChange(event) {
+     var inputted = event.target.value
+     this.setState({typed: inputted})
+     this.validate(inputted)
+   }
+
+//validate input of business-id
+  validate(inputted){
+    const text = inputted
+    if (text.match(/^\d{7}-\d$/)) {
+      this.setState({isDisabled: false})
+      this.setState({error: ""})
+    }
+  }
+
+// actions that happen after user has submitted their organisation-id, calls backend organisaton api
   handleClick(id) {
       const businessIdField = this.props.state.saveStatus.values.value.filter(value => value.key == "business-id")
       const language = this.props.state.configuration.lang
@@ -57,10 +81,10 @@ export default class BusinessIdSearch extends React.Component {
       axios.get("http://localhost:8080/api/organisations/?organisation-id=" + id + "&lang=" + language ).then(({ data })=> {
         const fieldNames = ["organization", "organization-email", "business-id", "organization-postal-address"]
         const dataFieldNames = ["name", "email",  "organisation-id", "contact"]
-
         fieldNames.map((item, key) => this.changeFieldValue(data, item, dataFieldNames[key]))
      })
    }
+
 
    render() {
      const translator = new Translator(this.translations)
@@ -80,9 +104,9 @@ export default class BusinessIdSearch extends React.Component {
            <form onSubmit={this.handleSubmit}>
             <label className="ModalLabel">
               <LocalizedString translations={this.translations} translationKey="business-id" lang={this.lang}/> :
-              <input className="soresu-text-field" type="text" ref={(input) => this.input = input} />
+              <input className={this.state.error} type="text" ref={(input) => this.input = input} onChange={ this.handleChange } />
             </label>
-            <input className="soresu-text-button" type="submit" value={translator.translate("get", this.lang)} />
+            <input className={"get-business-id" + " " + "soresu-text-button"} type="submit" value={translator.translate("get", this.lang)} disabled={this.state.isDisabled} />
           </form>
           <p><a href="#" onClick={this.closeModal}><LocalizedString translations={this.translations} translationKey="cancel" lang={this.lang}/></a></p>
         </div>
