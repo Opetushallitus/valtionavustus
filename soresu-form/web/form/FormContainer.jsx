aa49e63d62d6c2961a525c575ba431a2324ca7f9
Anne-Mari Silvast <anne-mari.silvast@gofore.com>
2017-09-20 15:43:16 +0300

    fixed a bug that showeedmodal on admin page

--- soresu-form/web/form/FormContainer.jsx
+++ soresu-form/web/form/FormContainer.jsx
@@ -6,16 +6,18 @@ import BusinessIdSearch from "./component/BusinessIdSearch.jsx"
 export default class FormContainer extends React.Component {
   constructor(props){
     super(props)
-    this.getFieldValue = this.getFieldValue.bind(this)
+    //this.getFieldValue = this.getFieldValue.bind(this)
   }
 
 
   getFieldValue(id){
-    const fieldValues = this.props.state.saveStatus.values.value
-    if (fieldValues.find(x => x.key === id) != undefined){
-      return fieldValues.filter(value => value.key == id)[0].value
-    } else{
-      return null
+    if (this.props.state.configuration.preview == false) {
+      const fieldValues = this.props.state.saveStatus.values.value
+      if (fieldValues.find(x => x.key === id) != undefined){
+        return fieldValues.filter(value => value.key == id)[0].value
+      } else{
+        return null
+      }
     }
   }
 
@@ -30,14 +32,18 @@ export default class FormContainer extends React.Component {
     }
     const formElement = React.createElement(formContainerClass, formElementProps)
 
-    // Check if page is refreshed and if in that case some values are missing.
-    const conditions =  ((this.props.state.saveStatus.savedObject != null) && ((this.props.state.saveStatus.savedObject.version == 1) || ((performance.navigation.type == 1) && ( ["organization", "organization-email", "business-id", "organization-postal-address"].map((item) => this.getFieldValue(item)).some(x => (x == "" || x == null))))))
 
+    const isPreviewPage = (this.props.state.saveStatus.savedObject == null)
+    const isAdminViewPage = (this.props.state.configuration.preview == true)
+    const areEmptyFields =  (( ["organization", "organization-email", "business-id", "organization-postal-address"].map((item) =>   this.getFieldValue(item)).some(x => (x == "" || x == null))))
+
+    // Check any of the values are missing, if so, show the modal
+    const isBusinessIdSearchNeeded = (!isPreviewPage && !isAdminViewPage && areEmptyFields)
 
     return (
       <section id={containerId} >
         {headerElements}
-        { (conditions) &&
+        { (isBusinessIdSearchNeeded) &&
           <BusinessIdSearch state={this.props.state} controller={controller}/> }
         {formElement}
       </section>
