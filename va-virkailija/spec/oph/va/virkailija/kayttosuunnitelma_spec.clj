92e0e747164302900b4058846b8fdca0bc882f1f
Tuomas Kareinen <tuomas.kareinen@reaktor.com>
2017-09-22 11:05:57 +0300

    Remove test description nesting, hopefully fixing JUnit reporting

--- va-virkailija/spec/oph/va/virkailija/kayttosuunnitelma_spec.clj
+++ va-virkailija/spec/oph/va/virkailija/kayttosuunnitelma_spec.clj
@@ -3,36 +3,33 @@
             [oph.va.virkailija.kayttosuunnitelma :as ks])
   (:import [java.math RoundingMode]))
 
-(describe "Käyttösuunnitelma"
-          (tags :kayttosuunnitelma)
+(describe "Käyttösuunnitelma, rounding decimals"
+  (it "rounds decimal with traditional rounding"
+      (should= "0.1" (str (ks/round-decimal 0.1 1)))
+      (should= "0.1" (str (ks/round-decimal 0.14 1)))
+      (should= "0.2" (str (ks/round-decimal 0.15 1)))
+      (should= "13.46" (str (ks/round-decimal 13.455 2))))
 
-          (describe "rounding decimals"
-                    (it "rounds decimal with traditional rounding"
-                        (should= "0.1" (str (ks/round-decimal 0.1 1)))
-                        (should= "0.1" (str (ks/round-decimal 0.14 1)))
-                        (should= "0.2" (str (ks/round-decimal 0.15 1)))
-                        (should= "13.46" (str (ks/round-decimal 13.455 2))))
+  (it "rounds decimal with floor rounding"
+      (should= "0.1" (str (ks/round-decimal 0.1 1 RoundingMode/FLOOR)))
+      (should= "0.1" (str (ks/round-decimal 0.10 1 RoundingMode/FLOOR)))
+      (should= "0.1" (str (ks/round-decimal 0.19 1 RoundingMode/FLOOR)))
+      (should= "13.11" (str (ks/round-decimal 13.119 2 RoundingMode/FLOOR))))
 
-                    (it "rounds decimal with floor rounding"
-                        (should= "0.1" (str (ks/round-decimal 0.1 1 RoundingMode/FLOOR)))
-                        (should= "0.1" (str (ks/round-decimal 0.10 1 RoundingMode/FLOOR)))
-                        (should= "0.1" (str (ks/round-decimal 0.19 1 RoundingMode/FLOOR)))
-                        (should= "13.11" (str (ks/round-decimal 13.119 2 RoundingMode/FLOOR))))
+  (it "rounds decimal with ceiling rounding"
+      (should= "0.1" (str (ks/round-decimal 0.1 1 RoundingMode/CEILING)))
+      (should= "0.1" (str (ks/round-decimal 0.10 1 RoundingMode/CEILING)))
+      (should= "0.2" (str (ks/round-decimal 0.11 1 RoundingMode/CEILING)))
+      (should= "13.12" (str (ks/round-decimal 13.111 2 RoundingMode/CEILING)))))
 
-                    (it "rounds decimal with ceiling rounding"
-                        (should= "0.1" (str (ks/round-decimal 0.1 1 RoundingMode/CEILING)))
-                        (should= "0.1" (str (ks/round-decimal 0.10 1 RoundingMode/CEILING)))
-                        (should= "0.2" (str (ks/round-decimal 0.11 1 RoundingMode/CEILING)))
-                        (should= "13.12" (str (ks/round-decimal 13.111 2 RoundingMode/CEILING)))))
+(describe "Käyttösuunnitelma, formatting decimals"
+  (it "formats decimal"
+      (should= "13" (ks/format-decimal 13)))
 
-          (describe "formatting decimals"
-                    (it "formats decimal"
-                        (should= "13" (ks/format-decimal 13)))
+  (it "removes trailing .0 from decimal"
+      (should= "10" (ks/format-decimal 10.000)))
 
-                    (it "removes trailing .0 from decimal"
-                        (should= "10" (ks/format-decimal 10.000)))
-
-                    (it "outputs comma as decimal separator"
-                        (should= "13,0001" (ks/format-decimal 13.0001)))))
+  (it "outputs comma as decimal separator"
+              (should= "13,0001" (ks/format-decimal 13.0001))))
 
 (run-specs)
