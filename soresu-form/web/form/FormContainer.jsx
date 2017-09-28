ec763ac9fe5a70e073aabd5b43b9c64dc64e4542
Anne-Mari Silvast <anne-mari.silvast@gofore.com>
2017-09-28 16:31:32 +0300

    spotting errors in tests

--- soresu-form/web/form/FormContainer.jsx
+++ soresu-form/web/form/FormContainer.jsx
@@ -2,24 +2,22 @@ import React from "react"
 import _ from "lodash"
 import BusinessIdSearch from "./component/BusinessIdSearch.jsx"
 
-
 export default class FormContainer extends React.Component {
   constructor(props){
     super(props)
-    this.getFieldValue = this.getFieldValue.bind(this)
+     this.getFieldValue = this.getFieldValue.bind(this)
   }
 
-
   getFieldValue(id){
-    if (this.props.state.configuration.preview === false) {
+      if (this.props.state.configuration.preview) {
+        return null
+      }
       const fieldValues = this.props.state.saveStatus.values.value
       if (fieldValues.find(x => x.key === id) != undefined){
         return fieldValues.filter(value => value.key == id)[0].value
-      } else{
-        return null
       }
+      return null
     }
-  }
 
   render() {
     const {state, controller, formContainerClass} = this.props
@@ -32,16 +30,24 @@ export default class FormContainer extends React.Component {
     }
     const formElement = React.createElement(formContainerClass, formElementProps)
 
-
+    const isTestProfile = this.props.state.configuration.develMode
     const isPreviewPage = this.props.state.saveStatus.savedObject === null
     const isAdminViewPage = this.props.state.configuration.preview === true
-    const areEmptyFields =  ["organization", "organization-email", "business-id", "organization-postal-address"].map((item) =>   this.getFieldValue(item)).some(x => (x == "" || x == null))
-    const isTestProfile = this.props.state.configuration.develMode
-    const isBusinessIdSearchNeeded = !isPreviewPage && !isAdminViewPage && areEmptyFields && !isTestProfile
+    const areEmptyFields =  !this.getFieldValue("organization") || !this.getFieldValue("organization-email") || !this.getFieldValue("business-id") || !this.getFieldValue("organization-postal-address")
+
+    const fieldValue = this.props.state.saveStatus.values.value
+    //const isOrganizationNull = fieldValue["organization"]
+
+    // Check any of the values are missing, if so, show the modal
+    const isBusinessIdSearchNeeded = !isPreviewPage && !isAdminViewPage && areEmptyFields
+
 
     return (
       <section id={containerId} >
         {headerElements}
+        {console.log(areEmptyFields)}
+        { (isBusinessIdSearchNeeded) &&
+          <BusinessIdSearch state={this.props.state} controller={controller}/> }
         {formElement}
       </section>
     )
