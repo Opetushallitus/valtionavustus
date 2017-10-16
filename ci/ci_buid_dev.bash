eda146d8ad0f57fdf1909f3b14a0ac4c54b9010c
Anne-Mari Silvast <anne-mari.silvast@gofore.com>
2017-10-16 20:36:04 +0300

    added first ci dev script to test jenkins connection

--- ci/ci_buid_dev.bash
+++ ci/ci_buid_dev.bash
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
-va_hakija_default_source_path="va-hakija/target/uberjar/hakija-*-standalone.jar"
-va_virkailija_default_source_path="va-virkailija/target/uberjar/virkailija-*-standalone.jar"
 
 echo "building valtionavustus"
 make clean
+make test
 make build
-
-echo "building admin-ui frontend"
-lein clean
-lein package
