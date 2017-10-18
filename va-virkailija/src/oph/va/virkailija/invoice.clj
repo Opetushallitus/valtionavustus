fd0c5777cb2859b006354adc630963119a0b957e
Matti Ahinko <matti.ahinko@gmail.com>
2017-10-18 13:39:55 +0300

    Add documents

--- va-virkailija/src/oph/va/virkailija/invoice.clj
+++ va-virkailija/src/oph/va/virkailija/invoice.clj
@@ -1,14 +1,6 @@
 (ns oph.va.virkailija.invoice
   (:require [clojure.data.xml :refer [element emit emit-str parse]]))
 
-(defn write-xml! [tags file]
-  (with-open [out-file (java.io.FileWriter. file)]
-    (emit tags out-file)))
-
-(defn read-xml [file]
-  (with-open [input (java.io.FileInputStream. file)]
-    (parse input)))
-
 (defn- create-supplier-tags [supplier]
   (element :Toimittaja {}
     (element :Y-tunnus {})
@@ -60,10 +52,26 @@
     (doall (map create-posting-tags (get invoice :rows [])))))
 
 (defn invoice-to-tags [invoice]
+  "Creates xml document (tags) of given invoice of Valtionavustukset maksatus.
+  Document should be valid document for VIA/Rondo."
   (element :VA-invoice {}
     (create-header-tags invoice)
     (create-postings-tags invoice)))
 
 (defn tags-to-str [tags]
+  "Converts XML document of clojure.data.xml.elements tags to a string."
   (emit-str tags))
+
+(defn write-xml! [tags file]
+  "Writes XML document to a file.
+  Document should be tags as clojure.data.xml.elements."
+  (with-open [out-file (java.io.FileWriter. file)]
+    (emit tags out-file)))
+
+(defn read-xml [file]
+  "Reads XML from file path and returns xml document of
+  clojure.data.xml.elements."
+  (with-open [input (java.io.FileInputStream. file)]
+    (parse input)))
+
 
