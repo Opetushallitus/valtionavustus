8a741f795176766c9c6614d3479327956b263925
Matti Ahinko <matti.ahinko@gmail.com>
2017-10-19 15:57:21 +0300

    Remove testing hardocded fields

--- va-virkailija/src/oph/va/virkailija/invoice.clj
+++ va-virkailija/src/oph/va/virkailija/invoice.clj
@@ -1,62 +1,10 @@
 (ns oph.va.virkailija.invoice
   (:require [clojure.data.xml :refer [element emit emit-str parse]]))
 
-(defn- create-supplier-tags [supplier]
-  (element :Toimittaja {}
-    (element :Y-tunnus {})
-    (element :Hlo-tunnus {})
-    (element :Nimi {})
-    (element :Postiosoite {})
-    (element :Paikkakunta {})
-    (element :Maa {})
-    (element :Iban-tili {})
-    (element :Pankkiavain {})
-    (element :Pankki-maa {})
-    (element :Kieli {})
-    (element :Valuutta {})))
-
-(defn- create-header-tags [invoice]
-  (element :Header {}
-    (element :Maksuera {})
-    (element :Laskunpaiva {})
-    (element :Erapvm {})
-    (element :Bruttosumma {})
-    (element :Maksuehto {})
-    (element :Pitkaviite {})
-    (element :Tositepvm {})
-    (element :Asiatarkastaja {})
-    (element :Hyvaksyja {})
-    (element :Tositelaji {})
-    (create-supplier-tags (get invoice :supplier))))
-
-(defn- create-posting-tags [row]
-  (element :Posting {}
-    (element :Summa {})
-    (element :LKP-tili {})
-    (element :ALV-koodi {})
-    (element :TaKp-tili {})
-    (element :Toimintayksikko {})
-    (element :Valtuusnro {})
-    (element :Projekti {})
-    (element :Toiminto {})
-    (element :Suorite {})
-    (element :AlueKunta {})
-    (element :Kumppani {})
-    (element :Seuko1 {})
-    (element :Seuko2 {})
-    (element :Varalla1 {})
-    (element :Varalla2 {})))
-
-(defn- create-postings-tags [invoice]
-  (element :Postings {}
-    (doall (map create-posting-tags (get invoice :rows [])))))
-
-(defn invoice-to-tags [invoice]
+(defn invoice-to-xml [invoice]
   "Creates xml document (tags) of given invoice of Valtionavustukset maksatus.
   Document should be valid document for VIA/Rondo."
-  (element :VA-invoice {}
-    (create-header-tags invoice)
-    (create-postings-tags invoice)))
+  (element invoice))
 
 (defn tags-to-str [tags]
   "Converts XML document of clojure.data.xml.elements tags to a string."
