eed7f57e0fed0da7c99ca82746e29808ce5dded7
Matti Ahinko <matti.ahinko@gmail.com>
2017-10-30 13:16:22 +0200

    Add application info and grant content

--- va-virkailija/resources/sql/hakija/avustushaku/get-payments.sql
+++ va-virkailija/resources/sql/hakija/avustushaku/get-payments.sql
@@ -1,1 +1,16 @@
-select * from payments where grant_id = :grant_id
+SELECT
+  payments.*, hakemukset.organization_name, hakemukset.project_name,
+  avustushaut.content AS grant_content
+FROM
+  payments
+LEFT JOIN
+  hakemukset
+  ON
+    hakemukset.id = payments.application_id
+    AND hakemukset.version = payments.application_version
+LEFT JOIN
+  avustushaut
+  ON
+    avustushaut.id = payments.grant_id
+WHERE payments.grant_id = :grant_id;
+
