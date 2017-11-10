1a8d8e6b8c00b880ab6dacc91209f7ad1e5082a5
Matti Ahinko <matti.ahinko@gmail.com>
2017-11-10 08:57:29 +0200

    Add get grant applications functions

--- va-virkailija/src/clojure/oph/va/hakija/grant_data.clj
+++ va-virkailija/src/clojure/oph/va/hakija/grant_data.clj
@@ -12,3 +12,28 @@
 (defn get-grant [grant-id]
   (convert-to-dash-keys
    (first (exec :form-db hakija-queries/get-grant {:grant_id grant-id}))))
+
+(defn get-grant-evaluations [grant-id]
+  (mapv convert-to-dash-keys
+        (exec :form-db hakija-queries/list-hakemus-paatos-email-statuses
+              {:avustushaku_id grant-id})))
+
+(defn- find-evaluation-by-id [col id]
+  (first (filter #(= (:hakemus-id %) id) col)))
+
+(defn get-grant-applications-with-evaluation [grant-id]
+  (let [evaluations (get-grant-evaluations grant-id)
+        convert
+        (fn [a]
+          (-> a
+              convert-to-dash-keys
+              (conj
+               {:evaluation (find-evaluation-by-id evaluations (:id a))})))]
+    (mapv convert
+          (exec :form-db hakija-queries/get-grant-applications
+                {:grant_id grant-id}))))
+
+(defn get-grant-applications [grant-id]
+  (mapv convert-to-dash-keys
+        (exec :form-db hakija-queries/get-grant-applications
+              {:grant_id grant-id})))
