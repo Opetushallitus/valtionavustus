d283652cc84373e40b8fed4d2fa7aed9d042f7bf
Matti Ahinko <matti.ahinko@gmail.com>
2017-11-13 10:41:15 +0200

    Add answers

--- va-virkailija/resources/sql/hakija/grants/get-grant-applications.sql
+++ va-virkailija/resources/sql/hakija/grants/get-grant-applications.sql
@@ -1,6 +1,7 @@
 SELECT
   h.id, h.created_at, h.version, h.budget_total, h.budget_oph_share,
-  h.organization_name, h.project_name, h.register_number, h.language
+  h.organization_name, h.project_name, h.register_number, h.language,
+  s.answers->'value' AS answers
 FROM
   hakija.hakemukset h
 JOIN
