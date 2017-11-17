86749947f4ce34239590eb12659c06d0b8640347
Tuomas Kareinen <tuomas.kareinen@reaktor.com>
2017-11-17 14:32:28 +0200

    Remove useless function call (config map is already evaluated)

--- va-virkailija/src/clojure/oph/va/virkailija/main.clj
+++ va-virkailija/src/clojure/oph/va/virkailija/main.clj
@@ -1,11 +1,9 @@
 (ns oph.va.virkailija.main
   (:use [oph.va.virkailija.server :only [start-server]])
-  (:require [oph.va.va-config :refer [init-secret-config]]
-            [oph.soresu.common.config :refer [config]])
+  (:require [oph.soresu.common.config :refer [config]])
   (:gen-class))
 
 (defn -main [& args]
-  (init-secret-config)
   (let [server-config (:server config)
         auto-reload? (:auto-reload? server-config)
         port (:port server-config)
