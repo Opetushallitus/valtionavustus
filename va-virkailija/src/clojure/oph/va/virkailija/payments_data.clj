a33e162d434d6af392f57588b2f45182e3baa421
Matti Ahinko <matti.ahinko@gmail.com>
2017-11-21 17:39:39 +0200

    Implement update payments properly

--- va-virkailija/src/clojure/oph/va/virkailija/payments_data.clj
+++ va-virkailija/src/clojure/oph/va/virkailija/payments_data.clj
@@ -1,33 +1,29 @@
 (ns oph.va.virkailija.payments-data
   (:require
    [oph.soresu.common.db :refer [exec]]
-   [oph.va.hakija.api :refer [convert-to-dash-keys]]
+   [oph.va.hakija.api :refer [convert-to-dash-keys convert-to-underscore-keys]]
    [clj-time.coerce :as c]
    [oph.va.virkailija.db.queries :as queries]))
 
-(defn get-payment [id]
-  (convert-to-dash-keys
-   (first
-    (exec :form-db queries/get-payment {:id id}))))
+(defn get-payment
+  ([id]
+   (convert-to-dash-keys
+    (first
+     (exec :form-db queries/get-payment {:id id}))))
+  ([id version]
+   (convert-to-dash-keys
+    (first
+     (exec :form-db queries/get-payment-version {:id id :version version})))))
 
 (defn close-version [id version]
   (exec :form-db queries/payment-close-version
         {:id id :version version}))
 
 (defn update-payment [payment-data]
-  (let [payment {:payment_id (:id payment-data)
-                 :state (:state payment-data)
-                 :document_type (:document-type payment-data)
-                 :invoice_date  (c/to-sql-time (:invoice-date payment-data))
-                 :due_date (c/to-sql-time (:due-date payment-data))
-                 :receipt_date (c/to-sql-time (:receipt-date payment-data))
-                 :transaction_account (:transaction-account payment-data)
-                 :currency (:currency payment-data)
-                 :partner (:partner payment-data)
-                 ; :inspector_email (:inspector-email payment-data)
-                 ; :acceptor_email (:acceptor-email payment-data)
-                 :organisation (:organisation payment-data)
-                 :installment_number (:installment-number payment-data)}]
+  (let [old-payment (get-payment (:id payment-data) (:version payment-data))
+        payment (dissoc (merge old-payment payment-data)
+                        :version :version-closed)]
     (close-version (:id payment-data) (:version payment-data))
     (convert-to-dash-keys
-     (first (exec :form-db queries/update-payment payment)))))
+     (first (exec :form-db queries/update-payment
+                  (convert-to-underscore-keys payment))))))
