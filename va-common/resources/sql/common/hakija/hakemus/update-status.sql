67c7597468555f1a23b7924a93275058bac3a8e2
Tuomas Kareinen <tuomas.kareinen@reaktor.com>
2017-11-22 11:38:00 +0200

    Format SQL syntax

--- va-common/resources/sql/common/hakija/hakemus/update-status.sql
+++ va-common/resources/sql/common/hakija/hakemus/update-status.sql
@@ -1,7 +1,25 @@
-INSERT INTO hakemukset(id, avustushaku, user_key, version, form_submission_id, form_submission_version,
-                       budget_total, budget_oph_share, organization_name, project_name, language, register_number,
-                       user_oid, user_first_name, user_last_name, user_email,
-                       status, status_change_comment, last_status_change_at,hakemus_type,parent_id)
+INSERT INTO hakemukset(
+       id,
+       avustushaku,
+       user_key,
+       version,
+       form_submission_id,
+       form_submission_version,
+       budget_total,
+       budget_oph_share,
+       organization_name,
+       project_name,
+       language,
+       register_number,
+       user_oid,
+       user_first_name,
+       user_last_name,
+       user_email,
+       status,
+       status_change_comment,
+       last_status_change_at,
+       hakemus_type,
+       parent_id)
 SELECT id,
        :avustushaku_id,
        :user_key,
@@ -25,4 +43,4 @@ SELECT id,
        parent_id
 FROM hakemukset
 WHERE user_key = :user_key AND form_submission_id = :form_submission_id
-GROUP BY id,hakemus_type,parent_id
+GROUP BY id, hakemus_type, parent_id
