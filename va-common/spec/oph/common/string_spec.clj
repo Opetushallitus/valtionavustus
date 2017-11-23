f5a064fec2c10493002b3521a3b9681aa19277df
Tuomas Kareinen <tuomas.kareinen@reaktor.com>
2017-11-23 08:03:17 +0200

    Add trimmed-or-nil

--- va-common/spec/oph/common/string_spec.clj
+++ va-common/spec/oph/common/string_spec.clj
@@ -3,6 +3,13 @@
             [oph.common.string :as common-string]))
 
 (describe "Common string tools"
+  (it "trims or returns nil"
+      (should-be-nil (common-string/trimmed-or-nil nil))
+      (should-be-nil (common-string/trimmed-or-nil ""))
+      (should-be-nil (common-string/trimmed-or-nil " "))
+      (should= "a" (common-string/trimmed-or-nil "a"))
+      (should= "a" (common-string/trimmed-or-nil " a\t\n")))
+
   (it "trims whitespace"
       (should= "foo" (common-string/trim-ws " \n\tfoo\n \r"))
       (should= "foo   bar" (common-string/trim-ws " \n\tfoo\n \rbar \n"))))
