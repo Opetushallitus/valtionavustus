b93ccfba97ee0ef621c3d0ad36c954748a8e824f
Matti Ahinko <matti.ahinko@gmail.com>
2017-11-23 16:38:33 +0200

    Add id

--- va-virkailija/resources/sql/virkailija/grants/get-grant-payments.sql
+++ va-virkailija/resources/sql/virkailija/grants/get-grant-payments.sql
@@ -1,5 +1,5 @@
 SELECT
-  p.version, p.version_closed, p.created_at, p.application_id,
+  p.id, p.version, p.version_closed, p.created_at, p.application_id,
   p.application_version, p.state, p.installment_number, p.organisation,
   p.document_type, p.invoice_date, p.due_date, p.receipt_date,
   p.transaction_account, p.currency, p.partner, p.inspector_email,
