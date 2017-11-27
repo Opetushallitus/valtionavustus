6dab024a542bab0caed9841d958d6e748a5cc7c1
Anne-Mari Silvast <anne-mari.silvast@gofore.com>
2017-11-27 14:09:41 +0200

    removed get-payment-by-id since it is no longer needed

--- va-virkailija/src/clojure/oph/va/hakija/api.clj
+++ va-virkailija/src/clojure/oph/va/hakija/api.clj
@@ -132,30 +132,6 @@
              (convert-to-underscore-keys %))))
       payments)))
 
-      (defn- payment->json [payment]
-       {:amount (:amount (first payment))
-        :receipt_date (:receipt_date (first payment))
-        :takp_account (:takp_account (first payment))
-        :due_date (:due_date (first payment))
-        :partner (:partner (first payment))
-        :lkp_account (:lkp_account (first payment))
-        :grant_id (:grant_id (first payment))
-        :long_ref (:long_ref (first payment))
-        :state (:state (first payment))
-        :currency (:currency (first payment))
-        :installment (:installment (first payment))
-        :invoice_date (:invoice_date (first payment))
-        :application_version (:application_version (first payment))
-        :id (:id (first payment))
-        :document_type (:document_type (first payment))
-        :transaction_account (:transaction_account (first payment))
-        :application_id (:application_id (first payment))
-        :acceptor_email (:acceptor_email (first payment))
-        :inspector_email (:inspector_email (first payment))
-        :created_at (:created_at (first payment))})
-
-(defn get-payment-by-id [payment-id]
-  (payment->json (exec :form-db hakija-queries/get-payment-by-id {:payment_id payment-id})))
 
 (defn- map-status-list [statuses]
   (map (fn [status] (new HakuStatus status)) statuses))
