095490664561f2c53c94c36da021a241aef3e297
Tuomas Kareinen <tuomas.kareinen@reaktor.com>
2017-11-27 19:26:22 +0200

    Remove oid status column from haku roles, clean up

--- va-virkailija/web/va/haku-details/HakuRoles.jsx
+++ va-virkailija/web/va/haku-details/HakuRoles.jsx
@@ -3,6 +3,7 @@ import ClassNames from 'classnames'
 import _ from 'lodash'
 
 import CSSTransitionGroup from 'soresu-form/web/form/component/wrapper/CSSTransitionGroup.jsx'
+import NameFormatter from 'va-common/web/va/util/NameFormatter'
 import VaUserSearchParameters from './VaUserSearchParameters'
 
 export default class HakuRoles extends Component {
@@ -21,9 +22,15 @@ export default class HakuRoles extends Component {
     const clearInputButtonClassname = ClassNames("remove", { enabled: hasInput })
 
     return (
-      <div id="haku-roles">
+      <div className="haku-roles">
         <table>
-          <thead><tr><th>Rooli</th><th></th><th>Nimi</th><th>Sähköposti</th></tr></thead>
+          <thead>
+            <tr>
+              <th className="haku-roles-role-column">Rooli</th>
+              <th className="haku-roles-name-column">Nimi</th>
+              <th className="haku-roles-email-column">Sähköposti</th>
+            </tr>
+          </thead>
           <CSSTransitionGroup transitionName="haku-roles-transition" component="tbody">
           {roleRows}
           </CSSTransitionGroup>
@@ -31,15 +38,17 @@ export default class HakuRoles extends Component {
 
         <div id="va-user-search-add-role">
           <div className="va-user-search-error-display"><span className={searchErrorClass}>Virhe henkilön haussa. Yritä uudestaan eri hakuehdoilla ja lataa sivu uudestaan, jollei se auta.</span></div>
-          <div className="person-adder-input">
-            Lisää uusi henkilö
-            <input id="va-user-search-input" type="text" placeholder={"Hae"} onChange={startSearch} disabled={!roles || !userHasEditPrivilege}/>
-            <button type="button" className={clearInputButtonClassname} title="Tyhjennä" disabled={!hasInput} onClick={(e) => {
-                const vaUserSearchInput = document.getElementById('va-user-search-input')
-                vaUserSearchInput.value = ''
-                startSearch(e)
-              }} />
-            <PersonSelectList vaUserSearch={vaUserSearch} avustushaku={avustushaku} controller={controller} />
+          <div>
+            <span>Lisää uusi henkilö: </span>
+            <div className="va-user-search-block">
+              <input id="va-user-search-input" type="text" placeholder={"Hae"} onChange={startSearch} disabled={!roles || !userHasEditPrivilege}/>
+                <button type="button" className={clearInputButtonClassname} title="Tyhjennä" disabled={!hasInput} onClick={(e) => {
+                  const vaUserSearchInput = document.getElementById('va-user-search-input')
+                  vaUserSearchInput.value = ''
+                  startSearch(e)
+                }} />
+                <PersonSelectList vaUserSearch={vaUserSearch} avustushaku={avustushaku} controller={controller} />
+            </div>
           </div>
         </div>
       </div>
@@ -53,15 +62,18 @@ class PersonSelectList extends React.Component {
     const {vaUserSearch, avustushaku, controller} = this.props
     const personRows = _.map(vaUserSearch.result.results, r => {
       const firstName = r["first-name"]
-      const lastName = r["surname"]
+      const surname = r["surname"]
       const email = r["email"]
       const oid = r["person-oid"]
-      const name = firstName ? firstName + " " + lastName : lastName
-      const newRole = { name: PersonSelectList.prettifyFirstAndLastName(name), email: email, role: null, oid: oid }
+      const newRole = {
+        name:  NameFormatter.onlyFirstForename(firstName) + " " + surname,
+        email: email,
+        role:  null,
+        oid:   oid
+      }
       const accessLevel = PersonSelectList.privilegesToClassAndDescription(r.privileges)
-      const displayText = name + " (" + email + ", "
-      const personIsInRolesAlready = _.some(avustushaku.roles, r => { return r.oid === oid })
-      const titleText = `${name}  <${email}>(${accessLevel.description}, oid ${oid})${personIsInRolesAlready ? ' (Käyttäjä on jo lisätty avustushakuun)' : ''}`
+      const personIsInRolesAlready = _.some(avustushaku.roles, r => r.oid === oid)
+      const titleText = `${firstName} ${surname} ${email ? "<" + email + "> " : ""}(${accessLevel.description}, oid ${oid})${personIsInRolesAlready ? ' (Käyttäjä on jo lisätty avustushakuun)' : ''}`
       const onClick = e => {
         e.preventDefault()
         if (!personIsInRolesAlready) {
@@ -69,12 +81,13 @@ class PersonSelectList extends React.Component {
         }
       }
 
-      return <li key={r["person-oid"]} title={titleText} className={personIsInRolesAlready ? "disabled" : null}>
-               <a href="javascript:" onClick={onClick} className={personIsInRolesAlready ? "disabled" : null}>
-                 <span className="person-description">{displayText}</span>
-                 <span className={"access-level " + accessLevel.className}>{accessLevel.description + ")"}</span>
-               </a>
-             </li>
+      return (
+        <li key={r["person-oid"]} title={titleText} className={personIsInRolesAlready ? "disabled" : null}>
+          <a href="javascript:" onClick={onClick} className={personIsInRolesAlready ? "disabled" : null}>
+            {firstName} {surname} ({email ? email + ", " : ""}{accessLevel.description})
+          </a>
+          </li>
+      )
     })
     const resultRows = personRows.length === 0 ? [<li key="no-results-row" className="no-results-row">Ei hakutuloksia.</li>] : personRows
     const searchResultClassNames = ClassNames(undefined, { loading: vaUserSearch.loading,
@@ -96,17 +109,9 @@ class PersonSelectList extends React.Component {
     }
     return { className: "no-va-access", description: "Ei VA-oikeuksia" }
   }
-
-  static prettifyFirstAndLastName(fullName) {
-    const splitted = fullName.split(' ')
-    return splitted.length > 1
-      ? splitted[0] + ' ' + _.last(splitted)
-      : splitted[0]
-  }
 }
 
 class RoleRow extends React.Component {
-
   constructor(props) {
     super(props)
     this.handleChange = this.handleChange.bind(this)
@@ -127,21 +132,19 @@ class RoleRow extends React.Component {
     const disableEditing = !userHasEditPrivilege || (thisRowIsMe && privileges && !privileges["edit-my-haku-role"])
     const removeTitleText = disableEditing && userHasEditPrivilege ? "Et voi poistaa itseltäsi oikeuksia hakuun" : "Poista"
     const onDelete = controller.deleteRole(avustushaku, role)
-    const hasOid = role.oid && role.oid.length > 0
-    const oidStatusClass = hasOid ? undefined : "error"
-    const oidStatusText = hasOid ? "Valtuutus OK" : "Ei valtuutettu"
     return (
       <tr>
-        <td>
+        <td className="haku-roles-role-column">
           <select onChange={this.handleChange} name="role" value={role.role} disabled={disableEditing}>
             <option value="presenting_officer">Valmistelija</option>
             <option value="evaluator">Arvioija</option>
           </select>
         </td>
-        <td className={oidStatusClass}>{oidStatusText}</td>
-        <td><input type="text" value={role.name} name="name" onChange={this.handleChange}/></td>
-        <td><input type="text" value={role.email} name="email" onChange={this.handleChange}/></td>
-        <td><button type="button" onClick={onDelete} className="remove" alt="Poista" title={removeTitleText} tabIndex="-1" disabled={disableEditing} /></td>
+        <td className="haku-roles-name-column"><input type="text" value={role.name} name="name" onChange={this.handleChange}/></td>
+        <td className="haku-roles-email-column">
+          <input type="text" value={role.email || ""} name="email" onChange={this.handleChange}/>
+          <button type="button" onClick={onDelete} className="remove haku-roles-remove" alt="Poista" title={removeTitleText} tabIndex="-1" disabled={disableEditing} />
+        </td>
       </tr>
     )
   }
