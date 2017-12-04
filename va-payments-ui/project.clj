d937b15fea75a537d1a3e3ef21afb4989ca2f257
Matti Ahinko <matti.ahinko@gmail.com>
2017-12-04 15:36:44 +0200

    Change path to virkailija

--- va-payments-ui/project.clj
+++ va-payments-ui/project.clj
@@ -36,7 +36,7 @@
   :figwheel {:http-server-root "."
              :nrepl-port 7002
              :nrepl-middleware ["cemerick.piggieback/wrap-cljs-repl"]
-             :css-dirs ["public/css"]}
+             :css-dirs ["../va-virkailija/resources/public/payments/css"]}
   :cljsbuild
   {:builds
    {:app
