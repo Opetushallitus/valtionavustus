ca93755b303105346f3c7e3fc3f838be71b515cc
Matti Ahinko <matti.ahinko@gmail.com>
2017-12-07 14:10:15 +0200

    Change ftp to sftp

--- va-virkailija/src/clojure/oph/va/virkailija/ftp_service.clj
+++ va-virkailija/src/clojure/oph/va/virkailija/ftp_service.clj
@@ -7,27 +7,26 @@
             [oph.va.virkailija.payments-data :as payments-data]
             [clojure.tools.logging :as log]))
 
-(defn send-sftp! [file ftp-config]
+(defn send-sftp! [file sftp-config]
   (let [agent (ssh/ssh-agent {:use-system-ssh-agent false})
-        session (ssh/session agent (:host-ip ftp-config)
-                             {:username (:username ftp-config)
-                              :password (:password ftp-config)
-                              :port (:port ftp-config)
+        session (ssh/session agent (:host-ip sftp-config)
+                             {:username (:username sftp-config)
+                              :password (:password sftp-config)
+                              :port (:port sftp-config)
                               :strict-host-key-checking :no})]
     (ssh/with-connection session
       (let [channel (ssh/ssh-sftp session)]
         (ssh/with-channel-connection channel
-          (ssh/sftp channel {} :put file (:remote_path ftp-config)))))))
+          (ssh/sftp channel {} :put file (:remote_path sftp-config)))))))
 
 (defn send-to-rondo! [payment-id]
-
   (let [payment (payments-data/get-payment payment-id)
-        ftp-config (:rondo-sftp config)
+        sftp-config (:rondo-sftp config)
         file (format "%s/payment-%d-%d.xml"
-                     (:local-path ftp-config) (:id payment)
+                     (:local-path sftp-config) (:id payment)
                      (System/currentTimeMillis))
         application (:application-id payment)]
     (invoice/write-xml! (invoice/payment-to-xml payment application) file)
-    (if (get-in config [:rondo-sftp :enabled?])
-      (send-sftp! file ftp-config)
-      (log/info (format "Would send %s to %s" file (:host-ip ftp-config))))))
+    (if (:enabled? sftp-config)
+      (send-sftp! file sftp-config)
+      (log/info (format "Would send %s to %s" file (:host-ip sftp-config))))))
