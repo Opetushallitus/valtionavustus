76ca011de66957367cf2210f523fbd2fea5d47d4
Matti Ahinko <matti.ahinko@gmail.com>
2017-12-11 15:36:48 +0200

    Change returning

--- va-virkailija/resources/sql/virkailija/payments/update-payment.sql
+++ va-virkailija/resources/sql/virkailija/payments/update-payment.sql
@@ -11,4 +11,8 @@ VALUES(
   :receipt_date::timestamptz, :transaction_account, :currency,
   :partner, :inspector_email, :acceptor_email, :installment_number,
   :organisation)
-RETURNING *;
+RETURNING
+  id, version, version_closed, created_at, application_id, application_version,
+  state, installment_number, organisation, document_type, invoice_date,
+  due_date, receipt_date, transaction_account, currency, partner,
+  inspector_email, acceptor_email;
