c4b26537a01c6902817cfbcf0550cb9b7cbe1650
Matti Ahinko <matti.ahinko@gmail.com>
2017-12-14 15:32:04 +0200

    Add rendering payment history

--- va-payments-ui/src/va_payments_ui/payments.cljs
+++ va-payments-ui/src/va_payments_ui/payments.cljs
@@ -35,3 +35,34 @@
          {:state (:payment-state payment-values)})]
     (mapv #(merge (map-to-payment %) selected-values) applications)))
 
+(defn render-history-item [i application]
+  [ui/table-row {:key i}
+   [ui/table-row-column (:created-at application)]
+   [ui/table-row-column (:version application)]
+   [ui/table-row-column (:state application)]
+   [ui/table-row-column (:invoice-date application)]
+   [ui/table-row-column (:due-date application)]
+   [ui/table-row-column (:receipt-date application)]
+   [ui/table-row-column (:transaction-account application)]
+   [ui/table-row-column (:document-type application)]
+   [ui/table-row-column (:inspector-email application)]
+   [ui/table-row-column (:acceptor-email application)]
+   [ui/table-row-column (:deleted application)]])
+
+(defn render-history [payments]
+  [ui/table {:fixed-header true :height "250px" :selectable false}
+   [ui/table-header {:adjust-for-checkbox false :display-select-all false}
+    [ui/table-row
+     [ui/table-header-column "Aika"]
+     [ui/table-header-column "Versio"]
+     [ui/table-header-column "Tila"]
+     [ui/table-header-column "Laskun päivä"]
+     [ui/table-header-column "Eräpäivä"]
+     [ui/table-header-column "Tositepäivä"]
+     [ui/table-header-column "Maksuliikemenotili"]
+     [ui/table-header-column "Tositelaji"]
+     [ui/table-header-column "Tarkastaja"]
+     [ui/table-header-column "Hyväksyjä"]
+     [ui/table-header-column "Poistettu"]]]
+   [ui/table-body {:display-row-checkbox false}
+    (doall (map-indexed render-history-item payments))]])
