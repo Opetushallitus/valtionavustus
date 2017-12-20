5edea19ab1876301978ca89db0e1e24ee24e4385
Matti Ahinko <matti.ahinko@gmail.com>
2017-12-20 12:28:55 +0200

    Move creating payments function

--- va-virkailija/src/clojure/oph/va/virkailija/payments_data.clj
+++ va-virkailija/src/clojure/oph/va/virkailija/payments_data.clj
@@ -3,7 +3,11 @@
    [oph.soresu.common.db :refer [exec]]
    [oph.va.hakija.api :refer [convert-to-dash-keys convert-to-underscore-keys]]
    [clj-time.coerce :as c]
-   [oph.va.virkailija.db.queries :as queries]))
+   [oph.va.hakija.api.queries :as hakija-queries]
+   [clj-time.core :as t]
+   [clj-time.coerce :as c]
+   [oph.va.virkailija.db.queries :as queries]
+   [oph.va.hakija.application-data :as application-data]))
 
 (defn get-payment
   ([id]
@@ -50,3 +54,35 @@
     (when (nil? result) (throw (Exception. "Failed to update payment")))
     (close-version (:id payment-data) (:version payment-data))
     result))
+
+(defn create-payment [payment-data]
+  (when
+   (not
+    (empty?
+     (exec :form-db hakija-queries/get-application-payments
+           {:application_id (:application-id payment-data)})))
+    (throw
+     (Exception. "Application already contains a payment")))
+  (let [application (application-data/get-application
+                      (:application-id payment-data))
+        payment {:application_id (:application-id payment-data)
+                 :application_version (:version application)
+                 :grant_id (:grant-id application)
+                 :state 0
+                 :document_type (get payment-data :document-type "XA")
+                 :invoice_date  (get payment-data :invoice-date
+                                     (c/to-sql-time (t/now)))
+                 :due_date (get payment-data :due-date
+                                (c/to-sql-time (t/now)))
+                 :receipt_date (get payment-data :receipt-date
+                                    (c/to-sql-time (t/now)))
+                 :transaction_account (get payment-data :transaction-account "")
+                 :currency (get payment-data :currency "EUR")
+                 :partner (get payment-data :partner "")
+                 :inspector_email (get payment-data :inspector-email "")
+                 :acceptor_email (get payment-data :acceptor-email "")
+                 :organisation (get payment-data :organisation "")
+                 :installment_number (get payment-data :installment-number 0)}
+        payment-id
+        (:id (first (exec :form-db hakija-queries/create-payment payment)))]
+    (get-payment payment-id)))
