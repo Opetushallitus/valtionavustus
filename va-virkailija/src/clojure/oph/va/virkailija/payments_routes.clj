a3dc20ef3dd75ad4922e25b3895ec5ff17a33185
Matti Ahinko <matti.ahinko@gmail.com>
2017-12-28 14:12:13 +0200

    Fix format

--- va-virkailija/src/clojure/oph/va/virkailija/payments_routes.clj
+++ va-virkailija/src/clojure/oph/va/virkailija/payments_routes.clj
@@ -10,47 +10,35 @@
 
 (defn- update-payment
   []
-  (compojure-api/PUT "/:payment-id/"
-                     [payment-id :as request]
-                     :path-params
-                     [payment-id :- Long]
-                     :query-params
-                     []
-                     :body
-                     [payment-data
-                      (compojure-api/describe virkailija-schema/Payment
-                                              "Update payment")]
-                     :return
-                     virkailija-schema/Payment
-                     :summary
-                     "Create new payment for application"
-                     (ok (payments-data/update-payment payment-data))))
+  (compojure-api/PUT
+    "/:payment-id/" [payment-id :as request]
+    :path-params [payment-id :- Long]
+    :query-params []
+    :body [payment-data
+     (compojure-api/describe virkailija-schema/Payment
+                             "Update payment")]
+    :return virkailija-schema/Payment
+    :summary "Create new payment for application"
+    (ok (payments-data/update-payment payment-data))))
 
 (defn- get-next-installment-number
   []
-  (compojure-api/GET "/next-installment-number/"
-                     []
-                     :path-params
-                     []
-                     :return
-                     virkailija-schema/PaymentInstallmentNumber
-                     :summary
-                     "Return next installment number"
-                     (ok (payments-data/next-installment-number))))
+  (compojure-api/GET
+    "/next-installment-number/" []
+    :path-params []
+    :return virkailija-schema/PaymentInstallmentNumber
+    :summary "Return next installment number"
+    (ok (payments-data/next-installment-number))))
 
 (defn- create-payment
   []
   (compojure-api/POST
-    "/"
-    []
-    :body
-    [payment-values
+    "/" []
+    :body [payment-values
      (compojure-api/describe virkailija-schema/Payment
                              "Create payments to Rondo")]
-    :return
-    virkailija-schema/Payment
-    :summary
-    "Create new payment for application. Payment will be sent to Rondo
+    :return virkailija-schema/Payment
+    :summary "Create new payment for application. Payment will be sent to Rondo
              and stored to database."
     (let [payment (or (application-data/get-application-payment
                         (:application-id payment-values))
@@ -69,8 +57,9 @@
                                           :state 2
                                           :filename filename))))))
 
-(compojure-api/defroutes routes
-                         "payment routes"
-                         (update-payment)
-                         (get-next-installment-number)
-                         (create-payment))
+(compojure-api/defroutes
+  routes
+  "payment routes"
+  (update-payment)
+  (get-next-installment-number)
+  (create-payment))
