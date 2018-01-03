bec0a7226e044eb3be1a639a6eb41e3b2f653ccb
Matti Ahinko <matti.ahinko@gmail.com>
2018-01-03 12:16:10 +0200

    Rename file

--- va-payments-ui/src/va_payments_ui/grants_ui.cljs
+++ va-payments-ui/src/va_payments_ui/grants_ui.cljs
@@ -1,10 +1,13 @@
-(ns va-payments-ui.grants
+(ns va-payments-ui.grants-ui
   (:require [reagent.core :as r]
             [cljsjs.material-ui]
             [cljs-react-material-ui.core :refer [get-mui-theme color]]
             [cljs-react-material-ui.reagent :as ui]
             [cljs-react-material-ui.icons :as ic]
-            [va-payments-ui.theme :refer [material-styles]]))
+            [va-payments-ui.theme :refer [material-styles]]
+            [cljs-time.format :as f]
+            [cljs-time.core :as t]
+            [va-payments-ui.utils :refer [update-all]]))
 
 (def status-str
   {"deleted" "Poistettu"
@@ -50,3 +53,4 @@
     (get-in grant [:content :operational-unit])]
    [:div [:label "Projekti: "] (get-in grant [:content :project])]
    [:div [:label "Toiminto: "] (get-in grant [:content :operation])]])
+
