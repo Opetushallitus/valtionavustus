28b14e7a37ba5bd9a0c936051aae4c77509cb849
Matti Ahinko <matti.ahinko@gmail.com>
2018-01-15 18:21:30 +0200

    Add user info

--- va-virkailija/resources/sql/virkailija/payments/update-payment.sql
+++ va-virkailija/resources/sql/virkailija/payments/update-payment.sql
@@ -2,7 +2,7 @@ INSERT INTO virkailija.payments (id, version, application_id,
   application_version, state, document_type, invoice_date, due_date,
   receipt_date, transaction_account, currency,
   partner, inspector_email, acceptor_email, installment_number, organisation,
-  filename)
+  filename, user_name, user_oid)
 VALUES(
   :id,
   (SELECT GREATEST(MAX(version), 0) + 1
@@ -11,7 +11,7 @@ VALUES(
   :invoice_date::timestamptz, :due_date::timestamptz,
   :receipt_date::timestamptz, :transaction_account, :currency,
   :partner, :inspector_email, :acceptor_email, :installment_number,
-  :organisation, :filename)
+  :organisation, :filename, :user_name, :user_oid)
 RETURNING
   id, version, version_closed, created_at, application_id, application_version,
   state, installment_number, organisation, document_type, invoice_date,
