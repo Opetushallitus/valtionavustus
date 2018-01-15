beca6e1d3f55abf47ad7e27a47f0cfa9e29c091b
Matti Ahinko <matti.ahinko@gmail.com>
2018-01-15 18:29:33 +0200

    Add user name

--- va-payments-ui/src/va_payments_ui/payments_ui.cljs
+++ va-payments-ui/src/va_payments_ui/payments_ui.cljs
@@ -5,8 +5,7 @@
             [va-payments-ui.applications :refer [state-to-str]]
             [va-payments-ui.utils :refer [to-simple-date-time to-simple-date]]))
 
-(defn render-history-item
-  [i application]
+(defn render-history-item [i application]
   [ui/table-row {:key i}
    [ui/table-row-column (to-simple-date-time (:created-at application))]
    [ui/table-row-column (:version application)]
@@ -20,10 +19,10 @@
    [ui/table-row-column (:acceptor-email application)]
    [ui/table-row-column
     (when (:deleted application)
-      (to-simple-date-time (:deleted application)))]])
+      (to-simple-date-time (:deleted application)))]
+   [ui/table-row-column (:user-name application)]])
 
-(defn render-history
-  [payments]
+(defn render-history [payments]
   [ui/table {:fixed-header true :selectable false}
    [ui/table-header {:adjust-for-checkbox false :display-select-all false}
     [ui/table-row [ui/table-header-column "Aika"]
@@ -31,19 +30,20 @@
      [ui/table-header-column "Laskun päivä"] [ui/table-header-column "Eräpäivä"]
      [ui/table-header-column "Tositepäivä"]
      [ui/table-header-column "Maksuliikemenotili"]
-     [ui/table-header-column "Tositelaji"] [ui/table-header-column "Tarkastaja"]
-     [ui/table-header-column "Hyväksyjä"] [ui/table-header-column "Poistettu"]]]
+     [ui/table-header-column "Tositelaji"]
+     [ui/table-header-column "Tarkastaja"]
+     [ui/table-header-column "Hyväksyjä"]
+     [ui/table-header-column "Poistettu"]
+     [ui/table-header-column "Käyttäjä"]]]
    [ui/table-body {:display-row-checkbox false}
     (doall (map-indexed render-history-item payments))]])
 
-(defn find-application-payment
-  [payments application-id application-version]
+(defn find-application-payment [payments application-id application-version]
   (first (filter #(and (= (:application-version %) application-version)
                        (= (:application-id %) application-id))
            payments)))
 
-(defn combine
-  [applications payments]
+(defn combine [applications payments]
   (mapv #(assoc %
           :payment (find-application-payment payments (:id %) (:version %)))
     applications))
