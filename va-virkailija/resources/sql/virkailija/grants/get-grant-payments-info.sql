f55fda9e1dd559161a4e22d33a1a65b7bf37a8e2
Matti Ahinko <matti.ahinko@gmail.com>
2018-01-17 16:44:43 +0200

    Add sql query for getting grant payments info

---
+++ va-virkailija/resources/sql/virkailija/grants/get-grant-payments-info.sql
@@ -0,0 +1,14 @@
+SELECT
+  SUM(a.budget_granted) AS total_granted,
+  COUNT(p.id) AS count
+FROM
+  hakija.hakemukset AS h
+JOIN
+  virkailija.payments p
+  ON
+    (p.application_id = h.id AND p.version_closed IS NULL AND
+     p.deleted IS NULL AND p.installment_number = :installment_number)
+JOIN
+  virkailija.arviot a ON a.hakemus_id = p.application_id
+WHERE
+  h.avustushaku = :grant_id AND h.version_closed IS NULL;
