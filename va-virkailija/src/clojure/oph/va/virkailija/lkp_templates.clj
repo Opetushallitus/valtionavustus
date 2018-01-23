398ef61f190d3f8559e40fe2d3a76d4cced61391
Matti Ahinko <matti.ahinko@gmail.com>
2018-01-23 12:16:39 +0200

    Add support for tests (hoc)

--- va-virkailija/src/clojure/oph/va/virkailija/lkp_templates.clj
+++ va-virkailija/src/clojure/oph/va/virkailija/lkp_templates.clj
@@ -1,14 +1,24 @@
-(ns oph.va.virkailija.lkp-templates)
+(ns oph.va.virkailija.lkp-templates
+  (require [clojure.java.io :as io]))
 
-(def templates (map #(-> % slurp read-string)
-                    (.listFiles (io/file (io/resource "lkp-templates")))))
+(def file-filter
+  (reify
+    java.io.FilenameFilter
+    (accept [this _ filename]
+      (.endsWith filename ".edn"))))
 
-(defn get-matching-account [{:keys [key value]}]
-  (some
-    (fn [t]
-      (when (= (:key t) key)
-        (get-in t [:accounts value])))
-    @templates))
+(def system-templates
+  (map #(-> % slurp read-string)
+       (.listFiles (io/file (io/resource "lkp-templates")) file-filter)))
 
-(defn get-lkp-account [answers]
-  (some get-matching-account answers))
+(defn create-account-matcher [templates]
+  (fn [{:keys [key value]}]
+    (some
+      (fn [t]
+        (when (= (:key t) key)
+          (get-in t [:accounts value])))
+      templates)))
+
+(defn get-lkp-account
+  ([templates answers] (some (create-account-matcher templates) answers))
+  ([answers] (get-lkp-account system-templates answers)))
