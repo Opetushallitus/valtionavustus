d9314618b08acfc5ab77363720cb1c1393c4dda8
Matti Ahinko <matti.ahinko@gmail.com>
2018-01-23 16:08:20 +0200

    Change to hardcoded templates with optional loading

--- va-virkailija/src/clojure/oph/va/virkailija/lkp_templates.clj
+++ va-virkailija/src/clojure/oph/va/virkailija/lkp_templates.clj
@@ -1,15 +1,27 @@
 (ns oph.va.virkailija.lkp-templates
   (require [clojure.java.io :as io]))
 
+(def built-in-templates ["lkp-templates/ownership_type.edn"
+                         "lkp-templates/radio_button_0.edn"])
+
 (def file-filter
   (reify
     java.io.FilenameFilter
     (accept [this _ filename]
       (.endsWith filename ".edn"))))
 
+(defn load-templates [files]
+  (map #(-> % slurp read-string) files))
+
+(defn list-custom-templates [path]
+  (let [dir (io/file path)]
+    (if (.exists dir)
+      (.listFiles (io/file path) file-filter)
+      [])))
+
 (def system-templates
-  (map #(-> % slurp read-string)
-       (.listFiles (io/file (io/resource "lkp-templates")) file-filter)))
+  (into (load-templates (map io/resource built-in-templates))
+        (load-templates (list-custom-templates "lkp-templates"))))
 
 (defn create-account-matcher [templates]
   (fn [{:keys [key value]}]
