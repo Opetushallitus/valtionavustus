115e2abbe9820806d955e7c5f936cc77a0686fa4
Matti Ahinko <matti.ahinko@gmail.com>
2018-01-23 14:38:28 +0200

    Add takp account

--- va-virkailija/src/clojure/oph/va/virkailija/schema.clj
+++ va-virkailija/src/clojure/oph/va/virkailija/schema.clj
@@ -317,6 +317,7 @@
    :budget-granted s/Int
    :costs-granted s/Int
    :lkp-account (s/maybe s/Str)
+   :takp-account (s/maybe s/Str)
    (s/optional-key :grant-id) s/Int
    (s/optional-key :evaluation) s/Any
    (s/optional-key :answers) [Answer]})
