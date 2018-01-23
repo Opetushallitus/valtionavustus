471beeccc606cf40b9111747f843b9f48a8b2edc
Silvast <anne-mari.silvast@gofore.com>
2018-01-23 18:03:02 +0200

    added possibility to get from ftp-server

--- va-virkailija/src/clojure/oph/va/virkailija/rondo_service.clj
+++ va-virkailija/src/clojure/oph/va/virkailija/rondo_service.clj
@@ -6,39 +6,30 @@
             [oph.soresu.common.config :refer [config]]
             [clojure.tools.logging :as log]))
 
-(defn send-sftp! [file sftp-config]
+(defn do-sftp! [method file sftp-config]
   (let [agent (ssh/ssh-agent {:use-system-ssh-agent false})
-        session (ssh/session agent (:host-ip sftp-config)
-                             {:username (:username sftp-config)
-                              :password (:password sftp-config)
-                              :port (:port sftp-config)
-                              :strict-host-key-checking :no})]
-    (ssh/with-connection session
-      (let [channel (ssh/ssh-sftp session)]
-        (ssh/with-channel-connection channel
-          (ssh/sftp channel {} :put file (:remote_path sftp-config)))))))
+      session (ssh/session agent (:host-ip sftp-config)
+                           {:username (:username sftp-config)
+                            :password (:password sftp-config)
+                            :port (:port sftp-config)
+                            :strict-host-key-checking :no})]
+  (ssh/with-connection session
+    (let [channel (ssh/ssh-sftp session)]
+      (ssh/with-channel-connection channel
+        (cond
+          (= method "put") (ssh/sftp channel {} :put file (:remote_path sftp-config))
+          (= method "get") (ssh/sftp channel {} :get (format "%s/%s" (:remote_path sftp-config) file) (format "%s/%s" (:local_path sftp-config) file))))))))
+
 
 (defn send-to-rondo! [{:keys [payment application grant filename]}]
   (let [sftp-config (:rondo-sftp config)
         file (format "%s/%s" (:local-path sftp-config) filename)]
     (invoice/write-xml! (invoice/payment-to-xml payment application grant) file)
     (if (:enabled? sftp-config)
-      (let [result (send-sftp! file sftp-config)]
+      (let [result (do-sftp! "put" file sftp-config)]
         (if (nil? result)
           {:success true}
           {:success false :value result}))
       (do
         (log/info (format "Would send %s to %s" file (:host-ip sftp-config)))
         {:success true}))))
-
-(defn get-sftp [file sftp-config]
-    (let [agent (ssh/ssh-agent {:use-system-ssh-agent false})
-        session (ssh/session agent (:host-ip sftp-config)
-                             {:username (:username sftp-config)
-                              :password (:password sftp-config)
-                              :port (:port sftp-config)
-                              :strict-host-key-checking :no})]
-    (ssh/with-connection session
-      (let [channel (ssh/ssh-sftp session)]
-        (ssh/with-channel-connection channel
-          (ssh/sftp channel {} :get (format "%s/%s" (:remote_path config) file) file))))))
