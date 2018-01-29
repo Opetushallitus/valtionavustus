42d0a5dfbe42a7066076ee56398a874d6c9da544
Matti Ahinko <matti.ahinko@gmail.com>
2018-01-29 12:19:22 +0200

    Change coercion

--- va-virkailija/resources/sql/virkailija/payments/update-payment.sql
+++ va-virkailija/resources/sql/virkailija/payments/update-payment.sql
@@ -8,8 +8,8 @@ VALUES(
   (SELECT GREATEST(MAX(version), 0) + 1
     FROM virkailija.payments WHERE id = :id AND deleted IS NULL),
   :application_id, :application_version, :state, :document_type,
-  :invoice_date::timestamptz, :due_date::timestamptz,
-  :receipt_date::timestamptz, :transaction_account, :currency,
+  :invoice_date::date, :due_date::date,
+  :receipt_date::date, :transaction_account, :currency,
   :partner, :inspector_email, :acceptor_email, :installment_number,
   :organisation, :filename, :user_name, :user_oid)
 RETURNING
