cbf0d4b056dd9ec7b8999dc6fce1e1f4721160ed
Matti Ahinko <matti.ahinko@gmail.com>
2018-02-14 15:15:53 +0200

    Add input and button

---
+++ va-admin-ui/src/oph/va/admin_ui/components/ui.cljs
@@ -0,0 +1,39 @@
+(ns oph.va.admin-ui.components.ui
+  (:require [clojure.string :as string]))
+
+(defn default-style [style]
+  (if (nil? (:margin style))
+      (assoc style :margin "5px")
+      style))
+
+(defn text-field [p]
+  (let [props
+        (assoc p :label-text (or (:floating-label-text p) (:label-text p)))]
+   [:div {:class "oph-field" :style (default-style (:style p))}
+    [:span {:class "oph-label" :aria-describedby "field-text"}
+     (:label-text props)]
+    [:input
+     (update (select-keys props [:value :type :onchange])
+             :class str "oph-input")]
+    (when-some [help-text (:help-text props)]
+      [:div {:class "oph-field-text"} help-text])]))
+
+(defn remove-nils [c] (disj c nil))
+
+(defn generate-button-class [props]
+  (->> (hash-set
+         "oph-button"
+         (:class props)
+         (when (:disabled props) "oph-button-disabled")
+         (when (:primary props) "oph-button-primary"))
+       remove-nils
+       (string/join " ")
+       string/trim))
+
+(defn button [p]
+  [:button
+   (-> p
+       (select-keys [:on-click :style :disabled])
+       (assoc :class (generate-button-class p))
+       (update :style default-style))
+   (:label p)])
