c599d17c3264f47d5c56f1bcb1e1199814c7e854
Matti Ahinko <matti.ahinko@gmail.com>
2018-02-19 13:32:31 +0200

    Fix namespaces

--- va-admin-ui/test/oph/va/admin_ui/grants_test.cljs
+++ va-admin-ui/test/oph/va/admin_ui/grants_test.cljs
@@ -1,11 +1,10 @@
 (ns oph.va.admin-ui.grants-test
   (:require [cljs.test :refer-macros [is are deftest testing use-fixtures]]
             [cljs-time.core :as t]
-            [oph.va.admin-ui.grants :as grants]))
+            [oph.va.admin-ui.payments.grants :as grants]))
 
 (deftest test-remove-old
   (let [grants [{:loppuselvitysdate (t/now) :status "resolved"}
                 {:loppuselvitysdate (t/minus (t/now) (t/months 13))
                  :status "resolved"}]]
     (is (= (count (grants/remove-old grants)) 1))))
-
