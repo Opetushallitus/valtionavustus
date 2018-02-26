9888aabea18abe2b1f601fd2dce1bc0b89b30a6e
Matti Ahinko <matti.ahinko@gmail.com>
2018-02-26 12:35:30 +0200

    Add data functions for va-code-values

---
+++ va-virkailija/src/clojure/oph/va/virkailija/va_code_values_data.clj
@@ -0,0 +1,17 @@
+(ns oph.va.virkailija.va-code-values-data
+  (:require [oph.soresu.common.db :refer [exec]]
+            [oph.va.virkailija.db.queries :as queries]
+            [oph.va.virkailija.utils :refer
+             [convert-to-dash-keys convert-to-underscore-keys]]))
+
+(defn get-va-code-values [value-type]
+  (map
+    convert-to-dash-keys
+    (exec :form-db queries/get-va-code-values {:value_type value-type})))
+
+(defn create-va-code-value [values]
+  (->> values
+      convert-to-underscore-keys
+      (exec :form-db queries/create-va-code-value)
+      first
+      convert-to-dash-keys))
