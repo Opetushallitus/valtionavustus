6c358a152604a99358b0eaf0bf49ffd19caf7ffc
Silvast <anne-mari.silvast@gofore.com>
2018-03-06 13:08:01 +0200

    added valueRenderer and optionRenderer to get custom label and values

--- va-virkailija/web/va/haku-details/AutoCompleteCodeValue.jsx
+++ va-virkailija/web/va/haku-details/AutoCompleteCodeValue.jsx
@@ -5,20 +5,30 @@ import BasicTextField from 'soresu-form/web/form/component/BasicTextField.jsx'
 import VirtualizedSelect from 'react-virtualized-select';
 import HttpUtil from 'soresu-form/web/HttpUtil'
 
+
+class codeLabels extends React.Component {
+    render() {
+      return
+				 <div>{this.props.state.option.code + " " + this.props.state.option["code-value"]} </div>}
+  }
+
 export default class AutocompleteCodeValue extends Component {
   constructor(props) {
     super(props)
     this.state = {
       selectValue: "",
+      showValue: "",
       avustushaku: this.props.avustushaku,
       controller: this.props.controller,
       codeType: this.props.codeType,
       id: this.props.id,
-      options: ""
+      options: []
     }
     this.setState = this.setState.bind(this)
     this.updateValue = this.updateValue.bind(this)
     this.getOptions = this.getOptions.bind(this)
+    this.NameOptionRenderer = this.NameOptionRenderer.bind(this)
+    this.codeValueRenderer = this.codeValueRenderer.bind(this)
   }
 
   componentDidMount() {
@@ -34,32 +44,52 @@ getOptions() {
     )
   }
 
-updateValue (newValue) {
+updateValue (code, codeValue) {
   this.setState({
-    selectValue: newValue,
-    operationalUnit: newValue
-  });
+    selectValue: code,
+    showValue: codeValue
+  })
   this.props.controller.onChangeListener(this.props.avustushaku, {
     id: this.state.id
-  }, newValue);
+  }, code);
+}
+
+NameOptionRenderer({key, labelKey, option, selectValue, style, valueArray, valueKey }) {
+  return (
+    <div
+      id="codeValue"
+      className="Select-input"
+      style={{ width: '97%' }}
+      key={key}
+      onClick={() => this.updateValue(option.code, option.code + " " + option["code-value"])}
+    >
+      {option.code + " " + option["code-value"]}
+    </div>
+  )
+}
+
+codeValueRenderer(option){
+  console.log(option)
+  return (
+    <div>{option.code + " " + option["code-value"]}</div>
+  )
+
 }
 
 
   render() {
     return (
       <VirtualizedSelect ref="codeValueSelect"
-          id="codeValue"
-					simpleValue
-					clearable
-          options={this.state.options}
-					value={this.state.selectValue}
-					onChange={this.updateValue}
-					searchable
-					labelKey="code-value"
-					valueKey="code"
+          labelKey='code'
           placeholder="Valitse listasta"
           style={{ width: '97%' }}
           backspaceRemoves={true}
+          options={this.state.options}
+          onChange={this.updateValue}
+          optionRenderer={this.NameOptionRenderer}
+          valueKey='code'
+          value={this.state.selectValue}
+          valueRenderer={this.codeValueRenderer}
 				/>
 
     );
