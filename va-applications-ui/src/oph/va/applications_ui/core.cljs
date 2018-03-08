9bee35268f1b63c2bc364e8baf81a8edfda21168
Matti Ahinko <matti.ahinko@gmail.com>
2018-03-08 13:43:09 +0200

    Add header

--- va-applications-ui/src/oph/va/applications_ui/core.cljs
+++ va-applications-ui/src/oph/va/applications_ui/core.cljs
@@ -3,8 +3,18 @@
             [oph.va.applications-ui.management :as management]
             [oph.va.applications-ui.translations :refer [get-translation]]))
 
+(defn header []
+  [:div {:class "topbar"}
+   [:div {:class "top-container"}
+    [:img {:src "/img/logo.png" :width 240 :height 68 :class "logo"
+           :alt "Opetushallitus / Utbildningsstyrelsen"}]
+    [:div {:class "topbar-right"}]]])
+
 (defn home-page []
-  (management/home-page {:lng :fi}))
+  [:div
+   (header)
+   [:div {:class "content"}
+    (management/home-page {:lng :fi})]])
 
 (defn mount-root [] (r/render [home-page] (.getElementById js/document "app")))
 
