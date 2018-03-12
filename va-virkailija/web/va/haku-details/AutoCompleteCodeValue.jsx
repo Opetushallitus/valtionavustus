578f1f4305177750b18a5c2cb3d7e3e429a478f8
Silvast <anne-mari.silvast@gofore.com>
2018-03-12 19:08:54 +0200

    corrected eslint errors

--- va-virkailija/web/va/haku-details/AutoCompleteCodeValue.jsx
+++ va-virkailija/web/va/haku-details/AutoCompleteCodeValue.jsx
@@ -1,11 +1,8 @@
 import React, { Component } from 'react'
-import Select from 'react-select';
-import { Async } from 'react-select';
-import 'react-select/less/default.less';
-import BasicTextField from 'soresu-form/web/form/component/BasicTextField.jsx'
-import VirtualizedSelect from 'react-virtualized-select';
+//import Select from 'react-select';
+import 'react-select/less/default.less'
+import VirtualizedSelect from 'react-virtualized-select'
 import HttpUtil from 'soresu-form/web/HttpUtil'
-
 
 export default class AutocompleteCodeValue extends Component {
   constructor(props) {
@@ -27,69 +24,65 @@ export default class AutocompleteCodeValue extends Component {
   }
 
   componentDidMount() {
-        this.setState({
-          options: this.getOptions()
-        })
-      }
+    this.setState({
+      options: this.getOptions()
+    })
+  }
 
-componentWillReceiveProps(nextProps) {
-   if (nextProps.avustushaku.id != this.state.avustushaku.id && this.props.avustushaku[this.props.codeType] != null) {
-   this.setState({selectValue: this.state.options.find(k => k.id==nextProps.avustushaku[this.props.codeType])})
-   }
- }
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.avustushaku.id !== this.state.avustushaku.id && this.props.avustushaku[this.props.codeType] !== null) {
+        this.setState({selectValue: this.state.options.find(k => k.id === nextProps.avustushaku[this.props.codeType])})
+    }
+  }
 
-getOptions() {
-  HttpUtil.get(`/api/v2/va-code-values/?value-type=${this.state.codeType}`)
-    .then(response => {
-      this.setState({options: response, selectValue: response.find(k => k.id==this.props.avustushaku[this.props.codeType])})})}
+  getOptions() {
+    HttpUtil.get(`/api/v2/va-code-values/?value-type=${this.state.codeType}`)
+      .then(response => {
+        this.setState({options: response, selectValue: response.find(k => k.id===this.props.avustushaku[this.props.codeType])})})}
 
-updateValue (option) {
-  this.setState({
-    selectValue: option
-  })
-  this.props.controller.onChangeListener(this.state.avustushaku, {id: this.state.id}, option.id)
-  this.props.avustushaku[this.state.codeType] = option.id
-}
+  updateValue (option) {
+    this.setState({
+      selectValue: option
+    })
+    this.props.controller.onChangeListener(this.state.avustushaku, {id: this.state.id}, option.id)
+    this.props.avustushaku[this.state.codeType] = option.id
+  }
 
-NameOptionRenderer({key, labelKey, option, selectValue, style, valueArray, valueKey }) {
-  return (
-    <div
-      className="Select-input"
-      style={{ width: '97%' }}
-      key={key}
-      onClick={() => this.updateValue(option)}
-    >
-      {option.code + " " + option["code-value"]}
-    </div>
-  )
-}
+  NameOptionRenderer({key, option }) {
+    return (
+      <div
+        className="Select-input"
+        style={{ width: "97%" }}
+        key={key}
+        onClick={() => this.updateValue(option)}
+      >
+        {option.code + " " + option["code-value"]}
+      </div>
+    )
+  }
 
-codeValueRenderer(option){
-  return (
-    <div>{option.code + " " + option["code-value"]}</div>
-  )
-
-}
+  codeValueRenderer(option){
+    return (
+      <div>{option.code + " " + option["code-value"]}</div>
+    )
+  }
 
 
   render() {
-    const currentValue = this.state.avustushaku[this.state.codeType] || ""
-    const value = this.state.selectValue && this.state.selectValue["code-value"]
     return (
-      <VirtualizedSelect ref="codeValueSelect"
-          id={this.state.codeType}
-          labelKey='code'
-          placeholder="Valitse listasta"
-          style={{ width: '97%' }}
-          backspaceRemoves={true}
-          options={this.state.options}
-          onChange={this.updateValue}
-          optionRenderer={this.NameOptionRenderer}
-          valueKey='code-value'
-          value={this.state.selectValue}
-          valueRenderer={this.codeValueRenderer}
-				/>
-
-    );
+      <VirtualizedSelect
+        id={this.state.codeType}
+        labelKey='code'
+        placeholder="Valitse listasta"
+        style={{ width: "97%" }}
+        backspaceRemoves={true}
+        options={this.state.options}
+        onChange={this.updateValue}
+        optionRenderer={this.NameOptionRenderer}
+        valueKey='code-value'
+        value={this.state.selectValue}
+        valueRenderer={this.codeValueRenderer}
+      />
+    )
   }
 }
