34feafd1bb5492c06f15f183ca8c6ed6aa50c704
Matti Ahinko <matti.ahinko@gmail.com>
2018-03-12 13:12:12 +0200

    Return only the latest

--- va-virkailija/resources/sql/virkailija/va_code_values/get-current-va-code-values-by-type.sql
+++ va-virkailija/resources/sql/virkailija/va_code_values/get-current-va-code-values-by-type.sql
@@ -1,6 +1,9 @@
 SELECT
+  DISTINCT ON (code)
   id, value_type, year, code, code_value
 FROM
   virkailija.va_code_values
 WHERE
   value_type = :value_type AND deleted IS NULL
+ORDER
+  BY code, year DESC
