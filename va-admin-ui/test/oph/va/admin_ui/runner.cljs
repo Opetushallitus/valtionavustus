dcbdc538eda423e0e567885c506668a195c67f8b
Matti Ahinko <matti.ahinko@gmail.com>
2018-03-13 10:29:38 +0200

    Add user test

--- va-admin-ui/test/oph/va/admin_ui/runner.cljs
+++ va-admin-ui/test/oph/va/admin_ui/runner.cljs
@@ -1,7 +1,9 @@
 (ns oph.va.admin-ui.runner
   (:require [doo.runner :refer-macros [doo-tests]]
             [oph.va.admin-ui.utils-test]
-            [oph.va.admin-ui.grants-test]))
+            [oph.va.admin-ui.grants-test]
+            [oph.va.admin-ui.user-test]))
 
 (doo-tests 'oph.va.admin-ui.utils-test
-           'oph.va.admin-ui.grants-test)
+           'oph.va.admin-ui.grants-test
+           'oph.va.admin-ui.user-test)
