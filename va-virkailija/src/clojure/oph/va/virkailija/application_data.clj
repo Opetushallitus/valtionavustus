5ccd08afcf25ff254d251616ba34b848da78ad8a
Matti Ahinko <matti.ahinko@gmail.com>
2018-03-15 12:18:12 +0200

    Add function for gettin all application payments

--- va-virkailija/src/clojure/oph/va/virkailija/application_data.clj
+++ va-virkailija/src/clojure/oph/va/virkailija/application_data.clj
@@ -7,28 +7,29 @@
             [oph.va.virkailija.db.queries :as virkailija-queries])
   (:import (oph.va.jdbc.enums)))
 
-(defn get-application
-  [id]
+(defn get-application [id]
   (convert-to-dash-keys (first (exec :form-db
                                      virkailija-queries/get-application
                                      {:application_id id}))))
 
-(defn get-application-with-evaluation-and-answers
-  [id]
+(defn get-application-with-evaluation-and-answers [id]
   (convert-to-dash-keys
     (first (exec :form-db
                  virkailija-queries/get-application-with-evaluation-and-answers
                  {:application_id id}))))
 
-(defn get-payments-history
-  [id]
+(defn get-payments-history [id]
   (mapv convert-to-dash-keys
     (exec :form-db
           virkailija-queries/get-payment-history
           {:application_id id})))
 
-(defn get-application-payment
-  [id]
+(defn get-application-payment [id]
   (convert-to-dash-keys (last (exec :form-db
                                     virkailija-queries/get-application-payment
                                     {:application_id id}))))
+
+(defn get-application-paymentos [id]
+  (map convert-to-dash-keys (exec :form-db
+                                  virkailija-queries/get-application-payments
+                                  {:application_id id})))
