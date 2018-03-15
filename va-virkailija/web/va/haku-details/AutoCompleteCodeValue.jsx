54b3f7f7e4990615be6d3dc2e7d7931de57693ea
Silvast <anne-mari.silvast@gofore.com>
2018-03-15 15:21:46 +0200

    switched back to working version of functions

--- va-virkailija/web/va/haku-details/AutoCompleteCodeValue.jsx
+++ va-virkailija/web/va/haku-details/AutoCompleteCodeValue.jsx
@@ -4,8 +4,25 @@ import VirtualizedSelect from 'react-virtualized-select'
 import HttpUtil from 'soresu-form/web/HttpUtil'
 
 export default class AutocompleteCodeValue extends Component {
+  constructor(props) {
+    super(props)
+    this.updateValue = this.updateValue.bind(this)
+    this.NameOptionRenderer = this.NameOptionRenderer.bind(this)
+    this.codeValueRenderer = this.codeValueRenderer.bind(this)
+  }
 
-  NameOptionRenderer: (key, option ) => {
+
+  updateValue (option) {
+    if (option == null) {
+      this.props.controller.onChangeListener(this.props.avustushaku, {id: this.props.id}, null)
+      this.props.avustushaku[this.state.codeType] = null
+    } else {
+      this.props.controller.onChangeListener(this.props.avustushaku, {id: this.props.id}, option.id)
+      this.props.avustushaku[this.props.codeType] = option.id
+      }
+  }
+
+  NameOptionRenderer({key, option }) {
     return (
       <div
         className="Select-input"
@@ -18,17 +35,8 @@ export default class AutocompleteCodeValue extends Component {
     )
   }
 
-  updateValue: (option) => {
-    if (option == null) {
-      this.props.controller.onChangeListener(this.props.avustushaku, {id: this.props.id}, null)
-      this.props.avustushaku[this.state.codeType] = null
-    } else {
-      this.props.controller.onChangeListener(this.props.avustushaku, {id: this.props.id}, option.id)
-      this.props.avustushaku[this.props.codeType] = option.id
-      }
-  }
 
-  codeValueRenderer: (option) => {
+  codeValueRenderer(option){
     return (
       <div>{option.code + " " + option["code-value"]}</div>
     )
