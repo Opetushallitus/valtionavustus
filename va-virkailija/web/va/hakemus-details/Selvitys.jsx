3d6a7357724c91d4ca76927447b8ee1b4ee39162
Matti Ahinko <matti.ahinko@gmail.com>
2018-03-20 16:18:27 +0200

    Add payment batch fields

--- va-virkailija/web/va/hakemus-details/Selvitys.jsx
+++ va-virkailija/web/va/hakemus-details/Selvitys.jsx
@@ -6,9 +6,23 @@ import SelvitysLink from './SelvitysLink.jsx'
 import SelvitysEmail from './SelvitysEmail.jsx'
 import PresenterComment from './PresenterComment.jsx'
 
+const localeString = (num) =>
+      (typeof Number.prototype.toLocaleString === "function") ?
+      num.toLocaleString() : num.toString()
+
+const renderPayment = (p, i) =>
+      <tr key={i}>
+        <td>{i + 1}. erä</td>
+        <td className="payment-money-column">
+          {localeString(p["payment-sum"])} €
+        </td>
+      </tr>
+
 export default class Selvitys extends Component {
   render() {
-    const {controller, hakemus, avustushaku, translations, selvitysType, userInfo} = this.props
+    const {controller, hakemus, avustushaku, translations, selvitysType,
+           userInfo} = this.props
+    const payments = this.props.payments || []
     const hasSelvitys = _.has(hakemus,`selvitys.${selvitysType}.answers`)
     const preview = _.eq(selvitysType, 'valiselvitys')
     const selvitysHakemus = _.get(hakemus,`selvitys.${selvitysType}`)
@@ -25,6 +39,66 @@ export default class Selvitys extends Component {
                                          form={form}
 
         />}
+        <div>
+          <h3>Maksuerät</h3>
+          <div>
+            Rahoitettavaa yhteensä {localeString(hakemus["budget-total"])} €
+          </div>
+          <div className="payment-info-columns">
+            <table className="payment-info-column">
+              <tbody>
+                <tr>
+                  <td>Omarahoitus</td>
+                  <td className="payment-money-column">
+                    {localeString(
+                      hakemus["budget-total"] - hakemus["budget-oph-share"])} €
+                  </td>
+                </tr>
+                <tr>
+                  <td>OPH:n avustus</td>
+                  <td className="payment-money-column">
+                    {localeString(hakemus["budget-oph-share"])} €
+                  </td>
+                </tr>
+                {payments.map(renderPayment)}
+                <tr>
+                  <td>{payments.length + 1}. erä</td>
+                  <td className="payment-money-column">
+                    <input/> €</td>
+                </tr>
+              </tbody>
+            </table>
+            <table className="payment-info-column">
+              <tbody>
+                <tr>
+                  <td>Omarahoitus-%</td>
+                  <td className="payment-money-column">
+                    {avustushaku.content["self-financing-percentage"]}%
+                  </td>
+                </tr>
+                <tr>
+                  <td>OPH:n rahoitus-%</td>
+                  <td className="payment-money-column">
+                    {100 - avustushaku.content["self-financing-percentage"]}%
+                  </td>
+                </tr>
+                <tr>
+                  <td>1. erä OPH:n avustussummasta</td>
+                  <td className="payment-money-column">
+                    {avustushaku.content["payment-min-first-batch"]}%
+                  </td>
+                </tr>
+                <tr>
+                  <td>2. erä OPH:n avustussummasta</td>
+                  <td className="payment-money-column">
+                    {100 - avustushaku.content["payment-min-first-batch"]}%
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <button>Lisää 2.erä maksatuslistaan</button>
+        </div>
         <SelvitysLink avustushaku={avustushaku} hakemus={hakemus} selvitysType={selvitysType} preview={preview} label="Linkki lomakkeelle"/>
         {hasSelvitys && <SelvitysEmail controller={controller}
                                        selvitysType={selvitysType}
