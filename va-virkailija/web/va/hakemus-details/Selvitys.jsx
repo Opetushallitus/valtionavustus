abdfbcd822fe12662e611c362c90fa0381369ae9
Matti Ahinko <matti.ahinko@gmail.com>
2018-03-21 14:56:11 +0200

    Move payment details to own file

--- va-virkailija/web/va/hakemus-details/Selvitys.jsx
+++ va-virkailija/web/va/hakemus-details/Selvitys.jsx
@@ -5,24 +5,12 @@ import SelvitysNotFilled from './SelvitysNotFilled.jsx'
 import SelvitysLink from './SelvitysLink.jsx'
 import SelvitysEmail from './SelvitysEmail.jsx'
 import PresenterComment from './PresenterComment.jsx'
-
-const localeString = (num) =>
-      (typeof Number.prototype.toLocaleString === "function") ?
-      num.toLocaleString() : num.toString()
-
-const renderPayment = (p, i) =>
-      <tr key={i}>
-        <td>{i + 1}. erä</td>
-        <td className="payment-money-column">
-          {localeString(p["payment-sum"])} €
-        </td>
-      </tr>
+import ApplicationPayments from './ApplicationPayments.jsx'
 
 export default class Selvitys extends Component {
   render() {
     const {controller, hakemus, avustushaku, translations, selvitysType,
            userInfo} = this.props
-    const payments = hakemus.payments || []
     const hasSelvitys = _.has(hakemus,`selvitys.${selvitysType}.answers`)
     const preview = _.eq(selvitysType, 'valiselvitys')
     const selvitysHakemus = _.get(hakemus,`selvitys.${selvitysType}`)
@@ -39,66 +27,8 @@ export default class Selvitys extends Component {
                                          form={form}
 
         />}
-        <div>
-          <h3>Maksuerät</h3>
-          <div>
-            Rahoitettavaa yhteensä {localeString(hakemus["budget-total"])} €
-          </div>
-          <div className="payment-info-columns">
-            <table className="payment-info-column">
-              <tbody>
-                <tr>
-                  <td>Omarahoitus</td>
-                  <td className="payment-money-column">
-                    {localeString(
-                      hakemus["budget-total"] - hakemus["budget-oph-share"])} €
-                  </td>
-                </tr>
-                <tr>
-                  <td>OPH:n avustus</td>
-                  <td className="payment-money-column">
-                    {localeString(hakemus["budget-oph-share"])} €
-                  </td>
-                </tr>
-                {payments.map(renderPayment)}
-                <tr>
-                  <td>{payments.length + 1}. erä</td>
-                  <td className="payment-money-column">
-                    <input/> €</td>
-                </tr>
-              </tbody>
-            </table>
-            <table className="payment-info-column">
-              <tbody>
-                <tr>
-                  <td>Omarahoitus-%</td>
-                  <td className="payment-money-column">
-                    {avustushaku.content["self-financing-percentage"]}%
-                  </td>
-                </tr>
-                <tr>
-                  <td>OPH:n rahoitus-%</td>
-                  <td className="payment-money-column">
-                    {100 - avustushaku.content["self-financing-percentage"]}%
-                  </td>
-                </tr>
-                <tr>
-                  <td>1. erä OPH:n avustussummasta</td>
-                  <td className="payment-money-column">
-                    {avustushaku.content["payment-min-first-batch"]}%
-                  </td>
-                </tr>
-                <tr>
-                  <td>2. erä OPH:n avustussummasta</td>
-                  <td className="payment-money-column">
-                    {100 - avustushaku.content["payment-min-first-batch"]}%
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-          <button>Lisää 2.erä maksatuslistaan</button>
-        </div>
+        <ApplicationPayments application={hakemus} grant={avustushaku}
+                             onAddPayment={controller.addPayment}/>
         <SelvitysLink avustushaku={avustushaku} hakemus={hakemus} selvitysType={selvitysType} preview={preview} label="Linkki lomakkeelle"/>
         {hasSelvitys && <SelvitysEmail controller={controller}
                                        selvitysType={selvitysType}
