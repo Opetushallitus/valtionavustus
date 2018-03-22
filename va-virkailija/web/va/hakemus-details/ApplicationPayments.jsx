0609d921593e3f827e10d91eb6da0b37c15ca9d4
Matti Ahinko <matti.ahinko@gmail.com>
2018-03-22 12:25:12 +0200

    Remove equal

--- va-virkailija/web/va/hakemus-details/ApplicationPayments.jsx
+++ va-virkailija/web/va/hakemus-details/ApplicationPayments.jsx
@@ -161,7 +161,7 @@ export default class ApplicationPayments extends Component {
           </table>
         </div>
         <button onClick={this.onAddPayment}
-                disabled={(grantLeft - this.state.newPaymentSum) <= 0}>
+                disabled={(grantLeft - this.state.newPaymentSum) < 0}>
           Lisää {payments.length + 1}. erä maksatuslistaan
         </button>
       </div>
