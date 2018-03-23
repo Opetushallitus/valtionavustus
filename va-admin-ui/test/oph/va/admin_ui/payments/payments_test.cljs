37c001765222949fafa0de39d81b3cc77f189553
Matti Ahinko <matti.ahinko@gmail.com>
2018-03-23 10:15:49 +0200

    Update tests

--- va-admin-ui/test/oph/va/admin_ui/payments/payments_test.cljs
+++ va-admin-ui/test/oph/va/admin_ui/payments/payments_test.cljs
@@ -1,18 +1,53 @@
 (ns oph.va.admin-ui.payments.payments-test
   (:require [cljs.test :refer-macros [is deftest]]
             [oph.va.admin-ui.payments.payments
-             :refer [multibatch-payable?]]))
+             :refer [multibatch-payable? singlebatch-payable?]]))
 
 (deftest test-multipatch-payable
   (is (multibatch-payable?
-        {:payments
-         [{:state 2 :payment-sum 363000}
-          {:state 1 :payment-sum 242000}]}))
+        [{:payments
+          [{:state 2 :payment-sum 363000}
+           {:state 2 :payment-sum 242000}]}
+         {:payments
+          [{:state 2 :payment-sum 363000}
+           {:state 1 :payment-sum 242000}]}]))
+  (is (multibatch-payable?
+        [{:payments
+          [{:state 1 :payment-sum 363000}
+           {:state 2 :payment-sum 242000}]}
+         {:payments
+          [{:state 2 :payment-sum 363000}
+           {:state 1 :payment-sum 242000}]}]))
+  (is (multibatch-payable?
+        [{:payments
+          [{:state 1 :payment-sum 363000}]}
+         {:payments
+          [{:state 1 :payment-sum 363000}
+           {:state 1 :payment-sum 242000}]}]))
   (is (not (multibatch-payable?
-             {:payments
-              [{:state 2 :payment-sum 363000}
-               {:state 2 :payment-sum 242000}]})))
+             [{:payments
+               [{:state 2 :payment-sum 363000}
+                {:state 2 :payment-sum 242000}]}])))
+    (is (not (multibatch-payable?
+             [{:payments
+               [{:state 2 :payment-sum 363000}
+                {:state 2 :payment-sum 242000}]}
+              {:payments
+               [{:state 2 :payment-sum 363000}
+                {:state 2 :payment-sum 242000}]}])))
   (is (not (multibatch-payable? nil)))
-  (is (not (multibatch-payable? {})))
-  (is (not (multibatch-payable? {:payments nil})))
-  (is (not (multibatch-payable? {:payments []}))))
+  (is (not (multibatch-payable? [{:payments nil}])))
+  (is (not (multibatch-payable? [{:payments []}]))))
+
+(deftest test-singlepatch-payable
+  (is (singlebatch-payable?
+        [{:payments
+          [{:state 2 :payment-sum 363000}
+           {:state 1 :payment-sum 242000}]}]))
+  (is (not (singlebatch-payable?
+             [{:payments
+               [{:state 2 :payment-sum 363000}
+                {:state 2 :payment-sum 242000}]}])))
+  (is (not (singlebatch-payable? nil)))
+  (is (singlebatch-payable? [{:payments nil}]))
+  (is (singlebatch-payable? [{:payments []}])))
