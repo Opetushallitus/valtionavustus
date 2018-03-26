7866b7e1ebf1a7912e4d5970c613fed39e781c18
Matti Ahinko <matti.ahinko@gmail.com>
2018-03-26 13:38:28 +0300

    Add getting code values

--- va-virkailija/resources/sql/virkailija/grants/get-resolved-grants-with-content.sql
+++ va-virkailija/resources/sql/virkailija/grants/get-resolved-grants-with-content.sql
@@ -1,10 +1,21 @@
 SELECT
-  id, created_at, form, content, status, register_number, valiselvitysdate,
-  loppuselvitysdate, form_loppuselvitys, form_valiselvitys, is_academysize,
-  haku_type
+  h.id, h.created_at, h.form, h.content, h.status, h.register_number,
+  h.valiselvitysdate, h.loppuselvitysdate, h.form_loppuselvitys,
+  h.form_valiselvitys, h.is_academysize, h.haku_type, h.operational_unit_id,
+  pu.code AS operational_unit, h.project_id,  p.code AS project,
+  h.operation_id, o.code AS operation
 FROM
-  avustushaut
+  avustushaut h
+LEFT JOIN
+  virkailija.va_code_values pu
+    ON pu.id = h.operational_unit_id
+LEFT JOIN
+  virkailija.va_code_values p
+    ON p.id = h.project_id
+LEFT JOIN
+  virkailija.va_code_values o
+    ON o.id = h.operation_id
 WHERE
-  status = 'resolved' OR status = 'published'
+  h.status = 'resolved' OR h.status = 'published'
 ORDER
-  BY created_at DESC;
+  BY h.created_at DESC;
