fe5315bc3d2608ef24c722e182a916fa10ef8b19
Matti Ahinko <matti.ahinko@gmail.com>
2018-03-26 15:49:34 +0300

    Add selecting proper values

--- va-virkailija/resources/sql/virkailija/grants/get-grant.sql
+++ va-virkailija/resources/sql/virkailija/grants/get-grant.sql
@@ -1,6 +1,19 @@
 SELECT
-  *
+  h.id, h.created_at, h.form, h.content, h.status, h.register_number,
+  h.valiselvitysdate, h.loppuselvitysdate, h.form_loppuselvitys,
+  h.form_valiselvitys, h.is_academysize, h.haku_type, h.operational_unit_id,
+  pu.code AS operational_unit, h.project_id,  p.code AS project,
+  h.operation_id, o.code AS operation
 FROM
-  avustushaut
+  avustushaut h
+LEFT JOIN
+  virkailija.va_code_values pu
+    ON (pu.id = h.operational_unit_id AND pu.deleted IS NULL)
+LEFT JOIN
+  virkailija.va_code_values p
+    ON (p.id = h.project_id AND p.deleted IS NULL)
+LEFT JOIN
+  virkailija.va_code_values o
+    ON (o.id = h.operation_id AND o.deleted IS NULL)
 WHERE
-  id = :grant_id;
+  h.id = :grant_id;
