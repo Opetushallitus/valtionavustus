9aa5dad1b61632be1f2af1c92a1c89c75df7501d
Matti Ahinko <matti.ahinko@gmail.com>
2018-04-04 16:26:31 +0300

    Add grant refuse if preview and enabled

--- va-hakija/web/va/VaForm.jsx
+++ va-hakija/web/va/VaForm.jsx
@@ -11,33 +11,39 @@ import VaChangeRequest from 'va-common/web/va/VaChangeRequest.jsx'
 import VaFormTopbar from './VaFormTopbar.jsx'
 import VaOldBrowserWarning from './VaOldBrowserWarning.jsx'
 
+import GrantRefuse from './GrantRefuse.jsx'
+
 import './style/main.less'
 
 export default class VaForm extends React.Component {
   render() {
     const {controller, state, hakemusType} = this.props
     const registerNumber = _.get(state.saveStatus.savedObject, "register-number", undefined)
+    const {saveStatus, configuration} = state
     const registerNumberDisplay = <VaHakemusRegisterNumber key="register-number"
                                                            registerNumber={registerNumber}
-                                                           translations={state.configuration.translations}
-                                                           lang={state.configuration.lang} />
-    const saveStatus = state.saveStatus
+                                                           translations={configuration.translations}
+                                                           lang={configuration.lang} />
     const changeRequest =  <VaChangeRequest key="change-request"
                                             hakemus={saveStatus.savedObject}
-                                            translations={state.configuration.translations}
-                                            lang={state.configuration.lang} />
+                                            translations={configuration.translations}
+                                            lang={configuration.lang} />
     const headerElements = [registerNumberDisplay, changeRequest]
-    const formContainerClass = state.configuration.preview ? FormPreview : Form
+    const formContainerClass = configuration.preview ? FormPreview : Form
     return(
       <div>
-        <VaOldBrowserWarning lang={state.configuration.lang}
-                             translations={state.configuration.translations.warning}
-                             devel={state.configuration.develMode}
+        <VaOldBrowserWarning lang={configuration.lang}
+                             translations={configuration.translations.warning}
+                             devel={configuration.develMode}
         />
         <VaFormTopbar controller={controller}
                       state={state}
                       hakemusType={hakemusType}
         />
+        {configuration.environment["grant-refuse"] &&
+          configuration.environment["grant-refuse"]["enabled?"] &&
+          configuration.preview &&
+          <GrantRefuse controller={controller} state={state}/>}
         <FormContainer controller={controller}
                        state={state}
                        formContainerClass={formContainerClass}
