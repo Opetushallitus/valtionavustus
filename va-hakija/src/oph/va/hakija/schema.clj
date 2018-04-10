ad7d2ac2b2c087c2d066cc701de5553dd0c1b995
Matti Ahinko <matti.ahinko@gmail.com>
2018-04-10 16:00:08 +0300

    Add refused

--- va-hakija/src/oph/va/hakija/schema.clj
+++ va-hakija/src/oph/va/hakija/schema.clj
@@ -13,7 +13,10 @@
    :status-comment (s/maybe s/Str)
    :register-number (s/maybe s/Str)
    :submission VaSubmission
-   :validation-errors SubmissionValidationErrors})
+   :validation-errors SubmissionValidationErrors
+   (s/optional-key :refused) (s/maybe s/Bool)
+   (s/optional-key :refused_comment) (s/maybe s/Str)
+   (s/optional-key :refused_at) (s/maybe s/Inst)})
 
 (s/defschema HakemusInfo
   "Hakemus with user key and language"
