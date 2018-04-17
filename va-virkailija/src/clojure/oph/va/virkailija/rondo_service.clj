c81caeeced005f0b5886c4a30356794e4663df7e
Matti Ahinko <matti.ahinko@gmail.com>
2018-04-17 10:47:23 +0300

    Move config to parameter

--- va-virkailija/src/clojure/oph/va/virkailija/rondo_service.clj
+++ va-virkailija/src/clojure/oph/va/virkailija/rondo_service.clj
@@ -3,56 +3,67 @@
             [ring.util.http-response :refer :all]
             [oph.va.hakija.api :as hakija-api]
             [oph.va.virkailija.invoice :as invoice]
-            [oph.soresu.common.config :refer [config]]
             [clojure.tools.logging :as log]
             [clojure.string :as strc]))
 
 (defn create-session
   [config]
-  (let [sftp-config (:rondo-sftp config)
-        agent (ssh/ssh-agent {:use-system-ssh-agent false})]
-        (ssh/session agent (:host-ip sftp-config)
-                           {:username (:username sftp-config)
-                            :password (:password sftp-config)
-                            :port (:port sftp-config)
+  (let [agent (ssh/ssh-agent {:use-system-ssh-agent false})]
+        (ssh/session agent (:host-ip config)
+                           {:username (:username config)
+                            :password (:password config)
+                            :port (:port config)
                             :strict-host-key-checking :no})))
 
-(defn do-sftp! [& {:keys [file method path]}]
+(defn do-sftp! [& {:keys [file method path config]}]
   (let [session (create-session config)
-        remote (:remote_path (:rondo-sftp config))
-        remote_from (:remote_path_from (:rondo-sftp config))]
+        remote (:remote_path config)
+        remote_from (:remote_path_from config)]
   (ssh/with-connection session
     (let [channel (ssh/ssh-sftp session)]
       (ssh/with-channel-connection channel
         (cond
           (= method :put) (ssh/sftp channel {} method file remote)
-          (= method :get) (ssh/sftp channel {} method (format "%s/%s" path (.getName (clojure.java.io/file file))) file)
+          (= method :get)
+          (ssh/sftp channel {} method
+                    (format "%s/%s" path (.getName (clojure.java.io/file file)))
+                    file)
           (= method :rm )(ssh/sftp channel {} method (format "%s/%s" path file))
-          (= method :cdls)(ssh/with-channel-connection channel (ssh/sftp channel {} :cd path) (ssh/ssh-sftp-cmd channel :ls ["*.xml"] :with-monitor))))))))
+          (= method :cdls)
+          (ssh/with-channel-connection channel
+            (ssh/sftp channel {} :cd path)
+            (ssh/ssh-sftp-cmd channel :ls ["*.xml"] :with-monitor))))))))
 
-(defn send-to-rondo! [{:keys [payment application grant filename batch]}]
-  (let [sftp-config (:rondo-sftp config)
-        file (format "%s/%s" (:local-path sftp-config) filename)]
+(defn send-to-rondo! [{:keys [payment application grant filename batch config]}]
+  (let [file (format "%s/%s" (:local-path config) filename)]
     (invoice/write-xml!
       (invoice/payment-to-xml
         {:payment payment :application application :grant grant :batch batch})
       file)
-    (if (:enabled? sftp-config)
-      (let [result (do-sftp! :method :put :file file :path (:remote_path (:rondo-sftp config)))]
+    (if (:enabled? config)
+      (let [result
+            (do-sftp! :method :put :file file :path (:remote_path config)
+                      :config config)]
         (if (nil? result)
           {:success true}
           {:success false :value result}))
       (do
-        (log/info (format "Would send %s to %s" file (:host-ip sftp-config)))
+        (log/info (format "Would send %s to %s" file (:host-ip config)))
         {:success true}))))
 
-(defn get-remote-file-list []
-  (let [result (do-sftp! :method :cdls :path (:remote_path_from (:rondo-sftp config)))]
-        (map #(last (strc/split % #"\s+")) (map str result))))
+(defn get-remote-file-list [config]
+  (let [result (do-sftp! :method :cdls :path (:remote_path_from config)
+                         :config config)]
+    (map #(last (strc/split % #"\s+")) (map str result))))
 
-(defn get-remote-file [filename]
-    (let [xml-file-path (format "%s/%s" (System/getProperty"java.io.tmpdir") filename)]
-    (do-sftp! :method :get :file xml-file-path :path (:remote_path_from (:rondo-sftp config)))))
+(defn get-remote-file [filename config]
+  (let [xml-file-path
+        (format "%s/%s" (System/getProperty"java.io.tmpdir") filename)]
+    (do-sftp! :method :get :file xml-file-path
+              :path (:remote_path_from config)
+              :config config)))
 
-(defn delete-remote-file [filename]
-  (do-sftp! :method :rm :file filename :path (:remote_path_from (:rondo-sftp config))))
+(defn delete-remote-file [filename config]
+  (do-sftp! :method :rm :file filename
+            :path (:remote_path_from config)
+            :config config))
