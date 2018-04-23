cdf826ae827cfdf77167c6ee087104375f0f8e4b
Matti Ahinko <matti.ahinko@gmail.com>
2018-04-23 12:20:51 +0300

    Add query for finding applications

---
+++ va-virkailija/resources/sql/virkailija/applications/find-applications.sql
@@ -0,0 +1,12 @@
+SELECT
+  id, created_at, version, budget_total, budget_oph_share, organization_name,
+  project_name, register_number, language,  avustushaku AS grant_id, refused,
+  refused_comment, refused_at
+FROM
+  hakija.hakemukset
+WHERE
+  version_closed IS NULL
+  AND
+    (register_number LIKE :search_term
+    OR project_name LIKE :search_term
+    OR organization_name LIKE :search_term)
