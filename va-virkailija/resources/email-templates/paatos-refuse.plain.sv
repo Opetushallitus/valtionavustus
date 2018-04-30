0f1e7afee9c24da8dd0211398dc538bf85b5933c
Matti Ahinko <matti.ahinko@gmail.com>
2018-04-30 09:19:47 +0300

    Add Swedish translation

---
+++ va-virkailija/resources/email-templates/paatos-refuse.plain.sv
@@ -0,0 +1,16 @@
+{{ register-number }} - {{ project-name }}
+
+Ansökan om understöd: {{ avustushaku-name }}
+
+Beslut link: {{& url }}
+
+Om du väljer att inte få ett bidrag kan du rapportera det i här länken: {{& refuse-url }}
+
+Den föredragande som ansvarar för ansökningen har nämnts i beslutet.
+
+Utbildningsstyrelsen
+Hagnäskajen 6
+PB 380, 00531 Helsingfors
+
+telefon 029 533 1000
+fornamn.efternamn@ubs.fi
