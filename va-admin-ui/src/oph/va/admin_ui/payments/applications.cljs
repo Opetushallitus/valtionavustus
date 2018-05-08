e8f6a0e062088ae8ec4aa3178e7178428fdad133
Matti Ahinko <matti.ahinko@gmail.com>
2018-05-08 15:19:41 +0300

    Restore using mui notation

--- va-admin-ui/src/oph/va/admin_ui/payments/applications.cljs
+++ va-admin-ui/src/oph/va/admin_ui/payments/applications.cljs
@@ -1,11 +1,10 @@
 (ns oph.va.admin-ui.payments.applications
-  (:require [reagent.core :as r]
-            [cljsjs.material-ui]
-            [cljs-react-material-ui.reagent :as ui]
+  (:require [cljsjs.material-ui]
             [cljs-react-material-ui.icons :as ic]
             [oph.va.admin-ui.theme :as theme]
             [oph.va.admin-ui.utils :refer [format]]
-            [oph.va.admin-ui.components.ui :as va-ui]))
+            [oph.va.admin-ui.components.table :as ui]
+            [cljs-react-material-ui.reagent :as mui]))
 
 (def str-limit 30)
 
@@ -21,49 +20,68 @@
     4 "Epäonnistunut"
     "Odottaa maksatusta"))
 
-(defn render-application-row [application on-info-clicked]
-  [[:span {:style {:text-align "right"}} (get application :register-number)]
-   (state-to-str (:state (last (:payments application))))
-   [:span {:title (:organization-name application)}
+(defn render-application [i application on-info-clicked is-admin?]
+  [ui/table-row {:key i :style (when (odd? i) theme/striped-row)}
+   [ui/table-row-column
+    (state-to-str (:state (last (:payments application))))]
+   [ui/table-row-column {:title (:organization-name application)}
     (:organization-name application)]
-   [:a
+   [ui/table-row-column
+    [:a
      {:target "_blank"
       :title (:project-name application)
       :href (format "/avustushaku/%d/hakemus/%d/arviointi/"
                     (:grant-id application)
-                    (:id application))}
-    [:span {:title (:project-name application)}
-     (:project-name application)]]
-   [{:style {:text-align "right"}}
-    [:span(.toLocaleString (get application :budget-granted 0)) " €"]]
-   (get-answer-value (:answers application) "bank-iban")
-   (get application :lkp-account)
-   (get application :takp-account)
-   [{:style {:text-align "right"}}
-    [:span (.toLocaleString (get application :budget-granted 0)) " €"]]
-   (when (not (nil? on-info-clicked))
-     [ui/icon-button {:on-click #(on-info-clicked (:id application))}
-      [ic/action-info-outline]])])
-
-(def header
-  [[:span {:style {:text-align "right"}} "Pitkäviite"] "Tila" "Toimittajan nimi"
-   "Hanke" [:span {:style {:text-align "right"}} "Maksuun"] "IBAN" "LKP-tili"
-   "TaKp-tili" [:span {:style {:text-align "right"}} "Tiliöinti"] "Lisätietoja"])
+                    (:id application))} (:project-name application)]]
+   [ui/table-row-column {:style theme/table-cell}
+    (.toLocaleString (get application :budget-granted 0)) " €"]
+   [ui/table-row-column {:style theme/table-cell}
+    (get-answer-value (:answers application) "bank-iban")]
+   [ui/table-row-column {:style (merge theme/table-cell {:text-align "right"})}
+    (get application :register-number)]
+   [ui/table-row-column {:style theme/table-cell}
+    (get application :lkp-account)]
+   [ui/table-row-column {:style theme/table-cell}
+    (get application :takp-account)]
+   [ui/table-row-column {:style theme/table-cell}
+    (.toLocaleString (get application :total-paid 0)) " €"]
+   (when is-admin?
+     [ui/table-row-column {:style theme/table-cell}
+    [mui/icon-button {:on-click #(on-info-clicked (:id application))}
+       [ic/action-info-outline]]])])
 
 (defn applications-table [{:keys [applications on-info-clicked is-admin?]}]
-  [va-ui/table
-   {:empty-text "Ei maksatuksia."}
-   (if is-admin? header (butlast header))
-   (map
-     #(render-application-row % (when is-admin? on-info-clicked))
-     applications)
-   ["" "" "" ""
-    [{:style {:text-align "right"}}
-     [:span
-      (.toLocaleString (reduce #(+ %1 (:budget-granted %2)) 0 applications))
-      " €"]]
-    "" "" ""
-    [{:style {:text-align "right"}}
-     [:span
-      (.toLocaleString (reduce #(+ %1 (:budget-granted %2)) 0 applications))
-      " €"]]]])
+  [:div
+   [ui/table {:fixed-header true :selectable false :body-style theme/table-body}
+    [ui/table-header {:adjust-for-checkbox false :display-select-all false}
+     [ui/table-row
+      [ui/table-header-column {:style theme/table-cell} "Tila"]
+      [ui/table-header-column {:style theme/table-cell} "Toimittajan nimi"]
+      [ui/table-header-column {:style theme/table-cell} "Hanke"]
+      [ui/table-header-column {:style theme/table-cell} "Myönnetty summa"]
+      [ui/table-header-column {:style theme/table-cell} "IBAN"]
+      [ui/table-header-column
+       {:style (merge theme/table-cell {:text-align "right"})}
+       "Pitkäviite"]
+      [ui/table-header-column {:style theme/table-cell} "LKP-tili"]
+      [ui/table-header-column {:style theme/table-cell} "TaKp-tili"]
+      [ui/table-header-column {:style theme/table-cell} "Maksettu"]
+      (when is-admin?
+        [ui/table-header-column {:style theme/table-cell} "Lisätietoja"])]]
+    [ui/table-body {:display-row-checkbox false}
+     (doall (map-indexed #(render-application %1 %2 on-info-clicked is-admin?)
+                         applications))]
+    [ui/table-footer {:adjust-for-checkbox false}
+     [ui/table-row
+      [ui/table-row-column]
+      [ui/table-row-column]
+      [ui/table-row-column {:style theme/table-cell} "Yhteensä"]
+      [ui/table-row-column {:style theme/table-cell}
+       (.toLocaleString (reduce #(+ %1 (:budget-granted %2)) 0 applications))
+       " €"]
+      [ui/table-row-column]
+      [ui/table-row-column]
+      [ui/table-row-column]
+      [ui/table-row-column]
+      [ui/table-row-column]
+      (when is-admin? [ui/table-row-column])]]]])
