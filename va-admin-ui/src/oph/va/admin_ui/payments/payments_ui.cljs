bf6e29d84e517d7677dc0d0db79d1022d7d91e76
Matti Ahinko <matti.ahinko@gmail.com>
2018-05-17 15:23:36 +0300

    Remove unused functions

--- va-admin-ui/src/oph/va/admin_ui/payments/payments_ui.cljs
+++ va-admin-ui/src/oph/va/admin_ui/payments/payments_ui.cljs
@@ -12,41 +12,6 @@
             [oph.va.admin-ui.components.ui :as va-ui]
             [clojure.string :refer [lower-case]]))
 
-(defn render-history-item [i application]
-  [ui/table-row {:key i}
-   [ui/table-row-column (to-simple-date-time (:created-at application))]
-   [ui/table-row-column (:version application)]
-   [ui/table-row-column (state-to-str (:state application))]
-   [ui/table-row-column (to-simple-date (:invoice-date application))]
-   [ui/table-row-column (to-simple-date (:due-date application))]
-   [ui/table-row-column (to-simple-date (:receipt-date application))]
-   [ui/table-row-column (:transaction-account application)]
-   [ui/table-row-column (:document-type application)]
-   [ui/table-row-column (:inspector-email application)]
-   [ui/table-row-column (:acceptor-email application)]
-   [ui/table-row-column
-    (when (:deleted application)
-      (to-simple-date-time (:deleted application)))]
-   [ui/table-row-column (:user-name application)]])
-
-(defn render-history [payments]
-  [ui/table {:fixed-header true :selectable false :height "300px"}
-   [ui/table-header {:adjust-for-checkbox false :display-select-all false}
-    [ui/table-row [ui/table-header-column "Aika"]
-     [ui/table-header-column "Versio"]
-     [ui/table-header-column "Tila"]
-     [ui/table-header-column "Laskun päivä"]
-     [ui/table-header-column "Eräpäivä"]
-     [ui/table-header-column "Tositepäivä"]
-     [ui/table-header-column "Maksuliikemenotili"]
-     [ui/table-header-column "Tositelaji"]
-     [ui/table-header-column "Tarkastaja"]
-     [ui/table-header-column "Hyväksyjä"]
-     [ui/table-header-column "Poistettu"]
-     [ui/table-header-column "Käyttäjä"]]]
-   [ui/table-body {:display-row-checkbox false}
-    (doall (map-indexed render-history-item payments))]])
-
 (defn render-payment [i payment]
   [table/table-row {:key i}
    [table/table-row-column {:style {:text-align "right"}}
