ce1ebe91859d99e726e71ac6555df4a1d6134dbd
Silvast <anne-mari.silvast@gofore.com>
2018-05-18 12:23:30 +0300

    first drafting of spec for rondo

--- va-virkailija/spec/oph/va/virkailija/rondo_service_spec.clj
+++ va-virkailija/spec/oph/va/virkailija/rondo_service_spec.clj
@@ -1,20 +1,39 @@
 (ns oph.va.virkailija.rondo-service-spec
   (:require [speclj.core
              :refer [describe it should tags]]
-            [oph.va.virkailija.rondo-service :refer [rondo-service]]))
+            [oph.va.virkailija.rondo-service :refer [rondo-service]]
+            [oph.va.virkailija.remote-file-service :refer [RemoteFileService]]))
 
 
-(defrecord TestFileService [config]
-        RemoteFileService
-                  (get-remote-file-list [_] (...))
-                  (do-sftp! [_] (...))
-                  (send-to-rondo! [_] (s...))
+(defrecord TestFileService [configuration]
+  RemoteFileService
+  (send-payment-to-rondo! [service payment-values] (send-payment! (assoc payment-values :config (:configuration service) :func do-sftp!)))
+  (get-remote-file-list [service]
+                        (let [result (do-sftp! :method :cdls
+                        :path (:remote_path_from (:configuration service))
+                        :config (:configuration service))]
+   (map #(last (strc/split % #"\s+")) (map str result))))
+  (get-local-path [service] (get (:configuration service) :local-path (System/getProperty "java.io.tmpdir")))
+  (get-remote-file [service filename]
+                   (let [xml-file-path (format "%s/%s" (get-local-file-path (:configuration service))  filename)]
+                      (do-sftp! :method :get
+                                :file xml-file-path
+                                :path (:remote_path_from (:configuration service))
+                                :config (:configuration service))))
+  (get-local-file [service filename]
+                  (format "%s/%s" (get-local-file-path (:configuration service)) filename))
+  (delete-remote-file [service filename]
+                      (do-sftp! :method :rm
+                            :file filename
+                            :path (:remote_path_from (:configuration service))
+                            :config (:configuration service))))
 
+(defn create-service [config]
+  (TestFileService. config))
 
-(tags :rondoservice)
+(def conf )
 
 (describe "Testing Rondo Service functions"
   (tags :rondoservice)
           (it "gets list of files in remote server"
-              (let [result (rget-remote-file-list config (fn [x] (str (:remote_path_from x))))]
-                (should= result (:token (first revoked-tokens))))) )
+              (let [result ) )
