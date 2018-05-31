7c237cd2170484510166571a7a6d283f03c22e16
Matti Ahinko <matti.ahinko@gmail.com>
2018-05-31 08:33:20 +0300

    Add running specs and add tags

--- va-virkailija/spec/oph/va/virkailija/invoice_spec.clj
+++ va-virkailija/spec/oph/va/virkailija/invoice_spec.clj
@@ -57,60 +57,72 @@
 
 (def response-xml (xml/sexp-as-element response-tags))
 
-(describe "Get answer value"
-          (it "gets answer value"
-              (should= "test@user.com"
-                       (invoice/get-answer-value answers "email")))
-          (it "returns nil if key not found"
-              (should= nil (invoice/get-answer-value answers "non-existing")))
-          (it "returns default if key not found"
-              (should= "default"
-                       (invoice/get-answer-value
-                         answers "non-existing" "default")))
-          (it "returns value if found altough default was given"
-              (should= "somevalue"
-                       (invoice/get-answer-value answers "key1" "default"))))
+(describe
+  "Get answer value"
+  (tags :invoice)
 
-(describe "Get batch number of payment"
-          (it "calculates batch id"
-              (should= "660017013" (invoice/get-batch-key payment)))
-          (it "returns nil if any needed value is nil"
-              (should= nil (invoice/get-batch-key nil))
-              (should= nil (invoice/get-batch-key {:some "Value"}))))
+  (it "gets answer value"
+      (should= "test@user.com"
+               (invoice/get-answer-value answers "email")))
+  (it "returns nil if key not found"
+      (should= nil (invoice/get-answer-value answers "non-existing")))
+  (it "returns default if key not found"
+      (should= "default"
+               (invoice/get-answer-value
+                 answers "non-existing" "default")))
+  (it "returns value if found altough default was given"
+      (should= "somevalue"
+               (invoice/get-answer-value answers "key1" "default"))))
 
-(describe "Get response XML element content"
-          (it "gets content of Pitkaviite"
-              (should=
-                '("1/234/2018")
-                (invoice/get-content response-xml
-                                     [:VA-invoice :Header :Pitkaviite])))
-          (it "gets content of Maksupvm"
-              (should=
-                '("2018-01-25")
-                (invoice/get-content response-xml
-                                     [:VA-invoice :Header :Maksupvm]))))
+(describe
+  "Get batch number of payment"
+  (tags :invoice)
 
-(describe "Handle response XML get value errors"
-          (it "returns nil if last not found"
-              (should=
-                nil
-                (invoice/get-content response-xml
-                                     [:VA-invoice :Header :Not-Found])))
-          (it "returns nil if first tag not found"
-              (should=
-                nil
-                (invoice/get-content response-xml
-                                     [:Not-Found :Header :Pitkaviite])))
-          (it "returns nil if xml is nil"
-              (should= nil (invoice/get-content nil [:Not-Valid :Child])))
-          (it "returns root content if keys are empty"
-              (should=
-                :VA-invoice
-                (:tag (first (invoice/get-content response-xml [])))))
-          (it "returns root content if keys are nil"
-              (should=
-                :VA-invoice
-                (:tag (first (invoice/get-content response-xml nil))))))
+  (it "calculates batch id"
+      (should= "660017013" (invoice/get-batch-key payment)))
+  (it "returns nil if any needed value is nil"
+      (should= nil (invoice/get-batch-key nil))
+      (should= nil (invoice/get-batch-key {:some "Value"}))))
+
+(describe
+  "Get response XML element content"
+  (tags :invoice)
+
+  (it "gets content of Pitkaviite"
+      (should=
+        '("1/234/2018")
+        (invoice/get-content response-xml
+                             [:VA-invoice :Header :Pitkaviite])))
+  (it "gets content of Maksupvm"
+      (should=
+        '("2018-01-25")
+        (invoice/get-content response-xml
+                             [:VA-invoice :Header :Maksupvm]))))
+
+(describe
+  "Handle response XML get value errors"
+  (tags :invoice)
+
+  (it "returns nil if last not found"
+      (should=
+        nil
+        (invoice/get-content response-xml
+                             [:VA-invoice :Header :Not-Found])))
+  (it "returns nil if first tag not found"
+      (should=
+        nil
+        (invoice/get-content response-xml
+                             [:Not-Found :Header :Pitkaviite])))
+  (it "returns nil if xml is nil"
+      (should= nil (invoice/get-content nil [:Not-Valid :Child])))
+  (it "returns root content if keys are empty"
+      (should=
+        :VA-invoice
+        (:tag (first (invoice/get-content response-xml [])))))
+  (it "returns root content if keys are nil"
+      (should=
+        :VA-invoice
+        (:tag (first (invoice/get-content response-xml nil))))))
 
 (describe
   "Invoice generate"
@@ -170,46 +182,46 @@
                   (:id grant)))]
 
           (should=
-          [:VA-invoice
-           [:Header
-            [:Maksuera (format "6600170%02d" (:batch-number batch))]
-            [:Laskunpaiva "2017-12-20"]
-            [:Erapvm "2017-12-27"]
-            [:Bruttosumma 20000]
-            [:Maksuehto "Z001"]
-            [:Pitkaviite "123/456/78"]
-            [:Tositepvm "2017-12-20"]
-            [:Asiatarkastaja "inspector@example.com"]
-            [:Hyvaksyja "acceptor@example.com"]
-            [:Tositelaji "XA"]
-            [:Toimittaja
-             [:Y-tunnus "1234567-1"]
-             [:Nimi "Test Organisation"]
-             [:Postiosoite "Someroad 1"]
-             [:Paikkakunta "Some City"]
-             [:Maa "Some Country"]
-             [:Iban-tili "FI4250001510000023"]
-             [:Pankkiavain "OKOYFIHH"]
-             [:Pankki-maa "FI"]
-             [:Kieli "fi"]
-             [:Valuutta "EUR"]]
-            [:Postings
-             [:Posting
-              [:Summa 20000]
-              [:LKP-tili "82300000"]
-              [:TaKp-tili "29103013"]
-              [:Toimintayksikko "6600100130"]
-              [:Projekti "6600A-M2024"]
-              [:Toiminto "6600151502"]
-              [:Kumppani "None"]]]]]
-          (invoice/payment-to-invoice
-            {:payment payment
-             :application application-with-evaluation
-             :grant (assoc grant
-                           :project "6600A-M2024"
-                           :operational-unit "6600100130"
-                           :operation "6600151502"
-                           :lkp-account "82500000")
-             :batch batch})))
+            [:VA-invoice
+             [:Header
+              [:Maksuera (format "6600170%02d" (:batch-number batch))]
+              [:Laskunpaiva "2017-12-20"]
+              [:Erapvm "2017-12-27"]
+              [:Bruttosumma 20000]
+              [:Maksuehto "Z001"]
+              [:Pitkaviite "123/456/78"]
+              [:Tositepvm "2017-12-20"]
+              [:Asiatarkastaja "inspector@example.com"]
+              [:Hyvaksyja "acceptor@example.com"]
+              [:Tositelaji "XA"]
+              [:Toimittaja
+               [:Y-tunnus "1234567-1"]
+               [:Nimi "Test Organisation"]
+               [:Postiosoite "Someroad 1"]
+               [:Paikkakunta "Some City"]
+               [:Maa "Some Country"]
+               [:Iban-tili "FI4250001510000023"]
+               [:Pankkiavain "OKOYFIHH"]
+               [:Pankki-maa "FI"]
+               [:Kieli "fi"]
+               [:Valuutta "EUR"]]
+              [:Postings
+               [:Posting
+                [:Summa 20000]
+                [:LKP-tili "82300000"]
+                [:TaKp-tili "29103013"]
+                [:Toimintayksikko "6600100130"]
+                [:Projekti "6600A-M2024"]
+                [:Toiminto "6600151502"]
+                [:Kumppani "None"]]]]]
+            (invoice/payment-to-invoice
+              {:payment payment
+               :application application-with-evaluation
+               :grant (assoc grant
+                             :project "6600A-M2024"
+                             :operational-unit "6600100130"
+                             :operation "6600151502"
+                             :lkp-account "82500000")
+               :batch batch}))))))
 
-        )))
+(run-specs)
