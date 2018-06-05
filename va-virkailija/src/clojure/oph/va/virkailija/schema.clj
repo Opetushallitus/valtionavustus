c1329061a8a0b95ab87f0142a8081749f7c3b398
Matti Ahinko <matti.ahinko@gmail.com>
2018-06-05 13:32:27 +0300

    Remove unused fields

--- va-virkailija/src/clojure/oph/va/virkailija/schema.clj
+++ va-virkailija/src/clojure/oph/va/virkailija/schema.clj
@@ -209,17 +209,6 @@
    :application-id  s/Int
    (s/optional-key :application-version) s/Int
    :state s/Int
-   (s/optional-key :batch-number) (s/maybe s/Int)
-   (s/optional-key :document-type) (s/maybe s/Str)
-   (s/optional-key :invoice-date) (s/maybe LocalDate)
-   (s/optional-key :due-date) (s/maybe LocalDate)
-   (s/optional-key :receipt-date) (s/maybe LocalDate)
-   (s/optional-key :transaction-account) (s/maybe s/Str)
-   (s/optional-key :currency) (s/maybe s/Str)
-   (s/optional-key :payment-term) (s/maybe s/Str)
-   (s/optional-key :partner) (s/maybe s/Str)
-   (s/optional-key :inspector-email) (s/maybe s/Str)
-   (s/optional-key :acceptor-email) (s/maybe s/Str)
    (s/optional-key :filename) (s/maybe s/Str)
    (s/optional-key :user-name) s/Str
    (s/optional-key :batch-id) (s/maybe s/Int)
