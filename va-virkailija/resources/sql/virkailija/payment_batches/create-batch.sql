bcaa106a10f7474fd2b7283f7da7c35aa8d994a1
Matti Ahinko <matti.ahinko@gmail.com>
2018-06-05 15:13:37 +0300

    Add missing comma and clean indent

--- va-virkailija/resources/sql/virkailija/payment_batches/create-batch.sql
+++ va-virkailija/resources/sql/virkailija/payment_batches/create-batch.sql
@@ -4,12 +4,10 @@ INSERT INTO
   document_id)
 VALUES
   ((SELECT GREATEST(MAX(batch_number), 0) + 1
-   FROM virkailija.payment_batches
-   WHERE date_part('year', created_at) = date_part('year', CURRENT_DATE)),
-  :invoice_date, :due_date, :receipt_date
-  :currency, :partner, :inspector_email, :acceptor_email, :grant_id,
-  :document_id)
+     FROM virkailija.payment_batches
+     WHERE date_part('year', created_at) = date_part('year', CURRENT_DATE)),
+     :invoice_date, :due_date, :receipt_date, :currency, :partner,
+     :inspector_email, :acceptor_email, :grant_id, :document_id)
 RETURNING
-  id, batch_number, invoice_date,
-  due_date, receipt_date, currency, partner,
+  id, batch_number, invoice_date, due_date, receipt_date, currency, partner,
   inspector_email, acceptor_email, grant_id, document_id
