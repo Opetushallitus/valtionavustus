0ad59c927596bf5ece72983ad3a8951dd87459d6
Matti Ahinko <matti.ahinko@gmail.com>
2018-06-06 16:24:11 +0300

    Remove phase

--- va-virkailija/resources/sql/virkailija/payment_batches/create-batch.sql
+++ va-virkailija/resources/sql/virkailija/payment_batches/create-batch.sql
@@ -1,13 +1,12 @@
 INSERT INTO
   virkailija.payment_batches (batch_number, invoice_date, due_date,
-  receipt_date, currency, partner, inspector_email, acceptor_email, grant_id,
-  phase)
+  receipt_date, currency, partner, inspector_email, acceptor_email, grant_id)
 VALUES
   ((SELECT GREATEST(MAX(batch_number), 0) + 1
      FROM virkailija.payment_batches
      WHERE date_part('year', created_at) = date_part('year', CURRENT_DATE)),
      :invoice_date, :due_date, :receipt_date, :currency, :partner,
-     :inspector_email, :acceptor_email, :grant_id, :phase)
+     :inspector_email, :acceptor_email, :grant_id)
 RETURNING
   id, batch_number, invoice_date, due_date, receipt_date, currency, partner,
-  inspector_email, acceptor_email, grant_id, phase
+  inspector_email, acceptor_email, grant_id
--- va-virkailija/resources/sql/virkailija/payment_batches/find-batches.sql
+++ va-virkailija/resources/sql/virkailija/payment_batches/find-batches.sql
@@ -1,6 +1,6 @@
 SELECT
   id, created_at, batch_number, invoice_date, due_date, receipt_date, currency,
-  partner, inspector_email, acceptor_email, grant_id, phase
+  partner, inspector_email, acceptor_email, grant_id
 FROM
   virkailija.payment_batches
 WHERE
