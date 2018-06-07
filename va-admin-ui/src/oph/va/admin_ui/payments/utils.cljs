4b0162b6a3bbf1dbb8950499dbbeeb12f7bab940
Matti Ahinko <matti.ahinko@gmail.com>
2018-06-07 14:53:33 +0300

    Add iso8601 functions

--- va-admin-ui/src/oph/va/admin_ui/payments/utils.cljs
+++ va-admin-ui/src/oph/va/admin_ui/payments/utils.cljs
@@ -13,7 +13,12 @@
 
 (def today-end (t/today-at 23 59 59))
 
+(def iso8601-formatter (:basic-date-time tf/formatters))
+
 (defn now [] (t/now))
+
+(defn to-iso-str [d]
+  (tf/unparse iso8601-formatter d))
 
 (defn- to-date [d f]
   (if (empty? d)
