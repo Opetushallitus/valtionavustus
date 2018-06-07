b6b29e8b3377c401d3ee8b3bccd24c4b13d8eaab
Matti Ahinko <matti.ahinko@gmail.com>
2018-06-07 13:26:20 +0300

    Move emails to document

--- va-virkailija/resources/sql/virkailija/payment_batches/create-batch.sql
+++ va-virkailija/resources/sql/virkailija/payment_batches/create-batch.sql
@@ -1,12 +1,11 @@
 INSERT INTO
   virkailija.payment_batches (batch_number, invoice_date, due_date,
-  receipt_date, currency, partner, inspector_email, acceptor_email, grant_id)
+  receipt_date, currency, partner, grant_id)
 VALUES
   ((SELECT GREATEST(MAX(batch_number), 0) + 1
      FROM virkailija.payment_batches
      WHERE date_part('year', created_at) = date_part('year', CURRENT_DATE)),
-     :invoice_date, :due_date, :receipt_date, :currency, :partner,
-     :inspector_email, :acceptor_email, :grant_id)
+     :invoice_date, :due_date, :receipt_date, :currency, :partner, :grant_id)
 RETURNING
   id, batch_number, invoice_date, due_date, receipt_date, currency, partner,
-  inspector_email, acceptor_email, grant_id
+  grant_id
--- va-virkailija/resources/sql/virkailija/payment_batches/get-batch-documents.sql
+++ va-virkailija/resources/sql/virkailija/payment_batches/get-batch-documents.sql
@@ -1,5 +1,5 @@
 SELECT
-  id, created_at, document_id, phase
+  id, created_at, document_id, phase, inspector_email, acceptor_email
 FROM
   virkailija.batch_documents
 WHERE
