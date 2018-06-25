c394f343cd2be8fd708cd43b818066270657e744
Matti Ahinko <matti.ahinko@gmail.com>
2018-06-25 14:35:55 +0300

    Move healthcheck to own file

---
+++ va-virkailija/src/clojure/oph/va/virkailija/healthcheck.clj
@@ -0,0 +1,20 @@
+(ns [oph.va.virkailija.healthcheck]
+  (:require [oph.va.virkailija.remote-file-service
+             :refer [get-remote-file-list]]
+            [oph.va.virkailija.rondo-service :as rondo-service]
+            [oph.va.virkailija.utils
+             :refer [with-timeout]]))
+
+(defonce ^:private status (atom []))
+
+(defn check-rondo-status []
+  (let [rondo-service (rondo-service/create-service
+                        (get-in config [:server :rondo-sftp]))
+        result (with-timeout
+            #(try
+               (get-remote-file-list rondo-service)
+               {:success true :error ""}
+               (catch Exception e
+                 {:success false :error (.getMessage e)}))
+            (get-in config [:server :healthcheck-timeout] 5000)
+            {:success false :error "Timeout"})]))
--- va-virkailija/src/clojure/oph/va/virkailija/routes.clj
+++ va-virkailija/src/clojure/oph/va/virkailija/routes.clj
@@ -42,11 +42,7 @@
             [oph.va.virkailija.va-code-values-routes
              :as va-code-values-routes]
             [oph.va.virkailija.payments-routes :as payments-routes]
-            [oph.va.virkailija.remote-file-service
-             :refer [get-remote-file-list]]
-            [oph.va.virkailija.rondo-service :as rondo-service]
-            [oph.va.virkailija.utils
-             :refer [with-timeout]])
+            [oph.va.virkailija.healthcheck :as healthcheck])
   (:import [java.io ByteArrayInputStream]))
 
 (def opintopolku-login-url
@@ -67,18 +63,8 @@
     (ok {})
     (not-found)))
 
-(defn- on-rondo-healthcheck []
-  (let [rondo-service (rondo-service/create-service
-                        (get-in config [:server :rondo-sftp]))]
-  (ok
-    (with-timeout
-      #(try
-         (get-remote-file-list rondo-service)
-         {:success true :error ""}
-         (catch Exception e
-           {:success false :error (.getMessage e)}))
-      (get-in config [:server :healthcheck-timeout] 5000)
-      {:success false :error "Timeout"}))))
+(defn- on-integration-healthcheck []
+  (ok healthcheck/list-status))
 
 (defn- without-id [x]
   (dissoc x :id))
