b9bf7e7bc066b01972503f50ab2d5c5669812d86
Matti Ahinko <matti.ahinko@gmail.com>
2018-06-27 10:59:35 +0300

    Add date filters

--- va-admin-ui/src/oph/va/admin_ui/payments/grants_ui.cljs
+++ va-admin-ui/src/oph/va/admin_ui/payments/grants_ui.cljs
@@ -3,11 +3,12 @@
             [oph.va.admin-ui.components.table :as table]
             [oph.va.admin-ui.theme :as theme]
             [oph.va.admin-ui.payments.utils
-             :refer [date-time-formatter sort-column!
-                     update-filters! sort-rows filter-rows]]
+             :refer [date-time-formatter sort-column! to-lower-str sort-rows
+                     update-filters!]]
             [reagent.core :as r]
             [clojure.string :refer [lower-case]]
-            [cljs-time.format :as f]))
+            [cljs-time.format :as f]
+            [cljs-time.core :as t]))
 
 (defn- grant-row [grant selected on-select]
   [table/table-row {:key (:id grant)
@@ -20,6 +21,27 @@
    [table/table-row-column (:status grant)]
    [table/table-row-column (f/unparse-local date-time-formatter (:start grant))]
    [table/table-row-column (f/unparse-local date-time-formatter (:end grant))]])
+
+(defn- same-day? [d1 d2]
+  (and (= (t/year d1) (t/year d2))
+       (= (t/month d1) (t/month d2))
+       (= (t/day d1) (t/day d2))))
+
+(defn- row-matches-key? [row filters]
+  (every?
+    (fn [[k v]]
+      (if (or (= k :start) (= k :end))
+        (same-day? v (get row k))
+        (> (.indexOf (to-lower-str (get row k)) v) -1)))
+    filters))
+
+(defn filter-rows [rows filters]
+  (filter #(row-matches-key? % filters) rows))
+
+(defn update-date-filters! [filters k v]
+  (if (nil? v)
+    (swap! filters dissoc k)
+    (swap! filters assoc k v)))
 
 (defn grants-table [props]
   (let [sort-params (r/atom {:sort-key nil :descend? true})
@@ -58,15 +80,17 @@
            [table/sortable-header-column
             {:title "Haku alkaa"
              :column-key :start
+             :field-type :date
              :sort-params @sort-params
              :on-sort #(sort-column! sort-params %)
-             :on-filter #(update-filters! filters %1 %2)}]
+             :on-filter #(update-date-filters! filters %1 (f/parse %2))}]
            [table/sortable-header-column
             {:title "Haku päättyy"
              :column-key :end
+             :field-type :date
              :sort-params @sort-params
              :on-sort #(sort-column! sort-params %)
-             :on-filter #(update-filters! filters %1 %2)}]]]
+             :on-filter #(update-date-filters! filters %1 (f/parse %2))}]]]
          [table/table-body
           (for [grant filtered-sorted-grants]
             (grant-row grant (= value (:id grant)) on-change))]]))))
