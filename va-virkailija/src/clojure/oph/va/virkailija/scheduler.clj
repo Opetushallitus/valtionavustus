3d464ed9eaad2c5f550802a18e4c90435f23f40b
Matti Ahinko <matti.ahinko@gmail.com>
2018-06-28 19:44:14 +0300

    Add light weight scheduler

---
+++ va-virkailija/src/clojure/oph/va/virkailija/scheduler.clj
@@ -0,0 +1,31 @@
+(ns oph.va.virkailija.scheduler
+  (:require [clojure.core.async
+             :refer [timeout go-loop chan poll! <! put! close!]]))
+
+(def units
+  {:ms 1
+   :second 1000
+   :minute 60000
+   :hour 3600000})
+
+(defn stop [c]
+  (put! c :stop)
+  (close! c))
+
+(defn to-ms [time unit]
+  (if-let [multiplier (get units unit)]
+    (* time multiplier)
+    (throw (.Exception "Unit not allowed"))))
+
+(defn after-ms [ms f & args]
+  (let [c (chan)]
+    (go-loop []
+      (<! (timeout ms))
+      (when-not (poll! c)
+        (do
+          (apply f args)
+          (recur))))
+    c))
+
+(defn after [time unit f & args]
+  (apply after-ms (to-ms time unit) f args))
