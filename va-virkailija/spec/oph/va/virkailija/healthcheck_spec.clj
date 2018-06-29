17b365f1e22b55dcf75ef2300a6ae73f8a05db9a
Matti Ahinko <matti.ahinko@gmail.com>
2018-06-29 13:39:20 +0300

    Fix conflicts

--- va-virkailija/spec/oph/va/virkailija/healthcheck_spec.clj
+++ va-virkailija/spec/oph/va/virkailija/healthcheck_spec.clj
@@ -1,10 +1,6 @@
 (ns oph.va.virkailija.healthcheck-spec
   (:require [speclj.core
-<<<<<<< HEAD
             :refer [should-throw should should-not describe
-=======
-            :refer [should-throw should describe
->>>>>>> 860f9eb6b735c7589fa0ee9444112cbc94bbf606
                     it tags run-specs]]
             [oph.va.virkailija.healthcheck :as h]
             [clj-time.core :as t]))
@@ -15,13 +11,9 @@
   (tags :healthcheck)
 
   (it "checks validate-checks function"
-<<<<<<< HEAD
       (should-not
         (:valid (first (h/validate-checks
                          [{:timestamp (t/minus (t/now) (t/minutes 20))}] 15))))
-=======
-      (prn (h/validate-checks [{:timestamp (t/now)}] 15))
->>>>>>> 860f9eb6b735c7589fa0ee9444112cbc94bbf606
       (should
         (:valid (first (h/validate-checks [{:timestamp (t/now)}] 15))))))
 
