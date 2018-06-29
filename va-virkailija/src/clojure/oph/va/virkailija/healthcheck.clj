762fd3570d3599d2954f2cb8a7b883ca481faf92
Matti Ahinko <matti.ahinko@gmail.com>
2018-06-29 10:13:01 +0300

    Replace quartz with simple shceduler

--- va-virkailija/src/clojure/oph/va/virkailija/healthcheck.clj
+++ va-virkailija/src/clojure/oph/va/virkailija/healthcheck.clj
@@ -6,13 +6,11 @@
             [oph.va.virkailija.utils :refer [with-timeout]]
             [clj-time.core :as time]
             [clj-time.format :as f]
-            [clojurewerkz.quartzite.scheduler :as qs]
-            [clojurewerkz.quartzite.triggers :as t]
-            [clojurewerkz.quartzite.jobs :refer [defjob] :as j]
-            [clojurewerkz.quartzite.schedule.cron
-             :refer [schedule cron-schedule]]))
+            [oph.va.virkailija.scheduler :as s]))
 
 (defonce ^:private status (atom []))
+
+(defonce ^:private scheduler (atom nil))
 
 (defn get-last-status []
   @status)
@@ -36,24 +34,16 @@
   (reset! status
           [(check-rondo-status)]))
 
-(defjob HealthCheckJob [ctx]
-  (update-status!))
 
 (defn start-schedule-status-update! []
-  (let [s (qs/start (qs/initialize))
-        job (j/build
-              (j/of-type HealthCheckJob)
-              (j/with-identity (j/key "jobs.HealthCheckJob")))
-        trigger
-        (t/build
-          (t/with-identity (t/key "triggers.HealthCheck"))
-          (t/start-now)
-          (t/with-schedule
-            (schedule
-              (cron-schedule
-                (get-in config [:integration-healthcheck :cron])))))]
-    (qs/schedule s job trigger)))
+  (when (nil? @scheduler)
+    (reset! scheduler
+            (s/after
+              (get-in config [:integration-healthcheck :interval-minutes])
+              :minute
+              update-status!))))
 
 (defn stop-schedule-status-update! []
-  (qs/delete-trigger
-    (qs/start (qs/initialize)) (t/key "triggers.HealthCheck")))
+  (when (some? @scheduler)
+    (s/stop @scheduler)
+    (reset! scheduler nil)))
