1ee0c7101035f5b9c1b5638faaff61df46c7b112
Matti Ahinko <matti.ahinko@gmail.com>
2018-06-29 12:40:02 +0300

    Add check validation and current timestamp

--- va-virkailija/src/clojure/oph/va/virkailija/healthcheck.clj
+++ va-virkailija/src/clojure/oph/va/virkailija/healthcheck.clj
@@ -4,7 +4,7 @@
              :refer [get-remote-file-list]]
             [oph.va.virkailija.rondo-service :as rondo-service]
             [oph.va.virkailija.utils :refer [with-timeout]]
-            [clj-time.core :as time]
+            [clj-time.core :as t]
             [clj-time.format :as f]
             [oph.va.virkailija.scheduler :as s]))
 
@@ -12,8 +12,32 @@
 
 (defonce ^:private scheduler (atom nil))
 
+(defn is-expired? [check limit]
+  (t/before? (:timestamp check) limit))
+
+(defn validate-checks [coll limit-minutes]
+   (let [limit
+         (t/minus
+           (t/now)
+           (t/minutes limit-minutes))]
+     (map #(assoc % :valid (not (is-expired? % limit))) coll)))
+
+(defn set-timestamps [coll]
+  (map
+    #(assoc %
+            :timestamp
+            (f/unparse (:basic-date-time f/formatters) (:timestamp %)))
+    coll))
+
 (defn get-last-status []
-  @status)
+  {:integrations (-> @status
+                     (validate-checks
+                       (+
+                         (get-in
+                           config [:integration-healthcheck :interval-minutes])
+                         5))
+                     set-timestamps)
+   :current-timestamp (f/unparse (:basic-date-time f/formatters) (t/now))})
 
 (defn check-rondo-status []
   (let [rondo-service (rondo-service/create-service
@@ -28,7 +52,7 @@
                  {:success false :error "Timeout"})]
     (assoc result
            :service "rondo"
-           :timestamp (f/unparse (:basic-date-time f/formatters) (time/now)))))
+           :timestamp (t/now))))
 
 (defn update-status! []
   (reset! status
