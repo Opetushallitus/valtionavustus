2e1ac43da08e4f08035abe792f00c75057590aad
Anne-Mari Silvast <anne-mari.silvast@gofore.com>
2018-07-04 18:40:23 +0300

    started using form-encode for generating url

--- va-common/src/oph/common/email_utils.clj
+++ va-common/src/oph/common/email_utils.clj
@@ -1,8 +1,9 @@
-(ns oph.common.email-utils)
+(ns oph.common.email-utils
+  (:require [ring.util.codec :refer [form-encode]]))
 
 (defn refuse-url [va-url avustushaku-id user-key lang token]
-   (let [lang-str (or (clojure.core/name lang) "fi")]
-     (str va-url "avustushaku/" avustushaku-id "/nayta?avustushaku=" avustushaku-id
-          "&hakemus=" user-key "&lang=" lang-str "&preview=true&token=" token "&refuse-grant=true")))
+   (let [lang-str (or (clojure.core/name lang) "fi")
+        url-parameters  (form-encode {:hakemus user-key :lang lang-str :preview true :token token :refuse-grant true})]
+     (str va-url "avustushaku/" avustushaku-id "/nayta?avustushaku=" avustushaku-id url-parameters)))
 
           
