4e58d3dca920dc65969b0b8688947c45394b118c
Anne-Mari Silvast <anne-mari.silvast@gofore.com>
2018-07-11 10:01:50 +0300

    corrected the use of preview=true in url in refuse-url and modify-url

--- va-common/src/oph/common/email_utils.clj
+++ va-common/src/oph/common/email_utils.clj
@@ -5,7 +5,8 @@
   (let [lang-str (or (clojure.core/name lang) "fi")
         refuse (= type "refuse") 
         modify (= type "modify")
-        url-parameters  (form-encode {:avustushaku avustushaku-id :hakemus user-key :lang lang-str :preview true :token token :refuse-grant refuse :modify-application modify})]
+        preview (= type "refuse")
+        url-parameters  (form-encode {:avustushaku avustushaku-id :hakemus user-key :lang lang-str :preview preview :token token :refuse-grant refuse :modify-application modify})]
 (str va-url "avustushaku/" avustushaku-id "/nayta?" url-parameters)))
 
  (defn modify-url [va-url avustushaku-id user-key lang token]
