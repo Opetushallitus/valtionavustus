ce7472517612be4ec359b0745d6f9056e271a60e
Matti Ahinko <matti.ahinko@gmail.com>
2018-08-21 10:02:01 +0300

    Add getting batch documents

--- va-virkailija/src/clojure/oph/va/virkailija/payment_batches_routes.clj
+++ va-virkailija/src/clojure/oph/va/virkailija/payment_batches_routes.clj
@@ -56,7 +56,9 @@
     :return schema/PaymentsCreateResult
     :summary "Create new payments for unpaid applications of grant. Payments
               will be sent to Rondo and stored to database."
-    (let [batch (data/get-batch id)
+    (let [batch (assoc
+                  (data/get-batch id)
+                  :documents (data/get-batch-documents id))
           c (data/send-payments
               {:batch batch
                :grant (grant-data/get-grant (:grant-id batch))
