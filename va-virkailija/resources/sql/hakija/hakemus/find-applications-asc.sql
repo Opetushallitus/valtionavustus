37cccd67cfc4c6aa50e13ba438d11e056c175459
Matti Ahinko <matti.ahinko@gmail.com>
2018-09-06 10:01:27 +0300

    Add parent id

--- va-virkailija/resources/sql/hakija/hakemus/find-applications-asc.sql
+++ va-virkailija/resources/sql/hakija/hakemus/find-applications-asc.sql
@@ -1,8 +1,8 @@
 SELECT
   h.id, h.created_at, h.version, h.budget_total, h.budget_oph_share,
-  h.organization_name, h.project_name, h.register_number, h.language,
-  h.avustushaku AS grant_id, h.refused, h.refused_comment, h.refused_at,
-  a.content#>'{name, fi}' AS grant_name
+  h.organization_name, h.project_name, h.register_number, h.parent_id,
+  h.language, h.avustushaku AS grant_id, h.refused, h.refused_comment,
+  h.refused_at, a.content#>'{name, fi}' AS grant_name
 FROM
   hakija.hakemukset h
 LEFT JOIN
