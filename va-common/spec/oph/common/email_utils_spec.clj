0953049e312467828f8226806023198ce5cd31df
Silvast <anne-mari.silvast@gofore.com>
2018-10-08 16:43:42 +0300

    fixed tests

--- va-common/spec/oph/common/email_utils_spec.clj
+++ va-common/spec/oph/common/email_utils_spec.clj
@@ -5,7 +5,7 @@
 (describe "Refuse URL"
           (it "generates proper refuse url"
               (should=
-                "http://exampleurl.local/avustushaku/4/nayta?avustushaku=4hakemus=1234567890&lang=fi&preview=true&token=abcde1234567890&refuse-grant=true&modify-application=false"
+                "http://exampleurl.local/avustushaku/4/nayta?avustushaku=4&hakemus=1234567890&lang=fi&preview=true&token=abcde1234567890&refuse-grant=true&modify-application=false"
                 (email-utils/refuse-url "http://exampleurl.local/" 4 "1234567890" :fi "abcde1234567890"))))
 
 (run-specs)
