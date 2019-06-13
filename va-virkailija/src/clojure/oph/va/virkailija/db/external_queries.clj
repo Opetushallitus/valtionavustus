95a4249b04cfaeb19455acc6c449e758e2d8663e
Timo Suomela <timo.suomela@reaktor.com>
2019-06-13 16:08:41 +0300

    Look for sql file in correct place

--- va-virkailija/src/clojure/oph/va/virkailija/db/external_queries.clj
+++ va-virkailija/src/clojure/oph/va/virkailija/db/external_queries.clj
@@ -1,4 +1,4 @@
 (ns oph.va.virkailija.db.external-queries
   (:require [yesql.core :refer [defquery]]))
 
-(defquery get-grants-for-year "sql/virkailija/grants/get-grants-for-year.sql")
+(defquery get-grants-for-year "sql/virkailija/external/get-grants-for-year.sql")
