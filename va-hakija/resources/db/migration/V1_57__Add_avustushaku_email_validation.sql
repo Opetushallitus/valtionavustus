7e2951d47b1081b023bb5dfd22fe0e89c93b5b48
Antero Fagerstedt <antero.fagerstedt@reaktor.com>
2019-10-11 19:21:17 +0300

    Tietokanta validoi email-kentän

---
+++ va-hakija/resources/db/migration/V1_57__Add_avustushaku_email_validation.sql
@@ -0,0 +1,5 @@
+CREATE EXTENSION IF NOT EXISTS citext;
+CREATE DOMAIN email AS citext
+  CHECK ( value ~ '^[a-zA-Z0-9.!#$%&''*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$' );
+
+ALTER TABLE avustushaku_roles ALTER COLUMN email TYPE email;
