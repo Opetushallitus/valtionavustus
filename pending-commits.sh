b2717be89a22bb369f8a40252acdbf0d0c336297
Henry Heikkinen <henry.heikkinen@reaktor.com>
2019-10-11 15:19:32 +0300

    Add script to which commits are waiting for deployment

---
+++ pending-commits.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -o errexit -o nounset -o pipefail
+
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+git fetch --tags --force > /dev/null
+
+function log_cmd {
+  local fmt="%C(bold blue)%h%C(reset) %C(green)(%cI)%C(reset) %s %C(cyan)<%an>%C(reset)"
+  git --no-pager log --pretty=format:"$fmt" --color --left-only "$@"
+}
+
+function print_pending_commits {
+  echo "# Commits for qa -> prod"
+  log_cmd green-qa...green-prod && echo
+
+  echo "# Commits for origin/master -> qa"
+  log_cmd origin/master...green-qa && echo
+
+  echo "# Commits for master -> origin/master"
+  log_cmd master...origin/master && echo
+}
+
+print_pending_commits "$@" | less --no-init --quit-if-one-screen --RAW-CONTROL-CHARS
