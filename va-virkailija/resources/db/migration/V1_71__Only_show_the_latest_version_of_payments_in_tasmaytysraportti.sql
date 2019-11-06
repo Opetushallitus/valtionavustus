69b34c24828daf1a265354736f10c4a0e2fc3e16
Timo Suomela <timo.suomela@reaktor.com>
2019-11-06 15:47:43 +0200

    VA-27 Only include the latest version of a payment in the täsmäytysraportti

---
+++ va-virkailija/resources/db/migration/V1_71__Only_show_the_latest_version_of_payments_in_tasmaytysraportti.sql
@@ -0,0 +1,41 @@
+DROP VIEW v_tasmaytysraportti_data;
+CREATE OR REPLACE VIEW v_tasmaytysraportti_data AS
+SELECT
+  h.avustushaku AS avustushaku
+  ,h.id AS hakemus
+  ,v.code_value AS toimintayksikko
+  ,h.organization_name AS toimittajan_nimi
+  ,iban.value AS pankkitili
+  ,p.payment_sum AS bruttosumma
+  ,h.register_number AS pitka_viite
+  ,t.account AS lkp_tili
+  ,ar.talousarviotili AS takp_tili
+  ,d.presenter_email AS asiatarkastaja
+  ,d.acceptor_email AS hyvaksyja
+  ,p.sent_to_maksatuspalvelu_at AS lahetetty_maksatuspalveluun
+  ,v.code AS toimintayksikko_koodi
+FROM virkailija.payments p
+JOIN hakija.hakemukset h ON h.id = p.application_id
+JOIN virkailija.arviot ar ON ar.hakemus_id = h.id
+JOIN hakija.avustushaut a ON a.id = h.avustushaku
+JOIN virkailija.va_code_values v ON v.id = a.operational_unit_id
+JOIN virkailija.payment_batches b ON b.id = p.batch_id
+JOIN virkailija.batch_documents d ON d.batch_id = b.id
+LEFT JOIN (
+  SELECT id, version, elem->>'value' as value
+  FROM hakija.form_submissions f, jsonb_array_elements(answers->'value') elem
+  WHERE f.version_closed IS NULL
+  AND elem->>'key' = 'bank-iban'
+) AS iban ON (h.form_submission_id = iban.id AND h.form_submission_version = iban.version)
+LEFT JOIN (
+  SELECT id, version, elem->>'value' as value
+  FROM hakija.form_submissions f, jsonb_array_elements(answers->'value') elem
+  WHERE f.version_closed IS NULL
+  AND elem->>'key' = 'radioButton-0'
+) AS lkp ON (h.form_submission_id = lkp.id AND h.form_submission_version = lkp.version)
+JOIN virkailija.lkp_tilit t ON t.id = lkp.value
+AND p.deleted IS NULL
+AND p.version_closed IS NULL
+AND h.version_closed IS NULL
+AND v.value_type = 'operational-unit'
+ORDER BY h.avustushaku DESC, h.id DESC;
