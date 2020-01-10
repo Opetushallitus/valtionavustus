97e605909bce89497c4fae19759ce2b21a40ac45
Antero Fagerstedt <antero.fagerstedt@reaktor.com>
2020-01-11 01:51:33 +0200

    Always wait for ports before running ui tests

--- run_ui_test.sh
+++ run_ui_test.sh
@@ -4,12 +4,48 @@ npm_version="6.13.4"
 set -o errexit -o nounset -o pipefail
 repo="$( cd "$( dirname "$0" )" && pwd )"
 
+HAKIJA_HOSTNAME=${HAKIJA_HOSTNAME:-"localhost"}
+VIRKAILIJA_HOSTNAME=${VIRKAILIJA_HOSTNAME:-"localhost"}
+
+function check_requirements {
+  if ! [[ -x "$(command -v curl)" ]]; then
+    echo "Curl is required, cannot continue"
+    exit 1
+  fi
+}
+
+function waitport {
+  i=0
+  HOSTNAME=$1
+  PORT=$2
+  MAX_WAIT=${3:-100}
+
+  while ! curl -s "http://${HOSTNAME}:${PORT}/" > /dev/null; do
+    echo "Waiting for port ${PORT} to respond on host ${HOSTNAME}, ${i}/${MAX_WAIT}"
+
+    if [[ $i -gt ${MAX_WAIT} ]]; then
+      echo "Port not responding and max time exceeded, exiting..."
+      exit 1
+    fi
+
+    sleep 1
+    ((i=i+1))
+  done
+
+  echo "Port ${PORT} is responding on host ${HOSTNAME}"
+}
+
 function main {
+  check_requirements
   init_nodejs
 
   export ADBLOCK=1
   cd "$repo"
   npm install
+
+  waitport ${HAKIJA_HOSTNAME} 8080 150
+  waitport ${VIRKAILIJA_HOSTNAME} 8081 150
+
   npx mocha "test/**/*Spec.js"
 }
 
--- run_ui_test_against_container.sh
+++ run_ui_test_against_container.sh
@@ -1,36 +1,8 @@
 #!/usr/bin/env bash
 set -o errexit -o nounset -o pipefail
 
-function check_requirements {
-  if ! [[ -x "$(command -v curl)" ]]; then
-    echo "Curl is required, cannot continue"
-    exit 1
-  fi
-}
-
-function waitport {
-  i=0
-  PORT=$1
-  MAX_WAIT=${2:-10}
-
-  while ! curl -s "http://localhost:${PORT}/" > /dev/null; do
-    echo "Waiting for port ${PORT} to respond, ${i}/${MAX_WAIT}"
-
-    if [[ $i -gt ${MAX_WAIT} ]]; then
-      echo "Port not responding and max time exceeded, exiting..."
-      exit 1
-    fi
-
-    sleep 1
-    ((i=i+1))
-  done
-
-  echo "Port ${PORT} is responding"
-}
-
 function main {
 
-  check_requirements
   VA_SECRET_CONF_FILE=${VA_SECRET_CONF_FILE:-"../valtionavustus-secret/config/secret-dev.edn"}
 
   cp ${VA_SECRET_CONF_FILE} ./secret-dev.edn
