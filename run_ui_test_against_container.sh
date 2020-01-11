01c4bd5074b0a5d44731d27720828896858629fd
Antero Fagerstedt <antero.fagerstedt@reaktor.com>
2020-01-11 12:46:28 +0200

    Don't pass secrets to container

--- run_ui_test_against_container.sh
+++ run_ui_test_against_container.sh
@@ -3,24 +3,13 @@ set -o errexit -o nounset -o pipefail
 
 function main {
 
-  VA_SECRET_CONF_FILE=${VA_SECRET_CONF_FILE:-"../valtionavustus-secret/config/secret-dev.edn"}
-
-  cp ${VA_SECRET_CONF_FILE} ./secret-dev.edn
   docker build -t "va-virkailija:latest" -f ./Dockerfile.virkailija ./
-  rm ./secret-dev.edn
-
   docker build -t "va-hakija:latest" -f ./Dockerfile.hakija ./
   docker build -t "va-ui-test:latest" -f ./Dockerfile.ui_test ./
 
   trap 'docker-compose -f ./docker-compose-ui-test.yml down' EXIT
 
   docker-compose -f ./docker-compose-ui-test.yml up --abort-on-container-exit
-
-  #waitport 8080 200
-  #waitport 8081 200
-
-  #export HEADLESS=true
-  #./run_ui_test.sh
 }
 
 main "$@"
