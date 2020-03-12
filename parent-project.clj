f9dc1c4481f93298fd11958af936f34ce509996f
Henry Heikkinen <henry.heikkinen@reaktor.com>
2020-03-12 13:54:41 +0200

    Update scala-cas library from 0.5.0 to 1.4.0

--- parent-project.clj
+++ parent-project.clj
@@ -74,7 +74,7 @@
                          [buddy/buddy-core "1.4.0"]
 
                          ;; cas
-                         [fi.vm.sade/scala-cas_2.11 "0.5.0-20180928.085554-29"]
+                         [fi.vm.sade/scala-cas_2.11 "1.4.0-SNAPSHOT"]
                          [org.http4s/http4s-blaze-client_2.11 "0.16.5"]
                          [org.http4s/http4s-client_2.11 "0.16.5"]
                          [org.http4s/http4s-dsl_2.11 "0.16.5"]
