2932ad932d7d6263b5524e6b05478da9e628eff9
Antero Fagerstedt <antero.fagerstedt@reaktor.com>
2020-04-01 17:59:00 +0300

    Changed local PG version to 12.2

--- run_database.sh
+++ run_database.sh
@@ -2,11 +2,25 @@
 set -o errexit -o nounset -o pipefail
 repo="$( cd "$( dirname "$0" )" && pwd )"
 
+PG_DATA_DIR=postgres-data-12-2
+MY_UID=`id -u`
+MY_GID=`id -g`
+
 function main {
   cd "$repo/scripts/postgres-docker"
-  docker build -t va-postgres:9.4 .
-  mkdir -p postgres-data
-  docker run --rm --name va-postgres -p 5432:5432 -v "$PWD/postgres-data:/var/lib/postgresql/data" va-postgres:9.4
+  docker build -t va-postgres:12.2 .
+  mkdir -p ${PG_DATA_DIR}
+
+  docker run --rm \
+    --name va-postgres \
+    --user "${MY_UID}:${MY_GID}" \
+    -e POSTGRES_USER=va \
+    -e POSTGRES_PASSWORD=va \
+    -p 5432:5432 \
+    -v "$PWD/${PG_DATA_DIR}:/var/lib/postgresql/data" \
+    -v /etc/passwd:/etc/passwd:ro \
+    va-postgres:12.2
+
 }
 
 main "$@"
