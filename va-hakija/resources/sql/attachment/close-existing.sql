cf5058badd620bbc48fcf52052117e59fe7ceac0
Henry Heikkinen <henry.heikkinen@reaktor.com>
2020-04-23 10:16:06 +0300

    OPHY-230 Fix SQL query for deleting attachments to work with new Postgres version

--- va-hakija/resources/sql/attachment/close-existing.sql
+++ va-hakija/resources/sql/attachment/close-existing.sql
@@ -1,4 +1,4 @@
-update attachments set (version_closed) = (now())
+update attachments set version_closed = now()
 where hakemus_id = :hakemus_id and
       field_id = :field_id and
       version_closed is null;
