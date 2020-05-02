0949c557d4df60027323169abbe2761c4cb6c2e4
Timo Suomela <timo.suomela@reaktor.com>
2020-05-02 18:44:14 +0300

    Create xml junit reports also for e2e specs

--- run_isolated_system_tests.sh
+++ run_isolated_system_tests.sh
@@ -60,7 +60,7 @@ function main {
   waitport ${HAKIJA_HOSTNAME} 8080 150
   waitport ${VIRKAILIJA_HOSTNAME} 8081 150
 
-  npx mocha "test/**/*Spec.js"
+  npx mocha ${MOCHA_ARGS} "test/**/*Spec.js"
   make clean build test
 }
 
