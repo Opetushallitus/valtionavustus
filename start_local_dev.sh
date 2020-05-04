00c0e07d8041b7dca44367b6b772f27dc4ba49cd
Santeri Horttanainen <santeri.horttanainen@reaktor.com>
2020-05-04 18:47:06 +0300

    Added java version check to start_local_dev.sh

--- start_local_dev.sh
+++ start_local_dev.sh
@@ -3,6 +3,12 @@
 command -v tmux > /dev/null 2>&1 || { echo >&2 "I require tmux but it's not installed. Aborting."; exit 1; }
 command -v nc > /dev/null 2>&1 || { echo >&2 "I require nc but it's not installed. Aborting."; exit 1; }
 
+JAVA_VERSION="$(java -version 2>&1 >/dev/null | grep 'openjdk version' | awk '{print $3}' | sed 's/"//g' )"
+if [[ "$JAVA_VERSION" != "1.8.0_"* ]]; then
+  echo "Missing required openjdk version 1.8.0_*"
+  exit 1;
+fi
+
 session="valtionavustus"
 
 tmux start-server
