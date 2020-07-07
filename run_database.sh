d8cb250f40a9dada771e46b11cec2ee56f4f1e44
Santeri Horttanainen <santeri.horttanainen@reaktor.com>
2020-07-07 08:54:46 +0300

    Use docker-compose instead of docker run since docker-compose closes when tmux session closes

--- run_database.sh
+++ run_database.sh
@@ -11,16 +11,12 @@ function main {
   docker build -t va-postgres:12.2 .
   mkdir -p ${PG_DATA_DIR}
 
-  docker run --rm \
-    --name va-postgres \
-    --user "${MY_UID}:${MY_GID}" \
-    -e POSTGRES_USER=va \
-    -e POSTGRES_PASSWORD=va \
-    -p 5432:5432 \
-    -v "$PWD/${PG_DATA_DIR}:/var/lib/postgresql/data" \
-    -v /etc/passwd:/etc/passwd:ro \
-    va-postgres:12.2
+  export PG_DATA_DIR
+  export MY_UID
+  export MY_GID
 
+  docker-compose down || true
+  docker-compose up --force-recreate
 }
 
 main "$@"
