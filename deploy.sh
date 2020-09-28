ec91773b4d88be311f5d04cff05fc0e5d8e04572
Timo Suomela <timo.suomela@reaktor.com>
2020-09-28 12:12:12 +0300

    Pull up starting and stopping dockerized servers into deploy.sh so that run_isolated_system_tests.sh can be run against local servers

--- deploy.sh
+++ deploy.sh
@@ -4,13 +4,64 @@ node_version="12.13.1"
 npm_version="6.13.4"
 repo="$( cd "$( dirname "$0" )" && pwd )"
 
+HAKIJA_HOSTNAME=${HAKIJA_HOSTNAME:-"localhost"}
+VIRKAILIJA_HOSTNAME=${VIRKAILIJA_HOSTNAME:-"localhost"}
+DOCKER_COMPOSE_FILE=./docker-compose-test.yml
+
 function main {
+  check_requirements
   init_nodejs
   set_env_vars
   clean
   build
+  start_system_under_test
   run_tests
   deploy_jars
+}
+
+function stop_system_under_test {
+  echo "Stopping system under test"
+  docker-compose -f ${DOCKER_COMPOSE_FILE} down --remove-orphans
+}
+trap stop_system_under_test EXIT
+
+function start_system_under_test {
+  echo "Starting system under test"
+  docker build -t "va-virkailija:latest" -f ./Dockerfile.virkailija ./
+  docker build -t "va-hakija:latest" -f ./Dockerfile.hakija ./
+
+  docker-compose -f ${DOCKER_COMPOSE_FILE} up -d
+
+  waitport ${HAKIJA_HOSTNAME} 8080 150
+  waitport ${VIRKAILIJA_HOSTNAME} 8081 150
+}
+
+function check_requirements {
+  if ! [[ -x "$(command -v curl)" ]]; then
+    echo "Curl is required, cannot continue"
+    exit 1
+  fi
+}
+
+function waitport {
+  i=0
+  HOSTNAME=$1
+  PORT=$2
+  MAX_WAIT=${3:-100}
+
+  while ! curl -s "http://${HOSTNAME}:${PORT}/" > /dev/null; do
+    echo "Waiting for port ${PORT} to respond on host ${HOSTNAME}, ${i}/${MAX_WAIT}"
+
+    if [[ $i -gt ${MAX_WAIT} ]]; then
+      echo "Port not responding and max time exceeded, exiting..."
+      exit 1
+    fi
+
+    sleep 1
+    ((i=i+1))
+  done
+
+  echo "Port ${PORT} is responding on host ${HOSTNAME}"
 }
 
 clean() {
--- run_isolated_system_tests.sh
+++ run_isolated_system_tests.sh
@@ -2,55 +2,6 @@
 set -o errexit -o nounset -o pipefail
 repo="$( cd "$( dirname "$0" )" && pwd )"
 
-HAKIJA_HOSTNAME=${HAKIJA_HOSTNAME:-"localhost"}
-VIRKAILIJA_HOSTNAME=${VIRKAILIJA_HOSTNAME:-"localhost"}
-DOCKER_COMPOSE_FILE=./docker-compose-test.yml
-
-function stop_system_under_test {
-  echo "Stopping system under test"
-  docker-compose -f ${DOCKER_COMPOSE_FILE} down --remove-orphans
-}
-trap stop_system_under_test EXIT
-
-function start_system_under_test {
-  echo "Starting system under test"
-  docker build -t "va-virkailija:latest" -f ./Dockerfile.virkailija ./
-  docker build -t "va-hakija:latest" -f ./Dockerfile.hakija ./
-
-  docker-compose -f ${DOCKER_COMPOSE_FILE} up -d
-
-  waitport ${HAKIJA_HOSTNAME} 8080 150
-  waitport ${VIRKAILIJA_HOSTNAME} 8081 150
-}
-
-function check_requirements {
-  if ! [[ -x "$(command -v curl)" ]]; then
-    echo "Curl is required, cannot continue"
-    exit 1
-  fi
-}
-
-function waitport {
-  i=0
-  HOSTNAME=$1
-  PORT=$2
-  MAX_WAIT=${3:-100}
-
-  while ! curl -s "http://${HOSTNAME}:${PORT}/" > /dev/null; do
-    echo "Waiting for port ${PORT} to respond on host ${HOSTNAME}, ${i}/${MAX_WAIT}"
-
-    if [[ $i -gt ${MAX_WAIT} ]]; then
-      echo "Port not responding and max time exceeded, exiting..."
-      exit 1
-    fi
-
-    sleep 1
-    ((i=i+1))
-  done
-
-  echo "Port ${PORT} is responding on host ${HOSTNAME}"
-}
-
 function initialize {
   export ADBLOCK=1
   cd "$repo"
@@ -62,9 +13,7 @@ function run_tests {
 }
 
 function main {
-  check_requirements
   initialize
-  start_system_under_test
   run_tests
 }
 
