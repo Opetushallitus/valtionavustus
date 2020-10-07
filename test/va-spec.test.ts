53f7d96ec388b3eef0506026bd67f035cf231282
Timo Suomela <timo.suomela@reaktor.com>
2020-10-07 17:28:51 +0300

    Wait for asynchronous event before continuing

--- test/va-spec.test.ts
+++ test/va-spec.test.ts
@@ -93,7 +93,7 @@ describe("Puppeteer tests", () => {
 
     await clickElement(page, "#arviointi-tab label[for='set-arvio-status-plausible']")
     await clearAndType(page, "#budget-edit-project-budget .amount-column input", "100000")
-    clickElement(page, "#arviointi-tab label[for='set-arvio-status-accepted']")
+    await clickElement(page, "#arviointi-tab label[for='set-arvio-status-accepted']")
     waitForArvioSave(page, avustushakuID, parseInt(hakemusID)),
 
     await resolveAvustushaku(page, avustushakuID)
