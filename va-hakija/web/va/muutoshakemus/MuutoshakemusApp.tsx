14832a922b696506d0f76f930128a970802cbead
Henry Heikkinen <henry.heikkinen@reaktor.com>
2020-10-08 17:07:28 +0300

    VA-40 Load avustushaku details to MuutoshakemusApp

--- va-hakija/web/va/muutoshakemus/MuutoshakemusApp.tsx
+++ va-hakija/web/va/muutoshakemus/MuutoshakemusApp.tsx
@@ -1,5 +1,9 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+// @ts-ignore
+import HttpUtil from "soresu-form/web/HttpUtil"
+// @ts-ignore
+import * as Bacon from "baconjs"
 
 const queryString = require('query-string')
 
@@ -27,20 +31,63 @@ export interface AppProps {
 
 export interface ContactPersonEditProps {
   lang: Language
+  avustushaku?: any
 }
 
 export function ContactPersonEdit (props: ContactPersonEditProps) {
-  const lang = props.lang
+  const { lang, avustushaku } = props
 
-  return <>
-    <h3>{translations.contactPersonEdit.haku[lang]}: INNOVATIIVISET OPPIMISYMPÄRISTÖT</h3>
-  </>
+  return (
+    <div>
+      <h3>{translations.contactPersonEdit.haku[lang]}: {avustushaku?.content?.name?.[lang]}</h3>
+    </div>
+  )
 }
 
-export function App(props: AppProps) {
-  return (
-    <ContactPersonEdit lang={props.lang}/>
-  );
+type AppState = {
+  status: 'LOADING'
+} | {
+  status: 'LOADED'
+  avustushaku: any
+}
+class App extends React.Component<AppProps, AppState>  {
+  unsubscribe: Function
+
+  constructor(props: AppProps) {
+    super(props)
+
+    this.state = { status: "LOADING" }
+    const avustushakuId = 2
+
+    const avustushaku = Bacon.fromPromise(HttpUtil.get(`/api/avustushaku/${avustushakuId}`))
+
+    this.unsubscribe = avustushaku.onValue((avustushaku: any) =>
+      this.setState({ status: "LOADED", avustushaku })
+    )
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe()
+  }
+
+  render() {
+    const {state, props} = this
+
+    if (state.status === "LOADING")
+      return <p>Ladataan...</p>
+
+    return (
+      <div>
+        <ContactPersonEdit lang={props.lang} avustushaku={state.avustushaku}/>
+        <Debug json={state} />
+      </div>
+  )
+  }
+}
+
+type DebugProps = { json: object }
+function Debug({ json }: DebugProps) {
+  return <pre>{JSON.stringify(json, null, 2)}</pre>
 }
 
 ReactDOM.render(
