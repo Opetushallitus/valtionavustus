cbb72a9c13f8ef3511c4be69d6988e3f42623fc7
Santeri Horttanainen <santeri.horttanainen@reaktor.com>
2020-10-12 11:29:11 +0300

    Remove java version check since it apparently wont work on two different computers. Also since we have jenv this check should be unnecessary

--- start-local-env.sh
+++ start-local-env.sh
@@ -11,12 +11,6 @@ require docker
 
 docker ps > /dev/null 2>&1 || { echo >&2 "Running 'docker ps' failed. Is docker daemon running? Aborting."; exit 1; }
 
-JAVA_VERSION="$(java -version 2>&1 >/dev/null | grep 'openjdk version' | awk '{print $3}' | sed 's/"//g' )"
-if [[ "$JAVA_VERSION" != "1.8.0_"* ]]; then
-  echo "Missing required openjdk version 1.8.0_*"
-  exit 1;
-fi
-
 session="valtionavustus"
 
 tmux kill-session -t $session
