ca91b5f7c0192db6ad403161c1ff3a93b1a60834
Antero Fagerstedt <antero.fagerstedt@reaktor.com>
2020-10-15 21:32:07 +0300

    VA-40: Extracted TopBar to a separate component

--- va-hakija/web/va/muutoshakemus/MuutoshakemusApp.tsx
+++ va-hakija/web/va/muutoshakemus/MuutoshakemusApp.tsx
@@ -8,6 +8,7 @@ import HttpUtil from 'soresu-form/web/HttpUtil'
 import 'soresu-form/web/form/style/main.less'
 import '../style/main.less'
 import {AvustuksenKayttoajanPidennys} from './components/jatkoaika/AvustuksenKayttoajanPidennys'
+import {TopBar} from './components/TopBar'
 
 type Language = 'fi' | 'sv'
 function validateLanguage(s: unknown): Language {
@@ -253,34 +254,11 @@ type AppShellProps = {
 function AppShell({ children, t, env }: AppShellProps) {
   return (
     <div>
-      <TopBar env={env} t={t} />
+      <TopBar env={env} title={t.hakemus} />
       <section className="soresu-form" id="container">
         {children}
       </section>
     </div>
-  )
-}
-
-type TopBarProps = { t: Translations, env: string }
-function TopBar({ t, env }: TopBarProps) {
-  return (
-    <section id="topbar">
-      <div id="top-container">
-        <img id="logo" src="img/logo-240x68@2x.png" width="240" height="68" alt="Opetushallitus / Utbildningsstyrelsen" />
-        <div className="topbar-right">
-          <div className="topbar-title-and-save-status">
-            <h1 id="topic">{t.hakemus}</h1>
-          </div>
-          <div>
-            <div className="important-info">
-              <div className="environment-info">
-                <div className="environment-info__name">{env}</div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
   )
 }
 
---
+++ va-hakija/web/va/muutoshakemus/components/TopBar.tsx
@@ -0,0 +1,25 @@
+import * as React from "react"
+
+type TopBarProps = { title: string, env: string }
+
+export function TopBar({ title, env }: TopBarProps) {
+  return (
+    <section id="topbar">
+      <div id="top-container">
+        <img id="logo" src="img/logo-240x68@2x.png" width="240" height="68" alt="Opetushallitus / Utbildningsstyrelsen" />
+        <div className="topbar-right">
+          <div className="topbar-title-and-save-status">
+            <h1 id="topic">{title}</h1>
+          </div>
+          <div>
+            <div className="important-info">
+              <div className="environment-info">
+                <div className="environment-info__name">{env}</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
