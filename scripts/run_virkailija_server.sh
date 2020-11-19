247f45f493403f6ba0d322ae564d598600a51e0b
Henry Heikkinen <henry.heikkinen@reaktor.com>
2020-11-19 11:40:02 +0200

    Install soresu-form and va-common during hakija server startup and make virkailija server wait until hakija server is running

--- scripts/run_virkailija_server.sh
+++ scripts/run_virkailija_server.sh
@@ -4,12 +4,21 @@ scriptdir="$( cd "$( dirname "$0" )" && pwd )"
 repo="$scriptdir/.."
 
 function main {
-  cd "$repo/soresu-form"
-  ../lein install
-  cd "$repo/va-common"
-  ../lein install
+  command -v nc > /dev/null 2>&1 || { echo >&2 "I require nc but it's not installed. Aborting."; exit 1; }
+
+  wait_for_hakija_server_to_be_started
+
   cd "$repo/va-virkailija"
   ../lein run
+}
+
+function wait_for_hakija_server_to_be_started {
+  local -r hakija_server_port="8080"
+
+  echo "wating for hakija server to accept connections on port $hakija_server_port"
+  while ! nc -z localhost $hakija_server_port; do
+    sleep 1
+  done
 }
 
 main "$@"
