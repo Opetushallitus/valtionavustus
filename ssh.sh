f55451ece0edd604f633865401d02baed46997d6
Henry Heikkinen <henry.heikkinen@reaktor.com>
2020-11-23 19:46:53 +0200

    OPHY-289 Make ssh.sh work on clean install by using the ssh.config from repo

--- ssh.sh
+++ ssh.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -o errexit -o nounset -o pipefail
-repo="$( cd "$( dirname "$0" )" && pwd )"
+source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/scripts/common-functions.sh"
 
 function usage {
   echo "Usage: $0 <env> <machine> [ssh-args...]"
@@ -17,22 +17,23 @@ function main {
   local env="$1"
   local server_name="$2"
 
+  local -r ssh_args="-F $repo/servers/ssh.config"
+
   case "$env-$server_name" in
     qa-ci)
-      ssh oph-va-ci-test01 "${@:3}"
+      ssh $ssh_args oph-va-ci-test01 "${@:3}"
       ;;
     qa-app)
-      ssh oph-va-app-test01 "${@:3}"
+      ssh $ssh_args oph-va-app-test01 "${@:3}"
       ;;
     prod-app)
-      ssh oph-va-app-prod01 "${@:3}"
+      ssh $ssh_args oph-va-app-prod01 "${@:3}"
       ;;
     prod-lb)
-      ssh oph-va-lb-prod01 "${@:3}"
+      ssh $ssh_args oph-va-lb-prod01 "${@:3}"
       ;;
     *)
-      echo >&2 "error: Invalid environment or host ($env, $server_name)"
-      exit 1
+      fatal "error: Invalid environment or host ($env, $server_name)"
   esac
 }
 
