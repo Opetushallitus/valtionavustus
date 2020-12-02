34f7462835c8d9d863f188f60e002df5b9a3b88d
Henry Heikkinen <henry.heikkinen@reaktor.com>
2020-12-02 17:15:50 +0200

    Remove unused file

--- va-hakija/src/oph/va/hakija/application_data.clj
+++
@@ -1,6 +0,0 @@
-(ns oph.va.hakija.application-data
-  (:require [oph.va.hakija.db :as va-db]))
-
-(defn create-application-token [user-key]
-  (let [application (va-db/get-hakemus user-key)]
-    (:token (va-db/create-application-token (:id application)))))
