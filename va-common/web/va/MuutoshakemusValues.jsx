286b5fad7c7357f66f91eca80cf944b2a091b8a3
Antero Fagerstedt <antero.fagerstedt@reaktor.com>
2021-01-12 19:31:26 +0200

    VA-40-11: Initial UI component for approved with changes

--- va-common/web/va/MuutoshakemusValues.jsx
+++ va-common/web/va/MuutoshakemusValues.jsx
@@ -40,24 +40,52 @@ export const MuutoshakemusValues = ({ muutoshakemus, hakemus, hakijaUrl, simpleP
           }
         </section>
       }
-      {a['haettu-kayttoajan-paattymispaiva'] &&
-        <section className="muutoshakemus-section">
-          <div className="muutoshakemus-row muutoshakemus__project-end-row">
-            <div>
-              <h3 className="muutoshakemus__header">Voimassaoleva päättymisaika</h3>
-              <div>{hakemus['project-end']}</div>
-            </div>
-            <div>
-              <h3 className="muutoshakemus__header">Haettu muutos</h3>
-              <div data-test-id="muutoshakemus-jatkoaika">{moment(a['haettu-kayttoajan-paattymispaiva']).format('DD.MM.YYYY')}</div>
-            </div>
-          </div>
-          <div className="muutoshakemus-row">
-            <h4 className="muutoshakemus__header">Hakijan perustelut</h4>
-            <div className="muutoshakemus__reason" data-test-id="muutoshakemus-jatkoaika-perustelu">{a['kayttoajan-pidennys-perustelut']}</div>
-          </div>
-        </section>
-      }
+      <HakemusFields hakemus={hakemus} muutoshakemus={muutoshakemus} />
     </React.Fragment>
+  )
+}
+
+const HakemusFields = ({ hakemus, muutoshakemus }) => {
+  if (!muutoshakemus['haettu-kayttoajan-paattymispaiva']) return null
+
+  const isApprovedWithChanges = muutoshakemus.status === 'accepted_with_changes'
+
+  const texts = isApprovedWithChanges ?
+    {
+      currentEndDateTitle: 'Vanha päättymisaika',
+      currentEndDateValue: hakemus['project-end'],
+      newEndDateTitle: 'Hyväksytty muutos',
+      newEndDateValue: muutoshakemus['paatos-hyvaksytty-paattymispaiva']
+    } :
+    {
+      currentEndDateTitle: 'Voimassaoleva päättymisaika',
+      currentEndDateValue: hakemus['project-end'],
+      newEndDateTitle: 'Haettu muutos',
+      newEndDateValue: muutoshakemus['haettu-kayttoajan-paattymispaiva'],
+    }
+
+  return (
+    <Asd {...texts} perustelut={muutoshakemus['kayttoajan-pidennys-perustelut'] } />
+  )
+}
+
+const Asd = ({ currentEndDateTitle, currentEndDateValue, newEndDateTitle, newEndDateValue, perustelut }) => {
+  return (
+    <section className="muutoshakemus-section">
+      <div className="muutoshakemus-row muutoshakemus__project-end-row">
+        <div>
+          <h3 className="muutoshakemus__header">{currentEndDateTitle}</h3>
+          <div>{currentEndDateValue}</div>
+        </div>
+        <div>
+          <h3 className="muutoshakemus__header">{newEndDateTitle}</h3>
+          <div data-test-id="muutoshakemus-jatkoaika">{moment(newEndDateValue).format('DD.MM.YYYY')}</div>
+        </div>
+      </div>
+      <div className="muutoshakemus-row">
+        <h4 className="muutoshakemus__header">Hakijan perustelut</h4>
+        <div className="muutoshakemus__reason" data-test-id="muutoshakemus-jatkoaika-perustelu">{perustelut}</div>
+      </div>
+    </section>
   )
 }
