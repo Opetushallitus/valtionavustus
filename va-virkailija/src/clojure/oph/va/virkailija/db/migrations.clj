80908972983a19f6fba4cceb0c298b703bd6ec01
Santeri Horttanainen <santeri.horttanainen@reaktor.com>
2021-02-04 12:25:20 +0200

    VA-40-14: Remove hakija-api dependecy

--- va-virkailija/src/clojure/oph/va/virkailija/db/migrations.clj
+++ va-virkailija/src/clojure/oph/va/virkailija/db/migrations.clj
@@ -1,7 +1,6 @@
 (ns oph.va.virkailija.db.migrations
   (:require [oph.soresu.common.db.migrations :as migrations]
             [oph.soresu.common.db :as common-db]
-            [oph.va.hakija.api :as hakija-api]
             [yesql.core :refer [defquery]])
   (:gen-class))
 
@@ -12,6 +11,9 @@
   {:rahoitusalue (:rahoitusalue rahoitusalue)
    :talousarviotilit ["momentti"]})
 
+(defn get-avustushaku [id]
+  (first (common-db/query "select * from avustushaut where id = ? and status <> 'deleted'" [id])))
+
 (defquery list-used-rahoitusalueet "db/migration/queries/m1_21-list-used-rahoitusalueet.sql")
 (defquery update-avustushaku-content! "db/migration/queries/m1_21-update-avustushaku-content.sql")
 (migrations/defmigration migrate-add-rahoitusalueet-for-avustushaut "1.21"
@@ -19,7 +21,7 @@
                          (let [used-rahoitusalueet (common-db/exec list-used-rahoitusalueet {})
                                avustushakujen-rahoitusalueet (group-by :avustushaku used-rahoitusalueet)]
                            (doseq [avustushaku-id (keys avustushakujen-rahoitusalueet)]
-                             (let [avustushaku (hakija-api/get-avustushaku avustushaku-id)
+                             (let [avustushaku (get-avustushaku avustushaku-id)
                                    avustushaun-rahoitusalueet (get avustushakujen-rahoitusalueet avustushaku-id)
                                    rahoitusalueet-json (map create-rahoitusalue-json avustushaun-rahoitusalueet)
                                    new-content (assoc (:content avustushaku) :rahoitusalueet rahoitusalueet-json)
