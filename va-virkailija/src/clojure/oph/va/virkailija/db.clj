10989a0cb5af821d9ad137aacaf666e0f4ffb40e
Antero Fagerstedt <antero.fagerstedt@reaktor.com>
2021-02-17 23:53:04 +0200

    VA-40-17: Don't create duplicate rows if one muutoshakemus has multiple email events. Use the latest email event instead. Duplicate rows broke the UI, as there were multiple muutoshakemuses with the same ID, but different email send times.

--- va-virkailija/src/clojure/oph/va/virkailija/db.clj
+++ va-virkailija/src/clojure/oph/va/virkailija/db.clj
@@ -60,7 +60,8 @@
                                                   ee.created_at as paatos_sent_at
                                                 FROM virkailija.muutoshakemus m
                                                 LEFT JOIN virkailija.paatos p ON m.paatos_id = p.id
-                                                LEFT JOIN virkailija.email_event ee ON m.id = ee.muutoshakemus_id AND ee.email_type = 'muutoshakemus-paatos' AND success = true
+                                                LEFT JOIN virkailija.email_event ee
+                                                  ON ee.id = (SELECT max(id) FROM virkailija.email_event WHERE muutoshakemus_id = m.id AND email_type = 'muutoshakemus-paatos' AND success = true)
                                                 WHERE m.hakemus_id = ?
                                                 ORDER BY id DESC" hakemus-id]
                                               {:identifiers #(.replace % \_ \-)}))]
