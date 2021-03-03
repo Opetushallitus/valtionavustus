1a8bb3493cae0df7f7f1bf46edc7f1009c4edfd7
Antero Fagerstedt <antero.fagerstedt@reaktor.com>
2021-03-04 00:59:58 +0200

    VA-40-17: Fixed tests which attempt to resolve avustushaku without setting start & end dates before.

--- va-virkailija/resources/sql/spec/hakija/set-all-grants-resolved.sql
+++ va-virkailija/resources/sql/spec/hakija/set-all-grants-resolved.sql
@@ -1,4 +1,6 @@
-UPDATE
-  hakija.avustushaut
-SET
-  status = 'resolved'
+UPDATE hakija.avustushaut SET
+    hankkeen_alkamispaiva = '1969-04-20',
+    hankkeen_paattymispaiva = '4200-04-20'
+WHERE hankkeen_alkamispaiva IS NULL AND hankkeen_paattymispaiva IS NULL;
+
+UPDATE hakija.avustushaut SET status = 'resolved'
