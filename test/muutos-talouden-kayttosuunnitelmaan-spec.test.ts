bded50a64db542b5dec007fdc65a4f9d65251649
Santeri Horttanainen <santeri.horttanainen@reaktor.com>
2021-04-08 11:13:01 +0300

    VA-41-4: Create separate file for talousarvion muutoshakemus tests

---
+++ test/muutos-talouden-kayttosuunnitelmaan-spec.test.ts
@@ -0,0 +1,80 @@
+import {Browser, Page} from 'puppeteer'
+import moment from 'moment'
+import {
+  mkBrowser,
+  log,
+  waitUntilMinEmails,
+  getAcceptedPäätösEmails,
+  getFirstPage,
+  setPageErrorConsoleLogger,
+  expectToBeDefined,
+  randomString,
+  getLinkToMuutoshakemusFromSentEmails,
+  ratkaiseBudjettimuutoshakemusEnabledAvustushaku
+} from './test-util'
+
+function createRandomHakuValues() {
+  return {
+    registerNumber: "230/2015",
+    avustushakuName: `Testiavustushaku (Muutospäätösprosessi) ${randomString()} - ${moment(new Date()).format('YYYY-MM-DD hh:mm:ss:SSSS')}`
+  }
+}
+
+const answers = {
+  contactPersonEmail: "erkki.esimerkki@example.com",
+  contactPersonName: "Erkki Esimerkki",
+  contactPersonPhoneNumber: "666",
+  projectName: "Rahassa kylpijät Ky Ay Oy",
+}
+
+describe('Talousarvion muuttaminen', () => {
+  let browser: Browser
+  let page: Page
+
+  beforeEach(() => {
+    log(`Starting test: ${expect.getState().currentTestName}`)
+  })
+
+  beforeAll(async () => {
+    browser = await mkBrowser()
+    page = await getFirstPage(browser)
+    setPageErrorConsoleLogger(page)
+  })
+
+  afterEach(() => {
+    log(`Finished test: ${expect.getState().currentTestName}`)
+  })
+
+  afterAll(async () => {
+    await page.close()
+    await browser.close()
+  })
+
+  describe("Hakija haluaa tehdä muutoshakemuksen talouden käyttösuunnitelmaan", () => {
+    let linkToMuutoshakemus: string
+    let avustushakuID: number
+    let hakemusID: number
+    const haku = createRandomHakuValues()
+
+    beforeAll(async () => {
+      const { avustushakuID: avustushakuId, hakemusID: hakemusId } = await ratkaiseBudjettimuutoshakemusEnabledAvustushaku(page, haku, answers)
+      avustushakuID = avustushakuId
+      hakemusID = hakemusId
+      linkToMuutoshakemus = await getLinkToMuutoshakemusFromSentEmails(avustushakuID, hakemusID)
+      expectToBeDefined(linkToMuutoshakemus)
+      await page.goto(linkToMuutoshakemus, { waitUntil: "networkidle0" })
+    })
+
+    it('ja oppia mahdollisuudesta tehdä muutoksia talouden käyttösuunnitelmaa hakemuksen päätöksen s.postista', async () => {
+      const emails = await waitUntilMinEmails(getAcceptedPäätösEmails, 1, avustushakuID, hakemusID)
+      emails.forEach(email => {
+        const emailContent = email.formatted
+        expect(emailContent).not.toContain('- Hakea muutosta hankkeen talouden käyttösuunnitelmaan')
+      })
+    })
+
+    it('ja nähdä voimassaolevan talousarvion klikattuaan “Haen muutosta hankkeen talouden käyttösuunnitelmaan” checkboksia', async () => {
+      expect(true).toEqual(true)
+    })
+  })
+})
