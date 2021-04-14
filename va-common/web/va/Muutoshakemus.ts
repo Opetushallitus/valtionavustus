e04c189f77a39dd3ff837ae60a3fd204825b2fa2
Tomi Karlstedt <tomi.karlstedt@reaktor.com>
2021-04-14 15:45:52 +0300

    VA-41-13 Fix finding the latest approved project end date

--- va-common/web/va/Muutoshakemus.ts
+++ va-common/web/va/Muutoshakemus.ts
@@ -1,4 +1,4 @@
-import moment, {Moment} from 'moment'
+import moment, { Moment } from 'moment'
 import { Muutoshakemus } from './types/muutoshakemus'
 
 export interface Avustushaku {
@@ -10,35 +10,32 @@ export function getProjectEndDate(avustushaku: Avustushaku, muutoshakemukset: Mu
 }
 
 export function getProjectEndMoment(avustushaku: Avustushaku, muutoshakemukset: Muutoshakemus[] | undefined): Moment {
-  const latestAcceptedMuutoshakemus = getLatestApprovedMuutoshakemusDate(muutoshakemukset)
+  const latestAcceptedMuutoshakemus = getLatestApprovedProjectEndDate(muutoshakemukset)
   return latestAcceptedMuutoshakemus ? latestAcceptedMuutoshakemus : dateStringToMoment(avustushaku['hankkeen-paattymispaiva'])
 }
 
-function getLatestApprovedMuutoshakemusDate(muutoshakemukset: Muutoshakemus[] | undefined): Moment | undefined {
+function sortByCreatedAtDescending(muutoshakemukset: Muutoshakemus[]): Muutoshakemus[] {
+  return [...muutoshakemukset].sort((m1, m2) => m1["created-at"] > m2["created-at"] ? -1 : 1)
+}
+
+function getLatestApprovedProjectEndDate(muutoshakemukset: Muutoshakemus[] | undefined): Moment | undefined {
   if (!muutoshakemukset) return undefined
 
-  function first(muutoshakemukset: Muutoshakemus[]): Muutoshakemus | undefined {
-    return [...muutoshakemukset].shift()
+  const acceptedWithProjectEnd = muutoshakemukset.filter(m => (m.status === 'accepted' || m.status === 'accepted_with_changes') && m['haen-kayttoajan-pidennysta'])
+  if (acceptedWithProjectEnd.length) {
+    const latestAcceptedMuutoshakemus = sortByCreatedAtDescending(acceptedWithProjectEnd)[0]
+    return latestAcceptedMuutoshakemus.status === 'accepted_with_changes'
+      ? dateStringToMoment(latestAcceptedMuutoshakemus["paatos-hyvaksytty-paattymispaiva"])
+      : dateStringToMoment(latestAcceptedMuutoshakemus["haettu-kayttoajan-paattymispaiva"])
+  } else {
+    return undefined
   }
-
-  function sortByCreatedAtDescending(muutoshakemukset: Muutoshakemus[]): Muutoshakemus[] {
-    return [...muutoshakemukset].sort((m1, m2) => m1["created-at"] > m2["created-at"] ? -1 : 1)
-  }
-
-  const acceptedMuutoshakemukset = muutoshakemukset.filter(m => m.status === 'accepted' || m.status === 'accepted_with_changes')
-  const latestAcceptedMuutoshakemus = first(sortByCreatedAtDescending(acceptedMuutoshakemukset))
-
-  if (!latestAcceptedMuutoshakemus) return undefined
-
-  return latestAcceptedMuutoshakemus.status === 'accepted_with_changes' ?
-    dateStringToMoment(latestAcceptedMuutoshakemus["paatos-hyvaksytty-paattymispaiva"]) :
-    dateStringToMoment(latestAcceptedMuutoshakemus["haettu-kayttoajan-paattymispaiva"])
 }
 
 export function dateStringToMoment(date: string | undefined): Moment {
   return moment(date, 'YYYY-MM-DD')
 }
 
-export function toFinnishDateFormat(date: { isValid: () => boolean, format: (string) => string  }): string | undefined {
+export function toFinnishDateFormat(date: Moment): string | undefined {
   return date.isValid() ? date.format('DD.MM.YYYY') : undefined
 }
