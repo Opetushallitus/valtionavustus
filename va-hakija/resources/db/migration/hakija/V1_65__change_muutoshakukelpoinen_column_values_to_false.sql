20ec5c4a4f4267f27365211f935fc4b6d52766c8
Santeri Horttanainen <santeri.horttanainen@reaktor.com>
2021-05-12 17:17:11 +0300

    Revert "VA-106-3: Change muutoshakukelpoinen values initially to false"
    This reverts commit d77d97211b9aed196e3563ca4fb78133dbec9924.

--- va-hakija/resources/db/migration/hakija/V1_65__change_muutoshakukelpoinen_column_values_to_false.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE hakija.avustushaut DROP COLUMN muutoshakukelpoinen;
-ALTER TABLE hakija.avustushaut ADD COLUMN muutoshakukelpoinen bool NOT NULL DEFAULT false;
-ALTER TABLE hakija.avustushaut ALTER COLUMN muutoshakukelpoinen DROP DEFAULT;
-
