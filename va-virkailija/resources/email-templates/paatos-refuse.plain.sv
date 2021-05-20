95d1b5703ff8b66a8133944ee69c672524b00472
Henry Heikkinen <henry.heikkinen@reaktor.com>
2021-05-20 15:04:58 +0300

    VA-99-3 Update päätös email templates

--- va-virkailija/resources/email-templates/paatos-refuse.plain.sv
+++ va-virkailija/resources/email-templates/paatos-refuse.plain.sv
@@ -1,22 +1,31 @@
 {{ register-number }} - {{ project-name }}
 
-Ansökan om understöd: {{ avustushaku-name }}
+{{ avustushaku-name }}
 
-Beslut link: {{& url }}
+Ni kan granska understödsbeslutet via denna länk: {{& url }}
 
-Om ni bestämmer er för att inte ta emot understödet kan ni göra en anmälan om detta via denna länk: {{& refuse-url }}
+Understödsmottagaren ska följa de villkor och begränsningar som beskrivs i understödsbeslutet och i dess bilagor.
 
-Om ni vill förandra kontaktinformationen kan ni göra en anmälan om detta via denna länk: {{& modify-url }}
+Om ni tar emot understödet i enlighet med beslutet, kan ni påbörja projektet. Understödsbeloppet betalas senast den dag som anges i beslutet.
 
-Anmälan ska göras senast det datum som framgår ur beslutet.
+Om ni inte tar emot understödet i enlighet med beslutet, ska ni meddela om detta till Utbildningsstyrelsen inom den tidsfrist som anges i beslutet. Anmälan ska göras i statsunderstödssystemet via denna länk: {{& refuse-url }}
 
-Om ni tar emot understödet behöver ni inte lämna in någon särskild anmälan om detta.
+Jos ei muutoshakukelpoinen haku, niin:
 
-Den föredragande som ansvarar för ansökningen har nämnts i beslutet.
+{{^include-muutoshaku-link}}Understödsmottagaren ansvarar för att kontaktuppgifterna till den som angetts som kontaktperson i statsunderstödssystemet är uppdaterade. Kontaktpersonen kan bytas ut via länken intill under hela understödets användningstid:{{/include-muutoshaku-link}}
+{{#include-muutoshaku-link}}Om det uppstår förändringar som inverkar på användningen av statsunderstödet ska man genast göra en skriftlig ändringsansökan. Man ska framföra tillräckliga motiveringar för ändringarna som ingår i ändringsansökan. I oklara situationer kan understödsmottagaren vara i kontakt med kontaktpersonen som anges i understödsbeslutet innan en ändringsansökan görs.
+
+Understödsmottagaren ansvarar för att kontaktuppgifterna till den person som angetts som kontaktperson i statsunderstödssystemet alltid är uppdaterade. Ni kan göra en ändringsansökan samt byta ut kontaktpersonen och göra ändringar i hens kontaktuppgifter under hela projektperiodens gång via följande länk:{{/include-muutoshaku-link}}
+{{& modify-url }}
+
+Begäranden om redovisningar och andra meddelanden som riktas till projektet skickas från adressen no-reply@valtionavustukset.oph.fi. De skickas både till projektets kontaktperson och till den officiella e-postadress som den sökande har angett.
+
+Mottagaren av understöd ska spara detta meddelande och länkarna som ingår i meddelandet.
+
+Vid behov ges närmare information av den person som angetts som kontaktperson i understödsbeslutet.
 
 Utbildningsstyrelsen
 Hagnäskajen 6
 PB 380, 00531 Helsingfors
-
 telefon 029 533 1000
-fornamn.efternamn@ubs.fi
+fornamn.efternamn@oph.fi
