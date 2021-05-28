6f841bd59e3633f2cb6c65297e6bd97deab0728f
Santeri Horttanainen <santeri.horttanainen@reaktor.com>
2021-05-28 15:02:51 +0300

    VA-117: Move create-menoluokka-rows to va-common

---
+++ va-common/src/oph/va/menoluokka/db.clj
@@ -0,0 +1,23 @@
+(ns oph.va.menoluokka.db
+  (:use [oph.soresu.common.db]
+        [clojure.data :as data]
+        [clojure.tools.trace :only [trace]])
+  (:require [oph.soresu.form.formutil :as formutil])
+  (:import [java.util Date]))
+
+(defn- answer->menoluokka-row [answers hakemus-id menoluokka]
+  {:menoluokka_id (:id menoluokka)
+   :hakemus_id hakemus-id
+   :amount (Integer/parseInt (formutil/find-answer-value answers (str (:type menoluokka) ".amount")))})
+
+(defn store-menoluokka-hakemus-rows [avustushaku-id hakemus-id answers]
+  (with-tx (fn [tx]
+             (let [menoluokka-types (query tx "SELECT id, type FROM virkailija.menoluokka WHERE avustushaku_id = ?" [avustushaku-id])
+                   menoluokka-rows (map (partial answer->menoluokka-row answers hakemus-id) menoluokka-types)]
+               (doseq [menoluokka menoluokka-rows]
+                 (execute! tx
+                           "INSERT INTO virkailija.menoluokka_hakemus (menoluokka_id, hakemus_id, amount)
+                            VALUES (?, ?, ?)
+                            ON CONFLICT (hakemus_id, menoluokka_id) DO UPDATE SET
+                              amount = EXCLUDED.amount"
+                           [(:menoluokka_id menoluokka) (:hakemus_id menoluokka) (:amount menoluokka)]))))))
--- va-virkailija/src/clojure/oph/va/virkailija/db.clj
+++ va-virkailija/src/clojure/oph/va/virkailija/db.clj
@@ -1,5 +1,6 @@
 (ns oph.va.virkailija.db
   (:use [oph.soresu.common.db]
+        [oph.va.menoluokka.db]
         [clojure.data :as data]
         [clojure.tools.trace :only [trace]])
   (:require [oph.soresu.form.formutil :as formutil]
@@ -264,23 +265,6 @@
                                                                          (:useDetailedCosts arvio)
                                                                          (:costsGranted arvio))]
             (:oph-share calculated-budget))))
-
-(defn- answer->menoluokka-row [answers hakemus-id menoluokka]
-  {:menoluokka_id (:id menoluokka)
-   :hakemus_id hakemus-id
-   :amount (Integer/parseInt (formutil/find-answer-value answers (str (:type menoluokka) ".amount")))})
-
-(defn store-menoluokka-hakemus-rows [avustushaku-id hakemus-id answers]
-  (with-tx (fn [tx]
-             (let [menoluokka-types (query tx "SELECT id, type FROM virkailija.menoluokka WHERE avustushaku_id = ?" [avustushaku-id])
-                   menoluokka-rows (map (partial answer->menoluokka-row answers hakemus-id) menoluokka-types)]
-               (doseq [menoluokka menoluokka-rows]
-                 (execute! tx
-                           "INSERT INTO virkailija.menoluokka_hakemus (menoluokka_id, hakemus_id, amount)
-                            VALUES (?, ?, ?)
-                            ON CONFLICT (hakemus_id, menoluokka_id) DO UPDATE SET
-                              amount = EXCLUDED.amount"
-                           [(:menoluokka_id menoluokka) (:hakemus_id menoluokka) (:amount menoluokka)]))))))
 
 (defn delete-menoluokka-hakemus-rows [hakemus-id]
   (execute! "DELETE FROM virkailija.menoluokka_hakemus WHERE hakemus_id = ?" [hakemus-id]))
