aa84a072fd571e536772c8b3463b2d44bd98a07a
Henry Heikkinen <henry.heikkinen@reaktor.com>
2021-07-28 14:12:51 +0300

    Translate PaatosUrl.js to TypeScript

--- va-virkailija/web/va/hakemus-details/PaatosUrl.ts
+++ va-virkailija/web/va/hakemus-details/PaatosUrl.ts
@@ -1,9 +1,9 @@
 export default class PaatosUrl {
-  static previewLink(avustushakuId, hakemusId) {
+  static previewLink(avustushakuId: number, hakemusId: number): string {
     return "/paatos/avustushaku/" + avustushakuId + "/hakemus/" + hakemusId
   }
 
-  static publicLink(avustushakuId, userKey) {
+  static publicLink(avustushakuId: number, userKey: string): string {
     return "/public/paatos/avustushaku/" + avustushakuId + "/hakemus/" + userKey
   }
 }
