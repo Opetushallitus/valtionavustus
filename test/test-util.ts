24686613a706d2cdff3e20e001fd255377a5e56d
Santeri Horttanainen <santeri.horttanainen@reaktor.com>
2021-08-10 13:46:25 +0300

    Fix screenshot file path

--- test/test-util.ts
+++ test/test-util.ts
@@ -918,7 +918,7 @@ export async function waitForClojureScriptLoadingDialogVisible(page: Page) {
   try {
     return await page.waitForSelector("[data-test-id=loading-dialog]", { visible: true })
   } catch (e) {
-    const screenshotFilePath = `${__dirname}/screenshot_${randomString()}.png`
+    const screenshotFilePath = `${__dirname}/screenshot_${randomString()}`
     await page.screenshot({ path: `${screenshotFilePath}.png`, fullPage: true })
     fs.writeFileSync(`${screenshotFilePath}.json`, JSON.stringify(expect.getState()))
     throw e
