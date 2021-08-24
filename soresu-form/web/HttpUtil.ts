e8e30f516ac71e566fc4bc8da5a9871f251a3ff9
Simo-Pekka Kerkelä <simo-pekka.kerkela@reaktor.com>
2021-08-24 16:16:16 +0300

    VA-130-2 convert LocalStorage and HttpUtil to ts

--- soresu-form/web/HttpUtil.ts
+++ soresu-form/web/HttpUtil.ts
@@ -1,33 +1,33 @@
-import axios from 'axios'
+import axios, {AxiosResponse} from 'axios'
 import Promise from 'bluebird'
 
 const errorHasResponse = error =>
   !!error.response && (typeof error.response === "object")
 
 export default class HttpUtil {
-  static get(url) {
+  static get(url: string) {
     return HttpUtil.handleResponse(axios.get(url))
   }
 
-  static post(url, jsonData) {
+  static post(url: string, jsonData?: any) {
     return HttpUtil.handleResponse(axios.post(url, jsonData))
   }
 
-  static put(url, requestData, options) {
+  static put(url: string, requestData?: FormData, options?: any) {
     return HttpUtil.handleResponse(axios.put(url, requestData, options))
   }
 
-  static putFile(url, file) {
+  static putFile(url: string, file: string | Blob) {
     const formData = new FormData()
     formData.append('file', file)
     return HttpUtil.put(url, formData)
   }
 
-  static delete(url) {
+  static delete(url: string) {
     return HttpUtil.handleResponse(axios.delete(url))
   }
 
-  static handleResponse(httpCall) {
+  static handleResponse(httpCall: Promise<AxiosResponse<any>>) {
     return Promise.resolve(httpCall)
       .then(response => response.data)
       .catch(errorHasResponse, error => {
@@ -41,15 +41,17 @@ export default class HttpUtil {
   }
 }
 
-export function HttpResponseError(message, response) {
-  this.message = message
-  this.name = "HttpResponseError"
-  this.response = response
+export class HttpResponseError extends Error {
+  response: any;
 
-  if (Error.captureStackTrace) {
+  constructor(message, response) {
+    super(message);
+    this.message = message
+    this.name = "HttpResponseError"
+    this.response = response
+    if (!Error.captureStackTrace) {
+      return;
+    }
     Error.captureStackTrace(this, HttpResponseError)
   }
 }
-
-HttpResponseError.prototype = Object.create(Error.prototype)
-HttpResponseError.prototype.constructor = HttpResponseError
--- va-virkailija/web/va/LocalStorage.ts
+++ va-virkailija/web/va/LocalStorage.ts
@@ -1,9 +1,9 @@
 export default class LocalStorage {
-  static avustushakuId() {
-    return parseInt(window.localStorage.getItem("avustushakuId"), 10)
+  static avustushakuId(): number {
+    return parseInt(window.localStorage.getItem("avustushakuId")!, 10)
   }
 
-  static saveAvustushakuId(id){
+  static saveAvustushakuId(id): void {
     window.localStorage.setItem("avustushakuId", id)
   }
 }
