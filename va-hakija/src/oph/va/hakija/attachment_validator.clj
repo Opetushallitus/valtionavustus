970f8256335d52eb8563abb0fae60feafe57a1f6
Tero Keinänen <tero.keinanen@reaktor.com>
2021-09-20 13:57:08 +0300

    OPHY-385: Replace usage of pantomime with plain Apache Tika
    Also removed tika-parsers because tika-core includes a detector that uses basic magic byte detection. Using the parsers is not required and just adds more dependencies. Removing tika-parsers removes depencency to jdom and the associated security issue.

--- va-hakija/src/oph/va/hakija/attachment_validator.clj
+++ va-hakija/src/oph/va/hakija/attachment_validator.clj
@@ -1,12 +1,16 @@
 (ns oph.va.hakija.attachment-validator
   (:require [clojure.string :as str]
-            [pantomime.mime :as mime]
-            [oph.soresu.common.config :refer [config]]))
+            [oph.soresu.common.config :refer [config]])
+  (:import [org.apache.tika Tika]
+           [org.apache.tika.mime MimeTypes MimeTypeException]))
 
 (def allowed-mime-types (-> config :server :attachment-mime-types))
 
+(defn- mime-type-of [file]
+  (.detect (new Tika) file))
+
 (defn validate-file-content-type [file provided-content-type]
-  (let [detected-content-type (mime/mime-type-of file)]
+  (let [detected-content-type (mime-type-of file)]
     (if (some #{detected-content-type} allowed-mime-types)
       {:provided-content-type provided-content-type
        :detected-content-type detected-content-type
@@ -16,8 +20,14 @@
        :allowed?              false
        :allowed-content-types allowed-mime-types})))
 
+(def default-mime-types (MimeTypes/getDefaultMimeTypes))
+
+(defn- extension-for-name [content-type]
+  (try (.getExtension (.forName default-mime-types content-type))
+       (catch MimeTypeException _ "")))
+
 (defn file-name-according-to-content-type [filename content-type]
-  (let [file-extension (mime/extension-for-name content-type)]
+  (let [file-extension (extension-for-name content-type)]
     (if (or (str/blank? file-extension) (.endsWith (.toLowerCase filename) file-extension))
       filename
       (str filename file-extension))))
