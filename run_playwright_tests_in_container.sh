cf61c2b4e869e07e3851cc1088101e8d28e30d95
Santeri Horttanainen <santeri.horttanainen@reaktor.com>
2021-10-22 15:42:52 +0300

    VA-142: Add proper health checks

--- run_playwright_tests_in_container.sh
+++ run_playwright_tests_in_container.sh
@@ -1,14 +1,6 @@
 #!/usr/bin/env bash
-function wait_until_virkailija_is_listening {
-  info "Waiting until port virkailija-server is listening"
-  while ! nc -z virkailija 8081; do
-    sleep 1
-  done
-}
 
 function main {
-  info "Waiting for hakija server to start..."
-  wait_until_virkailija_is_listening
   npm run playwright:test
 }
 
