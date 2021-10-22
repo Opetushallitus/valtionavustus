008a7bd14b316def52e33ead8977f85fc1479bdf
Santeri Horttanainen <santeri.horttanainen@reaktor.com>
2021-10-22 11:35:54 +0300

    VA-142: Revert "don't run playwright tests"
    This reverts commit 2fbfd0d7147262219720000f615a412e1904cdeb.

--- run_isolated_system_tests.sh
+++ run_isolated_system_tests.sh
@@ -3,6 +3,12 @@ set -o errexit -o nounset -o pipefail
 source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/scripts/common-functions.sh"
 
 function run_tests {
+	if running_on_jenkins;
+		then
+			docker-compose -f docker-compose-test.yml run test-runner ./run_playwright_tests_in_container.sh
+		else
+			npm run playwright:test
+	fi
 	npm run puppeteer:test "$@"
 	npm run soresu-form:test "$@"
 	npm run common:test "$@"
