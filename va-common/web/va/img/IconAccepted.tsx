b7bec925adf28d6b30386ac41758307a89d14664
Simo-Pekka Kerkelä <simo-pekka.kerkela@reaktor.com>
2021-10-22 15:43:00 +0300

    VA-142-1000 add test-id to icons

--- va-common/web/va/img/IconAccepted.tsx
+++ va-common/web/va/img/IconAccepted.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 export const IconAccepted: React.FC<{fill?: string}> = ({fill = '#159ECB'}) => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="12" viewBox="0 0 16 12" fill="none">
+  <svg data-test-id={'icon-accepted'} xmlns="http://www.w3.org/2000/svg" width="16" height="12" viewBox="0 0 16 12" fill="none">
     <path d="M13.5938 0.625L5.375 8.84375L2.375 5.8125C2.21875 5.6875 1.96875 5.6875 1.84375 5.8125L0.9375 6.71875C0.8125 6.84375 0.8125 7.09375 0.9375 7.25L5.125 11.4062C5.28125 11.5625 5.5 11.5625 5.65625 11.4062L15.0312 2.03125C15.1562 1.90625 15.1562 1.65625 15.0312 1.5L14.125 0.625C14 0.46875 13.75 0.46875 13.5938 0.625Z" fill={fill}/>
   </svg>
 )
