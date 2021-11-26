75c910c8331cef6d6894e2ec668210195193975a
Santeri Horttanainen <santeri.horttanainen@reaktor.com>
2021-11-26 16:25:58 +0200

    Refactor maksatuksetPage to use similar pattern as rest of the pages

--- playwright/tests/maksatukset/maksatukset.test.ts
+++ playwright/tests/maksatukset/maksatukset.test.ts
@@ -12,20 +12,7 @@ import {
   navigate
 } from '../../utils/navigate'
 import {
-  fillInMaksueranTiedot,
-  sendMaksatukset,
-  reloadPaymentPage,
-  gotoLähetetytMaksatuksetTab,
-  getBatchPitkäViite,
-  getBatchHanke,
-  getBatchIBAN,
-  getBatchLKPTili,
-  getBatchMaksuun,
-  getBatchStatus,
-  getBatchTaKpTili,
-  getBatchToimittajanNimi,
-  getExpectedPaymentXML,
-  getTiliönti,
+  MaksatuksetPage
 } from '../../pages/maksatuksetPage'
 
 import {
@@ -48,28 +35,29 @@ test.setTimeout(400000)
 correctOVTTest.setTimeout(400000)
 
 correctOVTTest('Maksatukset uses correct OVT when the operational unit is Palvelukeskus', async ({page, hakemus: {hakemusID}, codes: codeValues}) => {
+    const maksatuksetPage = MaksatuksetPage(page)
 
     await navigate(page, "/admin-ui/payments/")
-    await fillInMaksueranTiedot(page, "asha pasha", "essi.esittelija@example.com", "hygge.hyvaksyja@example.com")
+    await maksatuksetPage.fillInMaksueranTiedot("asha pasha", "essi.esittelija@example.com", "hygge.hyvaksyja@example.com")
     const dueDate = await getElementAttribute(page, '[id="Eräpäivä"]', 'value')
     if (!dueDate) throw new Error('Cannot find due date from form')
 
-    await sendMaksatukset(page)
-    await reloadPaymentPage(page)
+    await maksatuksetPage.sendMaksatukset()
+    await maksatuksetPage.reloadPaymentPage()
 
-    await gotoLähetetytMaksatuksetTab(page)
+    await maksatuksetPage.gotoLähetetytMaksatuksetTab()
     const { "register-number": registerNumber } = await getHakemusTokenAndRegisterNumber(hakemusID)
     const pitkaviite = `${registerNumber}_1 Erkki Esimerkki`
 
-    expect(await getBatchPitkäViite(page, 1)).toEqual(pitkaviite)
-    expect(await getBatchStatus(page, 1)).toEqual("Lähetetty")
-    expect(await getBatchToimittajanNimi(page, 1)).toEqual("Akaan kaupunki")
-    expect(await getBatchHanke(page, 1)).toEqual("Rahassa kylpijät Ky Ay Oy")
-    expect(await getBatchMaksuun(page, 1)).toEqual("99,999 €")
-    expect(await getBatchIBAN(page, 1)).toEqual("FI95 6682 9530 0087 65")
-    expect(await getBatchLKPTili(page, 1)).toEqual("82010000")
-    expect(await getBatchTaKpTili(page, 1)).toEqual("29103020")
-    expect(await getTiliönti(page, 1)).toEqual("99,999 €")
+    expect(await maksatuksetPage.getBatchPitkäViite(1)).toEqual(pitkaviite)
+    expect(await maksatuksetPage.getBatchStatus(1)).toEqual("Lähetetty")
+    expect(await maksatuksetPage.getBatchToimittajanNimi(1)).toEqual("Akaan kaupunki")
+    expect(await maksatuksetPage.getBatchHanke(1)).toEqual("Rahassa kylpijät Ky Ay Oy")
+    expect(await maksatuksetPage.getBatchMaksuun(1)).toEqual("99,999 €")
+    expect(await maksatuksetPage.getBatchIBAN(1)).toEqual("FI95 6682 9530 0087 65")
+    expect(await maksatuksetPage.getBatchLKPTili(1)).toEqual("82010000")
+    expect(await maksatuksetPage.getBatchTaKpTili(1)).toEqual("29103020")
+    expect(await maksatuksetPage.getTiliönti(1)).toEqual("99,999 €")
 
 
     await putMaksupalauteToMaksatuspalveluAndProcessIt(`
@@ -82,37 +70,38 @@ correctOVTTest('Maksatukset uses correct OVT when the operational unit is Palvel
       </VA-invoice>
     `)
 
-    await reloadPaymentPage(page)
-    await gotoLähetetytMaksatuksetTab(page)
-    expect(await getBatchStatus(page, 1)).toEqual("Maksettu")
+    await maksatuksetPage.reloadPaymentPage()
+    await maksatuksetPage.gotoLähetetytMaksatuksetTab()
+    expect(await maksatuksetPage.getBatchStatus(1)).toEqual("Maksettu")
 
     const maksatukset = await getAllMaksatuksetFromMaksatuspalvelu()
-    expect(maksatukset).toContainEqual(getExpectedPaymentXML(codeValues.project, codeValues.operation, codeValues.operationalUnit, pitkaviite, `${registerNumber}_1`, dueDate, '00372769790122'))
+    expect(maksatukset).toContainEqual(maksatuksetPage.getExpectedPaymentXML(codeValues.project, codeValues.operation, codeValues.operationalUnit, pitkaviite, `${registerNumber}_1`, dueDate, '00372769790122'))
   })
 
 test('work with pitkaviite without contact person name', async ({page, avustushakuID, hakemus: {hakemusID}}) => {
+    const maksatuksetPage = MaksatuksetPage(page)
     await navigate(page, "/admin-ui/payments/")
 
-    await fillInMaksueranTiedot(page, "asha pasha", "essi.esittelija@example.com", "hygge.hyvaksyja@example.com")
+    await maksatuksetPage.fillInMaksueranTiedot("asha pasha", "essi.esittelija@example.com", "hygge.hyvaksyja@example.com")
 
-    await sendMaksatukset(page)
+    await maksatuksetPage.sendMaksatukset()
 
     await removeStoredPitkäviiteFromAllAvustushakuPayments(avustushakuID)
-    await reloadPaymentPage(page)
+    await maksatuksetPage.reloadPaymentPage()
 
-    await gotoLähetetytMaksatuksetTab(page)
+    await maksatuksetPage.gotoLähetetytMaksatuksetTab()
     const { "register-number": registerNumber } = await getHakemusTokenAndRegisterNumber(hakemusID)
     const pitkaviite = registerNumber
 
-    expect(await getBatchPitkäViite(page, 1)).toEqual(pitkaviite)
-    expect(await getBatchStatus(page, 1)).toEqual("Lähetetty")
-    expect(await getBatchToimittajanNimi(page, 1)).toEqual("Akaan kaupunki")
-    expect(await getBatchHanke(page, 1)).toEqual("Rahassa kylpijät Ky Ay Oy")
-    expect(await getBatchMaksuun(page, 1)).toEqual("99,999 €")
-    expect(await getBatchIBAN(page, 1)).toEqual("FI95 6682 9530 0087 65")
-    expect(await getBatchLKPTili(page, 1)).toEqual("82010000")
-    expect(await getBatchTaKpTili(page, 1)).toEqual("29103020")
-    expect(await getTiliönti(page, 1)).toEqual("99,999 €")
+    expect(await maksatuksetPage.getBatchPitkäViite(1)).toEqual(pitkaviite)
+    expect(await maksatuksetPage.getBatchStatus(1)).toEqual("Lähetetty")
+    expect(await maksatuksetPage.getBatchToimittajanNimi(1)).toEqual("Akaan kaupunki")
+    expect(await maksatuksetPage.getBatchHanke(1)).toEqual("Rahassa kylpijät Ky Ay Oy")
+    expect(await maksatuksetPage.getBatchMaksuun(1)).toEqual("99,999 €")
+    expect(await maksatuksetPage.getBatchIBAN(1)).toEqual("FI95 6682 9530 0087 65")
+    expect(await maksatuksetPage.getBatchLKPTili(1)).toEqual("82010000")
+    expect(await maksatuksetPage.getBatchTaKpTili(1)).toEqual("29103020")
+    expect(await maksatuksetPage.getTiliönti(1)).toEqual("99,999 €")
 
     await putMaksupalauteToMaksatuspalveluAndProcessIt(`
       <?xml version="1.0" encoding="UTF-8" standalone="no"?>
@@ -124,34 +113,35 @@ test('work with pitkaviite without contact person name', async ({page, avustusha
       </VA-invoice>
     `)
 
-    await reloadPaymentPage(page)
-    await gotoLähetetytMaksatuksetTab(page)
-    expect(await getBatchStatus(page, 1)).toEqual("Maksettu")
+    await maksatuksetPage.reloadPaymentPage()
+    await maksatuksetPage.gotoLähetetytMaksatuksetTab()
+    expect(await maksatuksetPage.getBatchStatus(1)).toEqual("Maksettu")
   })
 
 test('work with pitkaviite with contact person name', async ({page, hakemus: {hakemusID}, codes: { project, operation, operationalUnit}}) => {
+    const maksatuksetPage = MaksatuksetPage(page)
     await navigate(page, "/admin-ui/payments/")
 
-    await fillInMaksueranTiedot(page, "asha pasha", "essi.esittelija@example.com", "hygge.hyvaksyja@example.com")
+    await maksatuksetPage.fillInMaksueranTiedot("asha pasha", "essi.esittelija@example.com", "hygge.hyvaksyja@example.com")
     const dueDate = await getElementAttribute(page, '[id="Eräpäivä"]', 'value')
     if (!dueDate) throw new Error('Cannot find due date from form')
 
-    await sendMaksatukset(page)
-    await reloadPaymentPage(page)
+    await maksatuksetPage.sendMaksatukset()
+    await maksatuksetPage.reloadPaymentPage()
 
-    await gotoLähetetytMaksatuksetTab(page)
+    await maksatuksetPage.gotoLähetetytMaksatuksetTab()
     const { "register-number": registerNumber } = await getHakemusTokenAndRegisterNumber(hakemusID)
     const pitkaviite = `${registerNumber}_1 Erkki Esimerkki`
 
-    expect(await getBatchPitkäViite(page, 1)).toEqual(pitkaviite)
-    expect(await getBatchStatus(page, 1)).toEqual("Lähetetty")
-    expect(await getBatchToimittajanNimi(page, 1)).toEqual("Akaan kaupunki")
-    expect(await getBatchHanke(page, 1)).toEqual("Rahassa kylpijät Ky Ay Oy")
-    expect(await getBatchMaksuun(page, 1)).toEqual("99,999 €")
-    expect(await getBatchIBAN(page, 1)).toEqual("FI95 6682 9530 0087 65")
-    expect(await getBatchLKPTili(page, 1)).toEqual("82010000")
-    expect(await getBatchTaKpTili(page, 1)).toEqual("29103020")
-    expect(await getTiliönti(page, 1)).toEqual("99,999 €")
+    expect(await maksatuksetPage.getBatchPitkäViite(1)).toEqual(pitkaviite)
+    expect(await maksatuksetPage.getBatchStatus(1)).toEqual("Lähetetty")
+    expect(await maksatuksetPage.getBatchToimittajanNimi(1)).toEqual("Akaan kaupunki")
+    expect(await maksatuksetPage.getBatchHanke(1)).toEqual("Rahassa kylpijät Ky Ay Oy")
+    expect(await maksatuksetPage.getBatchMaksuun(1)).toEqual("99,999 €")
+    expect(await maksatuksetPage.getBatchIBAN(1)).toEqual("FI95 6682 9530 0087 65")
+    expect(await maksatuksetPage.getBatchLKPTili(1)).toEqual("82010000")
+    expect(await maksatuksetPage.getBatchTaKpTili(1)).toEqual("29103020")
+    expect(await maksatuksetPage.getTiliönti(1)).toEqual("99,999 €")
 
 
     await putMaksupalauteToMaksatuspalveluAndProcessIt(`
@@ -164,11 +154,11 @@ test('work with pitkaviite with contact person name', async ({page, hakemus: {ha
       </VA-invoice>
     `)
 
-    await reloadPaymentPage(page)
-    await gotoLähetetytMaksatuksetTab(page)
-    expect(await getBatchStatus(page, 1)).toEqual("Maksettu")
+    await maksatuksetPage.reloadPaymentPage()
+    await maksatuksetPage.gotoLähetetytMaksatuksetTab()
+    expect(await maksatuksetPage.getBatchStatus(1)).toEqual("Maksettu")
 
     const maksatukset = await getAllMaksatuksetFromMaksatuspalvelu()
-    expect(maksatukset).toContainEqual(getExpectedPaymentXML(project, operation, operationalUnit, pitkaviite, `${registerNumber}_1`, dueDate))
+    expect(maksatukset).toContainEqual(maksatuksetPage.getExpectedPaymentXML(project, operation, operationalUnit, pitkaviite, `${registerNumber}_1`, dueDate))
   })
 
