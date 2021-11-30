622407983d143cd8d24af3b9ab2a534f7e19c689
Tomi Karlstedt <tomi.karlstedt@reaktor.com>
2021-11-30 10:40:26 +0200

    VA-153-2 Rename salt variable

--- va-common/src/oph/common/string.clj
+++ va-common/src/oph/common/string.clj
@@ -19,7 +19,14 @@
   (str token (datetime/date-string (datetime/now))))
 
 (defn derive-token-hash [token]
-  (subs (hashers/derive (get-hashed-str token) {:salt (:token-hash-secret config) :alg :bcrypt+sha512}) 14))
+  (subs
+    (hashers/derive
+      (get-hashed-str token)
+      {:salt (:officer-token-salt config) :alg :bcrypt+sha512})
+    14))
 
 (defn verify-token-hash [token hash]
-  (hashers/verify (get-hashed-str token) (str "bcrypt+sha512$" hash) {:salt (:token-hash-secret config)}))
+  (hashers/verify
+    (get-hashed-str token)
+    (str "bcrypt+sha512$" hash)
+    {:salt (:officer-token-salt config)}))
