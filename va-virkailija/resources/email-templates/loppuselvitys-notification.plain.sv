945d36cf05e36fd9775d5ad9e1d3e9fb71b132fc
Henry Heikkinen <henry.heikkinen@reaktor.com>
2021-12-20 09:28:14 +0200

    VA-170 Update loppuselvitys notification email
    Modified as requested on Flowdock

--- va-virkailija/resources/email-templates/loppuselvitys-notification.plain.sv
+++ va-virkailija/resources/email-templates/loppuselvitys-notification.plain.sv
@@ -4,6 +4,8 @@ Ni kan nu fylla i blanketten för slutredovisning för projektet {{ register-num
 
 Blanketten för slutredovisning finns på adressen {{& url }} och den ska lämnas in senast {{ selvitysdate }}.
 
+Förutnämnda slutdatum gäller projekt som inte har beviljats förlängning av understödets användningstid. Projekt som har beviljats förlängd användningstid ska bevara den bifogade länken till slutredovisningsblanketten och lämna in slutredovisningen i enlighet med tidtabellen som anges i ändringsbeslutet.
+
 Med vänlig hälsning
 
 {{ presenter-name }}
