3972e3acda0634b49434a5f7e778ec6d8a634bf0
Santeri Horttanainen <santeri.horttanainen@reaktor.com>
2022-01-05 21:04:55 +0200

    VA-138-5: drop alkamis and paattymispaiva constraints for migration

--- va-common/resources/db/migration/virkailija/V1_135__migrate_loppuselvitysdate_and_valiselvitysdate.sql
+++ va-common/resources/db/migration/virkailija/V1_135__migrate_loppuselvitysdate_and_valiselvitysdate.sql
@@ -4,6 +4,9 @@ ALTER TABLE avustushaut DROP COLUMN loppuselvitysdate;
 ALTER TABLE avustushaut ADD valiselvitysdate DATE;
 ALTER TABLE avustushaut ADD loppuselvitysdate DATE;
 
+ALTER TABLE avustushaut DROP CONSTRAINT nn_alkamispaiva;
+ALTER TABLE avustushaut DROP CONSTRAINT nn_paattymispaiva;
+
 UPDATE avustushaut 
 SET valiselvitysdate = to_date(selvitysdate_migration.valiselvitysdate, 'DD.MM.YYYY')
 FROM selvitysdate_migration
@@ -17,3 +20,9 @@ FROM selvitysdate_migration
 WHERE avustushaut.id = selvitysdate_migration.avustushaku_id
   AND selvitysdate_migration.loppuselvitysdate IS NOT NULL
   AND selvitysdate_migration.loppuselvitysdate SIMILAR TO '[0-9][0-9]\.[0-9][0-9]\.[0-9][0-9][0-9][0-9]';
+
+ALTER TABLE avustushaut ADD CONSTRAINT nn_alkamispaiva
+    check (hankkeen_alkamispaiva IS NOT NULL OR (status != 'published' AND status != 'resolved')) NOT VALID;
+
+ALTER TABLE avustushaut ADD CONSTRAINT nn_paattymispaiva
+    check (hankkeen_paattymispaiva IS NOT NULL OR (status != 'published' AND status != 'resolved')) NOT VALID;
