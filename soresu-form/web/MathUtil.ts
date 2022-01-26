9df426955127fca4e683c99ef2131fb2aad73c2f
Antero Fagerstedt <antero.fagerstedt@reaktor.com>
2022-01-26 16:09:53 +0200

    VA-134-2-2: Refactored MathUtils from class to object

--- soresu-form/web/MathUtil.ts
+++ soresu-form/web/MathUtil.ts
@@ -2,64 +2,77 @@ import _ from 'lodash'
 import Big, {BigSource} from 'big.js'
 
 interface Fraction {
-  nominator: number,
+  nominator: number
   denominator: number
 }
 
-export default class MathUtil {
-  static decimalShareRoundedUpOf(fraction: BigSource, total: BigSource): number {
-    const multiplied = Big(fraction).times(total)
+function decimalShareRoundedUpOf(fraction: BigSource, total: BigSource): number {
+  const multiplied = Big(fraction).times(total)
 
-    // detect rounding error: is the value in `multiplied` coercable to an integer?
-    const n1 = Number(multiplied)
-    const n2 = parseInt(multiplied.toString(), 10)
+  // detect rounding error: is the value in `multiplied` coercable to an integer?
+  const n1 = Number(multiplied)
+  const n2 = parseInt(multiplied.toString(), 10)
 
-    return n1 === n2 ? n1 : Number(multiplied.round(0, 3))
-  }
+  return n1 === n2 ? n1 : Number(multiplied.round(0, 3))
+}
 
-  static ratioShareRoundedUpOf(ratio: Fraction, total: BigSource) {
-    return MathUtil.decimalShareRoundedUpOf(Big(ratio.nominator).div(ratio.denominator), total)
-  }
+function ratioShareRoundedUpOf(ratio: Fraction, total: BigSource) {
+  return decimalShareRoundedUpOf(Big(ratio.nominator).div(ratio.denominator), total)
+}
 
-  static percentageShareRoundedUpOf(percentage: number, total: BigSource) {
-    return MathUtil.decimalShareRoundedUpOf(Big(percentage).div(100), total)
-  }
+function percentageShareRoundedUpOf(percentage: number, total: BigSource) {
+  return decimalShareRoundedUpOf(Big(percentage).div(100), total)
+}
 
-  static percentageOf(part: number, total: number): number {
-    return (part / total) * 100
-  }
+function percentageOf(part: number, total: number): number {
+  return (part / total) * 100
+}
 
-  static isNumeric(n: any): boolean {
-    const num = MathUtil.parseDecimal(n)
-    return !isNaN(num) && isFinite(num)
-  }
+function isNumeric(n: any): boolean {
+  const num = parseDecimal(n)
+  return !isNaN(num) && isFinite(num)
+}
 
-  static roundDecimal(number: number, digits: number, roundingMode: 'round' | 'floor' | 'ceil' = "round"): number {
-    return Number(Math[roundingMode](Number(number + "e" + digits)) + "e-" + digits)
-  }
+function roundDecimal(number: number, digits: number, roundingMode: 'round' | 'floor' | 'ceil' = "round"): number {
+  return Number(Math[roundingMode](Number(number + "e" + digits)) + "e-" + digits)
+}
 
-  static parseDecimal(value: any): number {
-    return _.isNumber(value)
-      ? (value === 0 ? 0 : value)
-      : parseFloat(normalizeDecimalStr("" + value))
-  }
+function parseDecimal(value: any): number {
+  return _.isNumber(value)
+    ? (value === 0 ? 0 : value)
+    : parseFloat(normalizeDecimalStr("" + value))
+}
 
-  static formatDecimal(number: number, separator = ","): string {
-    return ("" + number).replace(".", separator)
-  }
+function formatDecimal(number: number, separator = ","): string {
+  return ("" + number).replace(".", separator)
+}
 
-  static representsInteger(value: string): boolean {
-    const asString = "" + value
-    return parseInt(asString, 10).toString() === asString
-  }
+function representsInteger(value: string): boolean {
+  const asString = "" + value
+  return parseInt(asString, 10).toString() === asString
+}
 
-  static representsDecimal(value: string): boolean {
-    const normalized = normalizeDecimalStr("" + value)
-    const dotZeroTrimmed = normalized.replace(/\.0+$/, "")
-    return MathUtil.parseDecimal(normalized).toString() === dotZeroTrimmed
-  }
+function representsDecimal(value: string): boolean {
+  const normalized = normalizeDecimalStr("" + value)
+  const dotZeroTrimmed = normalized.replace(/\.0+$/, "")
+  return parseDecimal(normalized).toString() === dotZeroTrimmed
 }
 
 function normalizeDecimalStr(str: string): string {
   return str.replace(/,/g, ".")
 }
+
+const MathUtil = {
+  roundDecimal,
+  isNumeric,
+  representsInteger,
+  percentageShareRoundedUpOf,
+  ratioShareRoundedUpOf,
+  representsDecimal,
+  formatDecimal,
+  percentageOf,
+  decimalShareRoundedUpOf,
+  parseDecimal,
+}
+
+export default MathUtil
