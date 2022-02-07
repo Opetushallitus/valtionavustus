4be540484ef6aca3b2e72091be5251fdfb2da0e3
Ville Aaltonen <ville.aaltonen@reaktor.com>
2022-02-07 15:47:35 +0200

    VA-137-4: test laheta-loppuselvityspyynnot notification is send only after sending päätös

--- playwright/tests/email-notifications/laheta-loppuselvityspyynnot.test.ts
+++ playwright/tests/email-notifications/laheta-loppuselvityspyynnot.test.ts
@@ -8,6 +8,7 @@ import {
   lastOrFail
 } from "../../utils/emails";
 import {expectToBeDefined} from "../../utils/util";
+import {HakemustenArviointiPage} from "../../pages/hakemustenArviointiPage";
 
 const sendLahetaLoppuselvityspyynnotNotifications = (page: Page) =>
   page.request.post(`${VIRKAILIJA_URL}/api/test/send-laheta-loppuselvityspyynnot-notifications`, { failOnStatusCode: true })
@@ -79,5 +80,44 @@ Ongelmatilanteissa saat apua osoitteesta: valtionavustukset@oph.fi
       })
     })
   })
+  test.describe('when sending päätös', async () => {
+    loppuselvitysTest('send the notification only after sending päätös', async ({closedAvustushaku, page, avustushakuID, answers, ukotettuValmistelija}) => {
+      expectToBeDefined(closedAvustushaku)
+      await test.step('set loppuselvitys date', async () => {
+        const hakujenHallinta = new HakujenHallintaPage(page)
+        await hakujenHallinta.navigateToPaatos(avustushakuID)
+        await hakujenHallinta.setLoppuselvitysDate(moment().add(3, 'months').format('DD.MM.YYYY'))
+        await hakujenHallinta.waitForSave()
+      })
 
+      await test.step('make sure notifications are not send before päätös', async () => {
+        const emailsBefore = await getLahetaLoppuselvityspyynnotEmails(avustushakuID)
+        await sendLahetaLoppuselvityspyynnotNotifications(page)
+        const emailsAfter = await getLahetaLoppuselvityspyynnotEmails(avustushakuID)
+        expect(emailsBefore).toHaveLength(emailsAfter.length)
+      })
+
+      await test.step('send päätös', async () => {
+        const hakemustenArviointiPage = new HakemustenArviointiPage(page)
+        await hakemustenArviointiPage.navigate(avustushakuID)
+        const hakemusID = await hakemustenArviointiPage.acceptAvustushaku(avustushakuID, answers.projectName)
+
+        const hakujenHallintaPage = new HakujenHallintaPage(page)
+        await hakujenHallintaPage.navigate(avustushakuID)
+        await hakujenHallintaPage.resolveAvustushaku()
+
+        await hakemustenArviointiPage.navigate(avustushakuID)
+        await hakemustenArviointiPage.selectValmistelijaForHakemus(avustushakuID, hakemusID, ukotettuValmistelija)
+
+        await hakujenHallintaPage.navigateToPaatos(avustushakuID)
+
+        await hakujenHallintaPage.sendPaatos(avustushakuID)
+      })
+
+      const emailsBefore = await getLahetaLoppuselvityspyynnotEmails(avustushakuID)
+      await sendLahetaLoppuselvityspyynnotNotifications(page)
+      const emailsAfter = await getLahetaLoppuselvityspyynnotEmails(avustushakuID)
+      expect(emailsAfter.length).toBeGreaterThan(emailsBefore.length)
+    })
+  })
 })
