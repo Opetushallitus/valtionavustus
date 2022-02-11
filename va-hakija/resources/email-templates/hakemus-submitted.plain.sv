cf18a4574bba81c1ed0902d6f4a2c91e52da28ac
Tomi Karlstedt <tomi.karlstedt@reaktor.com>
2022-02-11 09:54:45 +0200

    VA-235 Add help text to hakemus-submitted templates

--- va-hakija/resources/email-templates/hakemus-submitted.plain.sv
+++ va-hakija/resources/email-templates/hakemus-submitted.plain.sv
@@ -4,6 +4,8 @@ Ansökningstid: {{ start-date }} klo {{ start-time }} — {{ end-date }} klo {{ 
 
 Du kan se ansökan via länken: {{& url }}
 
+Den sökande kan via länken uppdatera sin ansökan så länge ansökningstiden pågår. Då ska ansökan ändå inte skickas för behandling på nytt, utan efter att ansökan bearbetats ska man med hjälp av logguppgifterna uppe i hörnet av ansökningsblanketten kontrollera att uppdateringarna har sparats.
+
 Den föredragande som ansvarar för ansökningen har nämnts i ansökningsmeddelandet.
 
 Utbildningsstyrelsen
