50539c296546521377ab8bcf5c6fbde1cd910338
Tomi Karlstedt <tomi.karlstedt@reaktor.com>
2022-03-24 15:21:18 +0200

    VA-57-2 Use mocked user cache in vastuuvalmistelija tests

--- playwright/tests/vastuuvalmistelija.test.ts
+++ playwright/tests/vastuuvalmistelija.test.ts
@@ -3,7 +3,9 @@ import { test, expect } from '@playwright/test'
 import { defaultValues } from '../fixtures/defaultValues'
 import { HakujenHallintaPage } from '../pages/hakujenHallintaPage'
 
-defaultValues('Vastuuvalmistelija role', async ({ page }) => {
+defaultValues('Vastuuvalmistelija role', async ({ page, userCache }) => {
+  expect(userCache).toBeDefined()
+
   const hakujenHallinta = new HakujenHallintaPage(page)
   await hakujenHallinta.copyEsimerkkihaku()
 
@@ -13,16 +15,16 @@ defaultValues('Vastuuvalmistelija role', async ({ page }) => {
   })
 
   await test.step('can be set for a new user', async () => {
-    await hakujenHallinta.setVastuuvalmistelija('Karslted')
+    await hakujenHallinta.setVastuuvalmistelija('Viivi')
     await hakujenHallinta.page.reload()
-    await expect(hakujenHallinta.page.locator('[data-test-id="vastuuvalmistelija-name"]')).toHaveValue('Tomi Karslted')
-    await expect(hakujenHallinta.page.locator('[data-test-id="vastuuvalmistelija-email"]')).toHaveValue('')
+    await expect(hakujenHallinta.page.locator('[data-test-id="vastuuvalmistelija-name"]')).toHaveValue('Viivi Virkailija')
+    await expect(hakujenHallinta.page.locator('[data-test-id="vastuuvalmistelija-email"]')).toHaveValue('viivi.virkailja@exmaple.com')
   })
 
   await test.step('can not be set if the new user is a valmistelija already', async () => {
-    await hakujenHallinta.addValmistelija('Henry')
-    await hakujenHallinta.searchUsersForVastuuvalmistelija('Henry')
-    await expect(hakujenHallinta.page.locator('#vastuuvalmistelija li[title="Henry Heikkinen (VA-käyttäjä, oid 1.2.246.562.24.51126009730) (Käyttäjä on jo lisätty avustushakuun)"]')).toHaveAttribute('class', 'disabled')
+    await hakujenHallinta.addValmistelija('Päivi')
+    await hakujenHallinta.searchUsersForVastuuvalmistelija('Päivi')
+    await expect(hakujenHallinta.page.locator('#vastuuvalmistelija li[data-test-id="1.2.246.562.24.99000000001"]')).toHaveAttribute('class', 'disabled')
     await hakujenHallinta.clearUserSearchForVastuuvalmistelija()
   })
 
@@ -35,8 +37,8 @@ defaultValues('Vastuuvalmistelija role', async ({ page }) => {
   })
 
   await test.step('can not be set as a valmistelija', async () => {
-    await hakujenHallinta.searchUsersForRoles('Karslted')
-    await expect(hakujenHallinta.page.locator('li[title="Tomi Karslted (VA-käyttäjä, oid 1.2.246.562.24.67715408891) (Käyttäjä on jo lisätty avustushakuun)"]')).toHaveAttribute('class', 'disabled')
+    await hakujenHallinta.searchUsersForRoles('Viivi')
+    await expect(hakujenHallinta.page.locator('#roles-list li[data-test-id="1.2.246.562.24.99000000002"]')).toHaveAttribute('class', 'disabled')
     await hakujenHallinta.clearUserSearchForRoles()
   })
 })
