ed1c4aa925e090b85fd7ae69b209bf2bd3b45182
Tomi Karlstedt <tomi.karlstedt@reaktor.com>
2022-04-07 12:37:06 +0300

    Temporary revert to test dependencycheck
    Revert "OPHY-458 Upgrade postgres driver to fix CVE-2022-21724"
    
    This reverts commit 163509b0d73f76f1722cc4ee14e02bb2d45bf9f4.

--- parent-project.clj
+++ parent-project.clj
@@ -57,7 +57,7 @@
                          ;; database
                          [hikari-cp "1.8.1"]
                          [org.flywaydb/flyway-core "4.2.0"]
-                         [org.postgresql/postgresql "42.3.3"]
+                         [org.postgresql/postgresql "42.2.23"]
                          [yesql "0.5.3"]
 
                          ;; testing
