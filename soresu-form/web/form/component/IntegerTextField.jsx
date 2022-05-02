92a1d59abd7b9dfa30d0074befb5d8c7ca298789
Simo-Pekka Kerkelä <simo-pekka.kerkela@reaktor.com>
2022-05-02 17:33:37 +0300

    OPHY-462-1 convert IntegerTextField.jsx to tsx

--- soresu-form/web/form/component/IntegerTextField.jsx
+++
@@ -1,7 +0,0 @@
-import BasicTextField from "./BasicTextField";
-
-export default class IntegerTextField extends BasicTextField {
-  baseClassName() {
-    return "soresu-integer-field";
-  }
-}
---
+++ soresu-form/web/form/component/IntegerTextField.tsx
@@ -0,0 +1,9 @@
+import BasicTextField, { BasicTextFieldProps } from "./BasicTextField";
+
+interface IntegerTextFieldProps extends BasicTextFieldProps {}
+
+export default class IntegerTextField extends BasicTextField<IntegerTextFieldProps> {
+  baseClassName() {
+    return "soresu-integer-field";
+  }
+}
