3149773549cf277fdf1643183747bb37c447a2f5
Antero Fagerstedt <antero.fagerstedt@reaktor.com>
2022-05-05 11:47:15 +0300

    OPHY-462-1: Validate that money fields in VA support max 7 digits

--- soresu-form/web/form/MoneyValidator.ts
+++ soresu-form/web/form/MoneyValidator.ts
@@ -5,5 +5,13 @@ export function validateMoney(input: string) {
 }
 
 export function isValidMoney(input: string): boolean {
+  return isInteger(input) && hasMaxSevenDigits(parseInt(input, 10))
+}
+
+function isInteger(input: string): boolean {
   return /^[0-9]*$/.test(input) && isNumeric(input);
+}
+
+function hasMaxSevenDigits(input: number): boolean {
+  return input <= 9999999
 }
