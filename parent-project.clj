e29573c5fc5b7b970b8796bb1692caf5567d702b
Tomi Karlstedt <tomi.karlstedt@reaktor.com>
2022-05-12 10:18:57 +0300

    OPHY-477 Upgrade guava to see if it fixes any of the CVEs

--- parent-project.clj
+++ parent-project.clj
@@ -103,7 +103,7 @@
                          [com.cemerick/piggieback "0.2.2"]
                          [binaryage/devtools "0.9.10"]
                          [figwheel-sidecar "0.5.16"]
-                         [com.google.guava/guava "30.1.1-jre"
+                         [com.google.guava/guava "31.1-jre"
                           :exclusions [com.google.code.findbugs/jsr305]]
 
                          ;; other
