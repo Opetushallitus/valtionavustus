e3747c996b8e55d0d595ba546dfa1629ad99b6ac
Tomi Karlstedt <tomi.karlstedt@reaktor.com>
2022-06-20 16:01:56 +0300

    VA-260-2 Really remove arvioitu_maksupaiva from external api

--- va-virkailija/resources/sql/virkailija/grants/get-grants.sql
+++ va-virkailija/resources/sql/virkailija/grants/get-grants.sql
@@ -2,11 +2,7 @@ SELECT
   h.id, h.created_at, h.form, h.status, h.register_number, h.valiselvitysdate,
   h.loppuselvitysdate, h.form_loppuselvitys, h.form_valiselvitys,
   h.is_academysize, h.haku_type, operational_unit_id, project_id, operation_id,
-<<<<<<< Updated upstream
-  h.allow_visibility_in_external_system, h.arvioitu_maksupaiva
-=======
   h.allow_visibility_in_external_system
->>>>>>> Stashed changes
 FROM
   hakija.avustushaut h
 WHERE
