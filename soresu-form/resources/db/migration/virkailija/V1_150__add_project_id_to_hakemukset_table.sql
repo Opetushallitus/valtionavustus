98e51be2f8dec19a01a0939117999f3b84af9323
Antero Fagerstedt <antero.fagerstedt@reaktor.com>
2022-08-02 14:30:42 +0300

    VA-286-5: Fixed latest DB migration ID as duplicate IDs are not allowed

---
+++ soresu-form/resources/db/migration/virkailija/V1_150__add_project_id_to_hakemukset_table.sql
@@ -0,0 +1,9 @@
+alter table hakija.hakemukset
+add column project_id integer,
+add constraint project_id_fk foreign key (project_id) references virkailija.va_code_values (id);
+
+update hakija.hakemukset as h
+set project_id = a.project_id
+from hakija.avustushaut a
+where a.project_id is not null
+and h.avustushaku = a.id;
