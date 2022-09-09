2a06d100e46819560d7fc3e1bf0e8bee454034ce
Henry Heikkinen <henry.heikkinen@reaktor.com>
2022-09-09 13:44:03 +0300

    OPHY-527 Update snakeyaml to newest version and suppress last remaining vulnerability for month to wait for patch

--- parent-project.clj
+++ parent-project.clj
@@ -34,6 +34,7 @@
                          [compojure "1.6.2"]
                          [http-kit "2.5.3"]
                          [metosin/compojure-api "1.1.13"]
+                         [org.yaml/snakeyaml "1.31"] ; dependency of compojure-api -> explicitly updated for security patches
                          [ring/ring-codec "1.1.3"]
                          [ring/ring-core "1.8.1"]
                          [ring/ring-devel "1.8.1" :exclusions [hiccup]]
