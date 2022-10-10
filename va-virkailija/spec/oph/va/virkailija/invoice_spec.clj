0d3c8976dbb697dfe10e75fa21f4ba0ca7cec242
Antero Fagerstedt <antero.fagerstedt@reaktor.com>
2022-10-10 17:15:31 +0300

    VA-286-8-1: Leiningen invoice XML tests expect the correct project-code which was assigned to the hakemus, not the project-code which was assigned to the avustushaku

--- va-virkailija/spec/oph/va/virkailija/invoice_spec.clj
+++ va-virkailija/spec/oph/va/virkailija/invoice_spec.clj
@@ -287,7 +287,7 @@
                  [:postingAmount 20000]
                  [:accountingObject01 "6600100130"]
                  [:accountingObject02 "29103013"]
-                 [:accountingObject04 "6600A-M2024"]
+                 [:accountingObject04 (:project-code application-with-evaluation)]
                  [:accountingObject05 "6600151502"]
                  [:accountingObject08 "None"]
               ]]]]]
@@ -296,7 +296,6 @@
                :application application-with-evaluation
                :grant (-> grant
                           (assoc
-                            :project {:code "6600A-M2024"}
                             :operational-unit {:code "6600100130   "}
                             :operation {:code "6600151502"}
                             :lkp-account "82510000")
@@ -352,11 +351,10 @@
                              :application application
                              :grant (assoc grant
                                            :operational-unit {:code "123456789 "}
-                                           :project {:code "23456789"}
                                            :operation {:code "3456789"})
                              :batch (assoc batch :documents documents)})]
           (should=
-            (str "<?xml version=\"1.0\" encoding=\"UTF-8\"?><objects><object><header><toEdiID>003727697901</toEdiID><invoiceType>INVOICE</invoiceType><vendorName>Test Organisation</vendorName><addressFields><addressField1>Someroad 1</addressField1><addressField2>Some City</addressField2><addressField5>Some Country</addressField5></addressFields><vendorRegistrationId>1234567-1</vendorRegistrationId><bic>OKOYFIHH</bic><bankAccount>FI4250001510000023</bankAccount><invoiceNumber>123/456/78_1</invoiceNumber><longReference>123/456/78_1</longReference><documentDate>2018-04-16</documentDate><dueDate>2018-04-30</dueDate><paymentTerm>Z001</paymentTerm><currencyCode>EUR</currencyCode><grossAmount>20000</grossAmount><netamount>20000</netamount><vatamount>0</vatamount><voucherSeries>XE</voucherSeries><postingDate>2018-04-16</postingDate><ownBankShortKeyCode></ownBankShortKeyCode><handler><verifierName>presenter@local</verifierName><verifierEmail>presenter@local</verifierEmail><approverName>acceptor@local</approverName><approverEmail>acceptor@local</approverEmail><verifyDate>", (now-date) "</verifyDate><approvedDate>", (now-date), "</approvedDate></handler><otsData><otsBankCountryKeyCode>FI</otsBankCountryKeyCode></otsData><invoicesource>VA</invoicesource></header><postings><postingRows><postingRow><rowId>1</rowId><generalLedgerAccount>82310000</generalLedgerAccount><postingAmount>20000</postingAmount><accountingObject01>123456789</accountingObject01><accountingObject02>29103013</accountingObject02><accountingObject04>23456789</accountingObject04><accountingObject05>3456789</accountingObject05><accountingObject08>123456</accountingObject08></postingRow></postingRows></postings></object></objects>" )
+            (str "<?xml version=\"1.0\" encoding=\"UTF-8\"?><objects><object><header><toEdiID>003727697901</toEdiID><invoiceType>INVOICE</invoiceType><vendorName>Test Organisation</vendorName><addressFields><addressField1>Someroad 1</addressField1><addressField2>Some City</addressField2><addressField5>Some Country</addressField5></addressFields><vendorRegistrationId>1234567-1</vendorRegistrationId><bic>OKOYFIHH</bic><bankAccount>FI4250001510000023</bankAccount><invoiceNumber>123/456/78_1</invoiceNumber><longReference>123/456/78_1</longReference><documentDate>2018-04-16</documentDate><dueDate>2018-04-30</dueDate><paymentTerm>Z001</paymentTerm><currencyCode>EUR</currencyCode><grossAmount>20000</grossAmount><netamount>20000</netamount><vatamount>0</vatamount><voucherSeries>XE</voucherSeries><postingDate>2018-04-16</postingDate><ownBankShortKeyCode></ownBankShortKeyCode><handler><verifierName>presenter@local</verifierName><verifierEmail>presenter@local</verifierEmail><approverName>acceptor@local</approverName><approverEmail>acceptor@local</approverEmail><verifyDate>", (now-date) "</verifyDate><approvedDate>", (now-date), "</approvedDate></handler><otsData><otsBankCountryKeyCode>FI</otsBankCountryKeyCode></otsData><invoicesource>VA</invoicesource></header><postings><postingRows><postingRow><rowId>1</rowId><generalLedgerAccount>82310000</generalLedgerAccount><postingAmount>20000</postingAmount><accountingObject01>123456789</accountingObject01><accountingObject02>29103013</accountingObject02><accountingObject04>" (:project-code application) "</accountingObject04><accountingObject05>3456789</accountingObject05><accountingObject08>123456</accountingObject08></postingRow></postingRows></postings></object></objects>" )
             (xml/emit-str xml-invoice))))))
 
 (describe
