1213a966c2fdd63231ad86773760f7852ac9c26b
Antero Fagerstedt <antero.fagerstedt@reaktor.com>
2022-11-04 17:10:16 +0200

    OPHY-547-2: Test that project_id is not set to null when täydennyspyyntö has been sent

---
+++ playwright/tests/hakemuksen-arviointi/sending-taydennyspyynto-should-not-nullify-project_id.test.ts
@@ -0,0 +1,38 @@
+import { HakemustenArviointiPage } from "../../pages/hakemustenArviointiPage";
+import { submittedHakemusTest } from "../../fixtures/muutoshakemusTest";
+import { expectToBeDefined } from "../../utils/util";
+import { VIRKAILIJA_URL } from "../../utils/constants";
+import { expect } from "@playwright/test";
+
+submittedHakemusTest(
+  "Sending täydennyspyyntö does not nullify project_id from hakemus",
+  async ({ page, avustushakuID, closedAvustushaku }) => {
+    expectToBeDefined(closedAvustushaku.id);
+
+    const arviointiPage = new HakemustenArviointiPage(page);
+    await arviointiPage.navigateToLatestHakemusArviointi(avustushakuID);
+    const hakemusID = await arviointiPage.getHakemusID();
+
+    await arviointiPage.fillTäydennyspyyntöField(
+      "Voisitko täydentää hakemukseesi tiedot tilattavista pitsoista ja nunchakuista"
+    );
+    await arviointiPage.clickToSendTäydennyspyyntö(avustushakuID, hakemusID);
+
+    const response = await page.request.get(
+      `${VIRKAILIJA_URL}/api/avustushaku/${avustushakuID}/hakemus/${hakemusID}/project`
+    );
+    const project: ProjectResponse = await response.json();
+
+    expect(typeof project.id).toBe("number");
+    expect(project.id).toBeGreaterThan(1);
+  }
+);
+
+interface ProjectResponse {
+  id: number;
+  "value-type": string;
+  year: number;
+  code: string;
+  "code-value": string;
+  hidden: boolean | undefined;
+}
