ead20b3865dd5f63d101a4dc734c883057f02202
Ville Aaltonen <ville.aaltonen@reaktor.com>
2022-11-18 12:27:05 +0200

    VA-288-2: mark flaky test as fixme

--- playwright/tests/update-at-same-on-paatos-valiselvitys-loppuselvitys-tabs.test.ts
+++ playwright/tests/update-at-same-on-paatos-valiselvitys-loppuselvitys-tabs.test.ts
@@ -2,28 +2,33 @@ import { expect } from "@playwright/test";
 import { muutoshakemusTest as test } from "../fixtures/muutoshakemusTest";
 import { HakujenHallintaPage } from "../pages/hakujenHallintaPage";
 
-test("Shows the same updated date on the Päätös tab as on the Väliselvitys and Loppuselvitys tabs", async ({
-  page,
-  avustushakuID,
-}) => {
-  const hakujenHallintaPage = new HakujenHallintaPage(page);
+/*
+  selvitykset are not actually updated at the same time as paatos
+  and there can be a one-minute difference between the timestamps
+*/
+test.fixme(
+  "Shows the same updated date on the Päätös tab as on the Väliselvitys and Loppuselvitys tabs",
+  async ({ page, avustushakuID }) => {
+    const hakujenHallintaPage = new HakujenHallintaPage(page);
 
-  await hakujenHallintaPage.navigateToPaatos(avustushakuID);
-  const paatosUpdatedAt = await hakujenHallintaPage.paatosUpdatedAt
-    .locator(".date")
-    .textContent();
-
-  await hakujenHallintaPage.switchToValiselvitysTab();
-  const valiselvitysUpdatedAt = await hakujenHallintaPage.valiselvitysUpdatedAt
-    .locator(".date")
-    .textContent();
-
-  await hakujenHallintaPage.switchToLoppuselvitysTab();
-  const loppuselvitysUpdatedAt =
-    await hakujenHallintaPage.loppuselvitysUpdatedAt
+    await hakujenHallintaPage.navigateToPaatos(avustushakuID);
+    const paatosUpdatedAt = await hakujenHallintaPage.paatosUpdatedAt
       .locator(".date")
       .textContent();
 
-  expect(paatosUpdatedAt).toEqual(valiselvitysUpdatedAt);
-  expect(paatosUpdatedAt).toEqual(loppuselvitysUpdatedAt);
-});
+    await hakujenHallintaPage.switchToValiselvitysTab();
+    const valiselvitysUpdatedAt =
+      await hakujenHallintaPage.valiselvitysUpdatedAt
+        .locator(".date")
+        .textContent();
+
+    await hakujenHallintaPage.switchToLoppuselvitysTab();
+    const loppuselvitysUpdatedAt =
+      await hakujenHallintaPage.loppuselvitysUpdatedAt
+        .locator(".date")
+        .textContent();
+
+    expect(paatosUpdatedAt).toEqual(valiselvitysUpdatedAt);
+    expect(paatosUpdatedAt).toEqual(loppuselvitysUpdatedAt);
+  }
+);
