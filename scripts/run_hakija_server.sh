508b1de624e36402d8dba009dea7427afb345cdb
Timo Suomela <timo.suomela@reaktor.com>
2023-01-02 16:31:58 +0200

    No need to build a separate common library

--- scripts/run_hakija_server.sh
+++ scripts/run_hakija_server.sh
@@ -5,8 +5,6 @@ source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/common-functions.sh"
 function main {
   wait_for_container_to_be_healthy va-postgres
 
-  cd "$repo/soresu-form"
-  ../lein install
   cd "$repo"
   ./lein with-profile hakija-dev run -m oph.va.hakija.main
 }
