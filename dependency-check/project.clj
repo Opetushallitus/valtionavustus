1165ca13c2f14e4d6784ce30e9463fa06078f230
Ville Aaltonen <ville.aaltonen@reaktor.com>
2023-01-10 09:51:31 +0200

    OPHY-575: update dependency check to fix database exception;
    Build failed cause of https://github.com/jeremylong/DependencyCheck/issues/5220

--- dependency-check/project.clj
+++ dependency-check/project.clj
@@ -1,3 +1,6 @@
 (defproject nvd-helper "local"
   :description "nvd-clojure helper project"
-  :dependencies [[nvd-clojure "2.9.0"]])
+  :dependencies [
+                  [nvd-clojure "2.11.0"]
+                  [org.owasp/dependency-check-core "7.4.4"]
+                ])
