60f05ec8598fec931c5f1dcc5136662ef7e62da6
Timo Suomela <timo.suomela@reaktor.com>
2023-01-17 17:52:06 +0200

    OPHY-574 Update owasp dependency check and associated leiningen plugin
    This clears some false positives, among others CVE-2021-4277

--- dependency-check/project.clj
+++ dependency-check/project.clj
@@ -1,6 +1,6 @@
 (defproject nvd-helper "local"
   :description "nvd-clojure helper project"
   :dependencies [
-                  [nvd-clojure "2.11.0"]
-                  [org.owasp/dependency-check-core "7.4.4"]
+                  [nvd-clojure "2.12.0"]
+                  [org.owasp/dependency-check-core "8.0.0"]
                 ])
