69e14519738e9554f5538413a365602570a3e4c2
Simo-Pekka Kerkelä <simo-pekka.kerkela@reaktor.com>
2023-01-18 14:25:57 +0200

    update dependency-check-core 8.0.0->8.0.1

--- dependency-check/project.clj
+++ dependency-check/project.clj
@@ -3,5 +3,5 @@
   :plugins [[lein-ancient "0.7.0"]]
   :dependencies [
                   [nvd-clojure "2.12.0"]
-                  [org.owasp/dependency-check-core "8.0.0"]
+                  [org.owasp/dependency-check-core "8.0.1"]
                 ])
