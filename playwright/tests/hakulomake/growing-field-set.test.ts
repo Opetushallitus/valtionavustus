aac67fa2dbfce719e5f551ce6b03b5b1dd13bad5
Ville Aaltonen <ville.aaltonen@reaktor.com>
2023-01-23 12:31:55 +0200

    VA-334: show label for growing field set

--- playwright/tests/hakulomake/growing-field-set.test.ts
+++ playwright/tests/hakulomake/growing-field-set.test.ts
@@ -2,8 +2,97 @@ import { defaultValues } from "../../fixtures/defaultValues";
 import { HakujenHallintaPage } from "../../pages/hakujenHallintaPage";
 import { HakijaAvustusHakuPage } from "../../pages/hakijaAvustusHakuPage";
 import { expectToBeDefined } from "../../utils/util";
-import { TEST_Y_TUNNUS } from "../../utils/constants";
 import { Page, expect } from "@playwright/test";
+
+const form = {
+  content: [
+    {
+      fieldClass: "infoElement",
+      id: "name",
+      fieldType: "h1",
+    },
+    {
+      fieldClass: "wrapperElement",
+      id: "project-description",
+      fieldType: "growingFieldset",
+      children: [
+        {
+          fieldClass: "wrapperElement",
+          id: "project-description-1",
+          fieldType: "growingFieldsetChild",
+          children: [
+            {
+              label: {
+                fi: "Tavoite",
+                sv: "Mål",
+              },
+              fieldClass: "formField",
+              helpText: {
+                fi: "Kuvaa hankkeen tavoite ja siihen liittyvä toiminta sekä budjetti avustuskohteittain. Kirjoita molemmista avustuskohteista omat kuvaukset.",
+                sv: "Beskriv projektets mål och tillhörande verksamhet samt budgeten för ansökningsområdena. Ge separata beskrivningar för bägge ansökningsområdena.",
+              },
+              id: "project-description.project-description-1.goal",
+              params: {
+                size: "small",
+                maxlength: 800,
+              },
+              required: true,
+              fieldType: "textArea",
+            },
+            {
+              label: {
+                fi: "Toiminta",
+                sv: "Verksamhet",
+              },
+              fieldClass: "formField",
+              helpText: {
+                fi: "",
+                sv: "",
+              },
+              id: "project-description.project-description-1.activity",
+              params: {
+                size: "small",
+                maxlength: 800,
+              },
+              required: true,
+              fieldType: "textArea",
+            },
+            {
+              label: {
+                fi: "Budjetti",
+                sv: "Budget",
+              },
+              fieldClass: "formField",
+              helpText: {
+                fi: "Erittely siitä, mistä kustannukset muodostuvat (laskentaperusteena tuntimäärä/kurssimäärä x tunnin/kurssin hinta). Vie opetuksen lisäämistä koskeva summa talousarvion kohtaan Henkilöstömenot 1 ja ohjauksen ja tukitoimien lisäämistä koskeva summa kohtaan Henkilöstömenot 2.",
+                sv: "Specificera hur kostnaderna bildas (som beräkningsgrund används antal undervisningstimmar/antal kurser x priset per timme/kurs). För in summan som berör extra undervisning i budgeten under Personalkostnader 1 och summan som berör extra handledning och stödåtgärder under Personalkostnader 2.",
+              },
+              id: "project-description.project-description-1.result",
+              params: {
+                size: "small",
+                maxlength: 800,
+              },
+              required: true,
+              fieldType: "textArea",
+            },
+          ],
+        },
+      ],
+      params: {
+        showOnlyFirstLabels: false,
+      },
+      label: {
+        fi: "Hankkeen tavoitteet, toiminta ja budjetti",
+        sv: "Projektets mål, verksamhet och budget",
+      },
+    },
+  ],
+  rules: [],
+  created_at: "2023-01-23T06:57:30Z",
+  updated_at: "2023-01-23T08:40:36Z",
+};
+
+const formJsonString = JSON.stringify(form);
 
 const growingFieldSet = (page: Page, index: number) => {
   const indexStartsFromOne = index + 1;
@@ -40,6 +129,12 @@ const test = defaultValues.extend<Fixtures>({
     const avustushakuID = await hakujenHallintaPage.copyCurrentHaku();
     await test.step("create avustushaku", async () => {
       await hakujenHallintaPage.fillAvustushaku(hakuProps);
+      const formEditorPage = await hakujenHallintaPage.navigateToFormEditor(
+        avustushakuID
+      );
+      await formEditorPage.changeLomakeJson(formJsonString);
+      await formEditorPage.saveForm();
+      await hakujenHallintaPage.switchToHaunTiedotTab();
       await hakujenHallintaPage.publishAvustushaku();
     });
     const hakijaAvustusHakuPage = new HakijaAvustusHakuPage(page);
@@ -55,55 +150,16 @@ const test = defaultValues.extend<Fixtures>({
         answers.contactPersonEmail
       );
       await hakijaAvustusHakuPage.page.goto(hakemusUrl);
-      await hakijaAvustusHakuPage.fillInBusinessId(TEST_Y_TUNNUS);
-
-      await hakijaAvustusHakuPage.page.fill(
-        "#applicant-name",
-        answers.contactPersonName
-      );
-      await hakijaAvustusHakuPage.page.fill(
-        "#bank-iban",
-        "FI95 6682 9530 0087 65"
-      );
-      await hakijaAvustusHakuPage.page.fill("#bank-bic", "OKOYFIHH");
-      await hakijaAvustusHakuPage.page.fill("#signature", "tititötöö");
-      await hakijaAvustusHakuPage.page.fill(
-        "#signature-email",
-        "ville.aaltonen@reaktor.com"
-      );
-      await hakijaAvustusHakuPage.page.fill(
-        "#project-name",
-        answers.projectName
-      );
-      await hakijaAvustusHakuPage.page.click('[for="combined-effort.radio.0"]');
-      await hakijaAvustusHakuPage.page.fill("#project-goals", "testitöö");
-
-      await hakijaAvustusHakuPage.page.fill(
-        '[for="other-organizations.other-organizations-1.name"]',
-        "testing"
-      );
-      await hakijaAvustusHakuPage.page.fill(
-        '[for="other-organizations.other-organizations-1.email"]',
-        "ville.aaltonen@reaktor.com"
-      );
-      await hakijaAvustusHakuPage.page.fill("#project-measure", "test");
-      await hakijaAvustusHakuPage.page.fill("#project-announce", "testitee");
-      await hakijaAvustusHakuPage.page.fill("#project-effectiveness", "jeejee");
-      await hakijaAvustusHakuPage.page.fill(
-        "#project-spreading-plan",
-        "jepojee"
-      );
-      await hakijaAvustusHakuPage.page.fill(
-        '[id="coordination-costs-row.amount"]',
-        "1000"
-      );
-      await hakijaAvustusHakuPage.page.fill(
-        '[id="coordination-costs-row.description"]',
-        "testing"
-      );
-      await hakijaAvustusHakuPage.page.fill("#project-target", "bugit");
       const { first, second, third } = growingSets;
-      await test.step("growing mode starting set", async () => {
+      await test.step("growing set shows label", async () => {
+        const fieldSetLabel = hakijaAvustusHakuPage.page.locator(
+          "#project-description legend"
+        );
+        await expect(fieldSetLabel).toHaveText(
+          "Hankkeen tavoitteet, toiminta ja budjetti"
+        );
+      });
+      await test.step("growing mode intial state is correct", async () => {
         await expect(first.tavoite).toBeEnabled();
         await expect(first.toiminta).toBeEnabled();
         await expect(first.tulos).toBeEnabled();
@@ -128,8 +184,12 @@ test("can delete first growing field set child after page reload", async ({
   growingSets: { first, second, third },
 }) => {
   const hakijaAvustusHakuPage = new HakijaAvustusHakuPage(page);
+  const firstTavoite = "a";
+  const secondTavoite = "d";
+  const secondToiminta = "e";
+  const secondTulos = "f";
   await test.step("after filling first field", async () => {
-    await first.tavoite.fill("a");
+    await first.tavoite.fill(firstTavoite);
     await expect(second.tavoite).toBeEnabled();
     await expect(second.toiminta).toBeEnabled();
     await expect(second.tulos).toBeEnabled();
@@ -143,20 +203,22 @@ test("can delete first growing field set child after page reload", async ({
     await first.toiminta.fill("b");
     await first.tulos.fill("c");
     await expect(second.remove).toBeDisabled();
-    await second.tavoite.fill("d");
-    await second.toiminta.fill("e");
-    await second.tulos.fill("f");
+    await second.tavoite.fill(secondTavoite);
+    await second.toiminta.fill(secondToiminta);
+    await second.tulos.fill(secondTulos);
     await expect(second.remove).toBeEnabled();
     await hakijaAvustusHakuPage.waitForEditSaved();
   });
   await test.step("after reload can delete first field", async () => {
     await hakijaAvustusHakuPage.page.reload();
-    await expect(first.tavoite).toHaveValue("a");
+    await expect(first.tavoite).toHaveValue(firstTavoite);
     await first.remove.click();
-    await expect(first.tavoite).toHaveValue("d");
+    await expect(first.tavoite).toHaveValue(secondTavoite);
+    await expect(first.toiminta).toHaveValue(secondToiminta);
+    await expect(first.tulos).toHaveValue(secondTulos);
   });
-  await test.step("form is valid", async () => {
-    await expect(hakijaAvustusHakuPage.sendHakemusButton).toBeEnabled();
+  await test.step("form submits successfully", async () => {
+    await hakijaAvustusHakuPage.submitApplication();
   });
 });
 
@@ -170,14 +232,14 @@ test("first growing field set field is editable after reload", async ({
     await first.remove.click();
     await expect(first.tavoite).toHaveValue("");
   });
-  await test.step("after reload can fill field", async () => {
+  await test.step("after reload can still fill field", async () => {
     await page.reload();
     await first.tavoite.fill("a");
     await expect(first.tavoite).toHaveValue("a");
     await first.toiminta.fill("b");
     await first.tulos.fill("c");
   });
-  await test.step("form is valid", async () => {
-    await expect(hakijaAvustusHakuPage.sendHakemusButton).toBeEnabled();
+  await test.step("form submits successfully", async () => {
+    await hakijaAvustusHakuPage.submitApplication();
   });
 });
--- soresu-form/web/form/component/wrapper/GrowingFieldset.jsx
+++ soresu-form/web/form/component/wrapper/GrowingFieldset.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import ClassNames from "classnames";
 import _ from "lodash";
 import CSSTransitionGroup from "./CSSTransitionGroup.jsx";
+import BasicFieldComponent from "soresu-form/web/form/component/BasicFieldComponent";
 
-export default class GrowingFieldset extends React.Component {
+export default class GrowingFieldset extends BasicFieldComponent {
   className(className) {
     const field = this.props.field;
     const classNames = ClassNames(className, {
@@ -15,11 +16,13 @@ export default class GrowingFieldset extends React.Component {
   render() {
     const children = this.props.children;
     const htmlId = this.props.htmlId;
+    const labelText = this.props.field.label?.[this.props.lang];
     return (
       <fieldset
         id={htmlId}
         className={this.className("soresu-growing-fieldset")}
       >
+        {!!labelText && <legend>{labelText}</legend>}
         <ol>
           <CSSTransitionGroup
             component="span"
