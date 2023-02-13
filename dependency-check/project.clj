da7e21d83e8674fa1613c780291193c373fc96dc
Ville Aaltonen <ville.aaltonen@reaktor.com>
2023-02-13 13:54:59 +0200

    OPHY-592: update dependency-check-core

--- dependency-check/project.clj
+++ dependency-check/project.clj
@@ -3,5 +3,5 @@
   :plugins [[lein-ancient "0.7.0"]]
   :dependencies [
                   [nvd-clojure "2.13.0"]
-                  [org.owasp/dependency-check-core "8.0.2"]
+                  [org.owasp/dependency-check-core "8.1.0"]
                 ])
