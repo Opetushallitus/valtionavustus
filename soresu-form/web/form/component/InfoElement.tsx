84f05f141e896dc513063161d4eccc6744bb0740
Santeri Horttanainen <santeri.horttanainen@gmail.com>
2023-02-17 16:41:24 +0200

    VA-307: try to make sense out of infoelement types

--- soresu-form/web/form/component/InfoElement.tsx
+++ soresu-form/web/form/component/InfoElement.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 
 import ComponentFactory from "../ComponentFactory";
 import Translator from "../Translator";
@@ -8,194 +8,191 @@ import {
   AccordionElementPropertyMapper,
   LinkPropertyMapper,
 } from "./PropertyMapper";
+import { FieldType, LegacyTranslations, Language } from "soresu-form/web/va/types";
 
-export class BasicInfoComponent extends React.Component {
-  asDateTimeString(date) {
-    const timeLimiter = new Translator(
-      this.props.translations["misc"]
-    ).translate("time", this.props.lang, "KLO");
-    return (
-      DateUtil.asDateString(date) +
-      " " +
-      timeLimiter +
-      " " +
-      DateUtil.asTimeString(date)
-    );
-  }
+function asDateTimeString(translations: LegacyTranslations, lang: Language, date: Date) {
+  const timeLimiter = new Translator(translations["misc"]).translate(
+    "time",
+    lang,
+    "KLO"
+  );
+  return (
+    DateUtil.asDateString(date) +
+    " " +
+    timeLimiter +
+    " " +
+    DateUtil.asTimeString(date)
+  );
+}
 
-  translatedValue(valueId) {
-    const lang = this.props.lang;
-    const translations = this.props.translations;
-    const values = this.props.values;
-    const value = values[this.props.htmlId];
-    if (this.props[valueId]) {
-      return new Translator(this.props).translate(valueId, lang);
-    } else if (translations && translations[valueId]) {
-      return new Translator(translations).translate(valueId, lang);
-    } else if (value && value[valueId]) {
-      return new Translator(value).translate(valueId, lang);
-    } else {
-      return new Translator(values).translate(this.props.htmlId, lang);
-    }
+interface Props {
+  values: any
+  htmlId: string
+  lang: Language
+}
+
+function translatedValue(props: Props, valueId: "text" | "href" | "label") {
+  const lang = props.lang;
+  const values = props.values;
+  const value = values[props.htmlId];
+  if (value && value[valueId]) {
+    return new Translator(value).translate(valueId, lang);
+  } else {
+    return new Translator(values).translate(props.htmlId, lang);
   }
 }
 
-export class H1InfoElement extends BasicInfoComponent {
-  render() {
-    return <h1>{this.translatedValue("text")}</h1>;
+export function H1InfoElement(props: Props) {
+  return <h1>{translatedValue(props, "text")}</h1>;
+}
+
+export function H3InfoElement(props: Props) {
+  return <h3>{translatedValue(props, "text")}</h3>;
+}
+
+export function LinkInfoElement(props: Props) {
+  const translatedText = translatedValue(props, "text");
+  const translatedHref = translatedValue(props, "href");
+  const text = translatedText ? translatedText : translatedHref;
+  return (
+    <a
+      hidden={!translatedHref}
+      target="_blank"
+      rel="noopener noreferrer"
+      href={translatedHref}
+    >
+      {text}
+    </a>
+  );
+}
+
+export function ParagraphInfoElement(props: Props) {
+  return (
+    <p className="soresu-info-element">{translatedValue(props, "text")}</p>
+  );
+}
+
+interface AccordionInfoElementProps extends Props {
+  renderingParameters: {
+    initiallyOpen: boolean
   }
 }
 
-export class H3InfoElement extends BasicInfoComponent {
-  render() {
-    return <h3>{this.translatedValue("text")}</h3>;
-  }
-}
+function AccordionInfoElement(props: AccordionInfoElementProps) {
+  const [state, setState] = useState({open: props.renderingParameters.initiallyOpen});
 
-export class LinkInfoElement extends BasicInfoComponent {
-  render() {
-    const translatedText = this.translatedValue("text");
-    const translatedHref = this.translatedValue("href");
-    const text = translatedText ? translatedText : translatedHref;
-    return (
-      <a
-        hidden={!translatedHref}
-        target="_blank"
-        rel="noopener noreferrer"
-        href={translatedHref}
+  const handleClick = () => {
+    setState({
+      open: !state.open,
+    });
+  }
+
+  const values = props.values;
+  const key = props.htmlId;
+  const lang = props.lang;
+  const items = [];
+  const infoObject = values[props.htmlId];
+  for (let i = 0; i < infoObject.items.length; i++) {
+    const textContent = infoObject.items[i][lang];
+    items.push(<li key={key + "." + i}>{textContent}</li>);
+  }
+  const isOpen = state.open;
+  const openStateClassName = isOpen ? "open" : "closed";
+  return (
+    <div>
+      <span
+        onClick={handleClick}
+        className={"soresu-opener-handle " + openStateClassName}
       >
-        {text}
-      </a>
-    );
-  }
-}
-
-export class ParagraphInfoElement extends BasicInfoComponent {
-  render() {
-    return (
-      <p className="soresu-info-element">{this.translatedValue("text")}</p>
-    );
-  }
-}
-
-export class AccordionInfoElement extends BasicInfoComponent {
-  constructor(props) {
-    super(props);
-    this.handleClick = this.handleClick.bind(this);
-    this.state = { open: this.props.renderingParameters.initiallyOpen };
-  }
-
-  handleClick() {
-    this.setState({
-      open: !this.state.open,
-    });
-  }
-
-  render() {
-    const values = this.props.values;
-    const key = this.props.htmlId;
-    const lang = this.props.lang;
-    const items = [];
-    const infoObject = values[this.props.htmlId];
-    for (let i = 0; i < infoObject.items.length; i++) {
-      const textContent = infoObject.items[i][lang];
-      items.push(<li key={key + "." + i}>{textContent}</li>);
-    }
-    const isOpen = this.state.open;
-    const openStateClassName = isOpen ? "open" : "closed";
-    return (
-      <div>
-        <span
-          onClick={this.handleClick}
-          className={"soresu-opener-handle " + openStateClassName}
-        >
-          {super.translatedValue("label")}
-        </span>
-        <div
-          className="soresu-accordion"
-          ref={(el) => {
-            if (el) {
-              // Explicitly set max-height so that hiding/showing element
-              // can be animated with css transitions. Setting max-height
-              // is slightly faster than setting height on IE11.
-              el.style.maxHeight = isOpen ? el.scrollHeight + "px" : 0;
-            }
-          }}
-        >
-          <ul id={key}>{items}</ul>
-        </div>
+        {translatedValue(props, "label")}
+      </span>
+      <div
+        className="soresu-accordion"
+        ref={(el) => {
+          if (el) {
+            // Explicitly set max-height so that hiding/showing element
+            // can be animated with css transitions. Setting max-height
+            // is slightly faster than setting height on IE11.
+            el.style.maxHeight = isOpen ? el.scrollHeight + "px" : "0px";
+          }
+        }}
+      >
+        <ul id={key}>{items}</ul>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
-export class DateRangeInfoElement extends BasicInfoComponent {
-  render() {
-    const values = this.props.values;
-    const value = values[this.props.htmlId];
-    const start = new Date(value.start);
-    const startDateTime = this.asDateTimeString(start);
-    const end = new Date(value.end);
-    const endDateTime = this.asDateTimeString(end);
-
-    return (
-      <div>
-        {this.translatedValue("label")} {startDateTime} — {endDateTime}
-      </div>
-    );
-  }
+interface DateRangeProps extends Props {
+  translations: LegacyTranslations
 }
 
-export class EndOfDateRangeInfoElement extends BasicInfoComponent {
-  render() {
-    const values = this.props.values;
-    const value = values[this.props.htmlId];
-    const end = new Date(value.end);
-    const endDateTime = this.asDateTimeString(end);
-    return (
-      <div>
-        <label>{this.translatedValue("label")}</label>
-        <span>{endDateTime}</span>
-      </div>
-    );
-  }
+export function DateRangeInfoElement(props: DateRangeProps) {
+  const values = props.values;
+  const value = values[props.htmlId];
+  const start = new Date(value.start);
+  const startDateTime = asDateTimeString(props.translations, props.lang, start);
+  const end = new Date(value.end);
+  const endDateTime = asDateTimeString(props.translations, props.lang, end);
+
+  return (
+    <div>
+      {translatedValue(props, "label")} {startDateTime} — {endDateTime}
+    </div>
+  );
 }
 
-export default class InfoElement extends React.Component {
-  constructor(props) {
-    super(props);
-    const fieldTypeMapping = {
-      h1: H1InfoElement,
-      h3: H3InfoElement,
-      link: LinkInfoElement,
-      p: ParagraphInfoElement,
-      bulletList: AccordionInfoElement,
-      dateRange: DateRangeInfoElement,
-      endOfDateRange: EndOfDateRangeInfoElement,
-    };
-    const fieldPropertyMapping = {
-      h1: InfoElementPropertyMapper,
-      h3: InfoElementPropertyMapper,
-      link: LinkPropertyMapper,
-      p: InfoElementPropertyMapper,
-      bulletList: AccordionElementPropertyMapper,
-      dateRange: InfoElementPropertyMapper,
-      endOfDateRange: InfoElementPropertyMapper,
-    };
-    this.componentFactory = new ComponentFactory({
-      fieldTypeMapping: fieldTypeMapping,
-      fieldPropertyMapperMapping: fieldPropertyMapping,
-    });
-  }
+interface EndOfDateRangeProps extends Props {
+  translations: LegacyTranslations
+}
 
-  render() {
-    const controller = this.props.controller;
-    const fieldType = this.props.fieldType;
+function EndOfDateRangeInfoElement(props: EndOfDateRangeProps) {
+  const values = props.values;
+  const value = values[props.htmlId];
+  const end = new Date(value.end);
+  const endDateTime = asDateTimeString(props.translations, props.lang, end);
+  return (
+    <div>
+      <label>{translatedValue(props, "label")}</label>
+      <span>{endDateTime}</span>
+    </div>
+  );
+}
 
-    if (fieldType in controller.getCustomComponentTypeMapping()) {
-      return controller.createCustomComponent(this.props);
-    } else {
-      return this.componentFactory.createComponent(this.props);
-    }
+export default function InfoElement(props: {
+  controller: any;
+  fieldType: FieldType;
+  htmlId: string;
+}) {
+  const fieldTypeMapping = {
+    h1: H1InfoElement,
+    h3: H3InfoElement,
+    link: LinkInfoElement,
+    p: ParagraphInfoElement,
+    bulletList: AccordionInfoElement,
+    dateRange: DateRangeInfoElement,
+    endOfDateRange: EndOfDateRangeInfoElement,
+  };
+  const fieldPropertyMapping = {
+    h1: InfoElementPropertyMapper,
+    h3: InfoElementPropertyMapper,
+    link: LinkPropertyMapper,
+    p: InfoElementPropertyMapper,
+    bulletList: AccordionElementPropertyMapper,
+    dateRange: InfoElementPropertyMapper,
+    endOfDateRange: InfoElementPropertyMapper,
+  };
+  const componentFactory = new ComponentFactory({
+    fieldTypeMapping: fieldTypeMapping,
+    fieldPropertyMapperMapping: fieldPropertyMapping,
+  });
+
+  const controller = props.controller;
+  const fieldType = props.fieldType;
+
+  if (fieldType in controller.getCustomComponentTypeMapping()) {
+    return controller.createCustomComponent(props);
+  } else {
+    return componentFactory.createComponent(props);
   }
 }
