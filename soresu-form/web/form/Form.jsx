5c40710ab16ce4718e60a6a83d285ad86152cfb7
Santeri Horttanainen <santeri.horttanainen@gmail.com>
2023-02-17 16:42:41 +0200

    VA-307: fix infoelement import

--- soresu-form/web/form/Form.jsx
+++ soresu-form/web/form/Form.jsx
@@ -4,7 +4,7 @@ import _ from "lodash";
 import "./style/main.less";
 
 import FormComponent from "./component/FormComponent";
-import InfoElement from "./component/InfoElement.jsx";
+import InfoElement from "./component/InfoElement";
 import WrapperComponent from "./component/wrapper/WrapperComponent.jsx";
 import InputValueStorage from "./InputValueStorage.js";
 
