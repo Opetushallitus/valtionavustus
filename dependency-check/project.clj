75d6158996065ab12d32ce3c01eb907af3d33d8e
Timo Suomela <timo.suomela@reaktor.com>
2023-02-23 15:14:53 +0200

    OPHY-598 Update nvd-clojure to the latest version

--- dependency-check/project.clj
+++ dependency-check/project.clj
@@ -2,6 +2,6 @@
   :description "nvd-clojure helper project"
   :plugins [[lein-ancient "0.7.0"]]
   :dependencies [
-                  [nvd-clojure "2.13.0"]
+                  [nvd-clojure "3.0.1"]
                   [org.owasp/dependency-check-core "8.1.0"]
                 ])
