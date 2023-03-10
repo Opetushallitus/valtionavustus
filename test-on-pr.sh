2829d184893b922795e7cb22c0bc1e828fba93df
Santeri Horttanainen <santeri.horttanainen@gmail.com>
2023-03-10 12:35:31 +0200

    VA-353-2: Add dummy run tests on pr

---
+++ test-on-pr.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -o errexit -o nounset -o pipefail
+source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/scripts/common-functions.sh"
+
+function main {
+  exit 0
+}
+
+main "$@"
