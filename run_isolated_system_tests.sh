c736bb671e4f6fc6a276d2711c1eec6066431925
Timo Suomela <timo.suomela@reaktor.com>
2023-03-22 17:33:25 +0200

    Extract function

--- run_isolated_system_tests.sh
+++ run_isolated_system_tests.sh
@@ -3,6 +3,11 @@ set -o errexit -o nounset -o pipefail
 source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/scripts/common-functions.sh"
 
 function run_tests {
+  run_playwright_tests "$@"
+  make lein-test
+}
+
+function run_playwright_tests {
   if running_on_jenkins;
   then
     start-service test-runner
@@ -10,7 +15,6 @@ function run_tests {
     npm run prettier-check-project "$@"
     npm run playwright:test "$@"
   fi
-	make lein-test
 }
 
 function main {
