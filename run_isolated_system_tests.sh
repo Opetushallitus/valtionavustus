0ea51cf43826118d5a48ec4e6cc9e3b10517fba7
Timo Suomela <timo.suomela@reaktor.com>
2023-03-23 12:48:14 +0200

    Don't ignore the exit code of the playwright tests on jenkins

--- run_isolated_system_tests.sh
+++ run_isolated_system_tests.sh
@@ -12,9 +12,14 @@ function run_playwright_tests {
   if running_on_jenkins;
   then
     start-service test-runner
+    return $(playwright_test_runner_exit_code)
   else
     npm run playwright:test "$@"
   fi
+}
+
+function playwright_test_runner_exit_code {
+  docker ps --filter "status=exited"|grep test-runner|sed 's/.*\(Exited.*\) Less.*/\1/'|sed 's/.*\([0-9][0-9]*\).*/\1/'
 }
 
 function main {
