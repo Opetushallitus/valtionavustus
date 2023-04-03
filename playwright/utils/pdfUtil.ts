6504b8d6b8becefbfbbe96f620ca2b032dbd17bd
Ville Aaltonen <ville.aaltonen@reaktor.com>
2023-04-03 11:44:28 +0300

    OPHY-618: update pdfjs to latest

--- playwright/utils/pdfUtil.ts
+++ playwright/utils/pdfUtil.ts
@@ -1,9 +1,12 @@
-import { getDocument } from 'pdfjs-dist/legacy/build/pdf.js'
+import { getDocument } from 'pdfjs-dist'
 
 export const getPdfFirstPageTextContent = async (pdfData: Buffer) => {
   const pdf = await getDocument({ data: pdfData }).promise
   const firstPage = await pdf.getPage(1)
-  const allTextContent = await firstPage.getTextContent()
+  const allTextContent = await firstPage.getTextContent({
+    disableCombineTextItems: false,
+    includeMarkedContent: false,
+  })
   return allTextContent.items
     .flatMap((item) => {
       if ('str' in item) {
