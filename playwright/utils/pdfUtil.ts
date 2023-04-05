b9fa14fb94e47ad8d83994e26001ea1e0e946799
Riku Honkanen <riku.honkanen@reaktor.com>
2023-04-05 15:41:32 +0300

    Revert "OPHY-618: update pdfjs to latest"
    This reverts commit 6504b8d6b8becefbfbbe96f620ca2b032dbd17bd.

--- playwright/utils/pdfUtil.ts
+++ playwright/utils/pdfUtil.ts
@@ -3,10 +3,7 @@ import { getDocument } from 'pdfjs-dist/legacy/build/pdf.js'
 export const getPdfFirstPageTextContent = async (pdfData: Buffer) => {
   const pdf = await getDocument({ data: pdfData }).promise
   const firstPage = await pdf.getPage(1)
-  const allTextContent = await firstPage.getTextContent({
-    disableCombineTextItems: false,
-    includeMarkedContent: false,
-  })
+  const allTextContent = await firstPage.getTextContent()
   return allTextContent.items
     .flatMap((item) => {
       if ('str' in item) {
