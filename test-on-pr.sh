dac201eb6669c20967377d517eadb9b6e10d699d
Tero Keinänen <tero.keinanen@reaktor.com>
2023-04-11 17:58:18 +0300

    VA-353-2: Authenticate to Github Packages and specify va-hakija image

--- test-on-pr.sh
+++ test-on-pr.sh
@@ -7,8 +7,14 @@ source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/scripts/common-functio
 function main {
   build_jars
 
+  if [ -n "${GITHUB_SHA:-}" ]; then
+    export HAKIJA_IMAGE="ghcr.io/opetushallitus/va-hakija:$GITHUB_SHA"
+
+    docker compose --file "$DOCKER_COMPOSE_FILE" pull
+  fi
+
   readonly test_runner_service="test-runner"
-  docker-compose --file "${DOCKER_COMPOSE_FILE}" up \
+  docker-compose --file "$DOCKER_COMPOSE_FILE" up \
     --exit-code-from $test_runner_service \
     --build $test_runner_service
 }
