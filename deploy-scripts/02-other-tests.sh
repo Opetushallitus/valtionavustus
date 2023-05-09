a75abc9d412e28980aecc1c0a0c1cfcd0dba0328
Riku Honkanen <riku.honkanen@reaktor.com>
2023-05-09 16:05:40 +0300

    OPHY-619-4: Remove unnecessary sourcing of deplloy functions

--- deploy-scripts/02-other-tests.sh
+++ deploy-scripts/02-other-tests.sh
@@ -4,9 +4,6 @@ set -o errexit -o nounset -o pipefail
 # shellcheck source=../scripts/common-functions.sh
 source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../scripts/common-functions.sh"
 
-# shellcheck source=./deploy-functions.sh
-source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/deploy-functions.sh"
-
 function main {
     require_command docker
     require_docker_compose
