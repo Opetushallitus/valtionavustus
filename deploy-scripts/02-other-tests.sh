f63979f94150f9cc20017ac8abcfa9524bf7b161
Riku Honkanen <riku.honkanen@reaktor.com>
2023-05-09 13:27:28 +0300

    OPHY-619-4: Rename and move the github artifact jar into directory so hakija dockerfile finds it

--- deploy-scripts/02-other-tests.sh
+++ deploy-scripts/02-other-tests.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
 set -o errexit -o nounset -o pipefail
 
+# shellcheck source=../scripts/common-functions.sh
 source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../scripts/common-functions.sh"
+
+# shellcheck source=./deploy-functions.sh
+source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/deploy-functions.sh"
 
 function main {
     require_command docker
     require_docker_compose
     
     cd "$repo"
+    mkdir -p "$repo/target/uberjar/"
+    mv "${standalone_jar}" "$repo/target/uberjar/valtionavustus-0.1.0-SNAPSHOT-standalone.jar"
 
     start_gh_actions_group "Run lein tests"
 
