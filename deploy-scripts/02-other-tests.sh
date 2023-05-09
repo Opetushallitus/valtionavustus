903cc49f87e5e9930ac99137c18e3fd2669876ca
Riku Honkanen <riku.honkanen@reaktor.com>
2023-05-09 12:51:47 +0300

    OPHY-619-4: Run lein tests in github actions

--- deploy-scripts/02-other-tests.sh
+++ deploy-scripts/02-other-tests.sh
@@ -1,1 +1,20 @@
-echo "other tests"
+#!/usr/bin/env bash
+set -o errexit -o nounset -o pipefail
+
+source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../scripts/common-functions.sh"
+
+function main {
+    require_command docker
+    require_docker_compose
+    
+    cd "$repo"
+
+    start_gh_actions_group "Run lein tests"
+
+    start_system_under_test
+    make lein-test
+    stop_system_under_test
+
+    end_gh_actions_group
+}
+main "$@"
