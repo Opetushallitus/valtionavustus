9b893e1ef562676e26feec2390b8b8528c9bbb10
Riku Honkanen <riku.honkanen@reaktor.com>
2023-05-10 12:59:56 +0300

    OPHY-619-4: dont use -it in github actions because its not TTY device

--- deploy-scripts/02-other-tests.sh
+++ deploy-scripts/02-other-tests.sh
@@ -10,9 +10,13 @@ function main {
     
     cd "$repo"
 
+    if [[ ${GITHUB_ACTIONS:-} != "true" ]]
+        then local_args="-it"
+    fi
+
     start_gh_actions_group "Run lein tests"
     docker compose up -d db 
-    docker run -it --network container:va-postgres "$(docker build -q .)" with-profile test spec -f d
+    docker run "${local_args}" --network container:va-postgres "$(docker build -q .)" with-profile test spec -f d
     docker compose down
     end_gh_actions_group
 }
