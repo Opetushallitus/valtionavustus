f4f0e3ba356df4a367068e608e8244ea3573bd5e
Ville Aaltonen <ville.aaltonen@reaktor.com>
2023-08-18 15:31:21 +0300

    VA-374-9: refactor visibility check

--- playwright/tests/hakemuksen-arviointi/hakija-can-not-edit-loppuselvitys-after-its-asiatarkastettu.test.ts
+++ playwright/tests/hakemuksen-arviointi/hakija-can-not-edit-loppuselvitys-after-its-asiatarkastettu.test.ts
@@ -12,10 +12,10 @@ test('hakija can not edit loppuselvitys after information has been verified', as
 }) => {
   expect(asiatarkastettu)
   await navigate(page, loppuselvitysFormUrl)
-  expect(await page.innerText('span[id="textArea-0"]')).toEqual('Yhteenveto')
-  expect(await page.isHidden('textarea[id="textArea-0"]'))
+  await expect(page.locator('span[id="textArea-0"]')).toHaveText('Yhteenveto')
+  await expect(page.locator('textarea[id="textArea-0"]')).toBeHidden()
 
   const hakijaSelvitysPage = HakijaSelvitysPage(page)
-  await hakijaSelvitysPage.loppuselvitysWarning.waitFor({ state: 'attached' })
-  await hakijaSelvitysPage.submitButton.waitFor({ state: 'detached' })
+  await expect(hakijaSelvitysPage.loppuselvitysWarning).toBeVisible()
+  await expect(hakijaSelvitysPage.submitButton).toBeHidden()
 })
