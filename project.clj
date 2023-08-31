13586926e542a2b8aa7c1e992d88082d3eec95f9
Tero Keinänen <tero.keinanen@reaktor.com>
2023-08-31 13:18:54 +0300

    OPHY-649: Clean up project.clj by checking what dependencies are used and explaining most of managed dependency decisions

--- project.clj
+++ project.clj
@@ -25,112 +25,126 @@
   :plugins [[lein-environ "1.2.0"]
             [reifyhealth/lein-git-down "0.4.1"]]
 
-  :managed-dependencies [[org.clojure/clojure "1.11.1"]
-                         [nrepl "1.0.0"]
-                         [cider/cider-nrepl "0.37.0"]
+  :managed-dependencies [
 
-                         ;; http
-                         [org.apache.commons/commons-compress "1.23.0"]
-                         [org.apache.tika/tika-core "2.8.0"]
-                         [compojure "1.7.0"]
-                         [http-kit "2.7.0"]
+                         ;; [metosin/compojure-api 1.1.13] -> [metosin/ring-swagger 0.26.2] -> [metosin/scjsv 0.5.0] ->
+                         [com.github.java-json-tools/json-schema-validator "2.2.14"]
+                         ;; ... -> [com.github.java-json-tools/json-schema-validator "2.2.14"] ->
+                         ;; fixes https://nvd.nist.gov/vuln/detail/cve-2020-8908
+                         [com.google.guava/guava "32.1.2-android"]
+
+                         ;; [org.postgresql/postgresql "42.6.0"] -> 
+                         ;; ... -> [com.google.guava/guava "32.1.2-android"] ->
+                         [org.checkerframework/checker-qual "3.37.0"]
+
+                         ;; [yesql "0.5.3"] -> 
+                         ;; ... -> [clout "2.2.1"] -> 
+                         [instaparse "1.4.12"]
+
+                         ;; [clj-pdf "2.6.5"] ->
+                         ;; [buddy/buddy-sign "3.5.351"] -> [buddy/buddy-core "1.11.423"] ->
+                         [commons-codec "1.16.0"]
+
+                         ;; logging API
+                         [org.slf4j/slf4j-api "2.0.7"]
 
                          ; dependencies under compojure-api -> explicitly updated for security patches
                          [ring-middleware-format "0.7.5"]
                          [org.yaml/snakeyaml "1.33"]
 
-                         ;; json
-                         [cheshire "5.11.0"]
-                         [org.clojure/data.json "2.4.0"]
+
+
+                         ;;; ???-section starts here
                          [com.fasterxml.jackson.core/jackson-core ~jackson-version]
                          [com.fasterxml.jackson.core/jackson-databind ~jackson-version]
                          [com.fasterxml.jackson.core/jackson-annotations ~jackson-version]
                          [com.fasterxml.jackson.dataformat/jackson-dataformat-cbor ~jackson-version]
                          [com.fasterxml.jackson.dataformat/jackson-dataformat-smile ~jackson-version]
-                         [prismatic/schema "1.4.1"]
 
                          ;; testing
                          [speclj "3.4.3"]
                          [speclj-junit "0.0.11"]
 
-                         ;; configuration
-                         [environ "1.2.0"]
-
-                         ;; logging
-                         [org.apache.logging.log4j/log4j-core ~log4j-version]
-                         [org.apache.logging.log4j/log4j-slf4j2-impl ~log4j-version]
-                         [org.slf4j/slf4j-api "2.0.7"]
-                         [commons-logging "1.2"]
-                         [org.log4s/log4s_2.11 "1.10.0"]
-
                          ;; cas
-                         [fi.vm.sade/scala-cas_2.11 "2.2.3-SNAPSHOT"]
                          [org.http4s/http4s-blaze-client_2.11 ~http4s-version]
                          [org.http4s/http4s-client_2.11 ~http4s-version]
                          [org.http4s/http4s-dsl_2.11 ~http4s-version]
 
                          ;; other
                          [clojurewerkz/quartzite "2.1.0"]
-                         [com.cemerick/url "0.1.1"]
-                         [commons-codec "1.16.0"]
-                         [commons-io "2.13.0"]
-                         [instaparse "1.4.12"]
                          [metosin/ring-swagger-ui "4.18.1"]
-                         [org.apache.commons/commons-email "1.5"]
-                         [org.clojure/core.async "1.6.681"]
+                         
                          [org.clojure/core.memoize "1.0.257"]
-                         [org.clojure/data.xml "0.0.8"]
-                         [org.clojure/tools.trace "0.7.11"]
+
                          [org.scala-lang.modules/scala-xml_2.11 "1.3.1"]
                          [org.scala-lang/scala-library "2.11.12"]]
 
-  :dependencies [[org.clojure/clojure]
-                 [de.ubercode.clostache/clostache "1.4.0"]
-                 [org.apache.commons/commons-email]
-                 [org.clojure/core.async]
-                 [org.apache.logging.log4j/log4j-core]
-                 [org.apache.logging.log4j/log4j-slf4j2-impl]
-                 [org.apache.tika/tika-core]
-                 [nrepl]
-                 [buddy/buddy-auth "3.0.323"]
-                 [buddy/buddy-sign "3.5.351"]
-                 [com.cemerick/url :exclusions [com.cemerick/clojurescript.test]]
-                 [dk.ative/docjure "1.19.0"]
-                 [fi.vm.sade/scala-cas_2.11]
-                 [org.http4s/http4s-blaze-client_2.11]
-                 [org.clojure/data.json]
-                 [org.clojure/data.xml]
-                 [nivekuil/clj-ssh "d11634acf9857da4c7fc98258719a333af523cb8" :exclusions [com.jcraft/jsch.agentproxy.usocket-nc]]
-                 [com.jcraft/jsch "0.1.55"]
-                 [gov.nasa.earthdata/quartzite "2.2.1-SNAPSHOT"]
-                 [clj-pdf "2.6.5" :exclusions [org.apache.xmlgraphics/xmlgraphics-commons]]
-                 [cider/cider-nrepl]
-                 [http-kit]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [nrepl "1.0.0"]
+                 [cider/cider-nrepl "0.37.0"]
+                 [environ "1.2.0"]
+
+                 ;; clojure libs
+                 [org.clojure/core.async "1.6.681"]
+                 [org.clojure/data.xml "0.0.8"]
+                 [org.clojure/tools.trace "0.7.11"]
+                 [org.clojure/tools.logging "1.2.4"]
+
+                 ;; logging
+                 [org.apache.logging.log4j/log4j-core ~log4j-version]
+                 [org.apache.logging.log4j/log4j-slf4j2-impl ~log4j-version]
+
+                 ;; http
+                 [compojure "1.7.0"]
+                 [http-kit "2.7.0"]
+                 [metosin/compojure-api "1.1.13"]
                  [ring.middleware.conditional "0.2.0" :exclusions [ring]]
-                 [ring/ring-codec "1.2.0"]
-                 [ring/ring-core "1.10.0" :exclusions [commons-fileupload]]
-                 [commons-fileupload "1.5"]
-                 [ring/ring-devel "1.10.0"]
                  [radicalzephyr/ring.middleware.logger "0.6.0"]
+                 [ring/ring-codec "1.2.0"]
+                 [ring/ring-core "1.10.0"]
+                 [ring/ring-devel "1.10.0"]
                  [ring/ring-defaults "0.3.4"]
                  [ring/ring-session-timeout "0.3.0"]
                  [ring/ring-ssl "0.3.0"]
-                 [compojure]
-                 [metosin/compojure-api "1.1.13"]
-                 [com.github.java-json-tools/jackson-coreutils "2.0"  :exclusions [com.google.code.findbugs/jsr305]]
-                 [com.google.guava/guava "32.0.1-jre"]
-                 [cheshire]
-                 [prismatic/schema]
-                 [environ]
-                 [org.clojure/tools.logging "1.2.4"]
-                 [clj-time "0.15.2"]
-                 [org.clojure/tools.trace]
+                 [prismatic/schema "1.4.1"]
+                 [org.apache.tika/tika-core "2.8.0"] ; attachment handling
+
+                 ;; auth
+                 [buddy/buddy-auth "3.0.323"]
+                 [buddy/buddy-sign "3.5.351"]
+                 [fi.vm.sade/scala-cas_2.11 "2.2.3-SNAPSHOT"]
+                 [org.http4s/http4s-blaze-client_2.11]
+
+                 ;; json
+                 [cheshire "5.11.0"]
+                 [org.clojure/data.json "2.4.0"]
 
                  ;; database
                  [hikari-cp "1.8.3"]
                  [org.flywaydb/flyway-core "4.2.0"]
                  [org.postgresql/postgresql "42.6.0"]
-                 [yesql "0.5.3"]]
+                 [yesql "0.5.3"]
+
+                 ;; emails
+                 [org.apache.commons/commons-email "1.5"]
+
+                 ;; täsmäytysraportti
+                 [clj-pdf "2.6.5" :exclusions [org.apache.xmlgraphics/xmlgraphics-commons]]
+
+                 ;; maksatuspalvelu
+                 [nivekuil/clj-ssh "d11634acf9857da4c7fc98258719a333af523cb8" :exclusions [com.jcraft/jsch.agentproxy.usocket-nc]]
+                 [com.jcraft/jsch "0.1.55"]
+
+                 ;; miscellaneous
+                 [clj-time "0.15.2"]
+                 [joda-time "2.12.5"] ; also required by com.github.java-json-tools/json-schema-validator
+                                      ; and clj-time
+                 [de.ubercode.clostache/clostache "1.4.0"] ; email templates and some html templating somewhere
+
+                 ;; ????
+                 [com.cemerick/url "0.1.1" :exclusions [com.cemerick/clojurescript.test]] ; this is basically useless, we only use one function that would be trivial to implement
+                 [dk.ative/docjure "1.19.0"]
+                 [gov.nasa.earthdata/quartzite "2.2.1-SNAPSHOT"]]
 
   :profiles {:uberjar {:aot [oph.va.hakija.main]}
              :server-dev {:env {:config "va-hakija/config/dev.edn"
