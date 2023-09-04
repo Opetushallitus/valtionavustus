9598d3e6be257ee109ba82043d1b0b6e156b4702
Tero Keinänen <tero.keinanen@reaktor.com>
2023-09-04 17:31:41 +0300

    Get local db container from Compose since names are not explicit any more

--- scripts/psql-local.sh
+++ scripts/psql-local.sh
@@ -2,9 +2,13 @@
 set -o errexit -o nounset -o pipefail
 source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/common-functions.sh"
 
+readonly compose="docker compose -f docker-compose.yml -f docker-compose.local-dev.yml"
 function main {
   require_docker
-  docker exec -it --env PGOPTIONS="--search-path=public,hakija,virkailija" va-postgres psql postgres://va:va@localhost/va-dev
+  local container_name
+  container_name="$($compose ps --quiet db)"
+
+  docker exec -it --env PGOPTIONS="--search-path=public,hakija,virkailija" "$container_name" psql postgres://va:va@localhost/va-dev
 }
 
 main "$@"
