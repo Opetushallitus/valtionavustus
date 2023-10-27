c7a3b677b6047acc3d4b88da9af61c92bceef1e7
Riku Honkanen <riku.honkanen@reaktor.com>
2023-10-27 17:11:21 +0300

    VA-326: Remove infotext that is not needed anymore

--- va-virkailija/web/va/hakemusten-arviointi-page/hakemus-details/common-components/SelvitysNotFilled.jsx
+++ va-virkailija/web/va/hakemusten-arviointi-page/hakemus-details/common-components/SelvitysNotFilled.jsx
@@ -13,10 +13,8 @@ export default class SelvitysNotFilled extends Component {
       <div>
         <p>{title} ei ole vielä saapunut.</p>
         <p>
-          Selvityksen viimeinen toimituspäivämäärä on {dateString} tai 2 kuukautta hankkeen päättymisen
-          jälkeen.
+          Selvityksen viimeinen toimituspäivämäärä on {dateString}
         </p>
-
         <p>Yhteyshenkilöä muistutetaan automaattisesti sähköpostitse selvityksen täyttämisestä.</p>
       </div>
     )
