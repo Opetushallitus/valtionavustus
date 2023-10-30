5b19d96d7a34c352c3e973883678ec94f6b255b5
Riku Honkanen <riku.honkanen@reaktor.com>
2023-10-30 08:40:53 +0200

    VA-326: run prettier fix

--- va-virkailija/web/va/hakemusten-arviointi-page/hakemus-details/common-components/SelvitysNotFilled.jsx
+++ va-virkailija/web/va/hakemusten-arviointi-page/hakemus-details/common-components/SelvitysNotFilled.jsx
@@ -12,9 +12,7 @@ export default class SelvitysNotFilled extends Component {
     return (
       <div>
         <p>{title} ei ole vielä saapunut.</p>
-        <p>
-          Selvityksen viimeinen toimituspäivämäärä on {dateString}
-        </p>
+        <p>Selvityksen viimeinen toimituspäivämäärä on {dateString}</p>
         <p>Yhteyshenkilöä muistutetaan automaattisesti sähköpostitse selvityksen täyttämisestä.</p>
       </div>
     )
