2c10a3150ed20986234f2579d370376740665105
Antero Fagerstedt <antero.fagerstedt@reaktor.com>
2023-12-09 17:58:01 +0200

    VA-402-4: Test that finnish Jotpa logo is shown on finnish Jotpa hakemus

--- playwright/tests/jotpa/jotpa-hakemuksen-tayttaminen.test.ts
+++ playwright/tests/jotpa/jotpa-hakemuksen-tayttaminen.test.ts
@@ -1,16 +1,15 @@
-import { test, expect } from '@playwright/test'
+import { expect } from '@playwright/test'
 import { JotpaTest } from '../../fixtures/JotpaTest'
 import { HakijaAvustusHakuPage } from '../../pages/hakija/hakijaAvustusHakuPage'
 import { TEST_Y_TUNNUS } from '../../utils/constants'
 
 JotpaTest('Jotpa-hakemuksen täyttäminen', async ({ page, startedHakemus }) => {
-  test.fail()
   const { hakemusUrl } = startedHakemus
   await page.goto(hakemusUrl)
   const hakijaAvustusHakuPage = HakijaAvustusHakuPage(page)
   await hakijaAvustusHakuPage.fillInBusinessId(TEST_Y_TUNNUS)
 
   await JotpaTest.step('Hakemuksella näkyy Jotpan logo', async () => {
-    expect(true).toBe(false)
+    expect(await page.locator('#logo').screenshot()).toMatchSnapshot('jotpa-logo-fi.png')
   })
 })
