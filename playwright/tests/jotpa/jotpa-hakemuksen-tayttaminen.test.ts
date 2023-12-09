e711ca1516fd7c46d73fb5b9368d9a90c00499da
Antero Fagerstedt <antero.fagerstedt@reaktor.com>
2023-12-09 18:29:22 +0200

    VA-402-4: Test that swedish Jotpa logo is shown on swedish Jotpa hakemus

--- playwright/tests/jotpa/jotpa-hakemuksen-tayttaminen.test.ts
+++ playwright/tests/jotpa/jotpa-hakemuksen-tayttaminen.test.ts
@@ -3,13 +3,36 @@ import { JotpaTest } from '../../fixtures/JotpaTest'
 import { HakijaAvustusHakuPage } from '../../pages/hakija/hakijaAvustusHakuPage'
 import { TEST_Y_TUNNUS } from '../../utils/constants'
 
-JotpaTest('Jotpa-hakemuksen täyttäminen', async ({ page, startedHakemus }) => {
-  const { hakemusUrl } = startedHakemus
-  await page.goto(hakemusUrl)
+JotpaTest('Jotpa-hakemuksen täyttäminen', async ({ page, avustushakuID }) => {
   const hakijaAvustusHakuPage = HakijaAvustusHakuPage(page)
-  await hakijaAvustusHakuPage.fillInBusinessId(TEST_Y_TUNNUS)
 
-  await JotpaTest.step('Hakemuksella näkyy Jotpan logo', async () => {
-    expect(await page.locator('#logo').screenshot()).toMatchSnapshot('jotpa-logo-fi.png')
-  })
+  await JotpaTest.step(
+    'Suomenkielisellä hakemuksella näkyy suomenkielinen Jotpan logo',
+    async () => {
+      await hakijaAvustusHakuPage.navigate(avustushakuID, 'fi')
+      const hakemusUrl = await hakijaAvustusHakuPage.startApplication(
+        avustushakuID,
+        'buffy.summers@askjeeves.com'
+      )
+      await page.goto(hakemusUrl)
+      await hakijaAvustusHakuPage.fillInBusinessId(TEST_Y_TUNNUS)
+
+      expect(await page.locator('#logo').screenshot()).toMatchSnapshot('jotpa-logo-fi.png')
+    }
+  )
+
+  await JotpaTest.step(
+    'Ruotsinkielisellä hakemuksella näkyy ruotsinkielinen Jotpan logo',
+    async () => {
+      await hakijaAvustusHakuPage.navigate(avustushakuID, 'sv')
+      const hakemusUrl = await hakijaAvustusHakuPage.startApplication(
+        avustushakuID,
+        'faith.lehane@altavista.com'
+      )
+      await page.goto(hakemusUrl)
+      await hakijaAvustusHakuPage.fillInBusinessId(TEST_Y_TUNNUS)
+
+      expect(await page.locator('#logo').screenshot()).toMatchSnapshot('jotpa-logo-sv.png')
+    }
+  )
 })
