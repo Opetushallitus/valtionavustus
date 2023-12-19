ade0e2aeadd858e76fd1ad449f43bf4083dd93fe
Tero Keinänen <tero.keinanen@reaktor.com>
2023-12-19 17:07:28 +0200

    VA-407: Require accounts from environment on synth

--- cdk/lib/va-env-stage.ts
+++ cdk/lib/va-env-stage.ts
@@ -4,13 +4,19 @@ import { ValtionavustusEnvironment, setContext } from './va-context'
 
 const DEFAULT_REGION = 'eu-west-1'
 
-function maybeGetAccount(env: ValtionavustusEnvironment): string | undefined {
-  return process.env[`AWS_ACCOUNT_ID_${env.toUpperCase()}`]
+function getAccount(env: ValtionavustusEnvironment): string {
+  const envName = `AWS_ACCOUNT_ID_${env.toUpperCase()}`
+  const accountId = process.env[envName]
+  if (!accountId) {
+    throw new Error(`Missing ${envName}`)
+  }
+
+  return accountId
 }
 
 export class Environment extends cdk.Stage {
   constructor(scope: Construct, env: ValtionavustusEnvironment) {
-    const account = maybeGetAccount(env)
+    const account = getAccount(env)
     super(scope, env, { env: { region: DEFAULT_REGION, account } })
 
     setContext(this, env)
