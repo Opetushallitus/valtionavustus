4e863f7b2b7b58d7efc077dca9b9543542fc8e77
Tero Keinänen <tero.keinanen@reaktor.com>
2023-12-19 13:36:38 +0200

    VA-407: Set deployment role ARN as an environment secret in Github

--- cdk/scripts/bootstrap.sh
+++ cdk/scripts/bootstrap.sh
@@ -6,6 +6,8 @@ source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../../scripts/common-f
 
 readonly gh_org="opetushallitus"
 readonly gh_repo="valtionavustus"
+readonly gh_deployment_role_secret_prefix="DEPLOYMENT_ROLE"
+readonly dist_dir="$repo/cdk/dist"
 
 function create_env_in_github {
   info "Creating environment $ENV in Github repository $gh_org/$gh_repo"
@@ -14,6 +16,7 @@ function create_env_in_github {
 
 readonly cdk_outdir="cdk.out.bootstrap"
 readonly cdk_app="bin/bootstrap.js"
+readonly cdk_outputs="$dist_dir/outputs.json"
 
 function prepare_cdk {
   cd "$repo/cdk"
@@ -29,7 +32,7 @@ function deploy_cdk {
   cd "$repo/cdk"
   info "Deploying bootstrap stacks"
 
-  ./cdk.sh --app "$cdk_outdir" --build "true" deploy "$ENV/*"
+  ./cdk.sh --app "$cdk_outdir" --build "true" deploy --outputs-file "$cdk_outputs" "$ENV/*"
 }
 
 function bootstrap_cdk {
@@ -40,7 +43,6 @@ function bootstrap_cdk {
   require_cdk_context
 
   readonly context_variable_name="AWS_ACCOUNT_ID_$env_upper"
-  info "$context_variable_name"
   readonly aws_account_id="${!context_variable_name}"
   readonly aws_region="eu-west-1"
 
@@ -49,6 +51,19 @@ function bootstrap_cdk {
   export AWS_CONFIG_FILE="$VA_SECRETS_REPO/aws_config"
 
   npm run cdk -- bootstrap "$aws_account_id/$aws_region"
+}
+
+function set_deployment_role_secret {
+  info "Setting deployment role ARN in Github Actions secrets"
+  role_arn=$(jq --raw-output --exit-status ".[\"$ENV-github-actions-role\"].GithubActionOidcIamRoleArn" "$cdk_outputs")
+  readonly secret_name="${gh_deployment_role_secret_prefix}_$env_upper"
+  info "Deployment role ARN: $role_arn"
+  info "Setting $secret_name in $ENV"
+  echo -n "$role_arn" | \
+    gh secret set "$secret_name" \
+    --app actions \
+    --repo "$gh_org/$gh_repo" \
+    --env "$ENV"
 }
 
 function main {
@@ -67,12 +82,16 @@ function main {
   env_upper="$(echo "$ENV" | tr '[:lower:]' '[:upper:]')"
   readonly env_upper
 
+  rm -rf "$dist_dir"
+  mkdir -p "$dist_dir"
+
   info "Bootstrapping '$ENV' environment"
 
   create_env_in_github
   prepare_cdk
   bootstrap_cdk
   deploy_cdk
+  set_deployment_role_secret
 }
 
 main "$@"
