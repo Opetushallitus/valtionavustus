f00226e616ac3b9613a69d6f37358009ba9db623
Antero Fagerstedt <antero.fagerstedt@reaktor.com>
2023-12-19 13:05:04 +0200

    VA-400-5: Add attachment requirement to Jotpa hakemus

--- playwright/fixtures/JotpaTest.ts
+++ playwright/fixtures/JotpaTest.ts
@@ -1,13 +1,57 @@
 import { KoodienhallintaPage } from '../pages/virkailija/koodienHallintaPage'
 import { randomString } from '../utils/random'
 import { muutoshakemusTest } from './muutoshakemusTest'
+import muutoshakemusEnabledHakuLomakeJson from './prod.hakulomake.json'
 
 const jotpaToimintayksikkö = {
   name: 'Jatkuvan oppimisen ja työllisyyden palvelukeskus',
   code: '6600105300',
 }
 
-export const JotpaTest = muutoshakemusTest.extend({
+interface JotpaFixtures {
+  hakulomake: string
+}
+
+const requiredAttachmentField = {
+  fieldClass: 'wrapperElement',
+  id: 'mandatory-attachments',
+  fieldType: 'theme',
+  children: [
+    {
+      fieldClass: 'wrapperElement',
+      id: 'named-attachments-fieldset',
+      fieldType: 'fieldset',
+      children: [
+        {
+          label: {
+            fi: 'Edellisen tilikauden tuloslaskelma ja tase liitetietoineen',
+            sv: 'Edellisen tilikauden tuloslaskelma ja tase liitetietoineen på svenska',
+          },
+          fieldClass: 'formField',
+          helpText: {
+            fi: '',
+            sv: '',
+          },
+          id: 'previous-income-statement-and-balance-sheet',
+          required: true,
+          fieldType: 'namedAttachment',
+        },
+      ],
+    },
+  ],
+  label: {
+    fi: 'Vaaditut liitteet',
+    sv: 'Vaaditut liitteet på svenska',
+  },
+}
+
+const hakulomakeWithOneRequiredAttachment = {
+  ...muutoshakemusEnabledHakuLomakeJson,
+  content: [...muutoshakemusEnabledHakuLomakeJson.content, requiredAttachmentField],
+}
+
+export const JotpaTest = muutoshakemusTest.extend<JotpaFixtures>({
+  hakulomake: JSON.stringify(hakulomakeWithOneRequiredAttachment),
   codes: async ({ page }, use) => {
     const koodienhallintaPage = KoodienhallintaPage(page)
     await koodienhallintaPage.navigate()
