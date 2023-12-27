03d8dbf9f7b2a0a0a1a726f57c1dd0a8ecf097f5
Ville Aaltonen <ville.aaltonen@reaktor.com>
2023-12-27 13:09:07 +0200

    VA-394-1-6: remove test step

--- playwright/tests/hallinnoiavustuksia/hallinnoiavustuksia-excel-export.test.ts
+++ playwright/tests/hallinnoiavustuksia/hallinnoiavustuksia-excel-export.test.ts
@@ -31,57 +31,55 @@ test.describe('OPH', () => {
       expect(workbook.SheetNames).toMatchObject([SheetName])
       const sheet = workbook.Sheets[SheetName]
       const vireilletuloPvm = moment().format('DD.MM.YYYY')
-      await twoAcceptedHakemusTest.step('produces correct column labels', async () => {
-        const expectedRows: SheetRow[] = [
-          {
-            A1: 'valtionapuviranomainen',
-            B1: 'avustushakuAsianumero',
-            C1: 'avustushakuNimi',
-            D1: 'avustushakuAvustuslaji',
-            E1: 'avustushakuAlkaaPvm',
-            F1: 'avustushakuPaattyyPvm',
-            G1: 'avustushakuURL',
-            H1: 'avustusasiaAsianumero',
-            I1: 'avustusasiaVireilletuloPvm',
-            J1: 'avustusasiaKieli',
-            K1: 'avustusasiaVireillepanijaHenkiloTunnus',
-            L1: 'avustusasiaVireillepanijaHenkiloNimi',
-            M1: 'avustusasiaVireillepanijaYhteisoTunnus',
-            N1: 'avustusasiaVireillepanijaYhteisoNimi',
-            O1: 'avustushakemusHaettuKayttotarkoitus',
-            P1: 'avustushakemusHaettuAvustus',
-            Q1: 'avustushakemusAlueKunnat',
-            R1: 'avustushakemusAlueMaakunnat',
-            S1: 'avustushakemusAlueHyvinvointialueet',
-            T1: 'avustushakemusAlueValtiot',
-            U1: 'avustuspaatosPvm',
-            V1: 'avustuspaatosPerustelu',
-            W1: 'avustuspaatosTyyppi',
-            X1: 'avustuspaatosMyonnettyAvustus',
-            Y1: 'avustuspaatosHyvaksyttyKayttotarkoitus',
-            Z1: 'avustuspaatosKayttoaikaAlkaaPvm',
-            AA1: 'avustuspaatosKayttoaikaPaattyyPvm',
-            AB1: 'avustuspaatosMaksettuAvustus',
-            AC1: 'piilotaKayttotarkoitus',
-            AD1: 'piilotaVireillepanija',
-          },
-          {
-            A2: 'OPH',
-            B2: 'va-oph-2023-6',
-            H2: `1/${hakuProps.registerNumber}`,
-            I2: vireilletuloPvm,
-            J2: 'fi',
-          },
-          {
-            A3: 'OPH',
-            B3: 'va-oph-2023-6',
-            H3: `2/${hakuProps.registerNumber}`,
-            I3: vireilletuloPvm,
-            J3: 'fi',
-          },
-        ]
-        expectToFindRowWithValuesInSheet(sheet, expectedRows)
-      })
+      const expectedRows: SheetRow[] = [
+        {
+          A1: 'valtionapuviranomainen',
+          B1: 'avustushakuAsianumero',
+          C1: 'avustushakuNimi',
+          D1: 'avustushakuAvustuslaji',
+          E1: 'avustushakuAlkaaPvm',
+          F1: 'avustushakuPaattyyPvm',
+          G1: 'avustushakuURL',
+          H1: 'avustusasiaAsianumero',
+          I1: 'avustusasiaVireilletuloPvm',
+          J1: 'avustusasiaKieli',
+          K1: 'avustusasiaVireillepanijaHenkiloTunnus',
+          L1: 'avustusasiaVireillepanijaHenkiloNimi',
+          M1: 'avustusasiaVireillepanijaYhteisoTunnus',
+          N1: 'avustusasiaVireillepanijaYhteisoNimi',
+          O1: 'avustushakemusHaettuKayttotarkoitus',
+          P1: 'avustushakemusHaettuAvustus',
+          Q1: 'avustushakemusAlueKunnat',
+          R1: 'avustushakemusAlueMaakunnat',
+          S1: 'avustushakemusAlueHyvinvointialueet',
+          T1: 'avustushakemusAlueValtiot',
+          U1: 'avustuspaatosPvm',
+          V1: 'avustuspaatosPerustelu',
+          W1: 'avustuspaatosTyyppi',
+          X1: 'avustuspaatosMyonnettyAvustus',
+          Y1: 'avustuspaatosHyvaksyttyKayttotarkoitus',
+          Z1: 'avustuspaatosKayttoaikaAlkaaPvm',
+          AA1: 'avustuspaatosKayttoaikaPaattyyPvm',
+          AB1: 'avustuspaatosMaksettuAvustus',
+          AC1: 'piilotaKayttotarkoitus',
+          AD1: 'piilotaVireillepanija',
+        },
+        {
+          A2: 'OPH',
+          B2: 'va-oph-2023-6',
+          H2: `1/${hakuProps.registerNumber}`,
+          I2: vireilletuloPvm,
+          J2: 'fi',
+        },
+        {
+          A3: 'OPH',
+          B3: 'va-oph-2023-6',
+          H3: `2/${hakuProps.registerNumber}`,
+          I3: vireilletuloPvm,
+          J3: 'fi',
+        },
+      ]
+      expectToFindRowWithValuesInSheet(sheet, expectedRows)
     }
   )
 })
