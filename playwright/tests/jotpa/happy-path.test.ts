619b430255313e12001d87d6c59d687205be0d17
Riku Honkanen <riku.honkanen@reaktor.com>
2024-01-29 12:29:45 +0200

    VA-420-25: Create swedish valiselvitys submitted test

--- playwright/tests/jotpa/happy-path.test.ts
+++ playwright/tests/jotpa/happy-path.test.ts
@@ -10,6 +10,21 @@ import { randomString } from '../../utils/random'
 import { Answers } from '../../utils/types'
 import { swedishAnswers } from '../../utils/constants'
 import { expectToBeDefined } from '../../utils/util'
+
+const swedishSignature =
+  'Servicecentret för kontinuerligt lärande och sysselsättning\n' +
+  'Hagnäskajen 6\n' +
+  'PB 380, 00531 Helsingfors\n' +
+  'telefon 029 533 1000\n' +
+  'fornamn.efternamn@jotpa.fi'
+
+const finnishSignature =
+  'Jatkuvan oppimisen ja työllisyyden palvelukeskus\n' +
+  'Hakaniemenranta 6\n' +
+  'PL 380, 00531 Helsinki\n' +
+  'puhelin 029 533 1000\n' +
+  'etunimi.sukunimi@jotpa.fi'
+
 const jotpaSelvitysTest = selvitysTest.extend({
   codes: async ({ page }, use) => {
     const koodienhallintaPage = KoodienhallintaPage(page)
@@ -30,35 +45,6 @@ const swedishJotpaSelvitysTest = jotpaSelvitysTest.extend<{ answers: Answers }>(
 
 jotpaSelvitysTest(
   'Jotpa hakemus happy path all the way to the loppuselvitys ok',
-  async ({
-    acceptedHakemus: { hakemusID },
-    valiAndLoppuselvitysSubmitted,
-    asiatarkastus,
-    taloustarkastus,
-    hakuProps,
-  }) => {
-    test.step('väliselvitys email', async () => {
-      const emails = await waitUntilMinEmails(
-        getValiselvitysSubmittedNotificationEmails,
-        1,
-        hakemusID
-      )
-
-      const email = emails[0]
-      expect(email['from-address']).toEqual('no-reply@jotpa.fi')
-      expect(email.formatted).toContain(
-        'Jatkuvan oppimisen ja työllisyyden palvelukeskus\n' +
-          'Hakaniemenranta 6\n' +
-          'PL 380, 00531 Helsinki\n' +
-          'puhelin 029 533 1000\n' +
-          'etunimi.sukunimi@jotpa.fi'
-      )
-    })
-  }
-)
-
-swedishJotpaSelvitysTest(
-  'Swedish Jotpa hakemus happy path all the way to the loppuselvitys ok',
   async ({ acceptedHakemus: { hakemusID }, valiAndLoppuselvitysSubmitted }) => {
     expectToBeDefined(valiAndLoppuselvitysSubmitted)
 
@@ -71,6 +57,26 @@ swedishJotpaSelvitysTest(
 
       const email = emails[0]
       expect(email['from-address']).toEqual('no-reply@jotpa.fi')
+      expect(email.formatted).toContain(finnishSignature)
+    })
+  }
+)
+
+swedishJotpaSelvitysTest(
+  'Swedish Jotpa hakemus happy path all the way to the loppuselvitys ok',
+  async ({ acceptedHakemus: { hakemusID }, valiAndLoppuselvitysSubmitted }) => {
+    expectToBeDefined(valiAndLoppuselvitysSubmitted)
+
+    test.step('väliselvitys email', async () => {
+      const emails = await waitUntilMinEmails(
+        getValiselvitysSubmittedNotificationEmails,
+        1,
+        hakemusID
+      )
+
+      const email = emails[0]
+      expect(email['from-address']).toEqual('no-reply@jotpa.fi')
+      expect(email.formatted).toContain(swedishSignature)
     })
   }
 )
