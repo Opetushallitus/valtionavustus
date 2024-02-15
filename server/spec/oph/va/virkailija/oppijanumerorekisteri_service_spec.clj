e8556e5239fe80f7760ce005686b3ff59614f379
Riku Honkanen <riku.honkanen@reaktor.com>
2024-02-15 08:44:10 +0200

    OPHY-689-2: Get rid of cas client in oppijanumerorekisteri service

--- server/spec/oph/va/virkailija/oppijanumerorekisteri_service_spec.clj
+++ server/spec/oph/va/virkailija/oppijanumerorekisteri_service_spec.clj
@@ -1,11 +1,9 @@
 (ns oph.va.virkailija.oppijanumerorekisteri-service-spec
-  (:require [speclj.core :refer :all]
+  (:require [cheshire.core :as cheshire]
             [oph.va.virkailija.fake-http :as fake-http]
-            [oph.va.virkailija.url :as url]
-            [oph.va.virkailija.oppijanumerorekisteri-service :as onrs]))
+            [oph.va.virkailija.oppijanumerorekisteri-service :as onrs]
+            [speclj.core :refer :all]))
 
-(defmacro with-fake-service-client [responses & body]
-  `(fake-http/with-fake-http-client onrs/service-client ~responses ~@body))
 
 (def person-oid-1 "1.22.333.4444.24.10000000001")
 
@@ -18,6 +16,33 @@
 (def contact-info-data
   {:email "YHTEYSTIETO_SAHKOPOSTI"
    :phone "YHTEYSTIETO_PUHELIN"})
+
+(defn- make-mock-response [body]
+  {:opts
+   {:query-params
+    {:ticket "ST-123"},
+    :headers {"caller-id" "1.2.246.562.10.00000000001.valtionavustus"},
+    :method :get,
+    :url
+    "https://virkailija.testiopintopolku.fi/oppijanumerorekisteri-service/henkilo/123"},
+   :body
+   (cheshire/generate-string body),
+   :headers
+   {:date "Wed, 14 Feb 2024 14:50:47 GMT",
+    :content-disposition "inline;filename=f.txt",
+    :x-xss-protection "1; mode=block",
+    :x-content-type-options "nosniff",
+    :server "nginx",
+    :x-robots-tag "noindex, nofollow, noarchive",
+    :x-frame-options "SAMEORIGIN",
+    :strict-transport-security "max-age=16180339; includeSubDomains",
+    :content-type "application/json",
+    :content-encoding "gzip",
+    :connection "keep-alive",
+    :transfer-encoding "chunked",
+    :set-cookie
+    "JSESSIONID=123; Path=/oppijanumerorekisteri-service; Secure; HttpOnly; SameSite=Lax"},
+   :status 200})
 
 (defn- make-lang-data [lang-code]
   (when lang-code
@@ -38,63 +63,65 @@
    "asiointiKieli"     (make-lang-data business-lang)
    "yhteystiedotRyhma" (map-indexed make-contact-info-data contact-infos)})
 
+
+
 (describe "Oppijanumerorekisteri service"
-  (it "fetches person"
-      (let [user-data (make-user-data {:person-oid    person-oid-1
-                                       :native-lang   :fi
-                                       :business-lang :sv
-                                       :contact-infos [{:value "pvirt@example.com" :type :email}]})
-            expected  {:first-name "Pekka"
-                       :surname    "Virtanen"
-                       :lang       "sv"
-                       :email      "pvirt@example.com"}]
-        (with-fake-service-client {(onrs/make-person-url person-oid-1) user-data}
-          (should= expected (onrs/get-person person-oid-1)))))
+          (it "fetches person"
+              (let [user-data (make-user-data {:person-oid    person-oid-1
+                                               :native-lang   :fi
+                                               :business-lang :sv
+                                               :contact-infos [{:value "pvirt@example.com" :type :email}]})
+                    expected  {:first-name "Pekka"
+                               :surname    "Virtanen"
+                               :lang       "sv"
+                               :email      "pvirt@example.com"}]
+                (should= expected (onrs/try-response->va-user-info  (make-mock-response user-data)))))
 
-  (it "fetching person selects first non-nil lang"
-      (let [user-data (make-user-data {:person-oid  person-oid-1
-                                       :native-lang :sv})]
-        (with-fake-service-client {(onrs/make-person-url person-oid-1) user-data}
-          (should= "sv" (:lang (onrs/get-person person-oid-1))))))
+          (it "fetching person selects first non-nil lang"
+              (let [user-data (make-user-data {:person-oid  person-oid-1
+                                               :native-lang :sv})]
 
-  (it "fetching person falls back to fi lang if no other language was found"
-      (let [user-data (make-user-data {:person-oid person-oid-1})]
-        (with-fake-service-client {(onrs/make-person-url person-oid-1) user-data}
-          (should= "fi" (:lang (onrs/get-person person-oid-1))))))
+                (should= "sv" (:lang (onrs/try-response->va-user-info   (make-mock-response user-data))))))
 
-  (it "fetching person selects oph.fi email if available"
-      (let [user-data (make-user-data {:person-oid    person-oid-1
-                                       :contact-infos [{:value "0501001001"         :type :phone}
-                                                       {:value "pvirt1@example.com" :type :email}
-                                                       {:value "pvirt2@oph.fi" :type :email}]})]
-        (with-fake-service-client {(onrs/make-person-url person-oid-1) user-data}
-          (should= "pvirt2@oph.fi" (:email (onrs/get-person person-oid-1))))))
+          (it "fetching person falls back to fi lang if no other language was found"
+              (let [user-data (make-user-data {:person-oid person-oid-1})]
+                (should= "fi" (:lang (onrs/try-response->va-user-info   (make-mock-response user-data))))))
 
-  (it "fetching person selects first email if oph.fi address not available"
-      (let [user-data (make-user-data {:person-oid    person-oid-1
-                                       :contact-infos [{:value "0501001001"         :type :phone}
-                                                       {:value "pvirt1@example.com" :type :email}
-                                                       {:value "pvirt2@example.com" :type :email}]})]
-        (with-fake-service-client {(onrs/make-person-url person-oid-1) user-data}
-          (should= "pvirt1@example.com" (:email (onrs/get-person person-oid-1))))))
+          (it "fetching person selects oph.fi email if available"
+              (let [user-data (make-user-data {:person-oid    person-oid-1
+                                               :contact-infos [{:value "0501001001"         :type :phone}
+                                                               {:value "pvirt1@example.com" :type :email}
+                                                               {:value "pvirt2@oph.fi" :type :email}]})]
+                (should= "pvirt2@oph.fi" (:email (onrs/try-response->va-user-info   (make-mock-response user-data))))))
 
-  (it "fetches many persons"
-      (let [users-data {person-oid-1 (make-user-data {:person-oid    person-oid-1
-                                                      :native-lang   :fi
-                                                      :business-lang :sv
-                                                      :contact-infos [{:value "pvirt1@example.com" :type :email}]})
-                        person-oid-2 (make-user-data {:person-oid    person-oid-2
-                                                      :native-lang   :sv
-                                                      :business-lang :fi
-                                                      :contact-infos [{:value "pvirt2@example.com" :type :email}]})}
-            expected   (list {:first-name "Pekka"
-                              :surname    "Virtanen"
-                              :lang       "sv"
-                              :email      "pvirt1@example.com"}
-                             {:first-name "Pekka"
-                              :surname    "Virtanen"
-                              :lang       "fi"
-                              :email      "pvirt2@example.com"})]
-        (with-fake-service-client {(onrs/make-person-url person-oid-1) (get users-data person-oid-1)
-                                   (onrs/make-person-url person-oid-2) (get users-data person-oid-2)}
-          (should= expected (onrs/get-people [person-oid-1 person-oid-2]))))))
+          (it "fetching person selects first email if oph.fi address not available"
+              (let [user-data (make-user-data {:person-oid    person-oid-1
+                                               :contact-infos [{:value "0501001001"         :type :phone}
+                                                               {:value "pvirt1@example.com" :type :email}
+                                                               {:value "pvirt2@example.com" :type :email}]})]
+                  (should= "pvirt1@example.com" (:email (onrs/try-response->va-user-info   (make-mock-response user-data))))))
+
+          (it "fetches many persons"
+              (let [users-data {person-oid-1 (make-user-data {:person-oid    person-oid-1
+                                                              :native-lang   :fi
+                                                              :business-lang :sv
+                                                              :contact-infos [{:value "pvirt1@example.com" :type :email}]})
+                                person-oid-2 (make-user-data {:person-oid    person-oid-2
+                                                              :native-lang   :sv
+                                                              :business-lang :fi
+                                                              :contact-infos [{:value "pvirt2@example.com" :type :email}]})}
+                    futures (map (fn [x] (future x)) (list (make-mock-response (get users-data person-oid-2))))
+
+                    expected   (list {:first-name "Pekka"
+                                      :surname    "Virtanen"
+                                      :lang       "sv"
+                                      :email      "pvirt1@example.com"}
+                                     {:first-name "Pekka"
+                                      :surname    "Virtanen"
+                                      :lang       "fi"
+                                      :email      "pvirt2@example.com"})]
+
+
+                (should= expected (onrs/handle-first-and-futures (make-mock-response (get users-data person-oid-1)) futures))))
+
+          )
--- server/src/clojure/oph/va/virkailija/oppijanumerorekisteri_service.clj
+++ server/src/clojure/oph/va/virkailija/oppijanumerorekisteri_service.clj
@@ -1,16 +1,14 @@
 (ns oph.va.virkailija.oppijanumerorekisteri-service
-  (:require [oph.soresu.common.config :refer [config]]
-            [oph.va.virkailija.cas :as cas]
-            [oph.va.virkailija.http :as http]
-            [oph.va.virkailija.url :as url]))
+  (:require [clojure.data.json :as json]
+            [clojure.string :as str :refer [includes?]]
+            [oph.common.caller-id :as caller-id]
+            [oph.soresu.common.config :refer [config]]
+            [oph.va.virkailija.url :as url]
+            [org.httpkit.client :as hk-client]))
 
 (def service-base-url
   (when-not *compile-files*
     (str (get-in config [:opintopolku :url]) "/oppijanumerorekisteri-service")))
-
-(def ^:private service-client
-  (when-not *compile-files*
-    (delay (http/make-http-client (cas/make-cas-authenticating-client service-base-url)))))
 
 (defn- find-person-lang [person]
   (let [found (as-> person $
@@ -40,15 +38,67 @@
    :lang       (find-person-lang person)
    :email      (find-person-email person)})
 
+(defn get-tgt []
+  (let [opintopolku-url (-> config :opintopolku :url)
+        username (get-in config [:opintopolku :cas-service-username])
+        password (get-in config [:opintopolku :cas-service-password])
+        tgt-response @(hk-client/post (str opintopolku-url "/cas/v1/tickets") {:form-params {:username username :password password}})
+        location-header (-> tgt-response :headers :location)
+        tgt (re-find (re-pattern "TGT-.*") location-header)]
+    tgt))
+(defn get-st [tgt]
+  (let [opintopolku-url (-> config :opintopolku :url)
+        service (str service-base-url "/j_spring_cas_security_check")
+        st-res @(hk-client/post (str opintopolku-url "/cas/v1/tickets/" tgt) {:query-params {:service service} :headers {"caller-id" caller-id/caller-id
+                                                                                                                         "CSRF" caller-id/caller-id}})
+        st (slurp (:body st-res))]
+    st))
+
+(defn get-person-req [person-oid st]
+  (let [res @(hk-client/get (make-person-url person-oid) {:query-params {:ticket st}
+                                                          :headers {"caller-id" caller-id/caller-id}})]
+    res))
+(defn get-person-req-with-cookie-promise [person-oid jsessionid]
+  (let [res (hk-client/get (make-person-url person-oid) {:headers {"caller-id" caller-id/caller-id
+                                                                   "cookie"  jsessionid}})]
+    res))
+
+(defn try-parse-person-from-response [response]
+  (try
+    (json/read-str (:body response) :key-fn keyword)
+    (catch Exception e (throw (Exception. (str "Parsing body from response failed. Response: " response "\nError: " (.getMessage e)))))))
+
+(defn try-response->va-user-info [response]
+  (person->va-user-info (try-parse-person-from-response response)))
+
+(defn handle-first-and-futures [first-response futures]
+  (conj  (->> futures
+              (map deref)
+              (map (fn [r]
+                     (cond (= (:status r) 404) nil
+                           (= (:status r) 200) (try-response->va-user-info r)
+                           :else (throw (Exception. (str "Fetching person from oppijanumerorekisteri failed" r)))))))
+         (try-response->va-user-info first-response)))
+
+
+(defn get-all-people [person-oids]
+  (let [first-oid (first person-oids)
+        tgt (get-tgt)
+        st (get-st tgt)
+        first-person-response (get-person-req first-oid st)
+        cookies (str (-> first-person-response :headers :set-cookie))
+        jsession-id  (first (filter (fn [x] (includes? x "JSESSIONID")) (str/split cookies #";")))
+        rest-oids (rest person-oids)
+        futures (doall (map (fn [x] (get-person-req-with-cookie-promise x jsession-id)) rest-oids))]
+    (handle-first-and-futures first-person-response futures)))
+
+
 (defn get-person [person-oid]
-  (let [found (http/client-get-json @service-client (make-person-url person-oid))]
-    (person->va-user-info found)))
+  (let [tgt (get-tgt)
+        st (get-st tgt)
+        person-response (get-person-req person-oid st)]
+
+    (try-response->va-user-info person-response)))
 
 (defn get-people [person-oids]
-  (->> person-oids
-       (map make-person-url)
-       (http/client-pget-json @service-client)
-       (map (fn [r]
-              (cond (http/response-exception? 404 r) nil
-                    (instance? Exception r)          (throw r)
-                    :else                            (person->va-user-info r))))))
+  (get-all-people person-oids))
