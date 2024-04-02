5dd7dd076e80e66ebc1394db1dd132ee90522609
Simo-Pekka Kerkelä <simo-pekka.kerkela@reaktor.com>
2024-04-02 14:58:05 +0300

    VA-410-2 use version 15 of pg

--- cdk/lib/db-stack.ts
+++ cdk/lib/db-stack.ts
@@ -16,7 +16,7 @@ export class DbStack extends cdk.Stack {
 
     const parameterGroup = new ParameterGroup(this, 'VaPostgresParameterGroup', {
       engine: cdk.aws_rds.DatabaseClusterEngine.auroraPostgres({
-        version: cdk.aws_rds.AuroraPostgresEngineVersion.VER_13_13,
+        version: cdk.aws_rds.AuroraPostgresEngineVersion.VER_15_5,
       }),
       description: 'Custom parameter group for VA Postgres',
       parameters: {},
