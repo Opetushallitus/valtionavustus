13d2b8a3b588526110318b0e14b4d0a4dac93bad
Antero Fagerstedt <antero.fagerstedt@reaktor.com>
2024-04-24 16:45:58 +0300

    VA-413-4: Make sure that currently used hostnames resolve to their current IP addresses when switching to Route53

--- dns/test-local-dns.sh
+++ dns/test-local-dns.sh
@@ -5,7 +5,7 @@ echo "========================================"
 
 DNSTEST_IP=$(dig +short testi.statsunderstod.oph.fi) ## This A record does not exist in actual production name servers
 
-if [ "$DNSTEST_IP" = "86.50.28.144" ]; then
+if [ "$DNSTEST_IP" == "86.50.28.144" ]; then
   echo "Sun kone käyttää manuaalisia DNS-konffeja, testi voidaan ajaa..."
 else
   echo "Sun kone käyttää julkisia nimipalvelimia, testiä ei voida ajaa"
@@ -22,6 +22,17 @@ assert_domain_exists () {
   fi
 }
 
+assert_domain_resolves_to_ip () {
+  DOMAIN="${1}"
+  IP="${2}"
+  if [[ $(dig +short A "${DOMAIN}") != "${IP}" ]]; then
+    echo -e "\033[0;31m${DOMAIN} does not resolve to ${IP}"
+    exit 1
+  else
+    echo "${DOMAIN} resolves to ${IP}"
+  fi
+}
+
 assert_domain_exists "valtionavustukset.oph.fi"
 assert_domain_exists "testi.valtionavustukset.oph.fi"
 assert_domain_exists "dev.valtionavustukset.oph.fi"
@@ -34,4 +45,9 @@ assert_domain_exists "virkailija.valtionavustukset.oph.fi"
 assert_domain_exists "dev.virkailija.valtionavustukset.oph.fi" "SOA"
 assert_domain_exists "testi.virkailija.valtionavustukset.oph.fi"
 
-assert_domain_exists "dev.valtionavustukset.oph.fi"
+assert_domain_resolves_to_ip "valtionavustukset.oph.fi" "86.50.28.144"
+assert_domain_resolves_to_ip "statsunderstod.oph.fi" "86.50.28.144"
+assert_domain_resolves_to_ip "dev.valtionavustukset.oph.fi" "86.50.28.144"
+assert_domain_resolves_to_ip "testi.valtionavustukset.oph.fi" "86.50.28.144"
+assert_domain_resolves_to_ip "testi.virkailija.valtionavustukset.oph.fi" "86.50.28.144"
+assert_domain_resolves_to_ip "virkailija.valtionavustukset.oph.fi" "86.50.28.144"
