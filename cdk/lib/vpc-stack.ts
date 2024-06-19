f4e3aeda030cb90f5a0386bc05df31c3d37c0084
Antero Fagerstedt <antero.fagerstedt@reaktor.com>
2024-06-19 13:40:31 +0300

    VA-408: Use persistent elastic IP addresses for NAT GW. This allows IP whitelisting for e.g. Handi

--- cdk/lib/vpc-stack.ts
+++ cdk/lib/vpc-stack.ts
@@ -1,13 +1,31 @@
 import * as cdk from 'aws-cdk-lib'
 import { RemovalPolicy } from 'aws-cdk-lib'
 import { Environment } from './va-env-stage'
-import { SubnetType } from 'aws-cdk-lib/aws-ec2'
+import { CfnEIP, NatProvider, SubnetType } from 'aws-cdk-lib/aws-ec2'
 
 export class VpcStack extends cdk.Stack {
   public vpc: cdk.aws_ec2.Vpc
+  natGwElasticIPAddresses: CfnEIP[]
 
   constructor(scope: Environment, id: string, props?: cdk.StackProps) {
     super(scope, id, props)
+
+    const createElasticIP = (id: string, name: string) => {
+      const eip = new CfnEIP(this, id, {
+        tags: [{ key: 'Name', value: name }],
+      })
+      eip.applyRemovalPolicy(RemovalPolicy.RETAIN)
+      return eip
+    }
+
+    this.natGwElasticIPAddresses = [
+      createElasticIP('nat-gw-elastic-ip-1', 'NAT-GW-Elastic-IP-1'),
+      createElasticIP('nat-gw-elastic-ip-2', 'NAT-GW-Elastic-IP-2'),
+      createElasticIP('nat-gw-elastic-ip-3', 'NAT-GW-Elastic-IP-3'),
+    ]
+
+    const natGatewayEipAllocationIds = this.natGwElasticIPAddresses.map((ip) => ip.attrAllocationId)
+    const natGateway = NatProvider.gateway({ eipAllocationIds: natGatewayEipAllocationIds })
 
     const vpc = new cdk.aws_ec2.Vpc(this, 'VPC', {
       subnetConfiguration: [
@@ -27,6 +45,7 @@ export class VpcStack extends cdk.Stack {
           cidrMask: 24,
         },
       ],
+      natGatewayProvider: natGateway,
     })
 
     this.vpc = vpc
