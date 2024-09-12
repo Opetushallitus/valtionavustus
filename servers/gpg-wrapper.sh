7eacf2d2c34ba83d9590089f546ae2ad12e947bf
Antero Fagerstedt <antero.fagerstedt@reaktor.com>
2024-09-12 18:08:11 +0300

    OPHY-703: Remove Ansible Vaults

--- servers/gpg-wrapper.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-script_location=$( cd "$( dirname "$0" )" && pwd )
-
-VAULT_PW_FILENAME="$script_location/va-secrets.gpg"
-gpg --quiet --batch --use-agent --decrypt $VAULT_PW_FILENAME
