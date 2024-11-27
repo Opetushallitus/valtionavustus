6b2e0b95b3d696d887a0cf8c06856e36effce369
Ville Aaltonen <ville.aaltonen@reaktor.com>
2024-11-27 12:34:02 +0200

    Team has decided to remove lein dependency check, at least for now

--- dependency-check/project.clj
+++
@@ -1,6 +0,0 @@
-(defproject nvd-helper "local"
-  :description "nvd-clojure helper project"
-  :plugins [[lein-ancient "0.7.0"]]
-  :dependencies [
-                  [nvd-clojure "4.0.0"]
-                ])
