d48154486211aae981d5001287e974b9fa89ace2
Ville Aaltonen <ville.aaltonen@reaktor.com>
2025-04-07 12:29:15 +0300

    optimize listing query

--- server/resources/sql/hakija/avustushaku/get-avustushakus-for-listing.sql
+++ server/resources/sql/hakija/avustushaku/get-avustushakus-for-listing.sql
@@ -7,59 +7,64 @@ vastuuvalmistelijat AS (
   SELECT
     avustushaku AS avustushaku_id,
     name AS vastuuvalmistelija
-  FROM avustushakus AS avustushaku
-  JOIN hakija.avustushaku_roles ON avustushaku = avustushaku_id
-  WHERE hakija.avustushaku_roles.role = 'vastuuvalmistelija'
+  FROM hakija.avustushaku_roles
+  WHERE avustushaku IN (SELECT id FROM avustushakus) AND
+        hakija.avustushaku_roles.role = 'vastuuvalmistelija'
 ),
 paatokset_lahetetty AS (
   SELECT avustushaku_id, min(tapahtumaloki.created_at) AS paatokset_lahetetty
-  FROM avustushakus avustushaku
-  JOIN virkailija.tapahtumaloki USING (avustushaku_id)
-  WHERE success AND tapahtumaloki.tyyppi = 'paatoksen_lahetys'
+  FROM virkailija.tapahtumaloki
+  WHERE tapahtumaloki.avustushaku_id IN (SELECT id FROM avustushakus) AND
+        success AND
+        tapahtumaloki.tyyppi = 'paatoksen_lahetys'
   GROUP BY avustushaku_id
 ),
 maksatukset_lahetetty AS (
   SELECT
-    avustushaku.id AS avustushaku_id,
+    hakemus_version.avustushaku AS avustushaku_id,
     min(payments.created_at) AS maksatukset_lahetetty
-  FROM avustushakus avustushaku
-  JOIN hakija.hakemukset hakemus_version ON hakemus_version.avustushaku = avustushaku.id
+  FROM hakija.hakemukset hakemus_version
   JOIN virkailija.payments ON hakemus_version.id = payments.application_id AND hakemus_version.version = payments.application_version
-  WHERE payments.deleted is NULL AND payments.paymentstatus_id = 'sent'
-  GROUP BY avustushaku.id
+  WHERE hakemus_version.avustushaku IN (SELECT id FROM avustushakus) AND
+        payments.deleted is NULL AND
+        payments.paymentstatus_id = 'sent'
+  GROUP BY hakemus_version.avustushaku
 ),
 maksatukset_summa AS (
   SELECT
-    avustushaku.id AS avustushaku_id,
+    hakemus_version.avustushaku AS avustushaku_id,
     coalesce(sum(payment_sum), 0) AS maksatukset_summa
-  FROM avustushakus avustushaku
-  JOIN hakija.hakemukset hakemus_version ON hakemus_version.avustushaku = avustushaku.id
+  FROM hakija.hakemukset hakemus_version
   JOIN virkailija.payments ON hakemus_version.id = payments.application_id AND hakemus_version.version = payments.application_version
-  WHERE payments.version_closed is NULL AND payments.paymentstatus_id IN ('sent', 'paid')
-  GROUP BY avustushaku.id
+  WHERE hakemus_version.avustushaku IN (SELECT id FROM avustushakus) AND
+        payments.version_closed is NULL AND
+        payments.paymentstatus_id IN ('sent', 'paid')
+  GROUP BY hakemus_version.avustushaku
 ),
 valiselvityspyynnot_lahetetty AS (
   SELECT avustushaku_id, min(tapahtumaloki.created_at) AS valiselvitykset_lahetetty
-  FROM avustushakus avustushaku
-    JOIN virkailija.tapahtumaloki USING (avustushaku_id)
-  WHERE success AND tapahtumaloki.tyyppi = 'valiselvitys-notification'
+  FROM virkailija.tapahtumaloki
+  WHERE avustushaku_id IN (SELECT id FROM avustushakus) AND
+        success AND
+        tapahtumaloki.tyyppi = 'valiselvitys-notification'
   GROUP BY avustushaku_id
 ),
 loppuselvityspyynnot_lahetetty AS (
   SELECT avustushaku_id, min(tapahtumaloki.created_at) AS loppuselvitykset_lahetetty
-  FROM avustushakus avustushaku
-  JOIN virkailija.tapahtumaloki USING (avustushaku_id)
-  WHERE success AND tapahtumaloki.tyyppi = 'loppuselvitys-notification'
+  FROM virkailija.tapahtumaloki
+  WHERE avustushaku_id IN (SELECT id FROM avustushakus) AND
+        success AND
+        tapahtumaloki.tyyppi = 'loppuselvitys-notification'
   GROUP BY avustushaku_id
 ),
 use_detailed_costs AS (
-  SELECT DISTINCT ON (avustushaku.id)
-    avustushaku.id as avustushaku_id,
+  SELECT DISTINCT ON (h.avustushaku)
+    h.avustushaku as avustushaku_id,
     a.use_overridden_detailed_costs
-  FROM avustushakus avustushaku
-  JOIN hakija.hakemukset h ON h.avustushaku = avustushaku.id
+  FROM hakija.hakemukset h
   JOIN virkailija.arviot a ON a.hakemus_id = h.id
-  WHERE h.version_closed IS NULL AND
+  WHERE h.avustushaku IN (SELECT id FROM avustushakus) AND
+        h.version_closed IS NULL AND
         a.status = 'accepted'
 )
 SELECT
