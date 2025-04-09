191e90615192bd8b961d87ee5f3cf892e01330b4
Antero Fagerstedt <antero.fagerstedt@reaktor.com>
2025-04-09 17:02:40 +0300

    OPHY-722-3: Code changes required by Flyway to 6

--- server/src/clojure/oph/soresu/common/db/migrations.clj
+++ server/src/clojure/oph/soresu/common/db/migrations.clj
@@ -5,27 +5,26 @@
   (:require [clojure.tools.logging :as log]
             [oph.soresu.common.db :as db])
   (:import [org.flywaydb.core Flyway]
-           [org.flywaydb.core.api.migration.jdbc JdbcMigration]
-           [org.flywaydb.core.api.migration MigrationInfoProvider]
+           [org.flywaydb.core.api.migration JavaMigration]
            [org.flywaydb.core.api MigrationVersion]))
 
 (defn migrate [schema-name migration-paths]
   (log/info "Running db migrations, if any...")
-  (let [flyway (doto (Flyway.)
-                 (.setSchemas (into-array String [schema-name]))
-                 (.setDataSource (db/get-datasource))
-                 (.setLocations (into-array String migration-paths)))]
-    (try (.migrate flyway)
+  (let [config (Flyway/configure)]
+    (.schemas config (into-array String [schema-name]))
+    (.dataSource config (db/get-datasource))
+    (.locations config (into-array String migration-paths))
+    (try (.migrate (Flyway. config))
          (catch Throwable e
            (log/error e)
            (throw e)))))
 
 (defmacro defmigration [name version description & body]
   `(deftype ~name []
-     JdbcMigration
+    JavaMigration
      (migrate [this connection]
        ~@body)
 
-     MigrationInfoProvider
+     (getChecksum [this] nil)
      (getDescription [this] ~description)
      (getVersion [this] (MigrationVersion/fromVersion ~version))))
