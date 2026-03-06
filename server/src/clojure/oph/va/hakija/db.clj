85b50c6afb0f75816c5b8ea1bcec36d90f59b71b
Riku Honkanen <riku.honkanen@reaktor.com>
2026-03-06 15:40:49 +0200

    VA-477-2 Restrict yhteishanke organization updates to contact person and email only

--- server/src/clojure/oph/va/hakija/db.clj
+++ server/src/clojure/oph/va/hakija/db.clj
@@ -539,22 +539,19 @@
               [contact-name contact-email contact-phone hakemus-id]))
   (log/info (str "Successfully changed trusted contact person details with user-key: " user-key)))
 
-(defn- replace-yhteishanke-organizations [tx hakemus-id organizations]
+(defn- update-yhteishanke-organization-contacts [tx hakemus-id organizations]
   (when (feature-enabled? :enableYhteishankeEmails)
-    (log/info (str "Replacing yhteishanke organizations for hakemus: " hakemus-id))
-    (execute! tx
-              "DELETE FROM virkailija.yhteishanke_organization WHERE hakemus_id = ?"
-              [hakemus-id])
+    (log/info (str "Updating yhteishanke organization contacts for hakemus: " hakemus-id))
     (doseq [organization organizations]
       (execute! tx
-                "INSERT INTO virkailija.yhteishanke_organization
-                 (hakemus_id, organization_name, contact_person, email)
-                 VALUES (?, ?, ?, ?)"
-                [hakemus-id
-                 (:organizationName organization)
-                 (:contactPerson organization)
-                 (:email organization)]))
-    (log/info (str "Successfully replaced yhteishanke organizations for hakemus: " hakemus-id))))
+                "UPDATE virkailija.yhteishanke_organization
+                 SET contact_person = ?, email = ?
+                 WHERE hakemus_id = ? AND organization_name = ?"
+                [(:contactPerson organization)
+                 (:email organization)
+                 hakemus-id
+                 (:organizationName organization)]))
+    (log/info (str "Successfully updated yhteishanke organization contacts for hakemus: " hakemus-id))))
 
 (defn on-muutoshakemus [user-key hakemus-id avustushaku-id muutoshakemus]
   (with-tx (fn [tx]
@@ -567,7 +564,7 @@
              (when (contains? muutoshakemus :varayhteyshenkilo)
                (change-normalized-hakemus-trusted-contact-person-details tx user-key hakemus-id (get muutoshakemus :varayhteyshenkilo)))
              (when (contains? muutoshakemus :yhteishankkeenOsapuolet)
-               (replace-yhteishanke-organizations tx hakemus-id (get muutoshakemus :yhteishankkeenOsapuolet))))))
+               (update-yhteishanke-organization-contacts tx hakemus-id (get muutoshakemus :yhteishankkeenOsapuolet))))))
 
 (defn update-hakemus-tx [tx avustushaku-id user-key submission-version answers budget-totals hakemus]
   (let [register-number (or (:register_number hakemus)
