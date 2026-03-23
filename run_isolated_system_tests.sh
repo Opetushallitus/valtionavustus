2f1f486c5d044127d967547302f4bcbcd364b1ad
Antero Fagerstedt <antero.fagerstedt@reaktor.com>
2026-03-23 13:57:55 +0200

    Support --ui in isolated system test wrapper

--- run_isolated_system_tests.sh
+++ run_isolated_system_tests.sh
@@ -16,9 +16,21 @@ function playwright_test_runner_exit_code {
   docker ps --filter "status=exited"|grep test-runner|sed 's/.*\(Exited.*\) Less.*/\1/'|sed 's/.*\([0-9][0-9]*\).*/\1/'
 }
 
+normalized_playwright_args=()
+function normalize_playwright_args {
+  # Support `./run_isolated_system_tests.sh --ui` as a readable shorthand
+  # for npm's usual passthrough syntax: `./run_isolated_system_tests.sh -- --ui`.
+  if [ "${1:-}" = "--ui" ]; then
+    normalized_playwright_args=(-- "$@")
+  else
+    normalized_playwright_args=("$@")
+  fi
+}
+
 function main {
   init_nodejs
-  run_tests "$@"
+  normalize_playwright_args "$@"
+  run_tests "${normalized_playwright_args[@]}"
 }
 
 main "$@"
